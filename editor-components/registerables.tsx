1794a429d38a5cadbe07d677b6ed1b2dc13fb9c1
melihpeker42 <melihisapeker@gmail.com>
2023-06-08 13:14:37 +0300

    feat: new component notice modal

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -129,6 +129,7 @@ import NavbarComponent1 from "./navigators/navbar1/navbar1";
 import NavbarComponent2 from "./navigators/navbar2/navbar2";
 import NavbarComponent3 from "./navigators/navbar3/navbar3";
 import TermsOfUseModal from "./modal/terms-of-use/terms-of-use";
+import NoticeModal from "./modal/notice-modal/notice";
 
 
 function Registerables(composer: ComponentsRegistery) {
@@ -262,7 +263,8 @@ function Registerables(composer: ComponentsRegistery) {
     new FooterComponent5(),
     new FooterComponent6(),
     new FooterComponent7(),
-    new TermsOfUseModal()
+    new TermsOfUseModal(),
+    new NoticeModal()
   ]);
 }
 
