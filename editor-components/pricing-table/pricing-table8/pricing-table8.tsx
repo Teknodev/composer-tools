24d7214d8318f232cf47bc71f73eb0f439079c29
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-06-27 13:55:18 +0300

    fix: fixed pricing table interface

--- editor-components/pricing-table/pricing-table8/pricing-table8.tsx
+++ editor-components/pricing-table/pricing-table8/pricing-table8.tsx
@@ -4,70 +4,58 @@ import styles from "./pricing-table8.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 type IIconBoxes = {
+  list: any;
   title: string;
   description: string;
   image: string;
-  hasFeature: boolean;
   price: string;
-  featureCount: number;
   link: string;
+};
+type Buttons = {
+  buttonText: string;
+  url: string;
 };
 
 class PricingTable8 extends BasePricingTable {
   constructor(props?: any) {
     super(props, styles);
-    this.addProp({
-      type: "object",
-      key: "pricing-properties",
-      displayer: "Pricing Properties",
-      value: [
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "Chart Annotations",
-        },
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "Uptime Monitoring",
-        },
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "Weekly Reports",
-        },
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "Security Audit",
-        },
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "On-Demand Audit",
-        },
-        {
-          type: "string",
-          key: "property",
-          displayer: "Property",
-          value: "Priority Support",
-        },
-      ],
-    });
+
     this.addProp({
       type: "array",
-      key: "pricing-content",
-      displayer: "Pricing Content",
+      key: "buttons",
+      displayer: "Buttons",
       value: [
         {
           type: "object",
-          key: "pricing",
-          displayer: "Pricing",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Get Started",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Link",
+              value: "",
+            },
+          ],
+        },
+      ],
+    });
+
+    this.addProp({
+      type: "array",
+      key: "cards",
+      displayer: "Card",
+      value: [
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
           value: [
             {
               type: "string",
@@ -85,7 +73,7 @@ class PricingTable8 extends BasePricingTable {
               type: "string",
               key: "price",
               displayer: "Price",
-              value: "$9.99",
+              value: "$50",
             },
             {
               type: "string",
@@ -95,29 +83,54 @@ class PricingTable8 extends BasePricingTable {
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6475d7f2fba070002b758890?alt=media&timestamp=1685444578453",
             },
             {
-              type: "boolean",
-              key: "hasFeature",
-              displayer: "Has Feature",
-              value: true,
-            },
-            {
-              type: "number",
-              key: "featureCount",
-              displayer: "Feature Count",
-              value: 2,
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Link",
-              value: "",
+              type: "array",
+              key: "list",
+              displayer: "List Items",
+              value: [
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Chart Annotations",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Uptime Monitoring",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Weekly Reports",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Security Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "On-Demand Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Priority Support",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "pricing",
-          displayer: "Pricing",
+          key: "card",
+          displayer: "Card",
           value: [
             {
               type: "string",
@@ -135,39 +148,64 @@ class PricingTable8 extends BasePricingTable {
               type: "string",
               key: "price",
               displayer: "Price",
-              value: "$19.99",
+              value: "$100",
             },
             {
               type: "string",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6475d989fba070002b7588c6?alt=media&timestamp=1685446957790",
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6475d7f2fba070002b758890?alt=media&timestamp=1685444578453",
             },
             {
-              type: "boolean",
-              key: "hasFeature",
-              displayer: "Has Feature",
-              value: true,
-            },
-            {
-              type: "number",
-              key: "featureCount",
-              displayer: "Feature Count",
-              value: 4,
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Link",
-              value: "",
+              type: "array",
+              key: "list",
+              displayer: "List Items",
+              value: [
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Chart Annotations",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Uptime Monitoring",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Weekly Reports",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Security Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "On-Demand Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Priority Support",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "pricing",
-          displayer: "Pricing",
+          key: "card",
+          displayer: "Card",
           value: [
             {
               type: "string",
@@ -185,32 +223,57 @@ class PricingTable8 extends BasePricingTable {
               type: "string",
               key: "price",
               displayer: "Price",
-              value: "$39.99",
+              value: "$150",
             },
             {
               type: "string",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6475d7f2fba070002b758891?alt=media&timestamp=1685444578453",
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6475d7f2fba070002b758890?alt=media&timestamp=1685444578453",
             },
             {
-              type: "boolean",
-              key: "hasFeature",
-              displayer: "Has Feature",
-              value: true,
-            },
-            {
-              type: "number",
-              key: "featureCount",
-              displayer: "Feature Count",
-              value: 6,
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Link",
-              value: "",
+              type: "array",
+              key: "list",
+              displayer: "List Items",
+              value: [
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Chart Annotations",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Uptime Monitoring",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Weekly Reports",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Security Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "On-Demand Audit",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Priority Support",
+                },
+              ],
             },
           ],
         },
@@ -222,84 +285,48 @@ class PricingTable8 extends BasePricingTable {
     return "Pricing Table 8";
   }
 
-  renderFeatureIcons(pricing: IIconBoxes): JSX.Element[] {
-    const pricingProperties = this.castToObject<any[]>("pricing-properties");
-    const featureCount = pricing.featureCount;
-    const tickCount = featureCount;
-    const crossCount = 6 - featureCount;
-
-    const ticks: JSX.Element[] = Array.from(
-      { length: tickCount },
-      (_, index) => (
-        <div className={this.decorateCSS("feature-box")}>
-          <span className={this.decorateCSS("feature-title")}>
-            {pricingProperties[index].value}
-          </span>
-          <span
-            className={this.decorateCSS("feature-icon")}
-            key={`tick-${pricing.title}-${index}`}
-          >
-            ✔️
-          </span>
-        </div>
-      )
-    );
-
-    const crosses: JSX.Element[] = Array.from(
-      { length: crossCount },
-      (_, index) => (
-        <div className={this.decorateCSS("feature-box")}>
-          <span className={this.decorateCSS("feature-title")}>
-            {pricingProperties[tickCount + index].value}
-          </span>
-          <span
-            className={this.decorateCSS("feature-icon")}
-            key={`cross-${pricing.title}-${index}`}
-          >
-            ❌
-          </span>
-        </div>
-      )
-    );
-
-    return [...ticks, ...crosses];
-  }
-
   render() {
+    const buttons: Buttons[] = this.castToObject<Buttons[]>("buttons");
+  
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          {this.castToObject<IIconBoxes[]>("pricing-content").map(
-            (pricing: IIconBoxes, index: number) => (
-              <div className={this.decorateCSS("pricing")} key={index}>
-                <div className={this.decorateCSS("image-background")}>
-                  <img
-                    className={this.decorateCSS("image")}
-                    src={pricing.image}
-                    alt={pricing.title}
-                  />
-                </div>
-                <h3 className={this.decorateCSS("title")}>{pricing.title}</h3>
-                <p className={this.decorateCSS("description")}>
-                  {pricing.description}
-                </p>
-
-                <div>
-                  <p>{this.renderFeatureIcons(pricing)}</p>
-                </div>
-                <p className={this.decorateCSS("price")}>{pricing.price}</p>
-                <ComposerLink path={pricing.link} key={index}>
+          {this.castToObject<IIconBoxes[]>("cards").map((card: IIconBoxes, index: number) => (
+            <div className={this.decorateCSS("pricing")} key={index}>
+              <div className={this.decorateCSS("image-background")}>
+                <img
+                  className={this.decorateCSS("image")}
+                  src={card.image}
+                  alt={card.title}
+                />
+              </div>
+              <h3 className={this.decorateCSS("title")}>{card.title}</h3>
+              <p className={this.decorateCSS("description")}>
+                {card.description}
+              </p>
+  
+              <div>
+                <ul className={this.decorateCSS("list-group")}>
+                  {card.list.map((item: any, itemIndex: number) => (
+                    <li className={this.decorateCSS("li")} key={itemIndex}><span>✓</span>{item.value}</li>
+                  ))}
+                </ul>
+              </div>
+  
+              <p className={this.decorateCSS("price")}>{card.price}</p>
+  
+              {buttons.map((button: Buttons, buttonIndex: number) => (
+                <ComposerLink path={button.url} key={buttonIndex}>
                   <button className={this.decorateCSS("get-started-button")}>
-                    Get Started
+                    {button.buttonText}
                   </button>
                 </ComposerLink>
-              </div>
-            )
-          )}
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     );
-  }
-}
-
+  }  
+}  
 export default PricingTable8;
