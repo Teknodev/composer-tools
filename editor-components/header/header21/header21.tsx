830e08a90f0e2dbb8bc13fa4801e20a5c4f8e41d
FatihKizildag <fetiih.48@gmail.com>
2023-07-13 16:49:41 +0300

    test

--- editor-components/header/header21/header21.tsx
+++ editor-components/header/header21/header21.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import styles from "./header21.module.scss";
 import { BaseHeader } from "../../EditorComponent";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
+type icons = {
+  url: string;
+  image: string;
+};
 class HeaderComponent21 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
@@ -13,8 +18,8 @@ class HeaderComponent21 extends BaseHeader {
       value: [
         {
           type: "object",
-          displayer: "Item Left",
-          key: "item-left",
+          displayer: "Item Left Top",
+          key: "left-top-item",
           value: [
             {
               type: "string",
@@ -24,17 +29,11 @@ class HeaderComponent21 extends BaseHeader {
                 "I Create User-friendly Mobile Applications and Web Experiences ❤️",
             },
             {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Shop gret deals on MacBook, iPad, iPhone and more.",
-            },
-            {
               type: "image",
               displayer: "Image",
               key: "image",
               value:
-                "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/10/apple-shopping-event.jpg",
+                "https://a6n4d3q9.rocketcdn.me/wp-content/uploads/2023/03/w-developer-top-bg.jpg?id=55205",
             },
             {
               type: "object",
@@ -45,7 +44,7 @@ class HeaderComponent21 extends BaseHeader {
                   type: "string",
                   displayer: "Button Text",
                   key: "button-text",
-                  value: "Pre-Order Now",
+                  value: "Sign Up",
                 },
                 {
                   type: "page",
@@ -57,86 +56,22 @@ class HeaderComponent21 extends BaseHeader {
             },
           ],
         },
-        {
-          type: "object",
-          displayer: "Item Left Bottom",
-          key: "item-left-bottom",
-          value: [
-            {
-              type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "The New Google Pixel 7",
-            },
-            {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Shop gret deals on MacBook, iPad, iPhone and more.",
-            },
-            {
-              type: "image",
-              displayer: "Image",
-              key: "image",
-              value:
-                "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2022/10/apple-shopping-event.jpg",
-            },
-            {
-              type: "object",
-              displayer: "Button",
-              key: "button",
-              value: [
-                {
-                  type: "string",
-                  displayer: "Button Text",
-                  key: "button-text",
-                  value: "Pre-Order Now",
-                },
-                {
-                  type: "page",
-                  displayer: "Button URL",
-                  key: "button-url",
-                  value: "",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          type: "object",
-          displayer: "Item Right",
-          key: "item-right",
-          value: [
-            {
-              type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "Aurora Headset",
-            },
-            {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Shop gret deals on MacBook, iPad, iPhone and more.",
-            },
 
-            {
-              type: "image",
-              displayer: "Image",
-              key: "image",
-              value:
-                "https://woodmart.xtemos.com/mega-electronics/wp-content/uploads/sites/9/2023/02/logitech-aurora-headset.jpg",
-            },
+        {
+          type: "array",
+          displayer: "Directions",
+          key: "directions",
+          value: [
             {
               type: "object",
-              displayer: "Button",
-              key: "button",
+              displayer: "Direction-1",
+              key: "direction1",
               value: [
                 {
                   type: "string",
-                  displayer: "Button Text",
-                  key: "button-text",
-                  value: "Pre-Order Now",
+                  displayer: "Direction-1",
+                  key: "direction1",
+                  value: "Product Design",
                 },
                 {
                   type: "page",
@@ -145,6 +80,191 @@ class HeaderComponent21 extends BaseHeader {
                   value: "",
                 },
               ],
+            },
+            {
+              type: "object",
+              displayer: "Direction-2",
+              key: "direction2",
+              value: [
+                {
+                  type: "string",
+                  displayer: "Direction-1",
+                  key: "direction1",
+                  value: "UX Research",
+                },
+                {
+                  type: "page",
+                  displayer: "Button URL",
+                  key: "button-url",
+                  value: "",
+                },
+              ],
+            },
+            {
+              type: "object",
+              displayer: "Direction-3",
+              key: "direction3",
+              value: [
+                {
+                  type: "string",
+                  displayer: "Direction-1",
+                  key: "direction1",
+                  value: "Prototyping",
+                },
+                {
+                  type: "page",
+                  displayer: "Button URL",
+                  key: "button-url",
+                  value: "",
+                },
+              ],
+            },
+            {
+              type: "object",
+              displayer: "Direction-4",
+              key: "direction4",
+              value: [
+                {
+                  type: "string",
+                  displayer: "Direction-1",
+                  key: "direction1",
+                  value: "Prototyping",
+                },
+                {
+                  type: "page",
+                  displayer: "Button URL",
+                  key: "button-url",
+                  value: "",
+                },
+              ],
+            },
+            {
+              type: "object",
+              displayer: "Direction-5",
+              key: "direction5",
+              value: [
+                {
+                  type: "string",
+                  displayer: "Direction-1",
+                  key: "direction1",
+                  value: "Front-End Development",
+                },
+                {
+                  type: "page",
+                  displayer: "Button URL",
+                  key: "button-url",
+                  value: "",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "image",
+          displayer: "Right Image",
+          key: "rightImage",
+          value:
+            "https://a6n4d3q9.rocketcdn.me/wp-content/uploads/2023/03/w-developer-face-1.png.webp",
+        },
+      ],
+    });
+    this.addProp({
+      type: "array",
+      key: "socialMedias",
+      displayer: "Social Medias",
+      value: [
+        {
+          type: "object",
+          key: "box",
+          displayer: "Twitter",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              value: "https://cdn-icons-png.flaticon.com/512/3670/3670151.png",
+              displayer: "Image",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "box",
+          displayer: "Facebook",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              value: "https://cdn-icons-png.flaticon.com/512/4494/4494475.png",
+              displayer: "Image",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "box",
+          displayer: "Pinterest",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              value: "https://cdn-icons-png.flaticon.com/512/145/145808.png",
+              displayer: "Image",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "box",
+          displayer: "Linkedin",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              value: "https://cdn-icons-png.flaticon.com/512/145/145807.png",
+              displayer: "Image",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "box",
+          displayer: "Telegram",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              value: "https://cdn-icons-png.flaticon.com/512/2111/2111646.png",
+              displayer: "Image",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
             },
           ],
         },
@@ -158,10 +278,71 @@ class HeaderComponent21 extends BaseHeader {
 
   render() {
     let items: any = this.castToObject("items");
+    console.log(this.castToObject("socialMedias"));
+    console.log(items);
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("wrapper")}></div>
+          <div className={this.decorateCSS("wrapper")}>
+            <div className={this.decorateCSS("background-image")}>
+              <img src={items[0].image} alt={items[0].title} />
+            </div>
+            <div className={this.decorateCSS("left")}>
+              <div className={this.decorateCSS("top")}>
+                <div className={this.decorateCSS("item")}>
+                  <div className={this.decorateCSS("content")}>
+                    <h1 className={this.decorateCSS("title")}>
+                      {items[0].title}
+                    </h1>
+                    <div className={this.decorateCSS("button")}>
+                      {items[0].button[0].value}
+                    </div>
+                    <div className={this.decorateCSS("icons")}>
+                      <li>
+                        {this.castToObject<icons[]>("socialMedias").map(
+                          (icons: any, index: number) => (
+                            <ComposerLink key={index} path={icons.url}>
+                              <div className={this.decorateCSS("icon-div")}>
+                                <img
+                                  src={icons.image}
+                                  alt={`Small Image ${index + 1}`}
+                                  key={index}
+                                />
+                              </div>
+                            </ComposerLink>
+                          )
+                        )}
+                      </li>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={this.decorateCSS("bottom")}>
+                <ul>
+                  <a href={items[1].direction1[1].value}>
+                    {items[1].direction1[0].value}
+                  </a>
+                  <a href={items[1].direction2[1].value}>
+                    {items[1].direction2[0].value}
+                  </a>
+                  <a href={items[1].direction3[1].value}>
+                    {items[1].direction3[0].value}
+                  </a>
+                  <a href={items[1].direction4[1].value}>
+                    {items[1].direction4[0].value}
+                  </a>
+                  <a href={items[1].direction5[1].value}>
+                    {items[1].direction5[0].value}
+                  </a>
+                </ul>
+              </div>
+            </div>
+            <div className={this.decorateCSS("right")}>
+              <div className={this.decorateCSS("image")}>
+                <img src={items[2].value} alt={items[2].displayer} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
