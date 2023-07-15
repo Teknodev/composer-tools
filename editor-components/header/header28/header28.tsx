6593a96954c58053879f1210b916e35dcdf235f9
Kursat <draknes.gamin70@gmail.com>
2023-07-15 17:41:08 +0300

    fix: fixed responsiveness of the header

--- editor-components/header/header28/header28.tsx
+++ editor-components/header/header28/header28.tsx
@@ -193,7 +193,7 @@ class Header28 extends BaseHeader {
       dots: false,
       infinite: true,
       speed: 440,
-      autoplay: false,
+      autoplay: true,
       autoplaySpeed: 5000,
       slidesToShow: 1,
       slidesToScroll: 1,
