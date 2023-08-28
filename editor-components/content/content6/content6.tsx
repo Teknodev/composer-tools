5ce9904a468abb61328572c920c63a6583e5683a
Bartu Çakır <bartuuu_lol@hotmail.com>
2023-08-28 14:37:54 +0300

    feat: new contents and call to action

--- editor-components/content/content6/content6.tsx
+++ editor-components/content/content6/content6.tsx
@@ -1,131 +1,88 @@
 import * as React from "react";
 import { BaseContent } from "../../EditorComponent";
 import styles from "./content6.module.scss";
-
-type Card = {
-    title: string;
-    description: string;
-    image: string;
-};
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 class Content6 extends BaseContent {
     constructor(props?: any) {
         super(props, styles);
-
-
         this.addProp({
-            type: "string",
-            key: "title",
-            displayer: "Title",
-            value: "Is Our Artificial İntelligence Working?",
-        });
-
-        this.addProp({
-            type: "string",
-            key: "subtitle",
-            displayer: "Subtitle",
-            value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        });
-
-
-        this.addProp({
-            type: "string",
-            key: "description",
-            displayer: "Description",
-            value:
-                "AdCreative.ai is a data company with one of the largest databases of high-converting ad elements. It is the only company that can create conversion-focused ad elements using productive artificial intelligence.",
-        });
-
-        this.addProp({
-            type: "image",
-            key: "backgroundImage",
-            displayer: "BackgroundImage",
-            value: "",
-        });
-
-        this.addProp({
-            type: "array",
-            key: "cards",
-            displayer: "cards",
+            type: "object",
+            key: "left",
+            displayer: "Left Side",
             value: [
                 {
-                    type: "object",
-                    key: "card",
-                    displayer: "Card",
-                    value: [
-                        {
-                            type: "string",
-                            key: "title",
-                            displayer: "Title",
-                            value: "Crative Collection",
-                        },
-                        {
-                            type: "string",
-                            key: "description",
-                            displayer: "Description",
-                            value: "We gather creative content from all the leading social and visual platforms.",
-                        },
-                    ],
+                    type: "image",
+                    key: "leftImage",
+                    displayer: "Left Image",
+                    value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64ec569f057bdf002c2a9cfb?alt=media&timestamp=1693210248545"
                 },
                 {
-                    type: "object",
-                    key: "card",
-                    displayer: "Card",
+                    type: "array",
+                    key: "leftList",
+                    displayer: "Left List",
                     value: [
                         {
                             type: "string",
-                            key: "title",
-                            displayer: "Title",
-                            value: "Creative Analysis",
+                            key: "leftListItem",
+                            displayer: "Left List Item",
+                            value: "Lorem ipsum dolor sit amet."
                         },
                         {
                             type: "string",
-                            key: "description",
-                            displayer: "Description",
-                            value: "We analyze each collected ad element and create over 80 data points per visual.",
+                            key: "leftListItem",
+                            displayer: "Left List Item",
+                            value: "Dapibus ac facilisis in Libero id faucibus nisl tidonec."
                         },
-                    ],
+                        {
+                            type: "string",
+                            key: "leftListItem",
+                            displayer: "Left List Item",
+                            value: "Morbi leo risus Porta ac consectetur ac Vestibulum."
+                        },
+                        {
+                            type: "string",
+                            key: "leftListItem",
+                            displayer: "Left List Item",
+                            value: "Aliquam porta eros in euismod."
+                        },
+                    ]
+                }
+            ]
+        })
+        this.addProp({
+            type: "object",
+            key: "right",
+            displayer: "Right Side",
+            value: [
+                {
+                    type: "string",
+                    key: "rightTitle",
+                    displayer: "Right Title",
+                    value: "Welcome Our Store! You Will Be Able to Find any Types of Sunglasses You Want."
                 },
                 {
-                    type: "object",
-                    key: "card",
-                    displayer: "Card",
-                    value: [
-                        {
-                            type: "string",
-                            key: "title",
-                            displayer: "Title",
-                            value: "Machine Learning",
-                        },
-                        {
-                            type: "string",
-                            key: "description",
-                            displayer: "Description",
-                            value: "We train our unique machine learning model using the data points and the performance of the ad elements.",
-                        },
-                    ],
+                    type: "string",
+                    key: "buttonText",
+                    displayer: "Button Text",
+                    value: "About Us"
                 },
                 {
-                    type: "object",
-                    key: "card",
-                    displayer: "Card",
-                    value: [
-                        {
-                            type: "string",
-                            key: "title",
-                            displayer: "Title",
-                            value: "Creative Generation",
-                        },
-                        {
-                            type: "string",
-                            key: "description",
-                            displayer: "Description",
-                            value: "Our unique productive artificial intelligence rapidly generates high ROI ad elements and ranks them based on their expected conversion rates.",
-                        },
-                    ],
+                    type: "page",
+                    key: "link",
+                    displayer: "Button Link",
+                    value: ""
                 },
-            ],
+                {
+                    type: "image",
+                    key: "rightImage",
+                    displayer: "Right Image",
+                    value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64ec569f057bdf002c2a9cfa?alt=media&timestamp=1693210248545"
+                }
+            ]
         });
+
+        
     }
 
     getName(): string {
@@ -133,38 +90,36 @@ class Content6 extends BaseContent {
     }
 
     render() {
-        const styling = {
-            backgroundImage: `url('${this.getPropValue("backgroundImage")}')`,
-        };
         return (
             <div className={this.decorateCSS("container")}>
                 <div className={this.decorateCSS("max-content")}>
-                    <div className={this.decorateCSS("child")}>
-                        <h3 className={this.decorateCSS("subtitle")}>
-                            {this.getPropValue("subtitle")}
-                        </h3>
-                        <h2 className={this.decorateCSS("title")}>{this.getPropValue("title")} </h2>
-                        <div className={this.decorateCSS("description")}>
-                            <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
-                        </div>
-                        <div className={this.decorateCSS("wrapper")}>
-                            <div className={styles["bg-container"]} style={styling}>
-                                {this.castToObject<Card[]>("cards").map(
-                                    (card: Card, index: number) => (
-                                        <img className={this.decorateCSS("image")} src={card.image}></img>
+                    <div className={this.decorateCSS("content")}>
+                        <div className={this.decorateCSS("left")}>
+                            <img className={this.decorateCSS("img-left")} src={
+                                this.getPropValue("left")[0].value
+                            } alt="" />
+                            <ul className={this.decorateCSS("list")} >
+                                {this.getPropValue("left")[1].value.map((item: any) => {
+                                    return (
+                                        <li className={this.decorateCSS("list-item")}>✓ {item.value}</li>
                                     )
-                                )}
-                            </div>
-                            {this.castToObject<Card[]>("cards").map(
-                                (card: Card, index: number) => (
-                                    <div key={`cnt-6-${index}`} className={this.decorateCSS("card-content")}>
-                                        <div className={this.decorateCSS("card")}>
-                                            <h3>{card.title}</h3>
-                                            <p>{card.description}</p>
-                                        </div>
-                                    </div>
-                                )
-                            )}
+                                })}
+                            </ul>
+                        </div>
+                        <div className={this.decorateCSS("right")}>
+                            <h1 className={this.decorateCSS("title")}>
+                                {this.getPropValue("right")[0].value}
+                            </h1>
+                            <ComposerLink path={
+                                this.getPropValue("right")[2].value
+                            }>
+                                <button className={this.decorateCSS("button")}>
+                                    {this.getPropValue("right")[1].value}
+                                </button>
+                            </ComposerLink>
+                            <img className={this.decorateCSS("img-right")} src={
+                                this.getPropValue("right")[3].value
+                            } alt="" />
                         </div>
                     </div>
                 </div>
