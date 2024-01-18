5f5472bf26a9e49873bd27aa5d9534ce40d4197f
cannyrts <70090523+cannyrts@users.noreply.github.com>
2024-01-18 10:51:31 +0300

    feat: list 1,5 and testimonials 11 component changes

--- editor-components/testimonials/testimonials11/testimonials11.tsx
+++ editor-components/testimonials/testimonials11/testimonials11.tsx
@@ -16,7 +16,7 @@ class Testimonials11Page extends Testimonials {
       key: "cover-image",
       displayer: "Background Image",
       value:
-        "https://static.tildacdn.com/tild3234-3866-4266-a366-326532346231/sabbir-ahmed-VDww0Ma.png",
+        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     });
     this.addProp({
       type: "array",
