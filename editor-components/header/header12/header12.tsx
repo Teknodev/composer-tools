a70e5388e04ffb9b250bbfc26912b79cb7b81b93
Batu22 <batuhantemel14@gmail.com>
2024-01-29 17:32:33 +0300

    header12 first  commit

--- editor-components/header/header12/header12.tsx
+++ editor-components/header/header12/header12.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
 import { BaseHeader } from "../../EditorComponent";
 import styles from "./header12.module.scss";
+import ComposerSlider from "../../../composer-base-components/slider/slider";
 
-//CARD
-type Card = {
-    imageLeft : string ;
-    imageRight : string;
-    textLeft : string;
-    textRight : string;
-};
+
+//Left Slider
+type LeftSlider = {
+    imageLeft: string;
+    iconLeft: string;
+    textLeft: string;
+}
+
+//Right Slider
+type RightSlider = {
+    imageRight: string;
+    iconRight: string;
+    textRight: string;
+}
 
 //CLASS
 class Header12 extends BaseHeader {
+
+    //constructor
     constructor(props?: any) {
         super(props, styles);
 
+        // SLIDER LEFT
         this.addProp({
             type: "array",
-            key: "carouselCollection",
-            displayer: "Carousel Collection",
+            key: "carouselCollection1",
+            displayer: "Carousel Left",
             value: [
                 {
                     type: "object",
@@ -28,8 +39,14 @@ class Header12 extends BaseHeader {
                         {
                             type: "image",
                             key: "imageLeft",
-                            displayer: "imageLeft",
-                            value: "",
+                            displayer: "image Left",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/masonry15-1.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconLeft",
+                            displayer: "icon Left",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
@@ -37,11 +54,133 @@ class Header12 extends BaseHeader {
                             displayer: "Text Left",
                             value: "Autumn Stuff",
                         },
+                    ]
+                },
+                {
+                    type: "object",
+                    key: "carouselObject",
+                    displayer: "Carousel Object",
+                    value: [
+                        {
+                            type: "image",
+                            key: "imageLeft",
+                            displayer: "image Left",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/masonry18.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconLeft",
+                            displayer: "icon Left",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
+                        },
+                        {
+                            type: "string",
+                            key: "textLeft",
+                            displayer: "Text Left",
+                            value: "Breakfast",
+                        },
+                    ]
+                },
+                {
+                    type: "object",
+                    key: "carouselObject",
+                    displayer: "Carousel Object",
+                    value: [
+                        {
+                            type: "image",
+                            key: "imageLeft",
+                            displayer: "image Left",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/book_on_leaves.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconLeft",
+                            displayer: "icon Left",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
+                        },
+                        {
+                            type: "string",
+                            key: "textLeft",
+                            displayer: "Text Left",
+                            value: "The Notebook",
+                        },
+                    ]
+                },
+                {
+                    type: "object",
+                    key: "carouselObject",
+                    displayer: "Carousel Object",
+                    value: [
+                        {
+                            type: "image",
+                            key: "imageLeft",
+                            displayer: "image Left",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/masonry05.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconLeft",
+                            displayer: "icon Left",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
+                        },
+                        {
+                            type: "string",
+                            key: "textLeft",
+                            displayer: "Text Left",
+                            value: "Little Pumpkin",
+                        },
+                    ]
+                },
+                {
+                    type: "object",
+                    key: "carouselObject",
+                    displayer: "Carousel Object",
+                    value: [
+                        {
+                            type: "image",
+                            key: "imageLeft",
+                            displayer: "image Left",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/masonry13.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconLeft",
+                            displayer: "icon Left",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
+                        },
+                        {
+                            type: "string",
+                            key: "textLeft",
+                            displayer: "Text Left",
+                            value: "Autumn Evening",
+                        },
+                    ]
+                },
+            ]
+        })
+
+        // SLIDER RIGHT
+        this.addProp({
+            type: "array",
+            key: "carouselCollection2",
+            displayer: "Carousel Right",
+            value: [
+                {
+                    type: "object",
+                    key: "carouselObject",
+                    displayer: "Carousel Object",
+                    value: [
                         {
                             type: "image",
                             key: "imageRight",
-                            displayer: "imageRight",
-                            value: "",
+                            displayer: "image Right",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/food_drink04.jpg",
+                        },
+                        {
+                            type: "image",
+                            key: "iconRight",
+                            displayer: "icon Right",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
@@ -58,27 +197,21 @@ class Header12 extends BaseHeader {
                     value: [
                         {
                             type: "image",
-                            key: "imageLeft",
-                            displayer: "imageLeft",
-                            value: "",
-                        },
-                        {
-                            type: "string",
-                            key: "textLeft",
-                            displayer: "Text Left",
-                            value: "Breakfast",
+                            key: "imageRight",
+                            displayer: "image Right",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/coffee.jpg",
                         },
                         {
                             type: "image",
-                            key: "imageRight",
-                            displayer: "imageRight",
-                            value: "",
+                            key: "iconRight",
+                            displayer: "icon Right",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
                             key: "textRight",
                             displayer: "Text Right",
-                            value: "Coffe Time",
+                            value: "Coffee Time",
                         },
                     ]
                 },
@@ -89,21 +222,15 @@ class Header12 extends BaseHeader {
                     value: [
                         {
                             type: "image",
-                            key: "imageLeft",
-                            displayer: "imageLeft",
-                            value: "",
-                        },
-                        {
-                            type: "string",
-                            key: "textLeft",
-                            displayer: "Text Left",
-                            value: "The Notebook",
+                            key: "imageRight",
+                            displayer: "image Right",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/book_and_leaf.jpg",
                         },
                         {
                             type: "image",
-                            key: "imageRight",
-                            displayer: "imageRight",
-                            value: "",
+                            key: "iconRight",
+                            displayer: "icon Right",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
@@ -120,21 +247,15 @@ class Header12 extends BaseHeader {
                     value: [
                         {
                             type: "image",
-                            key: "imageLeft",
-                            displayer: "imageLeft",
-                            value: "",
-                        },
-                        {
-                            type: "string",
-                            key: "textLeft",
-                            displayer: "Text Left",
-                            value: "Little Pumpkin",
+                            key: "imageRight",
+                            displayer: "image Right",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/masonry11.jpg",
                         },
                         {
                             type: "image",
-                            key: "imageRight",
-                            displayer: "imageRight",
-                            value: "",
+                            key: "iconRight",
+                            displayer: "icon Right",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
@@ -151,21 +272,15 @@ class Header12 extends BaseHeader {
                     value: [
                         {
                             type: "image",
-                            key: "imageLeft",
-                            displayer: "imageLeft",
-                            value: "",
-                        },
-                        {
-                            type: "string",
-                            key: "textLeft",
-                            displayer: "Text Left",
-                            value: "Autumn Evening",
+                            key: "imageRight",
+                            displayer: "image Right",
+                            value: "https://demo-storage.com/pm/wp/aurel/dark/wp-content/uploads/2017/11/photo-1476283721796-dd935b062838.jpg",
                         },
                         {
                             type: "image",
-                            key: "imageRight",
-                            displayer: "imageRight",
-                            value: "",
+                            key: "iconRight",
+                            displayer: "icon Right",
+                            value: "https://cdn-icons-png.flaticon.com/512/656/656979.png",
                         },
                         {
                             type: "string",
@@ -178,21 +293,72 @@ class Header12 extends BaseHeader {
             ]
         })
 
-    }
+    } //constructor end
 
     getName(): string {
         return "Header 12";
     }
 
+    //RENDER
     render() {
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            autoplay: false,
+            autoplaySpeed: 2500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            vertical: true,
+            verticalSwiping: true,
+        };
+
+        //RETURN
         return (
-            <div
+            <div className={this.decorateCSS("container")}>
+                <div className={this.decorateCSS("max-content")}>
+                    <div className={this.decorateCSS("sliderContainer")}>
+                        <ComposerSlider {...settings}>
+                            {this.castToObject<LeftSlider[]>("carouselCollection1").map((item: LeftSlider, index: number) => (
+                                <div
+                                    className={this.decorateCSS("left-container")}
+                                    key={index}
+                                >
+                                    <img
+                                        src={item.imageLeft}
+                                        alt=""
+                                        key={index}
+                                    />
+                                    <div className={this.decorateCSS("imageArrows")}>{item.iconLeft}</div>
+                                    <h1 className={this.decorateCSS("title")}>{item.textLeft}</h1>
+                                </div>
 
-            >
+                            ))}
+                        </ComposerSlider>
 
+                        <ComposerSlider {...settings}>
+                            {this.castToObject<RightSlider[]>("carouselCollection2").map((item: RightSlider, index: number) => (
+
+                                <div
+                                    className={this.decorateCSS("right-container")}
+                                    key={index}
+                                >
+                                    <img
+                                        src={item.imageRight}
+                                        alt=""
+                                        key={index}
+                                    />
+                                    <div className={this.decorateCSS("imageArrows")}>{item.iconRight}</div>
+                                    <h1 className={this.decorateCSS("title")}>{item.textRight}</h1>
+                                </div>
+                            ))}
+                        </ComposerSlider>
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
+        ); // RETURN end
+    } // RENDER end
+
+} // CLASS end
 
 export default Header12;
