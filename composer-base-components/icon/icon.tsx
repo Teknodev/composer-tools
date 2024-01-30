a4f29fc758ba7dca5f1effbe4ee620c73c681edc
Şahin Ateş <ayazelar@gmail.com>
2024-01-30 12:28:27 +0300

    use jsx instead of React.jsx

--- composer-base-components/icon/icon.tsx
+++ composer-base-components/icon/icon.tsx
@@ -12,7 +12,7 @@ export function ComposerIcon({ name, propsIcon }: typesPropsIcon): JSX.Element {
   const lib = name.replace(/([a-z0-9])([A-Z])/g, '$1 $2').split(" ")[0].toLocaleLowerCase();
   // @ts-expect-error
   const ElementIcon: IconType = loadable(() => import(`react-icons/${lib}/index.js`), {
-    resolveComponent: (el: React.JSX.Element) => el[name as keyof JSX.Element]
+    resolveComponent: (el: JSX.Element) => el[name as keyof JSX.Element]
   });
 
   return <ElementIcon {...propsIcon} />
