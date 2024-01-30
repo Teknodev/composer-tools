8c9bc22b25154ba048c19048cf0df5337f6dd6d0
Şahin Ateş <ayazelar@gmail.com>
2024-01-30 13:59:32 +0300

    fix: import icon dynamicly

--- composer-base-components/icon/icon.tsx
+++ composer-base-components/icon/icon.tsx
@@ -11,7 +11,7 @@ interface typesPropsIcon {
 export function ComposerIcon({ name, propsIcon }: typesPropsIcon): JSX.Element {
   const lib = name.replace(/([a-z0-9])([A-Z])/g, '$1 $2').split(" ")[0].toLocaleLowerCase();
   // @ts-expect-error
-  const ElementIcon: IconType = loadable(() => import(`react-icons/${lib}/index.js`), {
+  const ElementIcon: IconType = loadable(() => import(`react-icons/${lib}/index`), {
     resolveComponent: (el: JSX.Element) => el[name as keyof JSX.Element]
   });
 
