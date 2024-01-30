7301292726e8990d21ec7e22c79adfdc0882f7c6
Şahin Ateş <ayazelar@gmail.com>
2024-01-30 14:56:16 +0300

    icons added

--- editor-components/header/header34/header34.tsx
+++ editor-components/header/header34/header34.tsx
@@ -1,116 +1,209 @@
 import * as React from "react";
-import styles from "./header34.module.scss";
 import { BaseHeader } from "../../EditorComponent";
+import styles from "./header34.module.scss";
+
+import ComposerSlider from "../../../composer-base-components/slider/slider";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
-import { TypeUsableComponentProps } from "../../EditorComponent";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type Button = {
-  buttonText: string;
-  url: string;
-  isPrimary: string;
-};
-
-class Header34 extends BaseHeader {
+class HeaderComponent34 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
-    this.addProp({
-      type: "image",
-      key: "image",
-      displayer: "Image",
-      value:
-        "https://dentalexcellenceturkey.com/wp-content/themes/dectheme/images/slide/main.jpg",
-    });
-    this.addProp({
-      type: "string",
-      key: "first-text",
-      displayer: "1st Text",
-      value: "Premium Quality Dental Care",
-    });
-    this.addProp({
-      type: "string",
-      key: "second-text",
-      displayer: "2nd Text",
-      value: " Dental treatments in Turkey",
-    });
-
-    let button: TypeUsableComponentProps = {
-      type: "object",
-      key: "button",
-      displayer: "Button",
-      value: [
-        {
-          type: "string",
-          key: "buttonText",
-          displayer: "Button Text",
-          value: "Get Free Quote",
-        },
-
-        {
-          type: "page",
-          key: "url",
-          displayer: "Button Link",
-          value: "",
-        },
-        {
-          type: "boolean",
-          key: "isPrimary",
-          displayer: "Is Primary",
-          value: false,
-        },
-      ],
-    };
 
     this.addProp({
       type: "array",
-      key: "buttons",
-      displayer: "Buttons",
-      value: [JSON.parse(JSON.stringify(button))],
+      key: "slider",
+      displayer: "Slider",
+      value: [
+        {
+          type: "object",
+          key: "slide",
+          displayer: "Slide",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Premium Quality Design"
+            },
+            {
+              type: "string",
+              key: "button_text",
+              displayer: "Button Text",
+              value: "PURCHASE INTACT"
+            },
+            {
+              type: "page",
+              key: "button_link",
+              displayer: "Button Navigate",
+              value: null
+            },
+          ]
+        },
+        {
+          type: "object",
+          key: "slide",
+          displayer: "Slide",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://images.unsplash.com/photo-1635350736475-c8cef4b21906?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Premium Quality Jobs"
+            },
+            {
+              type: "string",
+              key: "button_text",
+              displayer: "Button Text",
+              value: "CONTACT US"
+            },
+            {
+              type: "page",
+              key: "button_link",
+              displayer: "Button Navigate",
+              value: null
+            },
+          ]
+        },
+        {
+          type: "object",
+          key: "slide",
+          displayer: "Slide",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Premium Quality Clothes"
+            },
+            {
+              type: "string",
+              key: "button_text",
+              displayer: "Button Text",
+              value: "BUY"
+            },
+            {
+              type: "page",
+              key: "button_link",
+              displayer: "Button Navigate",
+              value: null
+            },
+          ]
+        }
+      ],
     });
+
+    this.setComponentState("animation-active", false);
+    this.setComponentState("active-index", 0);
+    this.setComponentState("display-none", true);
+    this.setComponentState("slider-ref", React.createRef());
+
+
+
   }
   getName(): string {
-    return "Header-34";
+    return "Header 37";
   }
+
+
+
+
   render() {
+    const settings = {
+      arrows: false,
+      dots: false,
+      infinite: true,
+      accessibility: false,
+      speed: 1700,
+      autoplay: false,
+      autoplaySpeed: 3000,
+      slidesToShow: 1,
+      draggable: false,
+      dotsClass: this.decorateCSS("dots"),
+      afterChange: (index: number) => {
+
+        this.setComponentState("animation-active", false);
+        this.setComponentState("display-none", true);
+
+      },
+      beforeChange: (oldIndex: number, newIndex: number) => {
+        if (oldIndex == newIndex) return;
+
+        this.setComponentState("from", oldIndex > newIndex ? "left" : "right");
+
+        this.setComponentState("display-none", false);
+        setTimeout(() => {
+          this.setComponentState("animation-active", true);
+          this.setComponentState("active-index", newIndex);
+        }, 100)
+      }
+    };
     return (
       <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div
-            className={this.decorateCSS("content")}
-            style={{
-              backgroundImage: `url(${this.getPropValue("image")})`,
-            }}
-          >
-            <div
-              className={this.decorateCSS("container")}>
-              <h6 className={this.decorateCSS("first-text")}>
-                {this.getPropValue("first-text")}
-              </h6>
-              <h3 className={this.decorateCSS("second-text")}>
-                {this.getPropValue("second-text")}
-              </h3>
-              <div className={this.decorateCSS("buttondiv")}>
-                {this.castToObject<Button[]>("buttons").map(
-                  (item: Button, indexButtons: number) => {
-                    return (
-                      <ComposerLink key={indexButtons} path={item.url}>
-                        <button
-                          className={`${this.decorateCSS("button")} ${
-                            item.isPrimary ? this.decorateCSS("primary") : ""
-                          }`}
-                        >
-                          {item.buttonText}
-                        </button>
-                      </ComposerLink>
-                    );
+
+        <ComposerSlider
+          {...settings}
+          ref={this.getComponentState("slider-ref")}
+          className={this.decorateCSS("carousel")}
+        >
+          {this.getPropValue("slider").map(
+            (item: any, indexSlider: number) => (
+
+              <div className={this.decorateCSS("content")} key={indexSlider}>
+                <img src={item.getPropValue("image")} />
+                <div className={this.decorateCSS("overlay")}></div>
+                <div className={`${this.decorateCSS("text-and-button")} ${this.getComponentState("animation-active") && this.decorateCSS("un-visible")}`}>
+                  <h1>{item.getPropValue("title")}</h1>
+                  <ComposerLink path={item.getPropValue("button_link")}>
+                    <button>{item.getPropValue("button_text")}</button>
+                  </ComposerLink>
+                </div>
+                <ComposerIcon name="GrFormNext" propsIcon={{
+                  className: `${this.decorateCSS("next-icon")} ${this.decorateCSS("arrow")} ${!this.getComponentState("display-none") && this.decorateCSS("un-visible")}`, size: 40, onClick: () => {
+                    this.getComponentState("slider-ref").current.slickNext()
                   }
-                )}
+                }} />
+                <ComposerIcon name="GrFormPrevious" propsIcon={{
+                  className: `${this.decorateCSS("prev-icon")} ${this.decorateCSS("arrow")} ${this.getComponentState("display-none") && this.decorateCSS("un-visible")}`, size: 40, onClick: () => {
+                    this.getComponentState("slider-ref").current.slickPrev()
+                  }
+                }} />
               </div>
-            </div>
-          </div>
+
+            )
+          )}
+
+        </ComposerSlider>
+        <div className={`${this.decorateCSS("overlay-animation-box")} ${this.getComponentState("animation-active") && this.decorateCSS("visible")} ${this.getComponentState("display-none") && this.decorateCSS("display-none")} ${this.decorateCSS(this.getComponentState("from"))}`} >
+          <img src={this.getPropValue("slider")[this.getComponentState("active-index")].value} />
         </div>
+
+
+
       </div>
     );
   }
 }
 
-export default Header34;
+export default HeaderComponent34;
--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -111,9 +111,6 @@ import HeaderComponent31 from "./header/header31/header31";
 import HeaderComponent32 from "./header/header32/header32";
 import HeaderComponent33 from "./header/header33/header33";
 import HeaderComponent34 from "./header/header34/header34";
-import HeaderComponent35 from "./header/header35/header35";
-import HeaderComponent36 from "./header/header36/header36";
-import HeaderComponent37 from "./header/header37/header37";
 import FeatureComponent1 from "./feature/feature1/feature1";
 import FeatureComponent2 from "./feature/feature2/feature2";
 import FeatureComponent3 from "./feature/feature3/feature3";
@@ -311,9 +308,6 @@ function Registerables(composer: ComponentsRegistery) {
     new HeaderComponent32(),
     new HeaderComponent33(),
     new HeaderComponent34(),
-    new HeaderComponent35(),
-    new HeaderComponent36(),
-    new HeaderComponent37(),
     new NavbarComponent1(),
     new NavbarComponent2(),
     new NavbarComponent3(),
