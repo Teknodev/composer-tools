1c1d18ac7b4d334f864e7d945f2c7df943445747
Kaan Aytaç <kaanaytac@Kaans-MacBook-Pro.local>
2024-02-19 16:31:10 +0300

    initial-commit

--- editor-components/stats/stats6/stats6.tsx
+++ editor-components/stats/stats6/stats6.tsx
@@ -11,87 +11,78 @@ type CardData = {
 class Stats6Page extends BaseStats {
   constructor(props?: any) {
     super(props, styles);
-
+    this.addProp({
+      type: "string",
+      key: "card-header",
+      displayer: "Card Header",
+      value: "Our achievements",
+    });
+    this.addProp({
+      type: "string",
+      key: "header-text",
+      displayer: "Header text",
+      value:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Seperated they live in.",
+    });
     this.addProp({
       type: "array",
       key: "card-list",
-      displayer: "Price List",
+      displayer: "List",
       value: [
         {
           type: "object",
           key: "card-lists",
-          displayer: "Pricing List",
+          displayer: "Single List",
           value: [
-            {
-              type: "image",
-              key: "CardImage",
-              displayer: "Card Image",
-              value:
-                "https://img.freepik.com/free-vector/illustration-social-connection_53876-35997.jpg?w=826&t=st=1680874120~exp=1680874720~hmac=9749f71cec2f7953e2b607e26a64ec090262cc3be6b266b42aa17bb6e83219e8",
-            },
             {
               type: "string",
               key: "CardTitle",
               displayer: "Card Title",
-              value: "20K+",
+              value: "400",
             },
             {
               type: "string",
               key: "CardDescription",
               displayer: "Card Description",
-              value: "daily user statistics",
+              value: "Customers are satisfied with our professional support",
             },
           ],
         },
         {
           type: "object",
           key: "card-lists",
-          displayer: "Pricing List",
+          displayer: "Single List",
           value: [
-            {
-              type: "image",
-              key: "CardImage",
-              displayer: "Card Image",
-              value:
-                "https://img.freepik.com/free-vector/online-document-form-digital-agreement-electronic-contract-internet-questionnaire-list-note-voting-ballot-poll-flat-design-element-concept-illustration_335657-2013.jpg?w=826&t=st=1680874535~exp=1680875135~hmac=ba9532b376576c6d90b616b8105df0b60587c41076a38695555133b05d66c844",
-            },
             {
               type: "string",
               key: "CardTitle",
               displayer: "Card Title",
-              value: "20K+",
+              value: "1000",
             },
             {
               type: "string",
               key: "CardDescription",
               displayer: "Card Description",
-              value: "total registers",
+              value: "Amazing preset options to be mixed and combined",
             },
           ],
         },
         {
           type: "object",
           key: "card-lists",
-          displayer: "Pricing List",
+          displayer: "Single List",
           value: [
-            {
-              type: "image",
-              key: "CardImage",
-              displayer: "Card Image",
-              value:
-                "https://img.freepik.com/free-vector/realistic-vector-icon-set-brown-carton-paper-bag-with-handles-isolated-white-background_134830-1278.jpg?w=826&t=st=1680874944~exp=1680875544~hmac=99a035ff8898b9790e1369ec71aff17cead1f41ea032890ed49f4c4da64d2fc4",
-            },
             {
               type: "string",
               key: "CardTitle",
               displayer: "Card Title",
-              value: "20K+",
+              value: "8000",
             },
             {
               type: "string",
               key: "CardDescription",
               displayer: "Card Description",
-              value: "categories",
+              value: "Average response time on live chat support channel",
             },
           ],
         },
@@ -102,21 +93,36 @@ class Stats6Page extends BaseStats {
   getName(): string {
     return "Stats 6";
   }
-
+  activeIndex: number = -1;
   render() {
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("stats6-page")}>
-            {this.castToObject<CardData[]>("card-list").map(
-              (data: any, index: number) => (
-                <div key={index} className={this.decorateCSS("card")}>
-                  <img src={data.CardImage} width={100} height={100} alt="" />
-                  <h4 className={this.decorateCSS("data-card-title")}>{data.CardTitle}</h4>
-                  <p className={this.decorateCSS("data-card-description")}>{data.CardDescription}</p>
-                </div>
-              )
-            )}
+          <div className={this.decorateCSS("sections-container")}>
+            <div className={this.decorateCSS("stats6-up")}>
+              <h2 className={this.decorateCSS("header")}>
+                {this.getPropValue("card-header")}
+              </h2>
+              <p className={this.decorateCSS("header-description")}>
+                {this.getPropValue("header-text")}
+              </p>
+            </div>
+            <div className={this.decorateCSS("stats6-page")}>
+              {this.castToObject<CardData[]>("card-list").map(
+                (data: any, index: number) => (
+                  <div key={index} className={this.decorateCSS("card")}>
+                    <div className={this.decorateCSS("single-card")}>
+                      <h2 className={this.decorateCSS("card-title")}>
+                        {data.CardTitle}
+                      </h2>
+                      <p className={this.decorateCSS("card-description")}>
+                        {data.CardDescription}
+                      </p>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
           </div>
         </div>
       </div>
