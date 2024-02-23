ce121b16deff3995210b51b7c8c952f06cd0b2ec
Melike Aksoy <melikeaksoy.melike@gmail.com>
2024-02-23 14:50:16 +0300

    download-1

--- editor-components/download/download1/download1.tsx
+++ editor-components/download/download1/download1.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import styles from "./download1.module.scss";
 import { BaseDownload } from "../../EditorComponent";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Button = {
   buttonText: string;
   url: string;
   isPrimary: boolean;
+  buttonIcon: string;
+  buttonColor: string
+
 };
 class DownloadCard1 extends BaseDownload {
   constructor(props?: any) {
@@ -15,49 +19,30 @@ class DownloadCard1 extends BaseDownload {
       type: "string",
       key: "title1",
       displayer: "Title-1",
-      value: "Download Our Free Mobile App Today",
+      value: "DON'T WASTE TIME,",
     });
 
     this.addProp({
       type: "string",
       key: "title2",
-      value: "Download Our Free Mobile App Today",
       displayer: "Title-2",
+      value: "Download Now!",
+
     });
 
 
     this.addProp({
       type: "string",
-      key: "text2",
-      value: "Enjoy exclusive offers and discounts when you download our app",
-      displayer: "Description-2",
-    });
+      key: "description",
+      displayer: "Description",
+      value: "Eleifend morbi velit porttitor sed ipsum imperdiet ac ullamcorper fusce.",
 
-    this.addProp({
-      type: "string",
-      key: "text3",
-      value: "Stay connected with our latest news and updates on the go",
-      displayer: "Description-3",
-    });
-
-    this.addProp({
-      type: "image",
-      key: "image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6436af1668c3c2002cd2fa38?alt=media&timestamp=1692972147210",
-      displayer: "Image",
-    });
-
-    this.addProp({
-      type: "boolean",
-      key: "reverse",
-      displayer: "Reverse",
-      value: false,
     });
 
     this.addProp({
       type: "array",
       key: "buttons",
-      displayer: "Button",
+      displayer: "Button ",
       value: [
         {
           type: "object",
@@ -68,7 +53,7 @@ class DownloadCard1 extends BaseDownload {
               type: "string",
               key: "buttonText",
               displayer: "Button Text",
-              value: "Download",
+              value: "PLAYSTORE",
             },
             {
               type: "page",
@@ -77,15 +62,83 @@ class DownloadCard1 extends BaseDownload {
               value: "",
             },
             {
-              type: "boolean",
-              key: "isPrimary",
-              displayer: "Is primary",
-              value: false,
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "In Button Icon",
+              value: "ImAndroid",
             },
+            {
+             type: "color",
+            key: "buttonColor",
+            displayer: "Button Color",
+            value: "LightGreen",
+          }
+          ],
+        },
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "APPSTORE",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
+            },
+            {
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "In Button Icon",
+              value: "GrApple",
+            },
+            { type: "color",
+            key: "buttonColor",
+            displayer: "Button Color",
+            value: "grey",
+          }
+          ],
+        },
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "WINDOWS",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
+            },
+            {
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "In Button Icon",
+              value: "FaWindows",
+            },
+            {
+              type: "color",
+              key: "buttonColor",
+              displayer: "Button Color",
+              value: "blue",
+            }
           ],
         },
       ],
     });
+
   }
 
   getName(): string {
@@ -96,52 +149,42 @@ class DownloadCard1 extends BaseDownload {
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div
-            className={`${this.decorateCSS("basic-page")} ${this.getPropValue("reverse") &&
-              this.decorateCSS("basic-page-reverse")
-              }`}
-          >
-            <div
-              className={`${this.decorateCSS("page-1")} ${this.getPropValue("reverse") &&
-                this.decorateCSS("page-1-border")
-                }`}
-            >
-              <div className={this.decorateCSS("box")}>
-                <p className={this.decorateCSS("title1")}>{this.getPropValue("title1")}</p>
-              </div>
-              <div className={this.decorateCSS("box")}>
-                <p className={this.decorateCSS("text2")}>
-                  <span className={this.decorateCSS("title1")}>✔</span>
-                  {this.getPropValue("text2")}
-                </p>
-                <p className={this.decorateCSS("text3")}>
-                  <span className={this.decorateCSS("title1")}>✔</span>
-                  {this.getPropValue("text3")}
-                </p>
-              </div>
-              <div className={this.decorateCSS("box")}>
-                {this.castToObject<Button[]>("buttons").map(
-                  (item: Button, index: number) => {
-                    return (
-                      <ComposerLink key={`dw-1-btn-${index}`} path={item.url}>
-                        <button
-                          className={`${this.decorateCSS("button")} ${item.isPrimary && this.decorateCSS("button-color")
-                            }`}
-                        >
-                          {item.buttonText}
-                        </button>
-                      </ComposerLink>
-                    );
-                  }
-                )}
-              </div>
-            </div>
-            <div className={this.decorateCSS("page-2")}>
-              <img className={this.decorateCSS("image")} src={this.getPropValue("image")} alt=""/>
-            </div>
+
+
+          <p className={this.decorateCSS("title1")}>{this.getPropValue("title1")}</p>
+          <p className={this.decorateCSS("title2")}>{this.getPropValue("title2")}</p>
+          <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
+
+
+          <div className={this.decorateCSS("box")}>
+            {this.castToObject<Button[]>("buttons").map(
+              (item: Button, index: number) => {
+                return (
+                  <ComposerLink key={`dw-1-btn-${index}`} path={item.url} >
+                    <button
+                      className={`${this.decorateCSS("button")} ${item.isPrimary && this.decorateCSS("button-color")
+                        }`}
+                    >
+                      <ComposerIcon name={item.buttonIcon} propsIcon={{
+                        className: this.decorateCSS("icon"),
+                        color: item.buttonColor,
+                      }} />
+                      
+
+                      {item.buttonText}
+                    </button>
+                  </ComposerLink>
+                );
+              }
+            )}
           </div>
+
         </div>
+
+
       </div>
+
+
     );
   }
 }
