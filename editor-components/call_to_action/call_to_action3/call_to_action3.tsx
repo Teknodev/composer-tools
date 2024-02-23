f24eb929bbb705c10ab38224c8eec55c62c23b65
Şahin Ateş <ayazelar@gmail.com>
2024-02-23 12:14:55 +0300

    feat: cal to action 3 changes

--- editor-components/call_to_action/call_to_action3/call_to_action3.tsx
+++ editor-components/call_to_action/call_to_action3/call_to_action3.tsx
@@ -14,39 +14,16 @@ class CallToAction3Page extends BaseCallToAction {
       type: "string",
       key: "title",
       displayer: "Title",
-      value: "Welcome to UDesign Hosting Company",
+      value: "Protecting Your People , Property & Life",
     });
     this.addProp({
       type: "string",
       key: "description",
       displayer: "Description",
-      value: "Lorem ipsum dolor sit amet, consectetur adipiscinglit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      value:
+        "OUR CUSTOMERS EVERYWHERE TRUST OUR FULLY ACCREDITED AND TAILORED SOLUTIONS ON THE OTHER HAND WE DENOUNCE WITH RIGHTEOUS INDIGNATION",
     });
-    this.addProp({
-      type: "object",
-      key: "callBox",
-      displayer: "Call Box",
-      value: [
-        {
-          type: "image",
-          key: "callImage",
-          displayer: "Call Image",
-          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64ec7626057bdf002c2aa322?alt=media&timestamp=1693218318902"
-        },
-        {
-          type: "string",
-          key: "callNo",
-          displayer: "Phone Number",
-          value: "1-800-123-4567"
-        },
-        {
-          type: "string",
-          key: "callDescription",
-          displayer: "Call Description",
-          value: "Call Us Now"
-        }
-      ]
-    })
+
     this.addProp({
       type: "array",
       key: "buttons",
@@ -61,15 +38,15 @@ class CallToAction3Page extends BaseCallToAction {
               type: "string",
               key: "buttonText",
               displayer: "Button Text",
-              value: "Get Started"
+              value: "LEARN MORE",
             },
             {
               type: "page",
               key: "buttonLink",
               displayer: "Button Link",
-              value: ""
-            }
-          ]
+              value: "",
+            },
+          ],
         },
         {
           type: "object",
@@ -80,26 +57,27 @@ class CallToAction3Page extends BaseCallToAction {
               type: "string",
               key: "buttonText",
               displayer: "About Us",
-              value: "About Us"
+              value: "GET IN TOUCH",
             },
             {
               type: "page",
               key: "buttonLink",
               displayer: "Button Link",
-              value: ""
-            }
-          ]
+              value: "",
+            },
+          ],
         },
-      ]
-    })
+      ],
+    });
     this.addProp({
       type: "image",
       key: "image",
       displayer: "Image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64ec7cc7057bdf002c2aa3ea?alt=media&timestamp=1693899536058"
-    })
+      value:
+        "https://20067062.fs1.hubspotusercontent-na1.net/hubfs/20067062/cta-bg.jpg",
+    });
   }
-  
+
   getName(): string {
     return "Call To Action 3";
   }
@@ -109,44 +87,27 @@ class CallToAction3Page extends BaseCallToAction {
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("call-to-action3-page")}>
-            <div className={this.decorateCSS("left")}>
+            <div className={this.decorateCSS("content-container")}>
               <h1 className={this.decorateCSS("title")}>
                 {this.getPropValue("title")}
               </h1>
-              <p className={this.decorateCSS("description")}>
+              <div className={this.decorateCSS("description")}>
                 {this.getPropValue("description")}
-              </p>
-              <div className={this.decorateCSS("call")}>
-                <img className={this.decorateCSS("call-image")} src={
-                  this.getPropValue("callBox")[0].value
-                } alt="" />
-                <div className={this.decorateCSS("call-texts")}>
-                  <h2 className={this.decorateCSS("call-no")}>
-                    {this.getPropValue("callBox")[1].value}
-                  </h2>
-                  <p className={this.decorateCSS("call-description")}>
-                    {this.getPropValue("callBox")[2].value}
-                  </p>
-                </div>
               </div>
+
               <div className={this.decorateCSS("buttons")}>
-                {
-                  this.castToObject<Button[]>("buttons").map((button: Button, index: number) => {
+                {this.castToObject<Button[]>("buttons").map(
+                  (button: Button, index: number) => {
                     return (
-                      <ComposerLink
-                        key={index}
-                        
-                        page={button.buttonLink}
-                      >
-                        <button className={this.decorateCSS("button")}>{button.buttonText}</button>
+                      <ComposerLink key={index} page={button.buttonLink}>
+                        <button className={this.decorateCSS("button")}>
+                          {button.buttonText}
+                        </button>
                       </ComposerLink>
                     );
-                  })
-                }
+                  }
+                )}
               </div>
-            </div>
-            <div className={this.decorateCSS("right")}>
-              <img className={this.decorateCSS("right-image")} src={this.getPropValue("image")} alt="" />
             </div>
           </div>
         </div>
