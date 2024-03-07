b4a68968fb47adc36d6b0c6863bb76e6601678c6
esraunall <esraaunall2@gmail.com>
2024-03-07 16:02:13 +0300

    team15

--- editor-components/team/team-15/team15.tsx
+++ editor-components/team/team-15/team15.tsx
@@ -11,12 +11,55 @@ type Card = {
 class Team15 extends Team {
   constructor(props?: any) {
     super(props, styles);
-
+    this.addProp({
+      type:"string",
+      key:"badge",
+      displayer:"Badge",
+      value:"OUR TEAM",
+    });
     this.addProp({
       type: "array",
       key: "cards",
       displayer: "cards",
       value: [
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Aleesha Charlotte",
+            },
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "CEO Founder",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            },
+            {
+              type: "string",
+              key: "firstIcon",
+              displayer: "First Icon",
+              value: "IoMdShare",
+            },
+            {
+              type: "string",
+              key: "secondIcon",
+              displayer: "Second Icon",
+              value: "IoMdMail ",
+            },
+          ],
+        },
         {
           type: "object",
           key: "card",
@@ -41,31 +84,17 @@ class Team15 extends Team {
               value:
                 "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
             },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
             {
               type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Andrew Tate",
+              key: "firstIcon",
+              displayer: "First Icon",
+              value: "IoMdShare",
             },
             {
               type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "Entrepreneur",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1600",
+              key: "secondIcon",
+              displayer: "Second Icon",
+              value: "IoMdMail ",
             },
           ],
         },
@@ -78,20 +107,32 @@ class Team15 extends Team {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "Adele",
+              value: "Lionel Messi",
             },
             {
               type: "string",
               key: "description",
               displayer: "Description",
-              value: "Singer",
+              value: "footballer",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+                "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            },
+            {
+              type: "string",
+              key: "firstIcon",
+              displayer: "First Icon",
+              value: "IoMdShare",
+            },
+            {
+              type: "string",
+              key: "secondIcon",
+              displayer: "Second Icon",
+              value: "IoMdMail ",
             },
           ],
         },
@@ -103,28 +144,7 @@ class Team15 extends Team {
     return "Team 15";
   }
 
-  render() {
-    return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")}>
-            {this.castToObject<Card[]>("cards").map((card: Card, indexCards: number) => (
-              <div key={indexCards} className={this.decorateCSS("listed")}>
-                <img className={this.decorateCSS("image")} src={card.image}></img>
-                <div className={this.decorateCSS("little-container")}>
-                  <p className={this.decorateCSS("title")}>{card.title}</p>
-          
-                  <p className={this.decorateCSS("title-description")}>
-                    {card.description}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
+  
 }
 
 export default Team15;
