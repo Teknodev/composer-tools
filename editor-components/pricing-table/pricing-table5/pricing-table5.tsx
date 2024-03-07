d49b6eef6bfb7dd1b4e4703ece44fad3f193d2f2
esraunall <esraaunall2@gmail.com>
2024-03-07 15:01:36 +0300

    pricing5

--- editor-components/pricing-table/pricing-table5/pricing-table5.tsx
+++ editor-components/pricing-table/pricing-table5/pricing-table5.tsx
@@ -2,245 +2,253 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BasePricingTable } from "../../EditorComponent";
 import styles from "./pricing-table5.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type PriceList = {
-  image: string;
-  title: string;
-  stars: number;
-  price: string;
-  duration: string;
-  buttonText: string;
-  link: string;
+type Accordions = {
+  accordion:string;
+  header:string;
+  icon:string;
+  description:string;
+  contents:string;
+  headerButton:string;
+  headerButtonUrl:string;
 };
+
 class PricingTable5 extends BasePricingTable {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
+      type: "string",
+      key: "badge",
+      displayer: "Badge",
+      value: "FLEXIBLE PRICING",
+    });
+    this.addProp({
+      type: "string",
+      key: "subtitle",
+      displayer: "Page Title",
+      value: "We are excited for our work and how it positively impacts clients. With over 12 years of experience we have been constantly providing excellent solutions.",
+    });
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Tailored pricing plans for everyone.",
+    });
+  
+    this.addProp({
       type: "array",
-      key: "price-list",
-      displayer: "Price List",
+      key: "accordions",
+      displayer: "Accordions",
       value: [
         {
           type: "object",
-          key: "pricing-list",
-          displayer: "Pricing List",
+          key: "accordion",
+          displayer: "Accordion",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64e3303f057bdf002c2955dd?alt=media&timestamp=1692610602790",
-              displayer: "Image",
+              type: "string",
+              key: "header",
+              displayer: "Question",
+              value: "Basic Plan",
+            },
+            {
+              type:"string",
+              key:"icon",
+              displayer:"Icon",
+              value:"IoIosArrowDropdownCircle"
             },
             {
               type: "string",
-              key: "title",
-              value: "Enterprise",
-              displayer: "Title",
+              key: "description",
+              displayer: "Description",
+              value: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
             },
             {
-              type: "number",
-              key: "stars",
-              value: 5,
-              displayer: "Stars",
+              type:"string",
+              key:"contents",
+              displayer:"Contents",
+              value:"$19.99 / Monthly"
             },
             {
-              type: "string",
-              key: "price",
-              value: "$50",
-              displayer: "Price",
+              type:"string",
+              key:"headerButton",
+              displayer:"Header Button",
+              value:"GET STARTED"
             },
             {
-              type: "string",
-              key: "duration",
-              value: "/week",
-              displayer: "Duration",
-            },
-            {
-              type: "string",
-              key: "buttonText",
-              value: "Get started",
-              displayer: "Button Text",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              type:"string",
+              key:"headerButtonUrl",
+              displayer:"Header Button Url",
+              value:" "
             },
           ],
         },
         {
           type: "object",
-          key: "pricing-list",
-          displayer: "Pricing List",
+          key: "accordion",
+          displayer: "Accordion",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64e3303f057bdf002c2955dd?alt=media&timestamp=1692610602790",
-              displayer: "Image",
+              type: "string",
+              key: "header",
+              displayer: "Question",
+              value: "Standard Plan",
+            },
+            {
+              type:"string",
+              key:"icon",
+              displayer:"Icon",
+              value:"IoIosArrowDropdownCircle"
             },
             {
               type: "string",
-              key: "title",
-              value: "Enterprise",
-              displayer: "Title",
+              key: "description",
+              displayer: "Description",
+              value: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
             },
             {
-              type: "number",
-              key: "stars",
-              value: 5,
-              displayer: "Stars",
+              type:"string",
+              key:"contents",
+              displayer:"Contents",
+              value:"$19.99 / Monthly"
             },
             {
-              type: "string",
-              key: "price",
-              value: "$150",
-              displayer: "Price",
+              type:"string",
+              key:"headerButton",
+              displayer:"Header Button",
+              value:"GET STARTED"
             },
             {
-              type: "string",
-              key: "duration",
-              value: "/month",
-              displayer: "Duration",
-            },
-            {
-              type: "string",
-              key: "buttonText",
-              value: "Get started",
-              displayer: "Button Text",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              type:"string",
+              key:"headerButtonUrl",
+              displayer:"Header Button Url",
+              value:" "
             },
           ],
         },
         {
           type: "object",
-          key: "pricing-list",
-          displayer: "Pricing List",
+          key: "accordion",
+          displayer: "Accordion",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64e3303f057bdf002c2955dd?alt=media&timestamp=1692610602790",
-              displayer: "Image",
+              type: "string",
+              key: "header",
+              displayer: "Question",
+              value: "Premium Plan",
+            },
+            {
+              type:"string",
+              key:"icon",
+              displayer:"Icon",
+              value:"IoIosArrowDropdownCircle"
             },
             {
               type: "string",
-              key: "title",
-              value: "Enterprise",
-              displayer: "Title",
+              key: "description",
+              displayer: "Description",
+              value: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod.",
             },
             {
-              type: "number",
-              key: "stars",
-              value: 5,
-              displayer: "Stars",
+              type:"string",
+              key:"contents",
+              displayer:"Contents",
+              value:"$19.99 / Monthly"
             },
             {
-              type: "string",
-              key: "price",
-              value: "$350",
-              displayer: "Price",
+              type:"string",
+              key:"headerButton",
+              displayer:"Header Button",
+              value:"GET STARTED"
             },
             {
-              type: "string",
-              key: "duration",
-              value: "/years",
-              displayer: "Duration",
-            },
-            {
-              type: "string",
-              key: "buttonText",
-              value: "Get started",
-              displayer: "Button Text",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              type:"string",
+              key:"headerButtonUrl",
+              displayer:"Header Button Url",
+              value:" "
             },
           ],
         },
       ],
     });
 
-    this.addProp({
-      type: "select",
-      key: "select",
-      displayer: "Location of Border",
-      value: "None",
-      additionalParams: {
-        selectItems: ["None", "Top" , "Bottom" , "All Around"],
-      }
-    });
-    this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 3,
-    });
+    this.setComponentState("activeIndex", -1);
+
   }
 
   getName(): string {
-    return "Pricing List 5";
+    return "Pricing 5";
+  }
+
+  handleBoxClick(index: number) {
+    const activeIndex = this.getComponentState("activeIndex");
+    this.setComponentState("activeIndex", activeIndex === index ? -1 : index);
   }
 
   render() {
-    const selectValue = this.getPropValue("select");
     return (
       <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          {this.castToObject<PriceList[]>("price-list").map(
-            (pricing: any, index: number) => (
-              <div className={this.decorateCSS("card-item-count")} style={{
-                width: 90 / this.getPropValue("itemCount") + "%",
-              }}>
-              <div key={index} className={`${this.decorateCSS("price")} ${selectValue === "Top" ? this.decorateCSS("border-top") : selectValue === "Bottom" ? this.decorateCSS("border-bottom") : selectValue === "All Around" ? this.decorateCSS("border-all") : ""}`}>
-                <img
-                  alt=""
-                  className={this.decorateCSS("image")}
-                  src={pricing.image}
-                  width={280}
-                  />
-                <h2 className={this.decorateCSS("title")}>
-                  {pricing.title}
-                  <div className={this.decorateCSS("stars")}>
-                    {[...Array(Number(pricing.stars))].map(
-                      (item: any, indexStars: number) => (
-                        <img key={`stars-${indexStars}`} alt="" src="https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/646c7aa5fba070002b749808?alt=media&timestamp=1684830888295"/>
-                    ))}
-                  </div>
-                </h2>
-                <h1 className={this.decorateCSS("price-text")}>
-                  {pricing.price}
-                  <p className={this.decorateCSS("duration-text")}>
-                    {pricing.duration}
-                  </p>
-                </h1>
-                <div className={this.decorateCSS("button-child")}>
-                  <ComposerLink path={pricing.link}>
-                    <span className={this.decorateCSS("button")}>
-                      {pricing.buttonText}
-                    </span>
-                  </ComposerLink>
+        <div className={this.decorateCSS("max-content")}>      
+          <div className={this.decorateCSS("page")}>
+            <div className={this.decorateCSS("text-center")}>    
+            <div className={this.decorateCSS("badge")}>
+            {this.getPropValue("badge")}
+          </div>
+          <div className={this.decorateCSS("title")}>
+            {this.getPropValue("title")}
+          </div>
+          <div className={this.decorateCSS("subtitle")}>
+            {this.getPropValue("subtitle")}
+          </div>
+          </div>
+          <div className={this.decorateCSS("accordions-table")}>
+            {this.castToObject<Accordions[]>("accordions").map
+            ((accordion:any,index:number)=>(
+              <div key={index} className={this.decorateCSS("accordions")}>
+              <div className={this.decorateCSS("box")}
+              onClick={() => this.handleBoxClick(index)}>
+
+              <div className={this.decorateCSS("accordion-header")}>
+                <div className={this.decorateCSS("header")}>{accordion.header} 
+                </div>           
+                <div className={this.decorateCSS("icon-container")}>
+                  <ComposerIcon
+                     name={accordion.icon}
+                     propsIcon={{
+                     className: this.decorateCSS("icon")
+                     }}
+                  />  
+                </div>
+                </div>
+                {this.getComponentState("activeIndex") === index && (
+                <div className={this.decorateCSS("text")}>
+                <div className={this.decorateCSS("description")}>{accordion.description} </div>
+                <div className={this.decorateCSS("c-h")}>
+                <div className={this.decorateCSS("contents")}>{accordion.contents} </div>
+                <button className={this.decorateCSS("headerButton")}>
+                  {accordion.headerButtonUrl !== " " ? (
+                  <ComposerLink path={accordion.headerButtonUrl} text={accordion.headerButton} />
+                   ) : (
+                  accordion.headerButton
+                  )}
+                </button>
                 </div>
               </div>
+                )}
+              </div>
+              </div>
+            ))}         
           </div>
-            )
-          )}
         </div>
       </div>
-      );
+    </div>
+    );
   }
 }
 
 export default PricingTable5;
+
+
+
+
