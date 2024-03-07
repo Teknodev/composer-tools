33e340d1ce6a1714e7c0e165c59155ac06b9ed2a
Şahin Ateş <ayazelar@gmail.com>
2024-03-07 15:59:02 +0300

    changes

--- editor-components/feature/feature1/feature1.tsx
+++ editor-components/feature/feature1/feature1.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature1.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type ICircleBoxes = {
   title: string;
   description: string;
-  image: string;
+  icon: string;
 };
 class Feature1 extends BaseFeature {
   constructor(props?: any) {
@@ -35,11 +36,11 @@ class Feature1 extends BaseFeature {
                 "Elon Musk is a well-known entrepreneur, inventor, and engineer who was born in South Africa in 1971. He is the CEO of several successful companies, including SpaceX, Tesla Inc., Neuralink, and The Boring Company.",
             },
             {
-              type: "image",
-              key: "image",
-              displayer: "Image",
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
               value:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Question_mark_white_icon.svg/1200px-Question_mark_white_icon.svg.png",
+                "FaQuestion",
             },
           ],
         },
@@ -62,11 +63,11 @@ class Feature1 extends BaseFeature {
                 "Mark Zuckerberg is a technology entrepreneur and the co-founder of Facebook, one of the world's largest social media platforms. He was born on May 14, 1984, in New York, United States. Zuckerberg founded Facebook.",
             },
             {
-              type: "image",
-              key: "image",
-              displayer: "Image",
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
               value:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Question_mark_white_icon.svg/1200px-Question_mark_white_icon.svg.png",
+                "FaQuestion",
             },
           ],
         },
@@ -89,11 +90,11 @@ class Feature1 extends BaseFeature {
                 "Under Bezos' leadership, Amazon.com has grown into one of the world's most valuable companies, with a market capitalization of over $1 trillion. Bezos has also become one of the wealthiest people in the world, with a net worth of over $100 billion.",
             },
             {
-              type: "image",
-              key: "image",
-              displayer: "Image",
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
               value:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Question_mark_white_icon.svg/1200px-Question_mark_white_icon.svg.png",
+                "FaQuestion",
             },
           ],
         },
@@ -104,6 +105,7 @@ class Feature1 extends BaseFeature {
       key: "itemCount",
       displayer: "Item count in a row",
       value: 3,
+      max: 3
     });
     this.addProp({
       type: "select",
@@ -127,7 +129,7 @@ class Feature1 extends BaseFeature {
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           {this.castToObject<ICircleBoxes[]>("circle-boxes-content").map(
-            (circlebox: any, index: number) => (
+            (circlebox: ICircleBoxes, index: number) => (
               <div
                 className={this.decorateCSS("card-item-count")}
                 style={{
@@ -136,10 +138,10 @@ class Feature1 extends BaseFeature {
                 key={index}
               >
                   <div className={this.decorateCSS(iconShape.toLowerCase())}>
-                    <div className={this.decorateCSS("image-background")}>
-                      <img
-                        className={this.decorateCSS("image")}
-                        src={circlebox.image}
+                    <div className={this.decorateCSS("icon-background")}>
+                      <ComposerIcon
+                        propsIcon={{className: this.decorateCSS("icon")}}
+                        name={circlebox.icon}
                       />
                     </div>
                     <h3 className={this.decorateCSS("title")}>{circlebox.title}</h3>
