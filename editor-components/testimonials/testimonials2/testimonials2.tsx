3be833d180a3a88a1411f51d9170a1c4264ea75d
esraunall <esraaunall2@gmail.com>
2024-03-07 15:39:38 +0300

    testimonials2

--- editor-components/testimonials/testimonials2/testimonials2.tsx
+++ editor-components/testimonials/testimonials2/testimonials2.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Testimonials } from "../../EditorComponent";
 import styles from "./testimonials2.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Item = {
   image: string;
@@ -15,15 +16,21 @@ class Testimonials2Page extends Testimonials {
     super(props, styles);
     this.addProp({
       type: "string",
+      key: "badge",
+      displayer: "Badge",
+      value: "WHAT OUR CLIENTS SAY",
+    });
+    this.addProp({
+      type: "string",
       key: "title",
       displayer: "Title",
-      value: "Testimonials",
+      value: "Happy Clients' Testimonials",
     });
     this.addProp({
       type: "number",
       key: "itemCount",
       displayer: "Item count in a row",
-      value: 3,
+      value: 5,
     });
     this.addProp({
       type: "array",
@@ -36,36 +43,30 @@ class Testimonials2Page extends Testimonials {
           displayer: "Card",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644930bdf72de2002caa9944?alt=media&timestamp=1682518219540",
+              type: "icon",
+              key: "icon",
               displayer: "Image",
+              value:
+                "RiStarSFill",
+            },       
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value:
+                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",           
             },
             {
               type: "string",
               key: "name",
-              value: "Erle Philomena",
-              displayer: "Name",
+              displayer: "name",
+              value: "Mary Sheram",
             },
             {
               type: "string",
-              key: "nameId",
-              value: "@Erle_Philomena",
-              displayer: "Name Id",
-            },
-            {
-              type: "string",
-              key: "description",
-              value:
-                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",
-              displayer: "Description",
-            },
-            {
-              type: "string",
-              key: "time",
-              value: "15 days ago",
-              displayer: "time",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Designer",
             },
           ],
         },
@@ -75,36 +76,30 @@ class Testimonials2Page extends Testimonials {
           displayer: "Card",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6449315ff72de2002caa998e?alt=media&timestamp=1682518379680",
+              type: "icon",
+              key: "icon",
               displayer: "Image",
+              value:
+                "RiStarSFill",
+            },       
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value:
+                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",           
             },
             {
               type: "string",
               key: "name",
-              value: "Shonda Kadence",
-              displayer: "Name",
+              displayer: "name",
+              value: "Mary Sheram",
             },
             {
               type: "string",
-              key: "nameId",
-              value: "@Shonda_Kadence",
-              displayer: "Name Id",
-            },
-            {
-              type: "string",
-              key: "description",
-              value:
-                "Grunge design is all about creating a rough, raw, and edgy look. It can be a great way to add personality and attitude to a design.",
-              displayer: "Description",
-            },
-            {
-              type: "string",
-              key: "time",
-              value: "5 month ago",
-              displayer: "time",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Designer",
             },
           ],
         },
@@ -114,39 +109,100 @@ class Testimonials2Page extends Testimonials {
           displayer: "Card",
           value: [
             {
-              type: "image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6449315ff72de2002caa998f?alt=media&timestamp=1682518379680",
+              type: "icon",
+              key: "icon",
               displayer: "Image",
+              value:
+                "RiStarSFill",
+            },       
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value:
+                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",           
             },
             {
               type: "string",
               key: "name",
-              value: "Laurie Darwin",
-              displayer: "Name",
+              displayer: "name",
+              value: "Mary Sheram",
             },
             {
               type: "string",
-              key: "nameId",
-              value: "@Laurie_Darwin",
-              displayer: "Name Id",
-            },
-            {
-              type: "string",
-              key: "description",
-              value:
-                "Responsive design is a must for modern websites and interfaces. It ensures that content is displayed optimally on different screen sizes and devices.",
-              displayer: "Description",
-            },
-            {
-              type: "string",
-              key: "time",
-              value: "2 years ago",
-              displayer: "time",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Designer",
             },
           ],
         },
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Image",
+              value:
+                "RiStarSFill",
+            },       
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value:
+                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",           
+            },
+            {
+              type: "string",
+              key: "name",
+              displayer: "name",
+              value: "Mary Sheram",
+            },
+            {
+              type: "string",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Designer",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Image",
+              value:
+                "RiStarSFill",
+            },       
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value:
+                "Flat design is characterized by simple, two-dimensional elements and a clean, minimal aesthetic. It's a great way to create a streamlined and modern look.",           
+            },
+            {
+              type: "string",
+              key: "name",
+              displayer: "name",
+              value: "Mary Sheram",
+            },
+            {
+              type: "string",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Designer",
+            },
+          ],
+        },
+       
       ],
     });
   }
@@ -159,6 +215,7 @@ class Testimonials2Page extends Testimonials {
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
+          <div className={this.decorateCSS("badge")}>{this.getPropValue("badge")}</div>
             <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
           <div className={this.decorateCSS("testimonials2")}>
               {this.castToObject<Item[]>("card-items").map(
@@ -171,15 +228,9 @@ class Testimonials2Page extends Testimonials {
                           minWidth: "250px",
                         }}>
                   <div key={index} className={this.decorateCSS("card")} >
-                    <div className={this.decorateCSS("profile")}>
-                      <img width={50} height={50} src={item.image} alt=""/>
-                      <div className={this.decorateCSS("profile-text")}>
-                        <h2 className={this.decorateCSS("item-name")}>{item.name}</h2>
-                        <p className={this.decorateCSS("item-name-id")}>{item.nameId}</p>
-                      </div>
-                    </div>
+                    <div className={this.decorateCSS("icon")}></div>
                     <span className={this.decorateCSS("item-description")}>{item.description}</span>
-                    <h5 className={this.decorateCSS("item-time")}>{item.time}</h5>
+                    <h5 className={this.decorateCSS("item-name")}>{item.name}</h5>
                   </div>
             </div>
                 )
