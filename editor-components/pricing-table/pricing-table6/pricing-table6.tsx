59cfde544672bbbad9bea85aaf89d3fb9c461c97
simalyolgosteren <148366227+simalyolgosteren@users.noreply.github.com>
2024-03-08 14:41:56 +0300

    commit

--- editor-components/pricing-table/pricing-table6/pricing-table6.tsx
+++ editor-components/pricing-table/pricing-table6/pricing-table6.tsx
@@ -23,35 +23,20 @@ class PricingMultipleTwo extends BasePricingTable {
       type: "string",
       key: "badge",
       displayer: "Badge",
-      value: "PRICING",
+      value: "AFFORDABLE PRICING",
     });
 
     this.addProp({
       type: "string",
       key: "title",
       displayer: "Title",
-      value: "Pricing Strategies",
-    });
-
-    this.addProp({
-      type: "string",
-      key: "description",
-      displayer: "Description",
-      value:
-        "Pricing is a crucial element of any business strategy, and small businesses need to adopt effective pricing strategies to stay competitive.",
-    });
-
-    this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 2,
+      value: "Choose the plan that's right for you",
     });
 
     this.addProp({
       type: "array",
-      key: "card",
-      displayer: "Card",
+      key: "plans",
+      displayer: "Plans",
       value: [
         {
           type: "object",
@@ -60,9 +45,15 @@ class PricingMultipleTwo extends BasePricingTable {
           value: [
             {
               type: "string",
-              key: "cardTitle1",
-              displayer: "Card Title-1",
-              value: "Enterprise",
+              key: "plan",
+              displayer: "Plan",
+              value: "Primary",
+            },
+            {
+              type: "string",
+              key: "gain",
+              displayer: "Your Gain",
+              value: "Save 20%",
             },
             {
               type: "string",
@@ -74,32 +65,13 @@ class PricingMultipleTwo extends BasePricingTable {
               type: "number",
               key: "price",
               displayer: "Price",
-              value: 50,
+              value: 20,
             },
             {
               type: "string",
               key: "per",
               displayer: "Per",
               value: "per month",
-            },
-
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "Basic features for up to 50 user",
-            },
-            {
-              type: "string",
-              key: "activeTag",
-              displayer: "Active Tag",
-              value: "POPULAR",
-            },
-            {
-              type: "boolean",
-              key: "active",
-              displayer: "Is Popular",
-              value: true,
             },
             {
               type: "array",
@@ -108,40 +80,34 @@ class PricingMultipleTwo extends BasePricingTable {
               value: [
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "50 Websites",
+                  key: "badge",
+                  displayer: "Badge",
+                  value: "PRIMARY",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "50 GB Disk Space",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Find places near hotels and camp",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "Unlimited Bandwidth",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Track record and reward programs",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "50 Email Accounts",
-                },
-                {
-                  type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "24/7/365 Support",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Shows transportation option",
                 },
               ],
             },
             {
               type: "array",
               key: "buttons",
-              displayer: "Button",
+              displayer: "Buttons",
               value: [
                 {
                   type: "object",
@@ -150,21 +116,15 @@ class PricingMultipleTwo extends BasePricingTable {
                   value: [
                     {
                       type: "string",
-                      key: "buttonTitle",
-                      displayer: "Button Title",
-                      value: "Get Started",
+                      key: "buttonText",
+                      displayer: "Button Text",
+                      value: "Join this plan",
                     },
                     {
                       type: "page",
                       key: "url",
-                      displayer: "Button Link",
+                      displayer: "Link",
                       value: "",
-                    },
-                    {
-                      type: "boolean",
-                      key: "isPrimary",
-                      displayer: "Is primary",
-                      value: true,
                     },
                   ],
                 },
@@ -179,9 +139,15 @@ class PricingMultipleTwo extends BasePricingTable {
           value: [
             {
               type: "string",
-              key: "cardTitle1",
-              displayer: "Card Title-1",
-              value: "Business",
+              key: "plan",
+              displayer: "Plan",
+              value: "Popular",
+            },
+            {
+              type: "string",
+              key: "gain",
+              displayer: "Your Gain",
+              value: "Save 30%",
             },
             {
               type: "string",
@@ -193,32 +159,13 @@ class PricingMultipleTwo extends BasePricingTable {
               type: "number",
               key: "price",
               displayer: "Price",
-              value: 100,
+              value: 19,
             },
             {
               type: "string",
               key: "per",
               displayer: "Per",
               value: "per month",
-            },
-
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "Basic features for up to 100 user",
-            },
-            {
-              type: "string",
-              key: "activeTag",
-              displayer: "Active Tag",
-              value: "POPULAR",
-            },
-            {
-              type: "boolean",
-              key: "active",
-              displayer: "Is Popular",
-              value: true,
             },
             {
               type: "array",
@@ -227,40 +174,34 @@ class PricingMultipleTwo extends BasePricingTable {
               value: [
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "100 Websites",
+                  key: "badge",
+                  displayer: "Badge",
+                  value: "PRIMARY",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "100 GB Disk Space",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Find places near hotels and camp",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "Unlimited Bandwidth",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Track record and reward programs",
                 },
                 {
                   type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "100 Email Accounts",
-                },
-                {
-                  type: "string",
-                  key: "liText",
-                  displayer: "List Item",
-                  value: "24/7/365 Support",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Shows transportation option",
                 },
               ],
             },
             {
               type: "array",
               key: "buttons",
-              displayer: "Button",
+              displayer: "Buttons",
               value: [
                 {
                   type: "object",
@@ -269,21 +210,109 @@ class PricingMultipleTwo extends BasePricingTable {
                   value: [
                     {
                       type: "string",
-                      key: "buttonTitle",
-                      displayer: "Button Title",
-                      value: "Get Started",
+                      key: "buttonText",
+                      displayer: "Button Text",
+                      value: "Join this plan",
                     },
                     {
                       type: "page",
                       key: "url",
-                      displayer: "Button Link",
+                      displayer: "Link",
                       value: "",
                     },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "listArray",
+          displayer: "List Items",
+          value: [
+            {
+              type: "string",
+              key: "plan",
+              displayer: "Plan",
+              value: "Premium",
+            },
+            {
+              type: "string",
+              key: "gain",
+              displayer: "Your Gain",
+              value: "Save 35%",
+            },
+            {
+              type: "string",
+              key: "currency",
+              displayer: "Currency",
+              value: "$",
+            },
+            {
+              type: "number",
+              key: "price",
+              displayer: "Price",
+              value: 28,
+            },
+            {
+              type: "string",
+              key: "per",
+              displayer: "Per",
+              value: "per month",
+            },
+            {
+              type: "array",
+              key: "list",
+              displayer: "List Items",
+              value: [
+                {
+                  type: "string",
+                  key: "badge",
+                  displayer: "Badge",
+                  value: "PRIMARY",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Find places near hotels and camp",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Track record and reward programs",
+                },
+                {
+                  type: "string",
+                  key: "property",
+                  displayer: "Property",
+                  value: "Shows transportation option",
+                },
+              ],
+            },
+            {
+              type: "array",
+              key: "buttons",
+              displayer: "Buttons",
+              value: [
+                {
+                  type: "object",
+                  key: "button",
+                  displayer: "Button",
+                  value: [
                     {
-                      type: "boolean",
-                      key: "isPrimary",
-                      displayer: "Is primary",
-                      value: true,
+                      type: "string",
+                      key: "buttonText",
+                      displayer: "Button Text",
+                      value: "Join this plan",
+                    },
+                    {
+                      type: "page",
+                      key: "url",
+                      displayer: "Link",
+                      value: "",
                     },
                   ],
                 },
@@ -293,123 +322,27 @@ class PricingMultipleTwo extends BasePricingTable {
         },
       ],
     });
+
+    this.addProp({
+      type: "string",
+      key: "contact",
+      displayer: "Contact",
+      value: "Contact",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "contactTitle",
+      displayer: "Contact Title",
+      value: "Looking for a corporate solution? Contact crafto",
+    });
   }
 
   getName(): string {
     return "Pricing 6";
   }
 
-  render() {
-    const littleScreen = {
-      width: "100%",
-      display: "flex",
-      flex: "wrap",
-      justifyContent: "center",
-    };
-
-    const wideScreen = {
-      width: 90 / this.getPropValue("itemCount") + "%",
-      display: "flex",
-      justifyContent: "center",
-    };
-
-    let style = function () {
-      if (window.innerWidth < 769) {
-        return littleScreen;
-      } else {
-        return wideScreen;
-      }
-    };
-
-    return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("page")}>
-            <div className={this.decorateCSS("up-page")}>
-              <div className={this.decorateCSS("badge")}>
-                {this.getPropValue("badge")}
-              </div>
-              <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-              <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
-            </div>
-
-            <div className={this.decorateCSS("down-page")}>
-              <div className={this.decorateCSS("cards")}>
-                {this.castToObject<Pricing[]>("card").map(
-                  (item: Pricing, indexCard: number) => {
-                    return (
-                      <div key={indexCard} style={style()}>
-                        <div className={this.decorateCSS("card")}>
-                          <div className={this.decorateCSS("card-up")}>
-                            <div className={this.decorateCSS("up-elements")}>
-                              <p className={this.decorateCSS("item-card-title1")}>{item.cardTitle1}</p>
-                              {item.active && (
-                                <div className={this.decorateCSS("tag")}>
-                                  {item.activeTag}
-                                </div>
-                              )}
-                            </div>
-                            <div className={this.decorateCSS("pricing")}>
-                              <div className={this.decorateCSS("price")}>
-                                <h1 className={this.decorateCSS("item-price")}>
-                                  {item.price}
-                                  <span className={this.decorateCSS("item-per")}>{item.per}</span>
-                                </h1>
-                                <div className={this.decorateCSS("currency")}>
-                                  <span className={this.decorateCSS("item-currency")}>{item.currency}</span>
-                                </div>
-                              </div>
-                              <p className={this.decorateCSS("item-description-p")}>{item.description}</p>
-                            </div>
-                            <div className={this.decorateCSS("button-group")}>
-                              {item.buttons.map((b: any, indexButtonGroup: number) => {
-                                return (
-                                  <ComposerLink key={indexButtonGroup} path={b.url}>
-                                    <button
-                                      className={
-                                        this.decorateCSS("button") +
-                                        " " +
-                                        (b.isPrimary
-                                          ? this.decorateCSS("primary")
-                                          : this.decorateCSS("secondary"))
-                                      }
-                                    >
-                                      {b.buttonTitle}
-                                    </button>
-                                  </ComposerLink>
-                                );
-                              })}
-                            </div>
-                          </div>
-                          <div className={this.decorateCSS("card-down")}>
-                            <ul className={this.decorateCSS("list-group")}>
-                              {item.list.map((el: any, indexListGroup: number) => {
-                                return (
-                                  <li
-                                    key={indexListGroup}
-                                    className={this.decorateCSS("li")}
-                                  >
-                                    <div className={this.decorateCSS("circle-icon")}>
-                                      <img src="https://cdn-icons-png.flaticon.com/512/33/33281.png" alt=""/>
-                                    </div>
-                                    {el.value}
-                                  </li>
-                                );
-                              })}
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+ 
 }
 
 export default PricingMultipleTwo;
