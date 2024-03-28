92113d4a4a8f6042430adafbf1cb83ad68a03846
Busraylvc <busra.yalvaac@gmail.com>
2024-03-28 13:42:46 +0300

    content8

--- editor-components/content/content8/content8.tsx
+++ editor-components/content/content8/content8.tsx
@@ -2,6 +2,15 @@ import * as React from "react";
 import { BaseContent } from "../../EditorComponent";
 import styles from "./content8.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ThirtyFpsRounded } from "@mui/icons-material";
+type Card = {
+  image: string;
+  header: string;
+  imgdescription: string;
+  comments: string;
+  time: string;
+  commentsUrl: URL;
+};
 class Content8 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
@@ -18,154 +27,189 @@ class Content8 extends BaseContent {
       value: "The Latest Updates News & Blog",
     });
     this.addProp({
-      type:"array",
-      key:"cards",
-      displayer:"Cards",
-      value:[
+      type: "array",
+      key: "cards",
+      displayer: "Cards",
+      value: [
         {
-          type:"object",
-          key:"card",
-          displayer:"",
-          value:[
+          type: "object",
+          key: "card",
+          displayer: "",
+          value: [
             {
               type: "image",
               key: "image",
               displayer: "Image",
-              value: "https://img.freepik.com/free-vector/corporate-website-abstract-concept-illustration_335657-1831.jpg",
+              value: "https://writebot-html.themetags.net/assets/img/blog-img-1.jpg",
             },
             {
-              type:"string",
-              key:"header",
-              displayer:"Header",
-              value:"All News",
+              type: "string",
+              key: "badge",
+              displayer: "Badge",
+              value: "All News",
             },
             {
-              type:"string",
-              key:"imgDescription",
-              displayer:"Img Description",
-              value:"Antenna Promises New era For Satellite Communication",
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "Antenna Promises New era For Satellite Communication",
             },
             {
-              type:"string",
-              key:"time",
-              displayer:"Time",
-              value:"March 18,2022",
+              type: "string",
+              key: "date",
+              displayer: "Date",
+              value: "March 18,2022",
             },
             {
-              type:"string",
-              key:"comments",
-              displayer:"Comments",
-              value:"Comments",
+              type: "string",
+              key: "details",
+              displayer: "Details",
+              value: "Comments",
+            },
+            {
+              type: "page",
+              key: "detailsUrl",
+              displayer: "Details Url",
+              value: ""
             },
           ]
         },
         {
-          type:"object",
-          key:"card",
-          displayer:"",
-          value:[
+          type: "object",
+          key: "card",
+          displayer: "",
+          value: [
             {
               type: "image",
               key: "image",
               displayer: "Image",
-              value: "https://img.freepik.com/free-vector/corporate-website-abstract-concept-illustration_335657-1831.jpg",
+              value: "https://writebot-html.themetags.net/assets/img/blog-img-2.jpg",
             },
             {
-              type:"string",
-              key:"header",
-              displayer:"Header",
-              value:"All News",
+              type: "string",
+              key: "badge",
+              displayer: "Badge",
+              value: "All News",
             },
             {
-              type:"string",
-              key:"imgDescription",
-              displayer:"Img Description",
-              value:"Antenna Promises New era For Satellite Communication",
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "Antenna Promises New era For Satellite Communication",
             },
             {
-              type:"string",
-              key:"time",
-              displayer:"Time",
-              value:"March 18,2022",
+              type: "string",
+              key: "date",
+              displayer: " Date",
+              value: "March 18,2022",
             },
             {
-              type:"string",
-              key:"comments",
-              displayer:"Comments",
-              value:"Comments",
+              type: "string",
+              key: "details",
+              displayer: "Details",
+              value: "Comments",
+            },
+            {
+              type: "page",
+              key: "detailsUrl",
+              displayer: "Details Url",
+              value: ""
             },
           ]
         },
         {
-          type:"object",
-          key:"card",
-          displayer:"",
-          value:[
+          type: "object",
+          key: "card",
+          displayer: "",
+          value: [
             {
               type: "image",
               key: "image",
               displayer: "Image",
-              value: "https://img.freepik.com/free-vector/corporate-website-abstract-concept-illustration_335657-1831.jpg",
+              value: "https://writebot-html.themetags.net/assets/img/blog-img-3.jpg",
             },
             {
-              type:"string",
-              key:"header",
-              displayer:"Header",
-              value:"All News",
+              type: "string",
+              key: "badge",
+              displayer: "Badge",
+              value: "All News",
             },
             {
-              type:"string",
-              key:"imgDescription",
-              displayer:"Img Description",
-              value:"Antenna Promises New era For Satellite Communication",
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "Antenna Promises New era For Satellite Communication",
             },
             {
-              type:"string",
-              key:"time",
-              displayer:"Time",
-              value:"March 18,2022",
+              type: "string",
+              key: "date",
+              displayer: "Date",
+              value: "March 18,2022",
             },
             {
-              type:"string",
-              key:"comments",
-              displayer:"Comments",
-              value:"Comments",
+              type: "string",
+              key: "details",
+              displayer: "Details",
+              value: "Comments",
+            },
+            {
+              type: "page",
+              key: "detailsUrl",
+              displayer: "Details Url",
+              value: ""
             },
           ]
         }
       ]
-    })    
+    })
+    this.addProp({
+      type: "number",
+      key: "itemCount",
+      displayer: "Item count in a row",
+      value: 3,
+      max: 3,
+    });
   }
-
   getName(): string {
     return "Content 8";
   }
 
-  // render() {
-  //   return (
-  //     <div className={this.decorateCSS("container")}>
-  //       <div className={this.decorateCSS("max-content")}>
-  //         <div className={this.decorateCSS("child")}>
-  //           <div className={this.decorateCSS("card")}>
-  //             <div className={this.decorateCSS("left")}>
-  //               <span className={this.decorateCSS("badge")}>
-  //                 {this.getPropValue("badge")}
-  //               </span>
-  //               <span className={this.decorateCSS("title")}>
-  //                 {this.getPropValue("title")}
-  //               </span>
-              
-  //             </div>
-  //             {/* <div className={this.decorateCSS("right")}>
-  //               <img src={this.getPropValue("image")} alt=""/>
-  //             </div> */}
-  //           </div>
-           
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-}
+  render() {
+    return (
+      <div className={this.decorateCSS("container")}>
+        <div className={this.decorateCSS("max-content")}>
+          <div className={this.decorateCSS("up-page")}>
+            <div className={this.decorateCSS("badge")}>{this.getPropValue("badge")}</div>
+            <div className={this.decorateCSS("title")}>{this.getPropValue("title")}</div>
+          </div>
+          <div className={this.decorateCSS("cards-box")} >
 
+            {this.castToObject<Card[]>("cards").map((card: any, index: number) =>
+              <div className={this.decorateCSS("card")} style={{
+                width: 90 / this.getPropValue("itemCount") + "%",
+              }}>
+                <div className={this.decorateCSS("card-div")}>
+                  <div className={this.decorateCSS("badge")}>{card.getPropValue("badge")}</div>
+                  <div className={this.decorateCSS("image-div")}>
+                    <img src={card.getPropValue("image")} alt={card.getPropValue("image")}
+                      className={this.decorateCSS("image")} />
+                  </div>
+                  <div className={this.decorateCSS("card-body")}>
+                    <div className={this.decorateCSS("description")}>{card.getPropValue("description")}</div>
+                    <div className={this.decorateCSS("time-comments")}>
+                      <h4 className={this.decorateCSS("date")}>{card.getPropValue("date")}</h4>
+                      <ComposerLink path={card.getPropValue("detailsUrl")}>
+                        <h4 className={this.decorateCSS("details")}>{card.getPropValue("details")}</h4>
+                      </ComposerLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 export default Content8;
