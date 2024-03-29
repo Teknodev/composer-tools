076c8a2fa06fcca0038911a79ed6c8f709474821
Şahin Ateş <ayazelar@gmail.com>
2024-03-29 17:45:50 +0300

    changes

--- editor-components/team/team-7/team7.tsx
+++ editor-components/team/team-7/team7.tsx
@@ -2,81 +2,82 @@ import * as React from "react";
 import styles from "./team7.module.scss";
 import { Team, TypeUsableComponentProps } from "../../EditorComponent";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+
+type Icons = {
+    url: string;
+    icon: string;
+  };  
 
 interface Card {
-    image: string,
-    name: string,
-    position: string
-    icons: { icon: string, url: string }[],
+    image: string;
+    name: string;
+    position: string;
+    icons: { icon: string; url: string }[];
 }
 
 class Team7 extends Team {
     constructor(props?: any) {
         super(props, styles);
-
         let twitter: TypeUsableComponentProps = {
             type: "object",
             key: "twitter",
-            displayer: "Twitter",
+            displayer: "Platform",
             value: [
-                {
-                    type: "image",
-                    key: "icon",
-                    displayer: "Platform Icon",
-                    value: "https://cdn-icons-png.flaticon.com/512/123/123728.png?w=826&t=st=1680695413~exp=1680696013~hmac=53c1e21ad33373dd862cdea82c2df246d0675f62cf171d9a1044da2cbb6cf6ac"
-                },
-                {
-                    type: "page",
-                    key: "url",
-                    displayer: "Url",
-                    value: ""
-                }
-            ]
-        };
-
-
+              {
+                type: "icon",
+                key: "icon",
+                displayer: "Platform Icon",
+                value: "IoLogoTwitter",
+              },
+              {
+                type: "page",
+                key: "url",
+                displayer: "Url",
+                value: "",
+              },
+            ],
+          };
 
         let facebook: TypeUsableComponentProps = {
             type: "object",
             key: "facebook",
             displayer: "Platform",
             value: [
-                {
-                    type: "image",
-                    key: "icon",
-                    displayer: "Platform Icon",
-                    value: "https://i.pinimg.com/originals/1f/28/95/1f28956fb27c5ef5f44cbafd250cb84b.png"
-                },
-                {
-                    type: "page",
-                    key: "url",
-                    displayer: "Url",
-                    value:""
-                }
-            ]
-        };
-
-
-
+              {
+                type: "icon",
+                key: "icon",
+                displayer: "Platform Icon",
+                value: "IoLogoFacebook",
+              },
+              {
+                type: "page",
+                key: "url",
+                displayer: "Url",
+                value: "",
+              },
+            ],
+          };    
+       
         let instagram: TypeUsableComponentProps = {
             type: "object",
             key: "instagram",
             displayer: "Platform",
             value: [
-                {
-                    type: "image",
-                    key: "icon",
-                    displayer: "Platform Icon",
-                    value: "https://cdn-icons-png.flaticon.com/512/121/121520.png"
-                },
-                {
-                    type: "page",
-                    key: "url",
-                    displayer: "Url",
-                    value: ""
-                }
-            ]
-        };
+              {
+                type: "icon",
+                key: "icon",
+                displayer: "Platform Icon",
+                value: "GrInstagram",
+              },
+              {
+                type: "page",
+                key: "url",
+                displayer: "Url",
+                value: "",
+              },
+            ],
+          };
 
         let card1: TypeUsableComponentProps = {
             type: "object",
@@ -106,9 +107,9 @@ class Team7 extends Team {
                     key: "icons",
                     displayer: "Social Media Platforms",
                     value: [
-                        JSON.parse(JSON.stringify(twitter)),
-                        JSON.parse(JSON.stringify(facebook)),
-                        JSON.parse(JSON.stringify(instagram)),
+                        twitter,
+                        facebook,
+                        instagram,
                     ]
                 }
             ]
@@ -142,9 +143,9 @@ class Team7 extends Team {
                     key: "icons",
                     displayer: "Social Media Platforms",
                     value: [
-                        JSON.parse(JSON.stringify(twitter)),
-                        JSON.parse(JSON.stringify(facebook)),
-                        JSON.parse(JSON.stringify(instagram)),
+                        twitter,
+                        facebook,
+                        instagram,
                     ]
                 }
             ]
@@ -178,9 +179,9 @@ class Team7 extends Team {
                     key: "icons",
                     displayer: "Social Media Platforms",
                     value: [
-                        JSON.parse(JSON.stringify(twitter)),
-                        JSON.parse(JSON.stringify(facebook)),
-                        JSON.parse(JSON.stringify(instagram)),
+                        twitter,
+                        facebook,
+                        instagram,
                     ]
                 }
             ]
@@ -214,9 +215,9 @@ class Team7 extends Team {
                     key: "icons",
                     displayer: "Social Media Platforms",
                     value: [
-                        JSON.parse(JSON.stringify(twitter)),
-                        JSON.parse(JSON.stringify(facebook)),
-                        JSON.parse(JSON.stringify(instagram)),
+                        twitter,
+                        facebook,
+                        instagram,
                     ]
                 }
             ]
@@ -237,22 +238,22 @@ class Team7 extends Team {
         });
 
         this.addProp({
-            type: "number",
-            key: "reverse",
-            displayer: "Reverse Item Count",
-            value: 4
-        });
-
-        this.addProp({
             type: "array",
             key: "team-members",
             displayer: "Team Members",
             value: [
-                JSON.parse(JSON.stringify(card1)),
-                JSON.parse(JSON.stringify(card2)),
-                JSON.parse(JSON.stringify(card3)),
-                JSON.parse(JSON.stringify(card4)),
+                card1,
+                card2,
+                card3,
+                card4,
             ],
+        });
+
+        this.addProp({
+            type: "number",
+            key: "reverse",
+            displayer: "Reverse Item Count",
+            value: 4
         });
     }
 
@@ -264,9 +265,7 @@ class Team7 extends Team {
 
     render() {
         return (
-            <div
-                className={this.decorateCSS("container")}
-            >
+            <div className={this.decorateCSS("container")}>
                 <div className={this.decorateCSS("max-content")}>
                     <div className={this.decorateCSS("basic-page")}>
                         <div className={this.decorateCSS("up-page")}>
@@ -275,6 +274,7 @@ class Team7 extends Team {
                         </div>
                         <div className={this.decorateCSS("down-page")}>
                             {this.castToObject<Card[]>("team-members").map((item: Card, indexCard: number) => {
+                                
                                 return (
                                     <div key={indexCard} style={{ width: 90 / this.getPropValue("reverse") + "%" }} className={this.decorateCSS("card")}>
                                         <img className={this.decorateCSS("person-image")} src={item.image} alt="" />
@@ -284,13 +284,19 @@ class Team7 extends Team {
                                                 <p className={this.decorateCSS("item-position")}>{item.position}</p>
                                             </div>
                                             <div className={this.decorateCSS("icons-bar")}>
-                                                 {item.icons.map((el: any, indexIcon: number) => {
-                                                    return (
-                                                        <ComposerLink path={el.url} key={indexIcon}>
-                                                            <img src={el.icon} alt="icon" className={this.decorateCSS("icon")}/>
-                                                        </ComposerLink>
-                                                    )
-                                                })}  
+                                            {item.icons && item.icons.map(
+                                           (card: Icons, indexIcons: number) => (
+                                          <ComposerLink
+                                            key={indexIcons}
+                                            path={card.url}
+                                         >
+                                            <ComposerIcon name={card.icon} 
+                                            propsIcon={{
+                                                className:this.decorateCSS("icon")
+                                            }}/>
+                                         </ComposerLink>
+                                            )
+                                            )}
                                             </div>
                                         </div>
                                     </div>
