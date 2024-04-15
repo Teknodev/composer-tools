e65496aaa308a728d286d739583b7cb054fd60d3
Şahin Ateş <ayazelar@gmail.com>
2024-04-15 11:14:08 +0300

    changes

--- editor-components/content/content11/content11.tsx
+++ editor-components/content/content11/content11.tsx
@@ -15,26 +15,12 @@ class Content11 extends BaseContent {
       displayer: "title",
       value: "ABOUT",
     });
+
     this.addProp({
-      type: "object",
-      key: "text-left",
-      displayer: "title-icon",
-      value: [
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "icon",
-              key: "icon",
-              value:
-                "GoChevronDown",
-              displayer: "Icon",
-            },
-          ],
-        },
-      ],
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "GoChevronDown"
     })
 
     this.addProp({
@@ -44,83 +30,6 @@ class Content11 extends BaseContent {
       value: "https://images.unsplash.com/photo-1590486803833-1c5dc8ddd4c8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9ncmFwaHl8ZW58MHx8MHx8fDA%3D"
     });
 
-    this.addProp({
-      type: "string",
-      key: "subtitle",
-      displayer: "title1",
-      value: "b.1991, Istanbul, TR",
-    });
-
-    this.addProp({
-      type: "string",
-      key: "title",
-      displayer: "description",
-      value:
-        "Amidst the ever-shifting landscapes of creativity, I emerge as a dynamic professional, blending boundless energy with an adaptable nature and a profoundly artistic perspective. My journey from experimenting with colors to orchestrating complex production processes and assuming the role of an art director has been underpinned by an unwavering commitment to professionalism. This commitment, combined with my innate creativity, allows me to seamlessly navigate challenges and infuse innovation into every project I undertake. My leadership prowess, guided by inclusivity and collaboration, has transformed teams into creative powerhouses. With a unique blend of traits that include energy, adaptability, professionalism, and an artistic essence, I'm poised to make a lasting impact by crafting narratives that resonate and inspire in the ever-evolving tapestry of the creative realm.",
-    });
-
-    this.addProp({
-      type: "string",
-      key: "education",
-      displayer: "title2",
-      value:
-        "Education",
-    });
-    this.addProp({
-      type: "array",
-      key: "item",
-      displayer: "Items",
-      value: [
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "education",
-              displayer: "education",
-              value:
-                "2020-2022 Anadolu University, Visual Communication Design PgD, Eskisehir, TR",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "education",
-              displayer: "education",
-              value:
-                "2013-2019, Mimar Sinan Fine Arts University, Photography BA, Istanbul, TR",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "education",
-              displayer: "education",
-              value:
-                "2011-2013 Akdeniz University, Fine Arts Photography, Antalya, TR",
-            },
-          ],
-        },
-      ],
-    });
-    this.addProp({
-      type: "string",
-      key: "workshop",
-      displayer: "title3",
-      value: "Exhibitions - Workshop",
-    });
     this.addProp({
       type: "array",
       key: "items",
@@ -133,12 +42,17 @@ class Content11 extends BaseContent {
           value: [
             {
               type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2011, ENGELSIZ SANAT , GROUP EXHIBITION -ANTALYA",
+              key: "title",
+              displayer: "Title",
+              value: "b.1991, LA, America",
             },
-          ],
+            {
+              type: "string",
+              displayer: "Description",
+              key: "description",
+              value: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum"
+            }
+          ]
         },
         {
           type: "object",
@@ -147,12 +61,17 @@ class Content11 extends BaseContent {
           value: [
             {
               type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2012, MAZl'DA HAYAT VAR!, DOCUMANTERY EXHIBITION-ANTALYA",
+              key: "title",
+              displayer: "Title",
+              value: "Education",
             },
-          ],
+            {
+              type: "string",
+              displayer: "Description",
+              key: "description",
+              value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?"
+            }
+          ]
         },
         {
           type: "object",
@@ -161,99 +80,22 @@ class Content11 extends BaseContent {
           value: [
             {
               type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2012, HUMAN AND CITY, DOCUMANTERY EXHIBITION -A.B.B.",
+              key: "title",
+              displayer: "Title",
+              value: "Exhibitions - Workshop",
             },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
             {
               type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2013, SANAT VE S0KAK, MIX EXHIBITON, ANTALYA BUYUKSEHIR BELEDIYESI",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2013, ANTALYA, MIX EXHIBITON, ANTALYA BUYUKSEHIR BELEDIYESI",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2013, KARMA SANATLAR, AKDENIZ UNIVERSITY -ANTALYA",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2015, YENI ARAYlSLAR, LIONS VE ROTARY KULUBU, ISTANBUL",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2015, DIGITAL OBSCURA, ARTIST 2015 TUYAP SANAT FUARI- ISTANBUL",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "workshop",
-              displayer: "workshop",
-              value:
-                "2015, DIGITAL OBSCURA, ARTIST 2015 TUYAP SANAT FUARI- ISTANBUL",
-            },
-          ],
-        },
-      ],
+              displayer: "Description",
+              key: "description",
+              value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?"
+            }
+          ]
+        }
+      ]
+
     });
+
     this.addProp({
       type: "array",
       key: "right-items",
@@ -346,73 +188,52 @@ class Content11 extends BaseContent {
     return "Content 11";
   }
   render() {
+    const image = this.getPropValue("image");
+    const title = this.getPropValue("box");
+    const icon = this.getPropValue("icon");
+    const rightItems = this.getPropValue("right-items");
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("boxs-text-left")}>
+          {title && <div className={this.decorateCSS("boxs-text-left")}>
             <div className={this.decorateCSS("box-up")}>
               <h2 className={this.decorateCSS("box")}>{this.getPropValue("box")}</h2>
             </div>
-            <div className={this.decorateCSS("text-left")}>
-              {this.getPropValue("text-left").map((leftItem: any) => {
-                return (
-                  <ComposerLink >
-                    <ComposerIcon
-                      propsIcon={{ className: this.decorateCSS("text") }}
-                      name={leftItem.value[0].value}
-                    />
-                  </ComposerLink>
-                );
-              })}
+            {icon && <div className={this.decorateCSS("text-left")}>
+              <ComposerIcon
+                propsIcon={{ className: this.decorateCSS("text") }}
+                name={this.getPropValue("icon")}
+              />
+
+            </div>}
+          </div>}
+          <div className={this.decorateCSS("content")}>
+            {image && <div className={this.decorateCSS("image-box")}>
+              <img className={this.decorateCSS("image")} src={this.getPropValue("image")} alt="faq image" />
+            </div>}
+            <div className={this.decorateCSS("content-right")}>
+              {this.castToObject<Array<{ title: string, description: string }>>("items").map(item =>
+                <div>
+                  <h2 className={this.decorateCSS("title")}>{item.title}</h2>
+                  <span className={this.decorateCSS("description")}>{item.description}</span>
+                </div>
+              )}
             </div>
           </div>
-          <div className={this.decorateCSS("content")}>      
-              <div className={this.decorateCSS("image-box")}>
-                <img className={this.decorateCSS("image")} src={this.getPropValue("image")} alt="faq image" />
-            </div>
-            <div className={this.decorateCSS("content-right")}>     
-                <h2 className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</h2>
-                <span className={this.decorateCSS("title")}>{this.getPropValue("title")}</span>
-                <div className={this.decorateCSS("education-box")}>
-                  <h2 className={this.decorateCSS("education")}>{this.getPropValue("education")}</h2>
-                </div>
-                <div className={this.decorateCSS("texts-education")}>
-                  {this.getPropValue("item").map(
-                    (listItem: any, index: number) => (
-                      <div className={this.decorateCSS("texts-education")}>
-                        {listItem.value[0].value}
-                      </div>
-                    )
-                  )}
-                </div>
-                <div className={this.decorateCSS("workshop-box")}>
-                  <h2 className={this.decorateCSS("workshop")}>{this.getPropValue("workshop")}</h2>
-                </div>
-                <div className={this.decorateCSS("texts-workshop")}>
-                  {this.getPropValue("items").map(
-                    (listItem: any, index: number) => (
-                      <div className={this.decorateCSS("texts-workshop")}>
-                        {listItem.value[0].value}
-                      </div>
-                    )
-                  )}
-                </div>
-            
-            </div>
-          </div>
-          <div className={this.decorateCSS("icons")}>
-                {this.getPropValue("right-items").map((leftItem: any) => {
-                  return (
-                    <ComposerLink >
-                      <ComposerIcon
-                        propsIcon={{ className: this.decorateCSS("Icon") }}
-                        name={leftItem.value[0].value}
-                      />
-                    </ComposerLink>
-                  );
-                })}
-              </div>
-          
+          {rightItems.length > 0 && <div className={this.decorateCSS("icons")}>
+            {rightItems.map((leftItem: any) => {
+              return (
+                <ComposerLink path={leftItem.value[1].value}>
+                  <ComposerIcon
+                    propsIcon={{ className: this.decorateCSS("Icon") }}
+                    name={leftItem.value[0].value}
+                  />
+                </ComposerLink>
+              );
+            })}
+          </div>}
+
         </div>
       </div>
 
