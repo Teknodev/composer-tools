cdf1fa473c332f529522f74d7cabdbb61a63b6e5
Melike Aksoy <109696149+melikeaksoy@users.noreply.github.com>
2024-05-09 23:39:33 +0300

    Stats3 (#159)
    * stats3
    
    * stats3
    
    * stats3
    
    * stats3
    
    * stats3
    
    ---------
    
    Co-authored-by: kenangsmv <rio.kenan@gmail.com>

--- editor-components/stats/stats3/stats3.tsx
+++ editor-components/stats/stats3/stats3.tsx
@@ -1,105 +1,247 @@
 import * as React from "react";
 import { BaseStats } from "../../EditorComponent";
 import styles from "./stats3.module.scss";
+import { SubTitle } from "chart.js";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type ICard = {
-  title: string;
-  description: string;
+type cards = {
+  url: string;
+  icon: string;
 };
 class Stats3Page extends BaseStats {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
       type: "string",
+      key: "subTitle",
+      displayer: "subTitle",
+      value: "ABOUT PERSONA",
+    });
+    this.addProp({
+      type: "string",
       key: "title",
       displayer: "Title",
-      value: "Daily user statistics",
+      value: "Passionate Minds , Compassionate Hearts",
+    });
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value:
+        "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.Etiam ac nisi tellus.Morbi at velit nisl.Donec ut felis libero.",
+    });
+    this.addProp({
+      type: "string",
+      key: "descriptionMore",
+      displayer: "DescriptionMore",
+      value:
+        "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.",
     });
     this.addProp({
       type: "array",
-      key: "card-content",
-      displayer: "Card Content",
+      key: "buttons",
+      displayer: "Button",
       value: [
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "button",
+          displayer: "Button",
           value: [
             {
               type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "20K+",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Read More",
             },
             {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "total sales",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "35K+",
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
             },
             {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "total registers",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "2.5M+",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "categories",
+              type: "boolean",
+              key: "isPrimary",
+              displayer: "Is primary",
+              value: false,
+              max: 3,
             },
           ],
         },
       ],
     });
+    this.addProp({
+      type: "image",
+      key: "backgroundImage",
+      displayer: "Card Background Image",
+      value:
+        "https://c.files.bbci.co.uk/6422/production/_97143652_b864328e-3368-4f03-bb44-996ddfd7e7f2.jpg",
+    });
+    this.addProp({
+      type: "object",
+      key: "card-content",
+      displayer: "Card Content",
+      value: [
+        {
+          type: "object",
+          key: "cards",
+          displayer: "Cards",
+          value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "icon",
+              value: "FaMedal",
+            },
+            {
+              type: "string",
+              key: "text",
+              displayer: "Text",
+              value: "12 th",
+            },
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Years Experience",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "cards",
+          displayer: "Cards",
+          value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "icon",
+              value: "GoPeople",
+            },
+            {
+              type: "string",
+              key: "text",
+              displayer: "Text",
+              value: "800 +",
+            },
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Happy Clients",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "cards",
+          displayer: "Cards",
+          value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "icon",
+              value: "TbHeartPlus",
+            },
+            {
+              type: "string",
+              key: "text",
+              displayer: "Text",
+              value: "100 +",
+            },
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Psycologist",
+            },
+          ],
+        },
+      ],
+    });
+    this.addProp({
+      type: "boolean",
+      key: "is_box_visible",
+      displayer: "is box visible",
+      value: true,
+    });
   }
-
   getName(): string {
     return "Stats 3";
   }
-
   render() {
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("stats3-page")}>
-            <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
             <div className={this.decorateCSS("bottom-child")}>
-              {this.castToObject<ICard[]>("card-content").map(
-                (cardData: any, indexCard: number) => (
-                  <div key={indexCard} className={this.decorateCSS("card")}>
-                    <h4 className={this.decorateCSS("card-data-title")}>{cardData.title}</h4>
-                    <p className={this.decorateCSS("card-data-description")}>{cardData.description}</p>
+              <div className={this.decorateCSS("text")}>
+                <h1 className={this.decorateCSS("subTitle")}>
+                  {this.getPropValue("subTitle")}
+                </h1>
+                <h1 className={this.decorateCSS("title")}>
+                  {this.getPropValue("title")}
+                </h1>
+                <h1 className={this.decorateCSS("description")}>
+                  {this.getPropValue("description")}
+                </h1>
+                {this.getPropValue("descriptionMore") !== "" && (
+                  <h3 className={this.decorateCSS("descriptionMore")}>
+                    {this.getPropValue("descriptionMore")}
+                  </h3>
+                )}
+
+                <div className={this.decorateCSS("box")}>
+                  {this.castToObject<any>("buttons").map((item: any, index: number) => {
+                    return (
+                      <ComposerLink key={`stats-${index}`} path={item.url}>
+                        <button
+                          className={`${this.decorateCSS("button")} ${
+                            item.isPrimary && this.decorateCSS("button-color")
+                          }`}
+                        >
+                          {item.buttonText}
+                        </button>
+                      </ComposerLink>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div className={this.decorateCSS("card")}>
+                <img
+                  src={this.getPropValue("backgroundImage")}
+                  alt="image"
+                  className={this.decorateCSS("image")}
+                />
+                {this.getPropValue("is_box_visible") && (
+                  <div className={this.decorateCSS("boxs")}>
+                    {this.castToObject<any>("card-content").map(
+                      (item: any, index: number) => {
+                        return (
+                          <ComposerLink key={`stats-${index}`} path={item.url}>
+                            <button>
+                              <ComposerIcon
+                                name={item.icon}
+                                propsIcon={{ className: this.decorateCSS("icon") }}
+                              />
+                              <div className={this.decorateCSS("text")}>
+                                <span className={this.decorateCSS("button-text")}>
+                                  {item.text}
+                                </span>
+                                <span className={this.decorateCSS("button-text")}>
+                                  {item.buttonText}
+                                </span>
+                              </div>
+                            </button>
+                          </ComposerLink>
+                        );
+                      }
+                    )}
                   </div>
-                )
-              )}
+                )}
+              </div>
             </div>
           </div>
         </div>
