67147c054ae2faebfa76554728796c3af6150ff1
kenangsmv <rio.kenan@gmail.com>
2024-05-10 00:22:24 +0300

    content-18

--- editor-components/content/content18/content18.tsx
+++ editor-components/content/content18/content18.tsx
@@ -5,7 +5,6 @@ import { ThirtyFpsSelect, Widgets } from "@mui/icons-material";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-
 class Content18 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
@@ -14,19 +13,19 @@ class Content18 extends BaseContent {
       progress: number;
       progressText: string;
     }
-    ;
-
     this.addProp({
       type: "image",
       key: "image1",
       displayer: "1st Image",
-      value: "https://www.verywellmind.com/thmb/aNigsYshgeBFgbu1_DsvnNsYEHs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1368615203-4c27e5cf53214c59b5243fffe2cb7f24.jpg",
+      value:
+        "https://www.verywellmind.com/thmb/aNigsYshgeBFgbu1_DsvnNsYEHs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1368615203-4c27e5cf53214c59b5243fffe2cb7f24.jpg",
     });
     this.addProp({
       type: "image",
       key: "image2",
       displayer: "2st Image",
-      value: "https://www.greatlakespsychologygroup.com/wp-content/uploads/2020/08/glpg-great-lakes-psychology-group-counseling-therapy-therapist-benefits-prepare.png",
+      value:
+        "https://www.greatlakespsychologygroup.com/wp-content/uploads/2020/08/glpg-great-lakes-psychology-group-counseling-therapy-therapist-benefits-prepare.png",
     });
     this.addProp({
       type: "string",
@@ -44,7 +43,8 @@ class Content18 extends BaseContent {
       type: "string",
       key: "description",
       displayer: "Description",
-      value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      value:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     });
     this.addProp({
       type: "array",
@@ -60,7 +60,7 @@ class Content18 extends BaseContent {
               type: "string",
               key: "progress-title",
               displayer: "Progress Title",
-              value: "Confidentiality"
+              value: "Confidentiality",
             },
             {
               type: "number",
@@ -72,15 +72,15 @@ class Content18 extends BaseContent {
               type: "string",
               key: "progress-text",
               displayer: "Progress Text",
-              value: "90%"
+              value: "90%",
             },
             {
               type: "icon",
               key: "icon",
               displayer: "icon",
               value: "CgPlayButtonO",
-            }
-          ]
+            },
+          ],
         },
         {
           type: "object",
@@ -99,20 +99,20 @@ class Content18 extends BaseContent {
               displayer: "Progress",
               value: 95,
             },
-           
+
             {
               type: "string",
               key: "progress-text",
               displayer: "Progress Text",
-              value: "95%"
+              value: "95%",
             },
-             {
+            {
               type: "icon",
               key: "icon",
               displayer: "icon",
               value: "CgPlayButtonO",
             },
-          ]
+          ],
         },
         {
           type: "object",
@@ -131,20 +131,20 @@ class Content18 extends BaseContent {
               displayer: "Progress",
               value: 98,
             },
-           
+
             {
               type: "string",
               key: "progress-text",
               displayer: "Progress Text",
-              value: "98%"
+              value: "98%",
             },
-             {
+            {
               type: "icon",
               key: "icon",
               displayer: "icon",
               value: "CgPlayButtonO",
             },
-          ]
+          ],
         },
         {
           type: "object",
@@ -163,14 +163,14 @@ class Content18 extends BaseContent {
               displayer: "Progress",
               value: 85,
             },
-           
+
             {
               type: "string",
               key: "progress-text",
               displayer: "Progress Text",
-              value: "85%"
+              value: "85%",
             },
-             {
+            {
               type: "icon",
               key: "icon",
               displayer: "icon",
@@ -178,7 +178,7 @@ class Content18 extends BaseContent {
             },
           ],
         },
-      ]
+      ],
     });
     this.addProp({
       type: "boolean",
@@ -186,54 +186,78 @@ class Content18 extends BaseContent {
       displayer: "progress-text-is-primary",
       value: true,
     });
-
-
-
   }
   getName(): string {
     return "Content 18";
   }
 
   render() {
-
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("page")}>
             <div className={this.decorateCSS("left-page")}>
               <div className={this.decorateCSS("up-image")}>
-                <img className={this.decorateCSS("image1")}
-                  src={this.getPropValue("image1")} alt="" />
+                <img
+                  className={this.decorateCSS("image1")}
+                  src={this.getPropValue("image1")}
+                  alt=""
+                />
               </div>
               <div className={this.decorateCSS("down-image")}>
-                <img className={this.decorateCSS("image2")}
-                  src={this.getPropValue("image2")} alt="" />
+                <img
+                  className={this.decorateCSS("image2")}
+                  src={this.getPropValue("image2")}
+                  alt=""
+                />
               </div>
             </div>
             <div className={this.decorateCSS("right-page")}>
-              <div className={this.decorateCSS("badge")}>{this.getPropValue("badge")}</div>
-              <div className={this.decorateCSS("title")}>{this.getPropValue("title")}</div>
-              <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
+              <div className={this.decorateCSS("badge")}>
+                {this.getPropValue("badge")}
+              </div>
+              <div className={this.decorateCSS("title")}>
+                {this.getPropValue("title")}
+              </div>
+              <p className={this.decorateCSS("description")}>
+                {this.getPropValue("description")}
+              </p>
 
               <div className={this.decorateCSS("progress-container")}>
-                {this.getPropValue("items").map((item: any, index: number) =>
+                {this.getPropValue("items").map((item: any, index: number) => (
                   <div className={this.decorateCSS("item")} key={index}>
-
-                     <div className={this.decorateCSS("progress-title-icon")}>
-                      <ComposerIcon name={item.value[3].value} propsIcon={{ className: this.decorateCSS("icon") }}></ComposerIcon>                  
-                    <div className={this.decorateCSS("progress-title")}>{item.value[0].value}</div>
-                     </div> 
-
-                    {this.getPropValue("progress-text-is-primary") && <div className={this.decorateCSS("progress-text-is-primary")}  >
-                      <div className={this.decorateCSS("progress-percent")}>
-                        <div className={this.decorateCSS("progress-text")} style={{ marginLeft: `${item.value[1].value}%` }}>{item.value[2].value}</div>
+                    <div className={this.decorateCSS("progress-title-container")}>
+                      <div className={this.decorateCSS("progress-title-icon")}>
+                        <ComposerIcon
+                          name={item.value[3].value}
+                          propsIcon={{ className: this.decorateCSS("icon") }}
+                        ></ComposerIcon>
                       </div>
-                      <div className={this.decorateCSS("progress-active")}>
-                        <div className={this.decorateCSS("progress-passive")} style={{ width: `${item.value[1].value}%` }}></div>
+                      <div className={this.decorateCSS("progress-title")}>
+                        {item.value[0].value}
                       </div>
-                    </div>}
+                    </div>
+
+                    {this.getPropValue("progress-text-is-primary") && (
+                      <div className={this.decorateCSS("progress-text-is-primary")}>
+                        <div className={this.decorateCSS("progress-percent")}>
+                          <div
+                            className={this.decorateCSS("progress-text")}
+                            style={{ marginLeft: `${item.value[1].value}%` }}
+                          >
+                            {item.value[2].value}
+                          </div>
+                        </div>
+                        <div className={this.decorateCSS("progress-active")}>
+                          <div
+                            className={this.decorateCSS("progress-passive")}
+                            style={{ width: `${item.value[1].value}%` }}
+                          ></div>
+                        </div>
+                      </div>
+                    )}
                   </div>
-                )}
+                ))}
               </div>
             </div>
           </div>
