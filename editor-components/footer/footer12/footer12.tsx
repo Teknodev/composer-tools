010ca985e62d6c0d7ebf46c6fd735ef3fc75ce4b
Busraylvc <busra.yalvaac@gmail.com>
2024-05-10 13:21:12 +0300

    footer12

--- editor-components/footer/footer12/footer12.tsx
+++ editor-components/footer/footer12/footer12.tsx
@@ -9,10 +9,29 @@ class Footer12Page extends BaseFooter {
     super(props, styles);
     this.addProp({
       type: "image",
-      key: "footerLogo",
-      displayer: "Footer Logo",
+      key: "logo",
+      displayer: "Logo",
       value: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
     });
+    this.addProp({
+      type: "page",
+      key: "logo-url",
+      displayer: "Logo Url",
+      value: ""
+    });
+    this.addProp({
+      type: "string",
+      key: "logo-text",
+      displayer: "Logo Text",
+      value: "Lorem",
+    });
+    this.addProp({
+      type: "page",
+      key: "logo-text-url",
+      displayer: "Logo Text Url",
+      value: ""
+    });
+
     this.addProp({
       type: "string",
       key: "text",
@@ -28,7 +47,7 @@ class Footer12Page extends BaseFooter {
     this.addProp({
       type: "page",
       key: "ref-text-page",
-      displayer: "Referral Text Url",
+      displayer: "Text Url",
       value: ""
     });
 
@@ -41,7 +60,7 @@ class Footer12Page extends BaseFooter {
     this.addProp({
       type: "page",
       key: "ref-text1-page",
-      displayer: "Referral Text 1 Url",
+      displayer: "Text 1 Url",
       value: ""
     });
 
@@ -50,20 +69,32 @@ class Footer12Page extends BaseFooter {
     return "Footer 12";
   }
   render() {
+    const logoImage = this.getPropValue("logo");
+    const logoText = this.getPropValue("logo-text");
+    const imageUrl = this.getPropValue("logo-url");
+    const textUrl = this.getPropValue("logo-text-url")
+
 
     return (
       <div className={this.decorateCSS("container")} >
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("content")}>
-            <img
-              alt=""
-              src={this.getPropValue("footerLogo")}
-              className={this.decorateCSS("img")}
-            />
+            {logoImage ?
+              <ComposerLink path={imageUrl}>
+                <img src={logoImage} className={this.decorateCSS("img")} alt="" />
+              </ComposerLink> :
+              <ComposerLink path={textUrl}>
+                <span className={this.decorateCSS("logo-text")}>{logoText}</span>
+              </ComposerLink>
+            }
             <div className={this.decorateCSS("right-content")}>
               <div className={this.decorateCSS("up-text")}>
-                <span className={this.decorateCSS("ref-text")}>{this.getPropValue("ref-text")}</span>
-                <span className={this.decorateCSS("ref-text1")}>{this.getPropValue("ref-text1")}</span>
+                <ComposerLink path={this.getPropValue("ref-text-page")}>
+                  <span className={this.decorateCSS("ref-text")}>{this.getPropValue("ref-text")}</span>
+                </ComposerLink>
+                <ComposerLink path={this.getPropValue("ref-text1-page")}>
+                  <span className={this.decorateCSS("ref-text1")}>{this.getPropValue("ref-text1")}</span>
+                </ComposerLink>
               </div>
               <div className={this.decorateCSS("down-text")}>
                 <span className={this.decorateCSS("text")}>{this.getPropValue("text")}</span>
