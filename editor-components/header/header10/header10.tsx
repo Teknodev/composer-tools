869478e49c043ee1210dd552a1e29c7f4f3879f4
Şahin Ateş <ayazelar@gmail.com>
2024-05-21 14:36:20 +0300

    feat: header 10

--- editor-components/header/header10/header10.tsx
+++ editor-components/header/header10/header10.tsx
@@ -1,82 +1,374 @@
 import * as React from "react";
-import { BaseHeader } from "../../EditorComponent";
+import { BaseHeader, TypeUsableComponentProps } from "../../EditorComponent";
 import styles from "./header10.module.scss";
+import ComposerSlider from "../../../composer-base-components/slider/slider";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 class Header10 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
+
+    type SliderObject = {
+      title: string;
+      description: string;
+      text: string;
+      image: string;
+      subtitle: string;
+      description1: string;
+      icons: { icon: string, url: string }[],
+    }
     this.addProp({
-      type: "string",
-      key: "subtitle",
-      displayer: "Subtitle",
-      value: "Effective Advertising",
+      type: "array",
+      key: "button",
+      displayer: "Buttons",
+      value: [
+        {
+          type: "object",
+          key: "buttonObject",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Get a guote",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Button Link",
+              value: "",
+            },
+          ],
+        },
+      ],
+    });
+    let twitter: TypeUsableComponentProps = {
+      type: "object",
+      key: "twitter",
+      displayer: "Twitter",
+      value: [
+        {
+          type: "icon",
+          key: "icon",
+          displayer: "Platform Icon",
+          value: "FaTwitter"
+        },
+        {
+          type: "page",
+          key: "url",
+          displayer: "Url",
+          value: ""
+        }
+      ]
+    };
+
+
+
+    let facebook: TypeUsableComponentProps = {
+      type: "object",
+      key: "facebook",
+      displayer: "Platform",
+      value: [
+        {
+          type: "icon",
+          key: "icon",
+          displayer: "Platform Icon",
+          value: "FaFacebook"
+        },
+        {
+          type: "page",
+          key: "url",
+          displayer: "Url",
+          value: ""
+        }
+      ]
+    };
+
+
+
+    let instagram: TypeUsableComponentProps = {
+      type: "object",
+      key: "instagram",
+      displayer: "Platform",
+      value: [
+        {
+          type: "icon",
+          key: "icon",
+          displayer: "Platform Icon",
+          value: "FaInstagram"
+        },
+        {
+          type: "page",
+          key: "url",
+          displayer: "Url",
+          value: ""
+        }
+      ]
+    };
+    this.addProp({
+      type: "icon",
+      key: "next_icon",
+      displayer: "Next icon",
+      value: "IoCaretForwardCircleOutline",
+
     });
     this.addProp({
-      type: "string",
-      key: "title",
-      displayer: "Title",
-      value: "How to Create Effective Ads",
+      type: "icon",
+      key: "prev_icon",
+      displayer: "Prev icon",
+      value: "IoCaretBackCircleOutline",
     });
     this.addProp({
-      type: "string",
-      key: "badge",
-      displayer: "Badge",
-      value: "Advertising",
+      type: "icon",
+      key: "ampersand_icon",
+      displayer: "Ampersand icon",
+      value: "TbAmpersand",
     });
+
+
+
+
     this.addProp({
-      type: "string",
-      key: "cardTitle",
-      displayer: "Card Title",
-      value: "The Power of Advertising",
+      type: "array",
+      key: "slider",
+      displayer: "Slider",
+      value: [
+        {
+          type: "object",
+          key: "sliderObject",
+          displayer: "Slider Object",
+          value: [
+
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Manificent ",
+            },
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "We make structures,dams,bridges,scyscrapers and much more.Resistance,design,flexibility and usability are the main factors that we keep in mind in every project.",
+            },
+            {
+              type: "string",
+              key: "imagetitle",
+              displayer: "Image Title",
+              value: "Structures ",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: " Image",
+              value: "https://images.pexels.com/photos/8910086/pexels-photo-8910086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            },
+            {
+              type: "string",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Stay Tuned",
+            },
+            {
+              type: "string",
+              key: "description1",
+              displayer: "Description 1",
+              value: "We are 24/7 available through our social media.Follow us to stay up to date",
+            },
+            {
+              type: "string",
+              key: "mobiltitle",
+              displayer: "Mobil Title",
+              value: "Manificent Structures",
+            },
+            {
+              type: "array",
+              key: "icons",
+              displayer: "Social Media Platforms",
+              value: [
+                JSON.parse(JSON.stringify(twitter)),
+                JSON.parse(JSON.stringify(facebook)),
+                JSON.parse(JSON.stringify(instagram)),
+              ]
+            }
+          ],
+        },
+        {
+          type: "object",
+          key: "sliderObject",
+          displayer: "Slider Object",
+          value: [
+
+            {
+              type: "string",
+              key: "title",
+              displayer: "Title",
+              value: "Manificent ",
+            },
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "We make structures,dams,bridges,scyscrapers and much more.Resistance,design,flexibility and usability are the main factors that we keep in mind in every project.",
+            },
+            {
+              type: "string",
+              key: "imagetitle",
+              displayer: "Image Title",
+              value: "Structures ",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: " Image",
+              value: "https://cdn.britannica.com/85/137785-050-CF5F4EAB/Manhattan-Bridge-East-River-New-York-City.jpg",
+            },
+            {
+              type: "string",
+              key: "subtitle",
+              displayer: "Subtitle",
+              value: "Stay Tuned",
+            },
+            {
+              type: "string",
+              key: "description1",
+              displayer: "Description 1",
+              value: "We are 24/7 available through our social media.Follow us to stay up to date",
+            },
+            {
+              type: "string",
+              key: "mobiltitle",
+              displayer: "Mobil Title",
+              value: "Manificent Structures",
+            },
+            {
+              type: "array",
+              key: "icons",
+              displayer: "Social Media Platforms",
+              value: [
+                JSON.parse(JSON.stringify(twitter)),
+                JSON.parse(JSON.stringify(facebook)),
+                JSON.parse(JSON.stringify(instagram)),
+              ]
+            }
+          ],
+        }
+      ]
     });
-    this.addProp({
-      type: "string",
-      key: "cardDescription",
-      displayer: "Card Description",
-      value:
-        "Effective advertising is crucial to the success of any business. By understanding your audience and crafting a compelling message, you can create ads that capture attention and drive action. Whether you're running ads on social media, television, or radio, it's important to remember that the goal is not just to create awareness but to ultimately generate sales.",
-    });
-    this.addProp({
-      type: "image",
-      key: "cardImage",
-      displayer: "Card Image",
-      value:
-        "https://assets.website-files.com/62d901050b694cd462d2879c/62ed19dbdf1bb80122008552_620bea4ea84d32768b6235c6_1aa.jpeg",
-    });
+    this.setComponentState("slider-ref", React.createRef());
   }
+
+
 
   getName(): string {
     return "Header 10";
   }
 
   render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
     return (
       <div className={this.decorateCSS("container")}>
-        <section className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("child")}>
-            <span className={this.decorateCSS("subtitle")}>
-              {this.getPropValue("subtitle")}
-            </span>
-            <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-            <div className={this.decorateCSS("card")}>
-              <div className={this.decorateCSS("left-content")}>
-                <span className={this.decorateCSS("badge")}>
-                  {this.getPropValue("badge")}
-                </span>
-                <div className={this.decorateCSS("text-child")}>
-                  <h1 className={this.decorateCSS("cardTitle")}>{this.getPropValue("cardTitle")}</h1>
-                  <p className={this.decorateCSS("description")}>{this.getPropValue("cardDescription")}</p>
+        <div className={this.decorateCSS("max-content")}>
+
+          <ComposerSlider
+            {...settings}
+            ref={this.getComponentState("slider-ref")}
+            className={this.decorateCSS("carousel")}
+          >
+            {this.getPropValue("slider").map((item: any, indexSlider: number) => (
+              <div className={this.decorateCSS("content")} key={indexSlider}>
+
+                <div className={this.decorateCSS("left")}>
+                  <h1 className={this.decorateCSS("title")}>{item.getPropValue("title")}</h1>
+                  <h1 className={this.decorateCSS("mobiltitle")}>{item.getPropValue("mobiltitle")}</h1>
+                  <p className={this.decorateCSS("description")}>{item.getPropValue("description")}</p>
+
+                  <ComposerIcon
+                    name={this.getPropValue("prev_icon")}
+                    propsIcon={{
+                      className: `${this.decorateCSS(
+                        "prev_icon"
+                      )} 
+                  }`,
+                      size: 45,
+                      onClick: () => {
+                        this.getComponentState("slider-ref").current.slickPrev();
+                      },
+                    }}
+                  />
+                  <ComposerIcon
+                    name={this.getPropValue("next_icon")}
+                    propsIcon={{
+                      className: `${this.decorateCSS(
+                        "next_icon"
+                      )} 
+                  }`,
+                      size: 45,
+                      onClick: () => {
+                        this.getComponentState("slider-ref").current.slickNext();
+                      },
+                    }}
+                  />
+
+                  {this.castToObject<[]>("button").map((item: any, indexButton: number) => {
+                    return (
+                      <ComposerLink key={`hdr-10-${indexButton}`} path={item.link}>
+                        <button className={this.decorateCSS("button")}>
+                          {item.buttonText}
+                        </button>
+                      </ComposerLink>
+                    );
+                  })}
+
+                </div>
+
+
+
+                <div className={this.decorateCSS("middle")}>
+
+
+                  <img src={item.getPropValue("image")} alt="" />
+
+
+                  <h1 className={this.decorateCSS("imagetitle")}>{item.getPropValue("imagetitle")}</h1>
+
+                </div>
+                <div className={this.decorateCSS("right")}>
+                  <div className={this.decorateCSS("icon")}>
+                    <ComposerIcon name={this.getPropValue("ampersand_icon")} />
+                  </div>
+                  <h2 className={this.decorateCSS("subtitle")}>{item.getPropValue("subtitle")}</h2>
+                  <p className={this.decorateCSS("description1")}>{item.getPropValue("description1")}</p>
+                  <div className={this.decorateCSS("icon-group")}>
+                    {item.getPropValue("icons").map((item: any, indexSlider: number) => (
+                      <ComposerLink path={item.getPropValue("url")}>
+                        <ComposerIcon name={item.getPropValue("icon")}
+                        />
+                      </ComposerLink>
+                    ))}
+                  </div>
                 </div>
               </div>
-              <div className={this.decorateCSS("right-content")}>
-                <img src={this.getPropValue("cardImage")} alt=""/>
-              </div>
-            </div>
-          </div>
-        </section>
+
+            ))}
+
+          </ComposerSlider>
+        </div>
       </div>
+
     );
   }
 }
+
 
 export default Header10;
