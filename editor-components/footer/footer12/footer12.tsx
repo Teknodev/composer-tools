1913b2f93d0ef261f80c769277176c64131ae80f
idriska <idriscumali@gmail.com>
2024-05-24 12:47:51 +0300

    fix: props structure

--- editor-components/footer/footer12/footer12.tsx
+++ editor-components/footer/footer12/footer12.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseFooter } from "../../EditorComponent";
 import styles from "./footer12.module.scss";
-import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 class Footer12Page extends BaseFooter {
   constructor(props?: any) {
@@ -17,7 +16,7 @@ class Footer12Page extends BaseFooter {
       type: "page",
       key: "logo-url",
       displayer: "Logo Url",
-      value: ""
+      value: "",
     });
     this.addProp({
       type: "string",
@@ -29,41 +28,59 @@ class Footer12Page extends BaseFooter {
       type: "page",
       key: "logo-text-url",
       displayer: "Logo Text Url",
-      value: ""
+      value: "",
     });
-
+    this.addProp({
+      type: "array",
+      key: "link-list",
+      displayer: "Link List",
+      value: [
+        {
+          type: "object",
+          key: "footer-text",
+          displayer: "Text Values",
+          value: [
+            {
+              type: "string",
+              key: "refText",
+              displayer: "Referral Text",
+              value: "Privacy Policy",
+            },
+            {
+              type: "page",
+              key: "path",
+              displayer: "Path",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "footer-text",
+          displayer: "Text Values",
+          value: [
+            {
+              type: "string",
+              key: "refText",
+              displayer: "Referral Text",
+              value: "Terms of Service",
+            },
+            {
+              type: "page",
+              key: "path",
+              displayer: "Path",
+              value: "",
+            },
+          ],
+        },
+      ],
+    });
     this.addProp({
       type: "string",
       key: "text",
       displayer: "Text",
       value: "Copyright © 2023 Teknodev LTD.All rights reserved.",
     });
-    this.addProp({
-      type: "string",
-      key: "ref-text",
-      displayer: "Referral Text",
-      value: "Privacy Policy"
-    });
-    this.addProp({
-      type: "page",
-      key: "ref-text-page",
-      displayer: "Text Url",
-      value: ""
-    });
-
-    this.addProp({
-      type: "string",
-      key: "ref-text1",
-      displayer: "Referral Text 1",
-      value: "Terms of Service"
-    });
-    this.addProp({
-      type: "page",
-      key: "ref-text1-page",
-      displayer: "Text 1 Url",
-      value: ""
-    });
-
   }
   getName(): string {
     return "Footer 12";
@@ -72,29 +89,30 @@ class Footer12Page extends BaseFooter {
     const logoImage = this.getPropValue("logo");
     const logoText = this.getPropValue("logo-text");
     const imageUrl = this.getPropValue("logo-url");
-    const textUrl = this.getPropValue("logo-text-url")
-
+    const textUrl = this.getPropValue("logo-text-url");
 
     return (
-      <div className={this.decorateCSS("container")} >
+      <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")}>
-            {logoImage ?
+          <div className={this.decorateCSS("section")}>
+            {logoImage ? (
               <ComposerLink path={imageUrl}>
                 <img src={logoImage} className={this.decorateCSS("img")} alt="" />
-              </ComposerLink> :
+              </ComposerLink>
+            ) : (
               <ComposerLink path={textUrl}>
                 <span className={this.decorateCSS("logo-text")}>{logoText}</span>
               </ComposerLink>
-            }
+            )}
             <div className={this.decorateCSS("right-content")}>
               <div className={this.decorateCSS("up-text")}>
-                <ComposerLink path={this.getPropValue("ref-text-page")}>
-                  <span className={this.decorateCSS("ref-text")}>{this.getPropValue("ref-text")}</span>
-                </ComposerLink>
-                <ComposerLink path={this.getPropValue("ref-text1-page")}>
-                  <span className={this.decorateCSS("ref-text1")}>{this.getPropValue("ref-text1")}</span>
-                </ComposerLink>
+                {this.castToObject<any[]>("link-list").map((item: any, index) => {
+                  return (
+                    <ComposerLink key={index} path={item.path}>
+                      <span className={this.decorateCSS("ref-text")}>{item.refText}</span>
+                    </ComposerLink>
+                  )
+                })}
               </div>
               <div className={this.decorateCSS("down-text")}>
                 <span className={this.decorateCSS("text")}>{this.getPropValue("text")}</span>
