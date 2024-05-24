c3883025fa22877f1b234b431d5557189ad6eb5f
idriska <idriscumali@gmail.com>
2024-05-24 11:40:55 +0300

    feat: formatted code

--- editor-components/header/header18/header18.tsx
+++ editor-components/header/header18/header18.tsx
@@ -62,7 +62,8 @@ class Header18 extends BaseHeader {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Painting is the application of pigments to a support surface that establishes an image,design or decoration.",
+              value:
+                "Painting is the application of pigments to a support surface that establishes an image,design or decoration.",
             },
             {
               type: "image",
@@ -75,9 +76,8 @@ class Header18 extends BaseHeader {
               type: "image",
               key: "cover_image",
               displayer: "Background Image",
-              value:
-                "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
-            }
+              value: "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
+            },
           ],
         },
 
@@ -108,7 +108,8 @@ class Header18 extends BaseHeader {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed ligula eu ligula congue vestibulum.",
+              value:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed ligula eu ligula congue vestibulum.",
             },
             {
               type: "image",
@@ -121,9 +122,8 @@ class Header18 extends BaseHeader {
               type: "image",
               key: "cover_image",
               displayer: "Background Image",
-              value:
-                "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
-            }
+              value: "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
+            },
           ],
         },
         {
@@ -153,7 +153,8 @@ class Header18 extends BaseHeader {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu justo sed libero consectetur consequat.",
+              value:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu justo sed libero consectetur consequat.",
             },
             {
               type: "image",
@@ -166,9 +167,8 @@ class Header18 extends BaseHeader {
               type: "image",
               key: "cover_image",
               displayer: "Background Image",
-              value:
-                "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
-            }
+              value: "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
+            },
           ],
         },
         {
@@ -198,7 +198,8 @@ class Header18 extends BaseHeader {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida felis sed nisl consequat, nec ultricies velit commodo.",
+              value:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida felis sed nisl consequat, nec ultricies velit commodo.",
             },
             {
               type: "image",
@@ -211,9 +212,8 @@ class Header18 extends BaseHeader {
               type: "image",
               key: "cover_image",
               displayer: "Background Image",
-              value:
-                "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
-            }
+              value: "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
+            },
           ],
         },
         {
@@ -243,7 +243,8 @@ class Header18 extends BaseHeader {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor justo ac lorem tincidunt, at convallis tortor efficitur.",
+              value:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor justo ac lorem tincidunt, at convallis tortor efficitur.",
             },
             {
               type: "image",
@@ -256,9 +257,8 @@ class Header18 extends BaseHeader {
               type: "image",
               key: "cover_image",
               displayer: "Background Image",
-              value:
-                "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
-            }
+              value: "https://img.freepik.com/free-photo/gray-texture_1253-246.jpg",
+            },
           ],
         },
       ],
@@ -319,7 +319,6 @@ class Header18 extends BaseHeader {
     });
     this.setComponentState("slider-ref", React.createRef());
     this.setComponentState("active-index", 0);
-
   }
 
   getName(): string {
@@ -337,95 +336,75 @@ class Header18 extends BaseHeader {
       slidesToShow: 1,
       slidesToScroll: 1,
       beforeChange: (oldIndex: number, newIndex: number) => {
-        if (oldIndex == newIndex) return;
+        if (oldIndex === newIndex) return;
         setTimeout(() => {
           this.setComponentState("active-index", newIndex);
         }, 1200);
       },
     };
 
-
     const sliderCount = this.castToObject<Item[]>("slider").length;
     const progressPercentage = ((this.getComponentState("active-index") + 1) / sliderCount) * 100;
+    
     return (
-      <div
-        className={this.decorateCSS("container")}
-      >
-        <div className={this.decorateCSS("max-content")} >
-
+      <div className={this.decorateCSS("container")}>
+        <div className={this.decorateCSS("max-content")}>
           <ComposerSlider
             {...settings}
             ref={this.getComponentState("slider-ref")}
             className={this.decorateCSS("slider-style")}
-
           >
-            {this.castToObject<Item[]>("slider").map(
-              (item: Item, index: number) => (
-                <div className={this.decorateCSS("slider")} key={index}>
-                  {item.cover_image && (
-                    <img
-                      className={this.decorateCSS("cover_image")}
-                      src={item.cover_image}
-                      alt=""
-
-                    />
-                  )}
-
-                  <div className={this.decorateCSS("card")}>
-                    <img
-                      src={item.image}
-                      alt={""}
-                      className={this.decorateCSS("image")}
-                    />
-                    <div className={this.decorateCSS("content")}>
-                      <div className={this.decorateCSS("text-content")}>
-                        <span className={this.decorateCSS("subtitle")}>{item.subtitle}</span>
-                        <span className={this.decorateCSS("title")}>{item.title}</span>
-                      </div>
-                      <div className={this.decorateCSS("pagination")}>
-                        <span className={this.decorateCSS("active-slide")}>
-                          {(this.getComponentState("active-index") + 1)
-                            .toString()
-                            .padStart(2, "0")}
-                        </span>
-                        <div className={this.decorateCSS("progress-bar")}>
-                          <div className={this.decorateCSS("active")} style={{ width: `${progressPercentage}%` }} />
-                        </div>
-                        <span className={this.decorateCSS("slide-count")}>
-                          {sliderCount.toString().padStart(2, "0")}
-                        </span>
-                      </div>
-
+            {this.castToObject<Item[]>("slider").map((item: Item, index: number) => (
+              <div className={this.decorateCSS("slider")} key={index}>
+                {item.cover_image && (
+                  <img className={this.decorateCSS("cover_image")} src={item.cover_image} alt="" />
+                )}
+                <div className={this.decorateCSS("card")}>
+                  <img src={item.image} alt={""} className={this.decorateCSS("image")} />
+                  <div className={this.decorateCSS("content")}>
+                    <div className={this.decorateCSS("text-content")}>
+                      <span className={this.decorateCSS("subtitle")}>{item.subtitle}</span>
+                      <span className={this.decorateCSS("title")}>{item.title}</span>
                     </div>
-                    <div className={this.decorateCSS("description-div")}>
-                      <span className={this.decorateCSS("description_title")}>{item.description_title}</span>
-                      <span className={this.decorateCSS("item-description")}>
-                        {item.description}
+                    <div className={this.decorateCSS("pagination")}>
+                      <span className={this.decorateCSS("active-slide")}>
+                        {(this.getComponentState("active-index") + 1).toString().padStart(2, "0")}
+                      </span>
+                      <div className={this.decorateCSS("progress-bar")}>
+                        <div
+                          className={this.decorateCSS("active")}
+                          style={{ width: `${progressPercentage}%` }}
+                        />
+                      </div>
+                      <span className={this.decorateCSS("slide-count")}>
+                        {sliderCount.toString().padStart(2, "0")}
                       </span>
                     </div>
                   </div>
+                  <div className={this.decorateCSS("description-div")}>
+                    <span className={this.decorateCSS("description_title")}>
+                      {item.description_title}
+                    </span>
+                    <span className={this.decorateCSS("item-description")}>{item.description}</span>
+                  </div>
                 </div>
-              )
-            )}
+              </div>
+            ))}
           </ComposerSlider>
           <div className={this.decorateCSS("page-bottom")}>
             <div className={this.decorateCSS("icons")}>
               {this.getPropValue("icons").map((item: any) => (
                 <ComposerLink path={item.getPropValue("navigate")}>
                   <span className={this.decorateCSS("name")}>{item.getPropValue("name")}</span>
-
                 </ComposerLink>
               ))}
             </div>
             <ComposerIcon
               name={this.getPropValue("prev_icon")}
               propsIcon={{
-                className: `${this.decorateCSS(
-                  "prev-icon"
-                )} ${this.decorateCSS("arrow")} ${!this.getComponentState("display-none") &&
-                this.decorateCSS("un-visible")
-                  }`,
-
+                className: `${this.decorateCSS("prev-icon")} ${this.decorateCSS("arrow")} ${
+                  !this.getComponentState("display-none") && this.decorateCSS("un-visible")
+                }`,
                 onClick: () => {
                   this.getComponentState("slider-ref").current.slickPrev();
                 },
@@ -434,12 +413,9 @@ class Header18 extends BaseHeader {
             <ComposerIcon
               name={this.getPropValue("next_icon")}
               propsIcon={{
-                className: `${this.decorateCSS(
-                  "next-icon"
-                )} ${this.decorateCSS("arrow")} ${!this.getComponentState("display-none") &&
-                this.decorateCSS("un-visible")
-                  }`,
-
+                className: `${this.decorateCSS("next-icon")} ${this.decorateCSS("arrow")} ${
+                  !this.getComponentState("display-none") && this.decorateCSS("un-visible")
+                }`,
                 onClick: () => {
                   this.getComponentState("slider-ref").current.slickNext();
                 },
