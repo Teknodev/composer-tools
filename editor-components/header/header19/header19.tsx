168607a5599b2e09996621ce0d94ea1887d81ad2
Şahin Ateş <ayazelar@gmail.com>
2024-06-03 11:55:24 +0300

    changes

--- editor-components/header/header19/header19.tsx
+++ editor-components/header/header19/header19.tsx
@@ -6,170 +6,163 @@ import ComposerLink from "../../../../custom-hooks/composer-base-components/Link
 class Header19 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
-
     this.addProp({
       type: "object",
-      displayer: "Items",
-      key: "items",
+      displayer: "Item Left",
+      key: "item-left",
       value: [
         {
-          type: "object",
-          displayer: "Item Left",
-          key: "item-left",
-          value: [
-            {
-              type: "boolean",
-              displayer: "Visibility",
-              key: "visibility",
-              value: true,
-            },
+          type: "boolean",
+          displayer: "Visibility",
+          key: "visibility",
+          value: true,
+        },
 
-            {
-              type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "MATTIS LAOREET SAPIEN",
-            },
-            {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Porta\nConsectetur\nImperdiet\nFrigilla",
-            },
-            {
-              type: "image",
-              displayer: "Image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad87b?alt=media&timestamp=1693306471422",
-            },
-            {
-              type: "object",
-              displayer: "Button",
-              key: "button",
-              value: [
-                {
-                  type: "string",
-                  displayer: "Button Text",
-                  key: "button-text",
-                  value: "READ MORE",
-                },
-                {
-                  type: "page",
-                  displayer: "Button Link",
-                  key: "button-url",
-                  value: "",
-                },
-              ],
-            },
-          ],
+        {
+          type: "string",
+          displayer: "Title",
+          key: "title",
+          value: "MATTIS LAOREET SAPIEN",
+        },
+        {
+          type: "string",
+          displayer: "Description",
+          key: "description",
+          value: "Porta\nConsectetur\nImperdiet\nFrigilla",
+        },
+        {
+          type: "image",
+          displayer: "Image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad87b?alt=media&timestamp=1693306471422",
         },
         {
           type: "object",
-          displayer: "Item Right",
-          key: "item-right",
+          displayer: "Button",
+          key: "button",
           value: [
             {
-              type: "boolean",
-              displayer: "Visibility",
-              key: "visibility",
-              value: true,
-            },
-            {
               type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "SEMPER",
+              displayer: "Button Text",
+              key: "buttontext",
+              value: "READ MORE",
             },
             {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Feugiat\nScelerisque\nImperdiet",
-            },
-
-            {
-              type: "image",
-              displayer: "Image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad87a?alt=media&timestamp=1693306471422",
-            },
-            {
-              type: "object",
-              displayer: "Button",
-              key: "button",
-              value: [
-                {
-                  type: "string",
-                  displayer: "Button Text",
-                  key: "button-text",
-                  value: "READ MORE",
-                },
-                {
-                  type: "page",
-                  displayer: "Button Link",
-                  key: "button-url",
-                  value: "",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          type: "object",
-          displayer: "Item Right Bottom Left",
-          key: "item-right-bottom-left",
-          value: [
-            {
-              type: "boolean",
-              displayer: "Visibility",
-              key: "visibility",
-              value: true,
-            },
-            {
-              type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "SEMPER",
-            },
-            {
-              type: "string",
-              displayer: "Description",
-              key: "description",
-              value: "Adipiscing\nSodales",
-            },
-
-            {
-              type: "image",
-              displayer: "Image",
-              key: "image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad878?alt=media&timestamp=1693306471422",
-            },
-            {
-              type: "object",
-              displayer: "Button",
-              key: "button",
-              value: [
-                {
-                  type: "string",
-                  displayer: "Button Text",
-                  key: "button-text",
-                  value: "READ MORE",
-                },
-                {
-                  type: "page",
-                  displayer: "Button Link",
-                  key: "button-url",
-                  value: "",
-                },
-              ],
+              type: "page",
+              displayer: "Button Link",
+              key: "buttonurl",
+              value: "",
             },
           ],
         },
       ],
-    });
+    })
+    this.addProp({
+      type: "object",
+      displayer: "Item Right",
+      key: "item-right",
+      value: [
+        {
+          type: "boolean",
+          displayer: "Visibility",
+          key: "visibility",
+          value: true,
+        },
+        {
+          type: "string",
+          displayer: "Title",
+          key: "title",
+          value: "SEMPER",
+        },
+        {
+          type: "string",
+          displayer: "Description",
+          key: "description",
+          value: "Feugiat\nScelerisque\nImperdiet",
+        },
+
+        {
+          type: "image",
+          displayer: "Image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad87a?alt=media&timestamp=1693306471422",
+        },
+        {
+          type: "object",
+          displayer: "Button",
+          key: "button",
+          value: [
+            {
+              type: "string",
+              displayer: "Button Text",
+              key: "buttontext",
+              value: "READ MORE",
+            },
+            {
+              type: "page",
+              displayer: "Button Link",
+              key: "buttonurl",
+              value: "",
+            },
+          ],
+        },
+      ],
+    })
+
+    this.addProp({
+      type: "object",
+      displayer: "Item Right Bottom Left",
+      key: "item-right-bottom-left",
+      value: [
+        {
+          type: "boolean",
+          displayer: "Visibility",
+          key: "visibility",
+          value: true,
+        },
+        {
+          type: "string",
+          displayer: "Title",
+          key: "title",
+          value: "SEMPER",
+        },
+        {
+          type: "string",
+          displayer: "Description",
+          key: "description",
+          value: "Adipiscing\nSodales",
+        },
+
+        {
+          type: "image",
+          displayer: "Image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64edce7d057bdf002c2ad878?alt=media&timestamp=1693306471422",
+        },
+        {
+          type: "object",
+          displayer: "Button",
+          key: "button",
+          value: [
+            {
+              type: "string",
+              displayer: "Button Text",
+              key: "buttontext",
+              value: "READ MORE",
+            },
+            {
+              type: "page",
+              displayer: "Button Link",
+              key: "buttonurl",
+              value: "",
+            },
+          ],
+        },
+      ],
+    })
 
     this.addProp({
       type: "object",
@@ -210,13 +203,13 @@ class Header19 extends BaseHeader {
             {
               type: "string",
               displayer: "Button Text",
-              key: "button-text",
+              key: "buttontext",
               value: "",
             },
             {
               type: "page",
               displayer: "Button Link",
-              key: "button-url",
+              key: "buttonurl",
               value: "",
             },
           ],
@@ -230,27 +223,31 @@ class Header19 extends BaseHeader {
   }
 
   render() {
-    let items: any = this.castToObject("items");
+
+    const itemLeft = this.castToObject<any>("item-left")
+    const itemRight = this.castToObject<any>("item-right")
+    const itemBottomLeft = this.castToObject<any>("item-right-bottom-left")
+    const itemBottomRight = this.castToObject<any>("item-right-bottom-right")
 
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          {items[0].visibility ? (
+          {itemLeft.visibility ? (
             <div className={this.decorateCSS("left")}>
               <div className={this.decorateCSS("item")}>
                 <div className={this.decorateCSS("background-image")}>
-                  <img src={items[0].image} alt={items[0].title} />
+                  <img src={itemLeft.image} alt={itemLeft.title} />
                 </div>
                 <div className={this.decorateCSS("content")}>
-                  <div className={this.decorateCSS("title")}>{items[0].title}</div>
+                  <div className={this.decorateCSS("title")}>{itemLeft.title}</div>
                   <div className={this.decorateCSS("description")}>
-                    {items[0].description}
+                    {itemLeft.description}
                   </div>
-                  {items[0].button[0].value ? (
+                  {itemLeft.button.buttontext ? (
                     <div>
-                      <ComposerLink path={items[0].button[1].value}>
+                      <ComposerLink path={itemLeft.button.buttonurl}>
                         <button className={this.decorateCSS("button")}>
-                          {items[0].button[0].value}
+                          {itemLeft.button.buttontext}
                         </button>
                       </ComposerLink>
                     </div>
@@ -261,22 +258,22 @@ class Header19 extends BaseHeader {
           ) : null}
 
           <div className={this.decorateCSS("right")}>
-            {items[1].visibility ? (
+            {itemRight.visibility ? (
               <div className={this.decorateCSS("top")}>
                 <div className={this.decorateCSS("item")}>
                   <div className={this.decorateCSS("background-image")}>
-                    <img src={items[1].image} alt={items[1].title} />
+                    <img src={itemRight.image} alt={itemRight.title} />
                   </div>
                   <div className={this.decorateCSS("content")}>
-                    <div className={this.decorateCSS("title")}>{items[1].title}</div>
+                    <div className={this.decorateCSS("title")}>{itemRight.title}</div>
                     <div className={this.decorateCSS("description")}>
-                      {items[1].description}
+                      {itemRight.description}
                     </div>
-                    {items[1].button[0].value ? (
+                    {itemRight.button.buttontext ? (
                       <div>
-                        <ComposerLink path={items[1].button[1].value}>
+                        <ComposerLink path={itemRight.button.buttonurl}>
                           <button className={this.decorateCSS("button")}>
-                            {items[1].button[0].value}
+                            {itemRight.button.buttontext}
                           </button>
                         </ComposerLink>
                       </div>
@@ -287,21 +284,21 @@ class Header19 extends BaseHeader {
             ) : null}
 
             <div className={this.decorateCSS("bottom")}>
-              {items[2].visibility ? (
+              {itemBottomLeft.visibility ? (
                 <div className={this.decorateCSS("item")}>
                   <div className={this.decorateCSS("background-image")}>
-                    <img src={items[2].image} alt={items[2].title} />
+                    <img src={itemBottomLeft.image} alt={itemBottomLeft.title} />
                   </div>
                   <div className={this.decorateCSS("content")}>
-                    <div className={this.decorateCSS("title")}>{items[2].title}</div>
+                    <div className={this.decorateCSS("title")}>{itemBottomLeft.title}</div>
                     <div className={this.decorateCSS("description")}>
-                      {items[2].description}
+                      {itemBottomLeft.description}
                     </div>
-                    {items[2].button[0].value ? (
+                    {itemBottomLeft.button.buttontext ? (
                       <div>
-                        <ComposerLink path={items[2].button[1].value}>
+                        <ComposerLink path={itemBottomLeft.button.buttonurl}>
                           <button className={this.decorateCSS("button")}>
-                            {items[2].button[0].value}
+                            {itemBottomLeft.button.buttontext}
                           </button>
                         </ComposerLink>
                       </div>
@@ -309,32 +306,31 @@ class Header19 extends BaseHeader {
                   </div>
                 </div>
               ) : null}
-              {this.getPropValue("item-right-bottom-right")[0].value ? (
+              {itemBottomRight.visibility ? (
                 <div className={this.decorateCSS("item")}>
                   <div className={this.decorateCSS("background-image")}>
                     <img
-                      src={this.getPropValue("item-right-bottom-right")[3].value}
-                      alt={this.getPropValue("item-right-bottom-right")[1].value}
+                      src={itemBottomRight.image}
+                      alt={itemBottomRight.title}
                     />
                   </div>
                   <div className={this.decorateCSS("content")}>
                     <div className={this.decorateCSS("title")}>
-                      {this.getPropValue("item-right-bottom-right")[1].value}
+                      {itemBottomRight.title}
                     </div>
                     <div className={this.decorateCSS("description")}>
-                      {this.getPropValue("item-right-bottom-right")[2].value}
+                      {itemBottomRight.description}
                     </div>
-                    {this.getPropValue("item-right-bottom-right")[4].value[0].value ? (
+                    {itemBottomRight.button.buttontext ? (
                       <div>
                         <ComposerLink
                           path={
-                            this.getPropValue("item-right-bottom-right")[4].value[1].value
+                            itemBottomRight.button.buttonurl
                           }
                         >
                           <button className={this.decorateCSS("button")}>
                             {
-                              this.getPropValue("item-right-bottom-right")[4].value[0]
-                                .value
+                              itemBottomRight.button.buttontext
                             }
                           </button>
                         </ComposerLink>
