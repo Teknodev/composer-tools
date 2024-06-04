7dd62b8fed0f53189916b8fde8c58e3409478c12
Christina <121299208+ChristinaTabor@users.noreply.github.com>
2024-06-04 18:14:49 +0300

    Team 3 (#195)

--- editor-components/team/team-3/team3.tsx
+++ editor-components/team/team-3/team3.tsx
@@ -2,17 +2,19 @@ import * as React from "react";
 import { Team, TypeUsableComponentProps } from "../../EditorComponent";
 import styles from "./team3.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type Card = {
+type Icons = {
+  url: string;
+  icon: string;
+};
+
+interface Card {
   profile: string;
   name: string;
   position: string;
-  url1: string;
-  url2: string;
-  url3: string;
-  url4: string;
-  platforms: { url: string; icon: string }[];
-};
+  icons: { icon: string; url: string }[];
+}
 
 class Team3 extends Team {
   constructor(props?: any) {
@@ -21,13 +23,13 @@ class Team3 extends Team {
     let twitter: TypeUsableComponentProps = {
       type: "object",
       key: "twitter",
-      displayer: "Twitter",
+      displayer: "Platform",
       value: [
         {
-          type: "image",
+          type: "icon",
           key: "icon",
           displayer: "Platform Icon",
-          value: "https://cdn-icons-png.flaticon.com/512/3670/3670151.png",
+          value: "FaTwitter",
         },
         {
           type: "page",
@@ -44,10 +46,30 @@ class Team3 extends Team {
       displayer: "Platform",
       value: [
         {
-          type: "image",
+          type: "icon",
           key: "icon",
           displayer: "Platform Icon",
-          value: "https://cdn-icons-png.flaticon.com/512/4494/4494475.png",
+          value: "BsFacebook",
+        },
+        {
+          type: "page",
+          key: "url",
+          displayer: "Url",
+          value: "",
+        },
+      ],
+    };
+
+    let linkedin: TypeUsableComponentProps = {
+      type: "object",
+      key: "linkedin",
+      displayer: "Platform",
+      value: [
+        {
+          type: "icon",
+          key: "icon",
+          displayer: "Platform Icon",
+          value: "ImLinkedin",
         },
         {
           type: "page",
@@ -60,177 +82,228 @@ class Team3 extends Team {
 
     this.addProp({
       type: "string",
-      key: "badge",
-      displayer: "Badge",
-      value: "Team",
-    });
-
-    this.addProp({
-      type: "string",
-      key: "subtitle",
-      displayer: "Subtitle",
+      key: "title",
+      displayer: "Title",
       value: "Our Team",
     });
     this.addProp({
       type: "string",
-      key: "description",
-      displayer: "Description",
-      value: "We work in collaboration, harmony, and trust to achieve our goals.",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: "The Best Team Ever!",
     });
 
-    this.addProp({
-      type: "array",
+    let card1: TypeUsableComponentProps = {
+      type: "object",
+      key: "card",
+      displayer: "Items",
+      value: [
+        {
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "Jacklyn Mia",
+        },
+        {
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "Ceo/Founder",
+        },
+        {
+          type: "array",
+          key: "icons",
+          displayer: "Icons",
+          value: [twitter, facebook, linkedin],
+        },
+      ],
+    };
+
+    let card2: TypeUsableComponentProps = {
+      type: "object",
       key: "card",
       displayer: "Card",
       value: [
         {
-          type: "object",
-          key: "items",
-          displayer: "Items",
-          value: [
-            {
-              type: "image",
-              key: "profile",
-              displayer: "Image",
-              value:
-                "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            },
-            {
-              type: "string",
-              key: "name",
-              displayer: "Person",
-              value: "Jacklyn Mia",
-            },
-            {
-              type: "string",
-              key: "position",
-              displayer: "Position",
-              value: "Ceo/Founder",
-            },
-            {
-              type: "array",
-              key: "platforms",
-              displayer: "Social Medias",
-              value: [
-                JSON.parse(JSON.stringify(twitter)),
-                JSON.parse(JSON.stringify(facebook)),
-              ],
-            },
-          ],
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1600",
         },
         {
-          type: "object",
-          key: "items",
-          displayer: "Items",
-          value: [
-            {
-              type: "image",
-              key: "profile",
-              displayer: "Image",
-              value:
-                "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            },
-            {
-              type: "string",
-              key: "name",
-              displayer: "Person",
-              value: "Kirsten Gertie",
-            },
-            {
-              type: "string",
-              key: "position",
-              displayer: "Position",
-              value: "Web Developer",
-            },
-            {
-              type: "array",
-              key: "platforms",
-              displayer: "Social Medias",
-              value: [
-                JSON.parse(JSON.stringify(twitter)),
-                JSON.parse(JSON.stringify(facebook)),
-              ],
-            },
-          ],
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "Kirsten Gertie",
         },
         {
-          type: "object",
-          key: "items",
-          displayer: "Items",
-          value: [
-            {
-              type: "image",
-              key: "profile",
-              displayer: "Image",
-              value:
-                "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            },
-            {
-              type: "string",
-              key: "name",
-              displayer: "Person",
-              value: "Lachlan Linnette",
-            },
-            {
-              type: "string",
-              key: "position",
-              displayer: "Position",
-              value: "UI/UX Developer",
-            },
-            {
-              type: "array",
-              key: "platforms",
-              displayer: "Social Medias",
-              value: [
-                JSON.parse(JSON.stringify(twitter)),
-                JSON.parse(JSON.stringify(facebook)),
-              ],
-            },
-          ],
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "Web Developer",
         },
         {
-          type: "object",
-          key: "items",
-          displayer: "Items",
-          value: [
-            {
-              type: "image",
-              key: "profile",
-              displayer: "Image",
-              value:
-                "https://images.pexels.com/photos/3785079/pexels-photo-3785079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            },
-            {
-              type: "string",
-              key: "name",
-              displayer: "Person",
-              value: "James Pearce",
-            },
-            {
-              type: "string",
-              key: "position",
-              displayer: "Position",
-              value: "Graphic Designer",
-            },
-            {
-              type: "array",
-              key: "platforms",
-              displayer: "Social Medias",
-              value: [
-                JSON.parse(JSON.stringify(twitter)),
-                JSON.parse(JSON.stringify(facebook)),
-              ],
-            },
-          ],
+          type: "array",
+          key: "icons",
+          displayer: "Social Medias",
+          value: [twitter, facebook, linkedin],
         },
       ],
+    };
+
+    let card3: TypeUsableComponentProps = {
+      type: "object",
+      key: "card",
+      displayer: "Card",
+      value: [
+        {
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/1800456/pexels-photo-1800456.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "Lachlan Linnette",
+        },
+        {
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "UI/UX Developer",
+        },
+        {
+          type: "array",
+          key: "icons",
+          displayer: "Social Medias",
+          value: [twitter, facebook, linkedin],
+        },
+      ],
+    };
+
+    let card4: TypeUsableComponentProps = {
+      type: "object",
+      key: "card",
+      displayer: "Card",
+      value: [
+        {
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/8434878/pexels-photo-8434878.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        },
+        {
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "James Pearce",
+        },
+        {
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "Graphic Designer",
+        },
+        {
+          type: "array",
+          key: "icons",
+          displayer: "Social Medias",
+          value: [twitter, facebook, linkedin],
+        },
+      ],
+    };
+
+    let card5: TypeUsableComponentProps = {
+      type: "object",
+      key: "card",
+      displayer: "Card",
+      value: [
+        {
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/5393594/pexels-photo-5393594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        },
+        {
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "Mary Johnson",
+        },
+        {
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "Creative Director",
+        },
+        {
+          type: "array",
+          key: "icons",
+          displayer: "Social Medias",
+          value: [twitter, facebook, linkedin],
+        },
+      ],
+    };
+
+    let card6: TypeUsableComponentProps = {
+      type: "object",
+      key: "card",
+      displayer: "Card",
+      value: [
+        {
+          type: "image",
+          key: "profile",
+          displayer: "Image",
+          value:
+            "https://images.pexels.com/photos/8434878/pexels-photo-8434878.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        },
+        {
+          type: "string",
+          key: "name",
+          displayer: "Person",
+          value: "Sarah Brown",
+        },
+        {
+          type: "string",
+          key: "position",
+          displayer: "Position",
+          value: "Marketing Manager",
+        },
+        {
+          type: "array",
+          key: "icons",
+          displayer: "Social Medias",
+          value: [twitter, facebook, linkedin],
+        },
+      ],
+    };
+
+    this.addProp({
+      type: "array",
+      key: "team-members",
+      displayer: "Team Members",
+      value: [card1, card2, card3, card4, card5, card6],
     });
 
     this.addProp({
       type: "number",
       key: "itemCount",
       displayer: "Item Count in a Row",
-      value: 4,
+      value: 3,
+      max: 3,
     });
   }
 
@@ -239,45 +312,65 @@ class Team3 extends Team {
   }
 
   render() {
+    const title = this.getPropValue("title");
+    const subtitle = this.getPropValue("subtitle");
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("page")}>
-            <div className={this.decorateCSS("text-group")}>
-              <h1 className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</h1>
-              <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
-            </div>
-            <div className={this.decorateCSS("down-page")}>
-              {this.castToObject<Card[]>("card").map((item: Card, indexCard: number) => {
-                return (
-                  <div
-                    key={indexCard}
-                    className={this.decorateCSS("all-card")}
-                    style={{
-                      width: 90 / this.getPropValue("itemCount") + "%",
-                    }}
-                  >
-                    <div className={this.decorateCSS("card")}>
-                      <img
-                        className={this.decorateCSS("profile-image")}
-                        src={item.profile}
-                        alt=""
-                      />
-                      <h2 className={this.decorateCSS("item-name")}>{item.name}</h2>
-                      <p className={this.decorateCSS("item-position")}>{item.position}</p>
-                      <div className={this.decorateCSS("icon-group")}>
-                        {item.platforms.map((item: any, indexIconGroup: number) => {
-                          return (
-                            <ComposerLink key={indexIconGroup} path={item.url}>
-                              <img src={item.icon} alt="" />
-                            </ComposerLink>
-                          );
-                        })}
+            {title && (
+              <div className={this.decorateCSS("text-group")}>
+                <div className={this.decorateCSS("title-up")}>
+                  <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
+                </div>
+                {subtitle && (
+                  <div className={this.decorateCSS("subtitle-up")}>
+                    <p className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</p>
+                  </div>
+                )}
+              </div>
+            )}
+            <div
+              className={this.decorateCSS("down-page")}
+              style={{ gridTemplateColumns: `repeat(${this.getPropValue("itemCount")}, 1fr)` }}
+            >
+              {this.castToObject<Card[]>("team-members").map((item: Card, indexCard: number) => (
+                <div key={indexCard} className={this.decorateCSS("all-card")}>
+                  <div className={this.decorateCSS("card")}>
+                    <div className={this.decorateCSS("card-items")}>
+                      <div className={this.decorateCSS("item-content")}>
+                        <div className={this.decorateCSS("box-image")}>
+                          {item.profile && (
+                            <img
+                              className={this.decorateCSS("profile-image")}
+                              src={item.profile}
+                              alt=""
+                            />
+                          )}
+                        </div>
+                        <div className={this.decorateCSS("box-text")}>
+                          <h2 className={this.decorateCSS("item-name")}>{item.name}</h2>
+                          <p className={this.decorateCSS("item-position")}>{item.position}</p>
+                          <div className={this.decorateCSS("icon-group")}>
+                            {item.icons &&
+                              item.icons.map((card: Icons, indexIcons: number) => (
+                                <ComposerLink key={indexIcons} path={card.url}>
+                                  <ComposerIcon
+                                    name={card.icon}
+                                    propsIcon={{
+                                      className: this.decorateCSS("icons"),
+                                    }}
+                                  />
+                                </ComposerLink>
+                              ))}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                );
-              })}
+                </div>
+              ))}
             </div>
           </div>
         </div>
