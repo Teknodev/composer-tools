054976bde58105bf25e2c2d8dd7a0f1398e2a1cd
Umut Baran Çiçek <20160808025@ogr.akdeniz.edu.tr>
2024-06-06 15:52:01 +0300

    content4

--- editor-components/content/content4/content4.tsx
+++ editor-components/content/content4/content4.tsx
@@ -2,224 +2,347 @@ import * as React from "react";
 import styles from "./content4.module.scss";
 import { BaseContent } from "../../EditorComponent";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-interface Card {
-  image: string;
-  title: string;
-  icon: string;
-  url: string;
+
+
+class Content4 extends BaseContent{
+  constructor(props?: any){
+  super(props, styles);
+  this.addProp({
+    type: "object",
+    key: "item-left",
+    displayer: "Item Left",
+    value: [
+    {
+      type: "boolean",
+      key: "Visibility",
+      displayer: "visibility",
+      value: true,
+    },
+    {
+      type: "page",
+      key: "url",
+      displayer: "Url",
+      value: "",
+    },
+    {
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon2",
+      displayer: "Icon2",
+      value: "",
+    },
+    {
+      type: "image",
+      key: "image",
+      displayer: "Image",
+      value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-01.jpg",
+    },    
+  ],
+  })
+  this.addProp({ 
+    type: "object",
+    displayer: "Item Right Top Left",
+    key: "item-right-top-left",
+    value: [
+    {
+      type: "boolean",
+      key: "Visibility",
+      displayer: "visibility",
+      value: true,
+    },
+    {
+      type: "page",
+      key: "url",
+      displayer: "Url",
+      value: "",
+    },
+    {
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon2",
+      displayer: "Icon2",
+      value: "",
+    },
+    {
+      type: "image",
+      key: "image",
+      displayer: "Image",
+      value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-03.jpg",
+    },    
+  ],
+  })
+  this.addProp({
+    type: "object",
+    displayer: "Item Right Top Right",
+    key: "item-right-top-right",
+    value: [
+    {
+      type: "boolean",
+      key: "Visibility",
+      displayer: "visibility",
+      value: true,
+    },
+    {
+      type: "page",
+      key: "url",
+      displayer: "Url",
+      value: "",
+    },
+    {
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Start business with mentors",
+    },
+    {
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "RxDividerVertical",
+    },
+    {
+      type: "icon",
+      key: "icon2",
+      displayer: "Icon2",
+      value: "LuChevronRight",
+    },
+    {
+      type: "image",
+      key: "image",
+      displayer: "Image",
+      value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-02.jpg",
+    },    
+  ],
+  })
+  this.addProp({
+    type: "object",
+    displayer: "Item Right Bottom Left",
+    key: "item-right-bottom-left",
+    value: [
+    {
+      type: "boolean",
+      key: "Visibility",
+      displayer: "visibility",
+      value: true,
+    },
+    {
+      type: "page",
+      key: "url",
+      displayer: "Url",
+      value: "",
+    },
+    {
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Achieve goals & coach fast",
+    },
+    {
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "RxDividerVertical",
+    },
+    {
+      type: "icon",
+      key: "icon2",
+      displayer: "Icon2",
+      value: "LuChevronRight",
+    },
+    {
+      type: "image",
+      key: "image",
+      displayer: "Image",
+      value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-04.jpg",
+    },    
+  ],
+  })
+  this.addProp({
+    type: "object",
+    displayer: "Item Right Bottom Right",
+    key: "item-right-bottom-right",
+    value: [
+    {
+      type: "boolean",
+      key: "Visibility",
+      displayer: "visibility",
+      value: true,
+    },
+    {
+      type: "page",
+      key: "url",
+      displayer: "Url",
+      value: "",
+    },
+    {
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "",
+    },
+    {
+      type: "icon",
+      key: "icon2",
+      displayer: "Icon2",
+      value: "",
+    },
+    {
+      type: "image",
+      key: "image",
+      displayer: "Image",
+      value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-05.jpg",
+    },    
+  ],
+  })
 }
-class Content4 extends BaseContent {
-  constructor(props?: any) {
-    super(props, styles);
-
-    this.addProp({
-      type: "array",
-      key: "cards",
-      displayer: "Cards",
-      value: [
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "https://halstein.qodeinteractive.com/portfolio-item/expertise-advice/",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-01.jpg",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "https://halstein.qodeinteractive.com/portfolio-item/rebranding/",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-02.jpg",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "https://halstein.qodeinteractive.com/portfolio-item/golden-hello/",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Start business with mentors",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "LiaBicycleSolid",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-03.jpg",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "https://halstein.qodeinteractive.com/portfolio-item/intern-program/",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Achieve goals & coach fast",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "LiaBicycleSolid",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-04.jpg",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "https://halstein.qodeinteractive.com/portfolio-item/expertise-advice/",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://halstein.qodeinteractive.com/wp-content/uploads/2021/10/h1-img-05.jpg",
-            },
-          ],
-        },
-      ],
-    });
-  }
-
   getName(): string {
     return "Content-4";
   }
-
+  
   render() {
-    const cards = this.castToObject<Card[]>("cards");
-    const leftPageCards = cards.slice(0, 1);
-    const rightPageCards = cards.slice(1);
+    const itemLeft = this.castToObject<any>("item-left");
+    const itemTopRight = this.castToObject<any>("item-right-top-left");
+    const itemTopLeft = this.castToObject<any>("item-right-top-right");
+    const itemBottomLeft = this.castToObject<any>("item-right-bottom-left");
+    const itemBottomRight = this.castToObject<any>("item-right-bottom-right");
+    
   
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("left-page")}>
-            {leftPageCards.map((item: Card, index: number) => (
-              <div key={index} className={this.decorateCSS("card")}>
-                <ComposerLink path={item.url} isFullWidth={true}>
-                  <img
-                    src={item.image}
-                    alt="category"
-                    className={this.decorateCSS("image")}
-                  />
-                  <div className={this.decorateCSS("category")}>
-                    <h2 className={this.decorateCSS("title")}>{item.title}</h2>
+          {itemLeft.Visibility ? (
+            <div className={this.decorateCSS("left")}>
+              <ComposerLink path={itemLeft.url} isFullWidth={true}>
+                <div className={this.decorateCSS("item")}>
+                  <div className={this.decorateCSS("background-image")}>
+                    <img src={itemLeft.image} alt="content" className={this.decorateCSS("image")}/>
+                  </div>
+                  {(itemLeft.title || itemLeft.icon || itemLeft.icon2) && (
+                    <div className={this.decorateCSS("content")}>
+                      {itemLeft.title && <h2 className={this.decorateCSS("title")}>{itemLeft.title}</h2>}
+                      <div className={this.decorateCSS("icons")}>
+                        {itemLeft.icon && <ComposerIcon name={itemLeft.icon} propsIcon={{ className: this.decorateCSS("icon1") }} />}
+                        {itemLeft.icon2 && <ComposerIcon name={itemLeft.icon2} propsIcon={{ className: this.decorateCSS("icon2") }} />}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </ComposerLink>
+            </div>
+          ):null}
+  
+          <div className={this.decorateCSS("right")}>
+            {(itemTopLeft.Visibility) || (itemTopRight.Visibility) ? (
+            <div className={this.decorateCSS("top")}>
+              {itemTopRight.Visibility ? (
+                <ComposerLink path={itemTopRight.url} isFullWidth={true}>
+                  <div className={this.decorateCSS("item")}>
+                    <div className={this.decorateCSS("background-image")}>
+                      <img src={itemTopRight.image} alt="content" className={this.decorateCSS("image")} />
+                    </div>
+                    {(itemTopRight.title || itemTopRight.icon || itemTopRight.icon2) && (
+                      <div className={this.decorateCSS("content")}>
+                        {itemTopRight.title && <h2 className={this.decorateCSS("title")}>{itemTopRight.title}</h2>}
+                        <div className={this.decorateCSS("icons")}>
+                          {itemTopRight.icon && <ComposerIcon name={itemTopRight.icon} propsIcon={{ className: this.decorateCSS("icon1") }} />}
+                          {itemTopRight.icon2 && <ComposerIcon name={itemTopRight.icon2} propsIcon={{ className: this.decorateCSS("icon2") }} />}
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </ComposerLink>
-              </div>
-            ))}
-          </div>
-          <div className={this.decorateCSS("right-page")}>
-            {rightPageCards.map((item: Card, index: number) => (
-              <div key={index} className={this.decorateCSS("card")}>
-                <ComposerLink path={item.url} isFullWidth={true}>
-                  <img
-                    src={item.image}
-                    alt="category"
-                    className={this.decorateCSS("image")}
-                  />
-                  <div className={this.decorateCSS("category")}>
-                    <h2 className={this.decorateCSS("title")}>{item.title}</h2>
+              ):null}
+              {itemTopLeft.Visibility ? (
+                <ComposerLink path={itemTopLeft.url} isFullWidth={true}>
+                  <div className={this.decorateCSS("item")}>
+                    <div className={this.decorateCSS("background-image")}>
+                      <img src={itemTopLeft.image} alt="content" className={this.decorateCSS("image")} />
+                    </div>
+                    {(itemTopLeft.title || itemTopLeft.icon || itemTopLeft.icon2) && (
+                      <div className={this.decorateCSS("content")}>
+                        {itemTopLeft.title && <h2 className={this.decorateCSS("title")}>{itemTopLeft.title}</h2>}
+                        <div className={this.decorateCSS("icons")}>
+                          {itemTopLeft.icon && <ComposerIcon name={itemTopLeft.icon} propsIcon={{ className: this.decorateCSS("icon1") }} />}
+                          {itemTopLeft.icon2 && <ComposerIcon name={itemTopLeft.icon2} propsIcon={{ className: this.decorateCSS("icon2") }} />}
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </ComposerLink>
-              </div>
-            ))}
+              ):null}
+            </div>
+            ):null}
+            {(itemBottomLeft.Visibility) || (itemBottomRight.Visibility) ? (
+              <div className={this.decorateCSS("bottom")}>
+                {itemBottomLeft.Visibility ? (
+                  <ComposerLink path={itemBottomLeft.url} isFullWidth={true}>
+                    <div className={this.decorateCSS("item")}>
+                      <div className={this.decorateCSS("background-image")}>
+                        <img src={itemBottomLeft.image} alt="content" className={this.decorateCSS("image")} />
+                      </div>
+                      {(itemBottomLeft.title || itemBottomLeft.icon || itemBottomLeft.icon2) && (
+                        <div className={this.decorateCSS("content")}>
+                          {itemBottomLeft.title && <h2 className={this.decorateCSS("title")}>{itemBottomLeft.title}</h2>}
+                          <div className={this.decorateCSS("icons")}>
+                            {itemBottomLeft.icon && <ComposerIcon name={itemBottomLeft.icon} propsIcon={{ className: this.decorateCSS("icon1") }} />}
+                            {itemBottomLeft.icon2 && <ComposerIcon name={itemBottomLeft.icon2} propsIcon={{ className: this.decorateCSS("icon2") }} />}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </ComposerLink>
+                ):null}
+                {itemBottomRight.Visibility ? (
+                  <ComposerLink path={itemBottomRight.url} isFullWidth={true}>
+                    <div className={this.decorateCSS("item")}>
+                      <div className={this.decorateCSS("background-image")}>
+                        <img src={itemBottomRight.image} alt="content" className={this.decorateCSS("image")} />
+                      </div>
+                      {(itemBottomRight.title || itemBottomRight.icon || itemBottomRight.icon2) && (
+                        <div className={this.decorateCSS("content")}>
+                          {itemBottomRight.title && <h2 className={this.decorateCSS("title")}>{itemBottomRight.title}</h2>}
+                          <div className={this.decorateCSS("icons")}>
+                            {itemBottomRight.icon && <ComposerIcon name={itemBottomRight.icon} propsIcon={{ className: this.decorateCSS("icon1") }} />}
+                            {itemBottomRight.icon2 && <ComposerIcon name={itemBottomRight.icon2} propsIcon={{ className: this.decorateCSS("icon2") }} />}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </ComposerLink>
+                ):null}
+
+            </div>
+            ):null}
           </div>
         </div>
       </div>
@@ -228,5 +351,7 @@ class Content4 extends BaseContent {
   
 }
 
-
 export default Content4;
+
+
+
