ba8988c0e9361387638e122d26cf6e4cefe2e04c
Yusuf <yusufbjk__44@hotmail.com>
2024-06-25 15:20:30 +0300

    feature3

--- editor-components/feature/feature3/feature3.tsx
+++ editor-components/feature/feature3/feature3.tsx
@@ -1,105 +1,205 @@
 import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature3.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type ISimpleBoxes = {
-  title: string;
-  description: string;
+type Logos = {
   image: string;
+  url:string;
 };
+
+type Cards = {
+  title: string;
+  icon: string;
+}
+
+
 class Feature3 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
 
-    
     this.addProp({
       type: "array",
-      key: "simple-boxes-content",
-      displayer: "Simple Boxes Content",
+      key: "logos",
+      displayer: "Logo Images",
       value: [
         {
-          type: "object",
-          key: "simple-boxes",
-          displayer: "Simple Boxes",
+          type:"object",
+          key:"logo",
+          displayer: "Logo",
           value: [
             {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Bill Gates",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:"Bill Gates is a prominent American entrepreneur and philanthropist who co-founded Microsoft Corporation, one of the world's largest software companies. He is also known for his significant contributions to the development of computers."
+              type: "page",
+              key: "link",
+              displayer: "Logo Link",
+              value: "",
             },
             {
               type: "image",
               key: "image",
-              displayer: "Image",
-              value: "https://upload.wikimedia.org/wikipedia/commons/a/af/Bill_Gates_-_Nov._8%2C_2019.jpg",
+              displayer: "Logo Image",
+              value: "https://template-kit.evonicmedia.com/layout49/wp-content/uploads/2023/10/new-logoipsum-289.png",
             },
-          ],
+        ]
         },
         {
-          type: "object",
-          key: "simple-boxes",
-          displayer: "Simple Boxes",
+          type:"object",
+          key:"logo",
+          displayer: "Logo",
           value: [
             {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Walt Disney",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "alt Disney (1901-1966) was an American entrepreneur, animator, voice actor, and film producer. He was the co-founder of The Walt Disney Company, which has become one of the world's largest and most influential entertainment conglomerates."
+              type: "page",
+              key: "link",
+              displayer: "Logo Link",
+              value: "",
             },
             {
               type: "image",
               key: "image",
-              displayer: "Image",
-              value: "https://www.themoviedb.org/t/p/original/zLSGIiGao4TaLqIwfqHggxO6RqA.jpg",
+              displayer: "Logo Image",
+              value: "https://template-kit.evonicmedia.com/layout49/wp-content/uploads/2023/10/logoipsum-250-1.png",
             },
-          ],
-        },{
-          type: "object",
-          key: "simple-boxes",
-          displayer: "Simple Boxes",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Steve Jobs",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value: "Steve Jobs (1955-2011) was a famous American entrepreneur and business magnate, who is best known for co-founding Apple Inc. and playing a key role in the development of personal computing and mobile devices."
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Steve_Jobs_Headshot_2010-CROP2.jpg/640px-Steve_Jobs_Headshot_2010-CROP2.jpg",
-            },
-          ],
+        ]
         },
-        
-      ],
+        {
+          type:"object",
+          key:"logo",
+          displayer: "Logo",
+          value: [
+            {
+              type: "page",
+              key: "link",
+              displayer: "Logo Link",
+              value: "",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Logo Image",
+              value: "https://template-kit.evonicmedia.com/layout49/wp-content/uploads/2023/10/logoipsum-244.png",
+            },
+        ]
+        },
+        {
+          type:"object",
+          key:"logo",
+          displayer: "Logo",
+          value: [
+            {
+              type: "page",
+              key: "link",
+              displayer: "Logo Link",
+              value: "",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Logo Image",
+              value: "https://template-kit.evonicmedia.com/layout49/wp-content/uploads/2023/10/new-logoipsum-265.png",
+            },
+        ]
+        },
+        {
+          type:"object",
+          key:"logo",
+          displayer: "Logo",
+          value: [
+            {
+              type: "page",
+              key: "link",
+              displayer: "Logo Link",
+              value: "",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Logo Image",
+              value: "https://template-kit.evonicmedia.com/layout49/wp-content/uploads/2023/10/new-logoipsum-254.png",
+            },
+        ]
+        },
+      ]
+    })
+
+    this.addProp({
+      type: "array",
+      key: "cards",
+      displayer: "Card",
+      value: [
+        {
+          type:"object",
+          key:"card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Search Engine Optimization",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "FcSearch",
+            },
+        ]
+        },
+        {
+          type:"object",
+          key:"card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Marketing & Advertisement",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "FcStatistics",
+            },
+        ]
+        },
+        {
+          type:"object",
+          key:"card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Reporting & Analysis",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "FcComboChart",
+            },
+        ]
+        },
+      ]
+    })
+
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "Lorem ipsum dolor sit amet consectetur. In nulla nunc arcu velit consectetur massa mauris molestie hac. Hac arcu amet nullam pellentesque. Urna eu suspendisse felis.",
     });
     this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 3,
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "The Right Digital Partner For Success"
     });
+
+   
   }
 
   getName(): string {
@@ -109,39 +209,56 @@ class Feature3 extends BaseFeature {
   render() {
     return (
       <div
-        className={this.decorateCSS("container")}
-        
-      >
+        className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-        {this.castToObject<ISimpleBoxes[]>("simple-boxes-content").map(
-          (simplebox: any, index: number) => (
-            <div className={this.decorateCSS("card-item-count")} style={{
-              width: 90 / this.getPropValue("itemCount") + "%",
-            }}>
-            <div
-              className={this.decorateCSS("simple-boxes")}
-              
-              key={index}
-            >
-              <img
-                alt=""
-                className={this.decorateCSS("image")}
+
+           <div className={this.decorateCSS("content")}>
+
+            <div className={this.decorateCSS("top-content")}>
+
+                <div className={this.decorateCSS("feature-item")}>
+                    <div className={this.decorateCSS("feature-circle")}>
+                     
+                    </div>
+                    <div className={this.decorateCSS("feature-contents")}>
+                        <h1 className={this.decorateCSS("title")}>
+                            {this.getPropValue("title")}
+                        </h1>
+
+                        <p className={this.decorateCSS("description")}>
+                          {this.getPropValue("description")}
+                      </p>
+                      </div>
+                </div>
                 
-                src={simplebox.image}
-              ></img>
-              <h3 className={this.decorateCSS("title")} >
-                {simplebox.title}
-              </h3>
-              <p
-                className={this.decorateCSS("long-text")}
-                
-              >
-                {simplebox.description}
-              </p>
-            </div>
-            </div>
-          )
-        )}
+                <div className={this.decorateCSS("cards")}>
+                  {this.castToObject<Cards[]>("cards").map(
+                      (card:Cards, index: number) => (
+                        <div className={this.decorateCSS("card")}>
+                            <ComposerIcon
+                              name={card.icon}
+                              propsIcon={{
+                                className: `${this.decorateCSS("card-icon")}`}}
+                            />
+                            <h1 className={this.decorateCSS("card-title")}>{card.title}</h1>
+                        </div>
+                      )
+                  )}
+                </div>
+              </div>
+
+              <div className={this.decorateCSS("bottom-content")}>
+              {this.castToObject<Logos[]>("logos").map(
+                    (logo:Logos, index: number) => (
+                      <div className={this.decorateCSS("logos")}>
+                          <img className={this.decorateCSS("logo-image")} src={logo.image} alt="logo image" />
+                      </div>
+                    )
+              )}
+              </div>
+
+           </div>
+
         </div>
       </div>
     );
