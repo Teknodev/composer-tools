19a04ae20af7a9b4ffa59ab7632ac172c8238200
yapmakutu <cakirahmetsahin@gmail.com>
2024-06-25 12:58:44 +0300

    progress

--- editor-components/feature/feature5/feature5.tsx
+++ editor-components/feature/feature5/feature5.tsx
@@ -2,105 +2,178 @@ import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature5.module.scss";
 
-type IBasicCard = {
-  title: string;
-  description: string;
-  image: string;
-};
 class Feature5 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
+
     this.addProp({
-      type: "array",
-      key: "basic-card",
-      displayer: "Basic Card",
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Life Hacks",
+    });
+
+    this.addProp({
+      type: "object",
+      key: "row1",
+      displayer: "first-row",
       value: [
         {
-          type: "object",
-          key: "basic",
-          displayer: "Basic",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://images01.nicepage.com/c461c07a441a5d220e8feb1a/a17abde8d83650a582a28432/users-with-speech-bubbles-vector_53876-82250.jpg",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Discover the Best Restaurants in Town",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Our website features a curated selection of the top-rated restaurants in your area. Browse our cards to find the perfect dining spot for any occasion, whether it's a romantic date night or a family-friendly brunch.",
-            },
-          ],
+          type: "image",
+          key: "left_image",
+          displayer: "Left Image",
+          value: "https://demo.tagdiv.com/newspaper_lifestyle_pro/wp-content/uploads/2020/06/30-1068x1602.jpg",
         },
         {
-          type: "object",
-          key: "basic",
-          displayer: "Basic",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://images01.nicepage.com/c461c07a441a5d220e8feb1a/3b242447f922540fbe750cab/fdf.jpg",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "The Benefits of Regular Exercise for a Healthy Lifestyle",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                " Discover the many advantages of incorporating regular exercise into your daily routine. From boosting your mood and energy levels to improving heart health and reducing the risk of chronic diseases, find out why exercise is crucial for a healthy lifestyle.",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "basic",
-          displayer: "Basic",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: "https://images01.nicepage.com/c461c07a441a5d220e8feb1a/8cc47b39e719570b996d9879/dsds.jpg",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "How to Build a Successful E-Commerce Business from Scratch",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Ready to start your own online store but not sure where to begin? Learn the essential steps to building a successful e-commerce business from scratch. From choosing the right products and platform to marketing strategies and customer service, we've got you covered.",
-            },
-          ],
+          type: "string",
+          key: "title",
+          displayer: "Title",
+          value: "The 10 Best Apps for Planning Your Next Trip",
         },
       ],
     });
+
     this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 3,
+      type: "object",
+      key: "row2",
+      displayer: "second-row",
+      value: [
+        {
+          type: "object",
+          key: "item1",
+          displayer: "Description and Link",
+          value: [
+            {
+              type: "string",
+              key: "text",
+              displayer: "Text",
+              value: "Newspaper is not only convenient to use, but it also uses very low resources and loads extremely fast. Welcome to the future!",
+            },
+            {
+              type: "string",
+              displayer: "explore",
+              key: "Explore-text",
+              value: "Explore",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "link",
+              value: ""
+            }
+          ]
+        },
+        {
+          type: "object",
+          key: "item2",
+          displayer: "Title and Image",
+          value: [
+            {
+              type: "string",
+              key: "text",
+              displayer: "Text",
+              value: "Don't Let Your Summer Hard Work Go to Waste",
+            },
+            {
+              type: "image",
+              key: "right-image",
+              displayer: "Right Image",
+              value: "https://demo.tagdiv.com/newspaper_lifestyle_pro/wp-content/uploads/2020/06/29-1068x1417.jpg",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "link",
+              value: ""
+            }
+          ]
+        },
+      ],
     });
+
+    this.addProp({
+      type: "array",
+      key: "row3",
+      displayer: "third-row",
+      value: [
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image1",
+              displayer: "Left Image",
+              value: "https://demo.tagdiv.com/newspaper_lifestyle_pro/wp-content/uploads/2020/06/28-696x696.jpg",
+            },
+            {
+              type: "string",
+              key: "sub-title",
+              displayer: "Sub-Title",
+              value: "Girl's Guide: Tricks to Save Time in the Morning",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "link",
+              value: ""
+            }
+          ]
+        },
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image2",
+              displayer: "Middle Image",
+              value: "https://demo.tagdiv.com/newspaper_lifestyle_pro/wp-content/uploads/2020/06/27-696x870.jpg",
+            },
+            {
+              type: "string",
+              key: "sub-title",
+              displayer: "Sub-Title",
+              value: "Motivation Monday: Only 3 Days Left of Summer",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "link",
+              value: ""
+            }
+          ]
+        },
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image3",
+              displayer: "Right Image",
+              value: "	https://demo.tagdiv.com/newspaper_lifestyle_pro/wp-content/uploads/2020/06/26-696x464.jpg",
+            },
+            {
+              type: "string",
+              key: "sub-title",
+              displayer: "Sub-Title",
+              value: "Five Rules For a Long, Healthy and Happy Life",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "link",
+              value: ""
+            }
+          ]
+        },
+      ],
+    },
+    );
   }
 
   getName(): string {
@@ -108,27 +181,59 @@ class Feature5 extends BaseFeature {
   }
 
   render() {
+    const title = this.getPropValue("title", { as_string: true });
+    const row1 = this.castToObject<{ left_image: string, title: JSX.Element }>("row1",);
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")}>
-            {this.castToObject<IBasicCard[]>("basic-card").map(
-              (basic: any, index: number) => (
-                <div className={this.decorateCSS("card-item-count")} style={{
-                  width: 90 / this.getPropValue("itemCount") + "%",
-                }}>
-                <div className={this.decorateCSS("basic")} key={index}>
-                  <img src={basic.image} alt="" />
-                  <h3 className={this.decorateCSS("title")}>{basic.title}</h3>
-                  <p className={this.decorateCSS("long-text")}>{basic.description}</p>
-                </div>
+          {
+            title &&
+            <h1 className={this.decorateCSS("title")}>
+              {this.getPropValue("title")}
+            </h1>
+          }
+          {(row1.left_image || this.castToString(row1.title)) && <div className={this.decorateCSS("row1")}>
+
+            {row1.left_image && <img className={this.decorateCSS("left-image")} src={
+              row1.left_image
+            } />}
+            {this.castToString(row1.title) && <h1 className={this.decorateCSS("title")}>
+              {row1.title}
+            </h1>}
+
+          </div>}
+          {/* <div className={this.decorateCSS("row2")}>
+            {this.getPropValue("row2").map((item: any) => {
+              return (
+                <div className={this.decorateCSS("basic")}>
+                  <p className={this.decorateCSS("text")}>
+                    {item.value[0].value}
+                  </p>
+                  <h1 className={this.decorateCSS("title")}>
+                    {item.value[1].value}
+                  </h1>
                 </div>
               )
-            )}
+            })}
           </div>
+          <div className={this.decorateCSS("row3")}>
+            {this.getPropValue("row3").map((item: any) => {
+              return (
+                <div className={this.decorateCSS("basic")}>
+                  <img className={this.decorateCSS("left-image")} src={
+                    item.value[0].value
+                  } alt="" />
+                  <h1 className={this.decorateCSS("title")}>
+                    {item.value[1].value}
+                  </h1>
+                </div>
+              )
+            })}
+          </div> */}
         </div>
       </div>
-    );
+    )
   }
 }
 
