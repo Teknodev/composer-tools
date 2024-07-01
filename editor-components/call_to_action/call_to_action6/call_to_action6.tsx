8646ec1447d1bb5a1e497bf6c18fe307a375cbbd
cannyrts <70090523+cannyrts@users.noreply.github.com>
2024-07-01 11:03:42 +0300

    Final Version Before PR

--- editor-components/call_to_action/call_to_action6/call_to_action6.tsx
+++ editor-components/call_to_action/call_to_action6/call_to_action6.tsx
@@ -2,51 +2,80 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseCallToAction } from "../../EditorComponent";
 import styles from "./call_to_action6.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 class CallToAction6Page extends BaseCallToAction {
   constructor(props?: any) {
     super(props, styles);
+
+    this.addProp({
+      type: "image",
+      key: "background_image",
+      displayer: "Background Image",
+      value:
+        "https://c4.wallpaperflare.com/wallpaper/869/24/987/hand-writing-book-man-wallpaper-preview.jpg",
+    });
+
     this.addProp({
       type: "string",
       key: "title",
       displayer: "Title",
-      value: "Get Started Today",
+      value: "Get notified on latest",
     });
+
     this.addProp({
       type: "string",
-      key: "buttonText",
-      displayer: "Button Text",
-      value: "Try it now",
+      key: "title2",
+      displayer: "Title 2",
+      value: "updates and new releases.",
     });
+
     this.addProp({
-      type: "page",
-      key: "link",
-      displayer: "Button Link",
-      value: "https://www.google.com/",
+      type: "string",
+      key: "placeholder",
+      displayer: "Place Holder",
+      value: "Enter E-mail Address",
     });
+
     this.addProp({
       type: "string",
       key: "comment",
       displayer: "Comment",
-      value: "I've been using this product for months now and it's been a game changer for my productivity. I can't imagine going back to my old ways.",
+      value: "* We promise we will never spam your account. *",
     });
+
     this.addProp({
-      type: "string",
-      key: "commentAuthor",
-      displayer: "Comment Author",
-      value: "- John Doe",
+      type: "object",
+      key: "button",
+      displayer: "Button",
+      value: [
+        {
+          type: "string",
+          key: "button_text",
+          displayer: "Button Text",
+          value: "Subscribe",
+        },
+        {
+          type: "page",
+          key: "link",
+          displayer: "Button Link",
+          value: "https://www.google.com/",
+        },
+      ],
     });
+
     this.addProp({
-      type: "image",
-      key: "leftImage",
-      displayer: "Left Image",
-      value: "https://images.unsplash.com/photo-1526512340740-9217d0159da9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmVydGljYWx8ZW58MHx8MHx8&w=1000&q=80",
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "IoIosArrowRoundForward",
     });
+
     this.addProp({
-      type: "image",
-      key: "rightImage",
-      displayer: "Right Image",
-      value: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Eiffel_Tower_Vertical.JPG",
+      type: "object",
+      key: "space",
+      displayer: "Empty Space",
+      value: [],
     });
   }
 
@@ -55,36 +84,82 @@ class CallToAction6Page extends BaseCallToAction {
   }
 
   render() {
+    const title = this.getPropValue("title", { as_string: true });
+    const title2 = this.getPropValue("title2", { as_string: true });
+    const placeholder = this.getPropValue("placeholder", { as_string: true });
+    const comment = this.getPropValue("comment", { as_string: true });
+
+    const button = this.castToObject<{
+      button_text: JSX.Element;
+      link: string;
+    }>("button");
+
+    const icon = this.getPropValue("icon");
+
+    const isButtonEmpty = icon || this.castToString(button.button_text);
+
     return (
       <div
         className={this.decorateCSS("container")}
-        
+        style={{
+          backgroundImage: `url(${this.getPropValue("background_image")})`,
+        }}
       >
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("call-to-action6-page")}>
-            <div className={this.decorateCSS("left-image")}>
-              <img className={this.decorateCSS("image")} src={this.getPropValue("leftImage")} alt=""/>
-            </div>
+            {title && (
+              <h1 className={this.decorateCSS("title")}>
+                {this.getPropValue("title")}
+              </h1>
+            )}
 
-            <div className={this.decorateCSS("call-to-action6-child")}>
-              <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-              <ComposerLink path={this.getPropValue("link")}>
-                <span
-                  className={this.decorateCSS("button")}
-                  
-                >
-                  {this.getPropValue("buttonText")}
-                </span>
+            {title2 && (
+              <h1 className={this.decorateCSS("title2")}>
+                {this.getPropValue("title2")}
+              </h1>
+            )}
+
+            <div className={this.decorateCSS("space")}></div>
+
+            {placeholder && (
+              <input
+                type="text"
+                className={this.decorateCSS("placeholder")}
+                placeholder={placeholder}
+              />
+            )}
+
+            {comment && (
+              <h3 className={this.decorateCSS("comment")}>
+                {this.getPropValue("comment")}
+              </h3>
+            )}
+
+            {isButtonEmpty && (
+              <ComposerLink
+                path={button.link}
+                isFullWidth={true}
+                className={this.decorateCSS("button-link")}
+              >
+                <div className={this.decorateCSS("button-container")}>
+                  <button className={this.decorateCSS("button")}>
+                    {button.button_text && (
+                      <div className={this.decorateCSS("button_text")}>
+                        {button.button_text}
+                      </div>
+                    )}
+                    {icon && (
+                      <ComposerIcon
+                        name={icon}
+                        propsIcon={{
+                          className: this.decorateCSS("icon"),
+                        }}
+                      />
+                    )}
+                  </button>
+                </div>
               </ComposerLink>
-              <div className={this.decorateCSS("comment")}>
-                <h3 className={this.decorateCSS("comment")}>{this.getPropValue("comment")}</h3>
-                <h5 className={this.decorateCSS("comment-author")}>{this.getPropValue("commentAuthor")}</h5>
-              </div>
-            </div>
-
-            <div className={this.decorateCSS("right-image")}>
-              <img className={this.decorateCSS("image")} src={this.getPropValue("rightImage")} alt=""/>
-            </div>
+            )}
           </div>
         </div>
       </div>
