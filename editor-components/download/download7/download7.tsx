1728de6630a9d6959aed3563a068c920f031c6d4
Emirhan Duman <emirdumanhan@gmail.com>
2024-07-04 12:10:44 +0300

    download-7

--- editor-components/download/download7/download7.tsx
+++ editor-components/download/download7/download7.tsx
@@ -2,105 +2,200 @@ import * as React from "react";
 import styles from "./download7.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseDownload, TypeUsableComponentProps } from "../../EditorComponent";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Button = {
-    buttonText: string;
-    url: string;
+  buttonText: string;
+  url: string;
+  buttonIcon: string;
 };
 
 class Download7 extends BaseDownload {
-    constructor(props?: any) {
-        super(props, styles);
+  constructor(props?: any) {
+    super(props, styles);
 
-        this.addProp({
-            type: "image",
-            key: "image",
-            displayer: "Background Image",
-            value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6436af5c68c3c2002cd2fa52?alt=media&timestamp=1719564433794",
-        });
+    this.addProp({
+      type: "image",
+      key: "image",
+      displayer: "Background Image",
+      value:
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6436af5c68c3c2002cd2fa52?alt=media&timestamp=1692795543393",
+    });
 
-        this.addProp({
-            type: "string",
-            key: "downContentTitle",
-            displayer: "Card Title",
-            value: "If you want to download it",
-        });
+    this.addProp({
+      type: "string",
+      key: "contentTitle",
+      displayer: "Card Title",
+      value: "DO IT NOW",
+    });
 
-        this.addProp({
-            type: "string",
-            key: "upContentTitle",
-            displayer: "Card Title",
-            value: "Download now by clicking the button",
-        });
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value:
+        "Join millions of users worldwide on Dash, where the power of social networking lies in your hands.",
+    });
 
-        this.addProp({
-            type: "string",
-            key: "description",
-            displayer: "Description",
-            value: "By offering a clear call-to-action, businesses and developers can increase the chances that users will download and use their product.",
-        });
+    this.addProp({
+      type: "array",
+      key: "buttonsLeft",
+      displayer: "Button Left",
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Download for Android",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
+            },
+            {
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "In Button Icon",
+              value: "FaAndroid",
+            },
+          ],
+        },
+      ],
+      additionalParams: {
+        maxElementCount: 2,
+      },
+    });
+    this.addProp({
+      type: "array",
+      key: "buttonsRight",
+      displayer: "Button Right",
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Download for iOS",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
+            },
+            {
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "In Button Icon",
+              value: "FaApple",
+            },
+          ],
+        },
+      ],
+      additionalParams: {
+        maxElementCount: 2,
+      },
+    });
+  }
 
-        let button: TypeUsableComponentProps = {
-            type: "object",
-            key: "button",
-            displayer: "Displayer",
-            value: [
-                {
-                    type: "string",
-                    key: "buttonText",
-                    displayer: "Button Text",
-                    value: "Download Now",
-                },
-                {
-                    type: "page",
-                    key: "url",
-                    displayer: "Button Link",
-                    value: "",
-                },
-            ],
-        };
+  getName(): string {
+    return "Download-7";
+  }
 
-        this.addProp({
-            type: "array",
-            key: "buttons",
-            displayer: "Buttons",
-            value: [JSON.parse(JSON.stringify(button))],
-        });
-    }
-
-    getName(): string {
-        return "Download-7";
-    }
-
-    render() {
-        return (
-            <div className={this.decorateCSS("container")}>
-                <div className={this.decorateCSS("max-content")}>
-                    <div className={this.decorateCSS("page")}>
-                        <div className={this.decorateCSS("image-child")}>
-                            <img className={this.decorateCSS("background-image")} src={this.getPropValue("image")} alt="" />
-                        </div>
-                        <div className={this.decorateCSS("up-content")}>
-                            <h1 className={this.decorateCSS("unContentTitle")}>{this.getPropValue("upContentTitle")}</h1>
-                            <div className={this.decorateCSS("button-group")}>
-                                {this.castToObject<Button[]>("buttons").map((button: Button , index: number) => {
-                                    return (
-                                        <ComposerLink key={`dw-7-btn-${index}`} path={button.url}>
-                                            <button className={this.decorateCSS("button")}>{button.buttonText}</button>
-                                        </ComposerLink>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                        <div className={this.decorateCSS("down-content")}>
-                            <h1 className={this.decorateCSS("downContentTitle")}>{this.getPropValue("downContentTitle")}</h1>
-                            <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  render() {
+    setTimeout(() => {
+      const content = document.querySelector(`.${this.decorateCSS("content")}`);
+      if (content) {
+        const elements = content.querySelectorAll(
+          `.${this.decorateCSS("contentTitle")}, .${this.decorateCSS(
+            "description"
+          )}, .${this.decorateCSS("button-groups")}`
         );
-    }
+
+        elements.forEach((el, index) => {
+          setTimeout(() => {
+            (el as HTMLElement).style.transform = "translateY(0)";
+          }, index * 200);
+        });
+      }
+    }, 100);
+    return (
+      <div className={this.decorateCSS("container")}>
+        <div className={this.decorateCSS("max-content")}>
+          <div
+            className={this.decorateCSS("page")}
+            style={{ backgroundImage: `url(${this.getPropValue("image")})` }}
+          >
+            <div className={this.decorateCSS("content")}>
+              <h6 className={this.decorateCSS("contentTitle")}>
+                {this.getPropValue("contentTitle")}
+              </h6>
+              <h3 className={this.decorateCSS("description")}>
+                {this.getPropValue("description")}
+              </h3>
+              <div className={this.decorateCSS("button-groups")}>
+                <div className={this.decorateCSS("button-group-left")}>
+                  {this.castToObject<Button[]>("buttonsLeft").map(
+                    (button: Button, index: number) => {
+                      return (
+                        <ComposerLink
+                          key={`dw-7-btn-left-${index}`}
+                          path={button.url}
+                        >
+                          <button className={this.decorateCSS("button")}>
+                            <ComposerIcon
+                              propsIcon={{
+                                className: this.decorateCSS("button-icon"),
+                              }}
+                              name={button.buttonIcon}
+                            />
+                            {button.buttonText}
+                          </button>
+                        </ComposerLink>
+                      );
+                    }
+                  )}
+                </div>
+                <div className={this.decorateCSS("button-group-right")}>
+                  {this.castToObject<Button[]>("buttonsRight").map(
+                    (button: Button, index: number) => {
+                      return (
+                        <ComposerLink
+                          key={`dw-7-btn-right-${index}`}
+                          path={button.url}
+                        >
+                          <button className={this.decorateCSS("button")}>
+                            <ComposerIcon
+                              propsIcon={{
+                                className: this.decorateCSS("button-icon"),
+                              }}
+                              name={button.buttonIcon}
+                            />
+                            {button.buttonText}
+                          </button>
+                        </ComposerLink>
+                      );
+                    }
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Download7;
