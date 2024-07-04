dedf25ea3e42c1b91a3c1fa005d2cd9118de0265
Goktug Berke <gungorengoktugberke@gmail.com>
2024-07-04 18:52:57 +0300

    initial commit

--- editor-components/slider/slider8/slider8.tsx
+++ editor-components/slider/slider8/slider8.tsx
@@ -1,19 +1,43 @@
 import Slider from "react-slick";
 import * as React from "react";
-import { BaseSlider } from "../../EditorComponent";
+import { BaseSlider, TypeUsableComponentProps } from "../../EditorComponent";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./slider8.module.scss";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Card = {
   image: string;
+  url: string;
+  button: string;
   imagesubtitle: string;
   imagetitle: string;
   imagedescription: string;
 };
+
+interface IAnimationProps {
+  animationState: string;
+  startingAnimation: string;
+  endingAnimation: string;
+} 
+
 class Slider8 extends BaseSlider {
   constructor(props?: any) {
     super(props, styles);
+
+    this.addProp({
+      type: "icon",
+      key: "prev_icon",
+      displayer: "Prev icon",
+      value: "FaArrowLeftLong",
+    });
+    this.addProp({
+      type: "icon",
+      key: "next_icon",
+      displayer: "Next icon",
+      value: "FaArrowRightLong",
+    });
 
     this.addProp({
       type: "array",
@@ -29,27 +53,33 @@ class Slider8 extends BaseSlider {
               type: "string",
               key: "imagesubtitle",
               displayer: "Image Subtitle",
-              value: "Kevin E.",
-            },
-            {
-              type: "string",
-              key: "imagetitle",
-              displayer: "Image Title",
-              value: "Solutions Architect",
-            },
-            {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "This product has changed my life! It's incredibly useful and packed with creative features. I would highly recommend it to everyone!",
+              value: "The Petronas Twin Towers",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6477170dfba070002b75a124?alt=media&timestamp=1685526270154",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/facade-modern-building-concept-with-glass-panels-with-straight-lines-by-day-copy-1.jpg",
+            },
+            {
+              type: "object",
+              key: "button",
+              displayer: "Button",
+              value: [
+                {
+                  type: "string",
+                  key: "buttonText",
+                  displayer: "Button Text",
+                  value: "VIEW PROJECT →",
+                },
+                {
+                  type: "page",
+                  key: "link",
+                  displayer: "Link",
+                  value: "",
+                },
+              ],
             },
           ],
         },
@@ -62,27 +92,73 @@ class Slider8 extends BaseSlider {
               type: "string",
               key: "imagesubtitle",
               displayer: "Image Subtitle",
-              value: "Alice R.",
-            },
-            {
-              type: "string",
-              key: "imagetitle",
-              displayer: "Image Title",
-              value: "Marketing Specialist",
-            },
-            {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "Working with this company has been an absolute pleasure. Their professionalism and attention to detail are unmatched. I highly recommend their services.",
+              value: "Multi-Family Housing",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6477170dfba070002b75a122?alt=media&timestamp=1685526270154",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architecture-3d-rendering-illustration-modern-minimal-house-with-natural-landscape-6.jpg",
+            },
+            {
+              type: "object",
+              key: "button",
+              displayer: "Button",
+              value: [
+                {
+                  type: "string",
+                  key: "buttonText",
+                  displayer: "Button Text",
+                  value: "VIEW PROJECT →",
+                },
+                {
+                  type: "page",
+                  key: "link",
+                  displayer: "Link",
+                  value: "",
+                },
+              ],
+            },
+            
+          ],
+        },
+        {
+          type: "object",
+          key: "slidercontent",
+          displayer: "Slider Content",
+          value: [
+            {
+              type: "string",
+              key: "imagesubtitle",
+              displayer: "Image Subtitle",
+              value: "The Lighthouse",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/daniel-stone-A4TtfFo_qRw-unsplash.jpg",
+            },
+            {
+              type: "object",
+              key: "button",
+              displayer: "Button",
+              value: [
+                {
+                  type: "string",
+                  key: "buttonText",
+                  displayer: "Button Text",
+                  value: "VIEW PROJECT →",
+                },
+                {
+                  type: "page",
+                  key: "link",
+                  displayer: "Link",
+                  value: "",
+                },
+              ],
             },
           ],
         },
@@ -95,27 +171,20 @@ class Slider8 extends BaseSlider {
               type: "string",
               key: "imagesubtitle",
               displayer: "Image Subtitle",
-              value: "John D.",
+              value: "The Arches",
             },
             {
               type: "string",
-              key: "imagetitle",
-              displayer: "Image Title",
-              value: "Software Engineer",
-            },
-            {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "I have been consistently impressed with the results achieved by this team. Their strategic approach and innovative ideas have greatly contributed to our marketing success.",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "VIEW PROJECT → ",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6477170dfba070002b75a123?alt=media&timestamp=1685526270154",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architectural-3d-rendering-illustration-modern-minimal-house-with-natural-landscape.jpg",
             },
           ],
         },
@@ -128,47 +197,122 @@ class Slider8 extends BaseSlider {
               type: "string",
               key: "imagesubtitle",
               displayer: "Image Subtitle",
-              value: "Emily W.",
-            },
-            {
-              type: "string",
-              key: "imagetitle",
-              displayer: "Image Title",
-              value: "Graphic Designer",
-            },
-            {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "The product I purchased exceeded my expectations. It is well-designed, user-friendly, and has greatly improved my daily routine. I cannot recommend it enough.",
+              value: "Riverside Residence",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6477170dfba070002b75a121?alt=media&timestamp=1685526270154",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/low-angle-red-geometrical-structure-blue-sky.jpg",
+            },
+            {
+              type: "object",
+              key: "button",
+              displayer: "Button",
+              value: [
+                {
+                  type: "string",
+                  key: "buttonText",
+                  displayer: "Button Text",
+                  value: "VIEW PROJECT →",
+                },
+                {
+                  type: "page",
+                  key: "link",
+                  displayer: "Link",
+                  value: "",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "slidercontent",
+          displayer: "Slider Content",
+          value: [
+            {
+              type: "string",
+              key: "imagesubtitle",
+              displayer: "Image Subtitle",
+              value: "Dream House",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/joel-filipe-PkE6yQwE-6Y-unsplash.jpg",
+            },
+            {
+              type: "object",
+              key: "button",
+              displayer: "Button",
+              value: [
+                {
+                  type: "string",
+                  key: "buttonText",
+                  displayer: "Button Text",
+                  value: "VIEW PROJECT →",
+                },
+                {
+                  type: "page",
+                  key: "link",
+                  displayer: "Link",
+                  value: "",
+                },
+              ],
             },
           ],
         },
       ],
     });
 
+    this.setComponentState(
+      "prevSlide",
+      this.castToObject<Card[]>("header").length - 1
+    );
+    this.setComponentState("active-index", 0);/* */
+    this.setComponentState("titleAnimationClass", "animate__fadeInUp");
+    //this.setComponentState("activeSlide", 0);
+    //this.setComponentState("nextSlide", 1);
+    this.setComponentState("buttonAnimationClass", "animate__fadeInUp");
+    this.setComponentState("slider-ref", React.createRef());/* */
+
   }
   getName(): string {
     return "Slider 8";
   }
+
+  handleAnimationEnd = ({
+    animationState,
+    startingAnimation,
+    endingAnimation,
+  }: IAnimationProps) => {
+    if (this.getComponentState(animationState) === endingAnimation) {
+      this.setComponentState(animationState, startingAnimation);
+    }
+  };
   render() {
     const settings = {
       arrows: false,
       dots: true,
       infinite: true,
       speed: 700,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
+      beforeChange: (oldIndex: number, newIndex: number) => {
+        if (oldIndex == newIndex) return;
+        this.setComponentState("buttonAnimationClass", "animate__fadeOutDown");
+        this.setComponentState("titleAnimationClass", "animate__fadeOutDown");
+        this.setComponentState("descriptionAnimationClass", "animate__fadeOut");
+        setTimeout(() => {
+          this.setComponentState("active-index", newIndex);
+        }, 1200);
+      }
     };
     return (
       <div className={this.decorateCSS("container")}>
@@ -198,6 +342,45 @@ class Slider8 extends BaseSlider {
                           </h1>
                           <h3 className={this.decorateCSS("item-title")}>{item.imagetitle}</h3>
                           <p className={this.decorateCSS("item-description")}>{item.imagedescription}</p>
+                        </div>
+                        <div className={this.decorateCSS("")}>
+                        <div className={this.decorateCSS("arrows")}>
+                <div
+                  className={this.decorateCSS("prev-arrow")}
+                  onClick={() => {
+                    this.getComponentState("slider-ref").current.slickPrev();
+                  }}
+                >
+                  <ComposerIcon
+                    name={this.getPropValue("prev_icon")}
+                    propsIcon={{
+                      className: `${this.decorateCSS("arrow")}`,
+                      size: 20,
+                    }}
+                  />
+                  <span className={this.decorateCSS("arrow-text")}>
+                    {this.getPropValue("prev-arrow-text")}
+                  </span>
+                </div>
+
+                <div
+                  className={this.decorateCSS("next-arrow")}
+                  onClick={() => {
+                    this.getComponentState("slider-ref").current.slickNext();
+                  }}
+                >
+                  <span className={this.decorateCSS("arrow-text")}>
+                    {this.getPropValue("next-arrow-text")}
+                  </span>
+                  <ComposerIcon
+                    name={this.getPropValue("next_icon")}
+                    propsIcon={{
+                      className: `${this.decorateCSS("arrow")}`,
+                      size: 20,
+                    }}
+                  />
+                </div>
+              </div>
                         </div>
                       </div>
                     </div>
