018076083a0b52ad352679a74f3d11dc1df3f845
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2024-07-08 12:44:57 +0300

    logoClouds3

--- editor-components/logo-clouds/logo-comp3/logo-comp3.tsx
+++ editor-components/logo-clouds/logo-comp3/logo-comp3.tsx
@@ -15,74 +15,75 @@ class LogoComp3Page extends LogoClouds {
       type: "string",
       key: "title",
       displayer: "Title",
-      value: "Logo Clouds",
+      value: "Associated Brand",
     });
     this.addProp({
       type: "string",
-      key: "description",
-      displayer: "description",
-      value: "Sponsors can range from small local businesses to multinational corporations and can sponsor everything from sports teams to music festivals to non-profit organizations.",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: "Brands Available",
     });
     this.addProp({
+      type: "number",
+      key: "itemCount",
+      displayer: "Item Count in a Row",
+      value: 4,
+      max: 6,
+    });
+
+    this.addProp({
       type: "array",
-      key: "image-items",
-      displayer: "Images",
+      key: "items",
+      displayer: "Items",
       value: [
         {
-          type: "object",
-          key: "section",
-          displayer: "Section",
-          value: [
-            {
-              type: "array",
-              key: "items",
-              displayer: "Items",
-              value: [
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d5?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d4?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d2?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d1?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-                {
-                  type: "image",
-                  key: "image",
-                  value:
-                    "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d0?alt=media&timestamp=1719584962573",
-                  displayer: "Image",
-                },
-              ],
-            },
-          ],
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d1?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d4?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d2?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
+          displayer: "Image",
         },
       ],
     });
@@ -93,28 +94,48 @@ class LogoComp3Page extends LogoClouds {
   }
 
   render() {
+    const itemCount = this.getPropValue("itemCount") || 4;
+    const columnCount = Math.min(itemCount, 6);
     return (
-      <div
-        className={this.decorateCSS("container")}
-        
-      >
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("logo-comp3-page")}>
+      <div className={`${this.decorateCSS("container")}`}>
+        <div className={`${this.decorateCSS("max-content")}`}>
+          <div className={`${this.decorateCSS("logo-comp3-page")}`}>
+            <div className={`${this.decorateCSS("titles")} `}>
+              <h1 className={this.decorateCSS("title")}>
+                {this.getPropValue("title")}
+              </h1>
+              <div className={this.decorateCSS("title-line")}></div>
+              <h1 className={this.decorateCSS("subtitle")}>
+                {this.getPropValue("subtitle")}
+              </h1>
+            </div>
             <section>
               <center>
-                {this.castToObject<ISection[]>("image-items").map(
-                  (section: any, index: number) => (
-                    <div key={index} className={this.decorateCSS("image-child")}>
-                      {section.items.map((item: any, index: number) => (
-                        <img className={this.decorateCSS("image")} key={index} width={50} height={50} src={item.value} alt=""/>
-                      ))}
-                    </div>
-                  )
-                )}
+                <div
+                  className={this.decorateCSS("image-child")}
+                  style={{
+                    gridTemplateColumns: `repeat(${columnCount}, minmax(200px, 1fr))`,
+                  }}
+                >
+                  {this.getPropValue("items").map(
+                    (item: any, index: number) => (
+                      <div
+                        className={this.decorateCSS(
+                          `img-container-${itemCount}`
+                        )}
+                        key={index}
+                      >
+                        <img
+                          className={this.decorateCSS("image")}
+                          src={item.value}
+                          alt=""
+                        />
+                      </div>
+                    )
+                  )}
+                </div>
               </center>
             </section>
-            <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-            <h3 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h3>
           </div>
         </div>
       </div>
