3fee2899d0089ef9aeb0bb095a7f2d8acefa4851
Emirhan Duman <emirdumanhan@gmail.com>
2024-07-10 12:32:33 +0300

    slider8

--- editor-components/slider/slider8/slider8.tsx
+++ editor-components/slider/slider8/slider8.tsx
@@ -4,13 +4,27 @@ import { BaseSlider } from "../../EditorComponent";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./slider8.module.scss";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Card = {
   image: string;
-  imagesubtitle: string;
+  backgroundImage: string;
   imagetitle: string;
-  imagedescription: string;
+  url: string;
 };
+
+type Button = {
+  buttonText: string;
+  url: string;
+  buttonIcon: string;
+};
+
+type ButtonNav = {
+  prevIcon: string;
+  nextIcon: string;
+};
+
 class Slider8 extends BaseSlider {
   constructor(props?: any) {
     super(props, styles);
@@ -27,29 +41,63 @@ class Slider8 extends BaseSlider {
           value: [
             {
               type: "string",
-              key: "imagesubtitle",
-              displayer: "Image Subtitle",
-              value: "Kevin E.",
-            },
-            {
-              type: "string",
               key: "imagetitle",
               displayer: "Image Title",
-              value: "Solutions Architect",
+              value: "The Petronas Twin Towers",
             },
             {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "This product has changed my life! It's incredibly useful and packed with creative features. I would highly recommend it to everyone!",
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cc95bd2970002c6296f0?alt=media&timestamp=1719584962578",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/facade-modern-building-concept-with-glass-panels-with-straight-lines-by-day-copy-1.jpg",
+            },
+            {
+              type: "image",
+              key: "backgroundImage",
+              displayer: "Background Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/facade-modern-building-concept-with-glass-panels-with-straight-lines-by-day-copy-1.jpg",
+            },
+
+          ],
+        },
+        {
+          type: "object",
+          key: "slidercontent",
+          displayer: "Slider Content",
+          value: [
+            {
+              type: "string",
+              key: "imagetitle",
+              displayer: "Image Title",
+              value: "Multi-Family Housing",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
+            },
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architecture-3d-rendering-illustration-modern-minimal-house-with-natural-landscape-6.jpg",
+            },
+            {
+              type: "image",
+              key: "backgroundImage",
+              displayer: "Background Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architecture-3d-rendering-illustration-modern-minimal-house-with-natural-landscape-6.jpg",
             },
           ],
         },
@@ -60,30 +108,31 @@ class Slider8 extends BaseSlider {
           value: [
             {
               type: "string",
-              key: "imagesubtitle",
-              displayer: "Image Subtitle",
-              value: "Alice R.",
-            },
-            {
-              type: "string",
               key: "imagetitle",
               displayer: "Image Title",
-              value: "Marketing Specialist",
+              value: "The LightHouse",
             },
             {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "Working with this company has been an absolute pleasure. Their professionalism and attention to detail are unmatched. I highly recommend their services.",
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cc95bd2970002c6296ee?alt=media&timestamp=1719584962578",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/daniel-stone-A4TtfFo_qRw-unsplash.jpg",
             },
+            {
+              type: "image",
+              key: "backgroundImage",
+              displayer: "Background Image",
+              value:
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/daniel-stone-A4TtfFo_qRw-unsplash.jpg",
+            },
+
           ],
         },
         {
@@ -93,121 +142,224 @@ class Slider8 extends BaseSlider {
           value: [
             {
               type: "string",
-              key: "imagesubtitle",
-              displayer: "Image Subtitle",
-              value: "John D.",
-            },
-            {
-              type: "string",
               key: "imagetitle",
               displayer: "Image Title",
-              value: "Software Engineer",
+              value: "The Arches",
             },
             {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "I have been consistently impressed with the results achieved by this team. Their strategic approach and innovative ideas have greatly contributed to our marketing success.",
+              type: "page",
+              key: "url",
+              displayer: "URL",
+              value: "",
             },
             {
               type: "image",
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cc95bd2970002c6296ef?alt=media&timestamp=1719584962578",
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "slidercontent",
-          displayer: "Slider Content",
-          value: [
-            {
-              type: "string",
-              key: "imagesubtitle",
-              displayer: "Image Subtitle",
-              value: "Emily W.",
-            },
-            {
-              type: "string",
-              key: "imagetitle",
-              displayer: "Image Title",
-              value: "Graphic Designer",
-            },
-            {
-              type: "string",
-              key: "imagedescription",
-              displayer: "Image Description",
-              value:
-                "The product I purchased exceeded my expectations. It is well-designed, user-friendly, and has greatly improved my daily routine. I cannot recommend it enough.",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architectural-3d-rendering-illustration-modern-minimal-house-with-natural-landscape.jpg",
             },
             {
               type: "image",
-              key: "image",
-              displayer: "Image",
+              key: "backgroundImage",
+              displayer: "Background Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cc95bd2970002c6296ed?alt=media&timestamp=1719584962578",
+                "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/architectural-3d-rendering-illustration-modern-minimal-house-with-natural-landscape.jpg",
             },
           ],
         },
       ],
     });
 
+    this.addProp({
+      type: "array",
+      key: "buttons",
+      displayer: "Buttons",
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "VIEW PROJECT",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "",
+            },
+            {
+              type: "icon",
+              key: "buttonIcon",
+              displayer: "Button Icon",
+              value: "FaArrowRight",
+            },
+          ],
+        },
+      ],
+      additionalParams: {
+        maxElementCount: 2,
+      },
+    });
+
+    this.addProp({
+      type: "array",
+      key: "nav-buttons",
+      displayer: "Nav Buttons",
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "icon",
+              key: "prevIcon",
+              displayer: "Button Icon",
+              value: "FaArrowLeftLong",
+            },
+            {
+              type: "icon",
+              key: "nextIcon",
+              displayer: "Button Icon",
+              value: "FaArrowRightLong",
+            },
+          ],
+        },
+      ],
+      additionalParams: {
+        maxElementCount: 2,
+      },
+    });
+
+    this.setComponentState("slider-ref", React.createRef());
   }
+
   getName(): string {
     return "Slider 8";
   }
+
   render() {
     const settings = {
       arrows: false,
       dots: true,
       infinite: true,
-      speed: 700,
-      autoplay: true,
+      speed: 1500,
+      autoplay: false,
       autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
+      vertical: true,
+      verticalSwiping: true,
     };
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div
-            className={this.decorateCSS("wrapper")}
-          >
+          <div className={this.decorateCSS("wrapper")}>
             <div className={this.decorateCSS("slider-parent")}>
-              <Slider {...settings} className={this.decorateCSS("carousel")}>
+              <Slider
+                {...settings}
+                className={this.decorateCSS("carousel")}
+                ref={this.getComponentState("slider-ref")}
+              >
                 {this.castToObject<Card[]>("header").map(
-                  (item: Card, index: number) => (
-                    <div
-                      className={this.decorateCSS("slider-inner-div")}
-                      key={`sld-8-${index}`}
-                    >
-                      <div className={this.decorateCSS("content-div")}>
-                        <div className={this.decorateCSS("img-div")}>
-                          <img
-                            alt=""
-                            src={item.image}
-                            className={this.decorateCSS("img")}
-                          />
+                  (item: Card, index: number) => {
+                    return (
+                      <div
+                        className={this.decorateCSS("slider-inner-div")}
+                        key={`sld-8-${index}`}
+                      >
+                        <div
+                          className={this.decorateCSS("content")}
+                          style={{ backgroundImage: `url("${item.backgroundImage}")` }}
+                        >
+                          <div
+                            className={this.decorateCSS("content-div")}
+                          >
+                            <div
+                              className={this.decorateCSS("image")}
+                              style={{ backgroundImage: `url(${item.image})` }}
+                            >
+                            </div>
+                            <h3 className={this.decorateCSS("item-title")}>
+                              {item.imagetitle}
+                            </h3>
+                            <div className={this.decorateCSS("buttons")}>
+                              {this.castToObject<Button[]>("buttons").map(
+                                (button: Button, index: number) => (
+                                  <ComposerLink
+                                    key={`dw-7-btn-left-${index}`}
+                                    path={button.url}
+                                  >
+                                    <button className={this.decorateCSS("button")}>
+                                      {button.buttonText}
+                                      <ComposerIcon
+                                        propsIcon={{
+                                          className: this.decorateCSS("button-icon"),
+                                        }}
+                                        name={button.buttonIcon}
+                                      />
+                                    </button>
+                                  </ComposerLink>
+                                )
+                              )}
+                            </div>
+                            <div className={this.decorateCSS("nav-buttons")}>
+                              {this.castToObject<ButtonNav[]>("nav-buttons").map(
+                                (button: ButtonNav, index: number) => (
+                                  <button
+                                    className={this.decorateCSS("nav-button")}
+                                    onClick={() => {
+                                      this.getComponentState("slider-ref").current.slickPrev();
+                                    }}
+                                  >
+                                    <ComposerIcon
+                                      name={button.prevIcon}
+                                      propsIcon={{
+                                        className: `${this.decorateCSS("arrow")}`,
+                                        size: 20,
+                                      }}
+                                    />
+                                  </button>
+                                )
+                              )}
+                              {this.castToObject<ButtonNav[]>("nav-buttons").map(
+                                (button: ButtonNav, index: number) => (
+                                  <button
+                                    className={this.decorateCSS("nav-button")}
+                                    onClick={() => {
+                                      this.getComponentState("slider-ref").current.slickNext();
+                                    }}
+                                  >
+                                    <ComposerIcon
+                                      name={button.nextIcon}
+                                      propsIcon={{
+                                        className: `${this.decorateCSS("arrow")}`,
+                                        size: 20,
+                                      }}
+                                    />
+                                  </button>
+                                )
+                              )}
+                            </div>
+                          </div>
                         </div>
-                        <div className={this.decorateCSS("header-page")}>
-                          <h1 className={this.decorateCSS("first-header")}>
-                            {item.imagesubtitle}
-                          </h1>
-                          <h3 className={this.decorateCSS("item-title")}>{item.imagetitle}</h3>
-                          <p className={this.decorateCSS("item-description")}>{item.imagedescription}</p>
-                        </div>
+
                       </div>
-                    </div>
-                  )
+                    )
+                  }
                 )}
               </Slider>
             </div>
           </div>
-        </div>
-      </div>
+        </div >
+      </div >
     );
   }
 }
