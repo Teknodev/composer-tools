57dc3cfc352d20538ea8572f1fd8bd9e14cae49c
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-07-11 15:07:21 +0300

    content13

--- editor-components/content/content13/content13.tsx
+++ editor-components/content/content13/content13.tsx
@@ -2,151 +2,63 @@ import * as React from "react";
 import { BaseContent } from "../../EditorComponent";
 import styles from "./content13.module.scss";
 
-type Card = {
-  title: string;
-  description: string;
-  image: string;
-};
 
-class Content4 extends BaseContent {
+class Content13 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
 
-    let serverimg1 = require("./server-1.png");
-    let serverimg2 = require("./server-2.png");
-    let serverimg3 = require("./server-3.png");
-
     this.addProp({
       type: "string",
-      key: "title",
-      displayer: "Title",
-      value: "Create a Landing Page In a Minute Not Weeks",
+      key: "about-title",
+      displayer: "about-title",
+      value: "About resorts",
     });
 
     this.addProp({
       type: "string",
-      key: "subtitle",
-      displayer: "Subtitle",
-      value: "Key Featured For Landing Page",
+      key: "maintitle",
+      displayer: "maintitle",
+      value: "Relax at the luxury resorts around the entire world.",
     });
 
     this.addProp({
-      type: "array",
-      key: "content-card",
-      displayer: "Content Card",
-      value: [
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Web Hosting",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Web hosting refers to the service of providing space on a server to host a website or web application on the internet. Web hosting providers offer various hosting plans and packages to suit different website requirements, including shared hosting, dedicated hosting.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: serverimg1,
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Innovative Idea",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Innovative ideas often stem from identifying gaps in existing solutions or finding new ways to leverage technology or resources. They require a willingness to think outside the box, take risks, and challenge conventional thinking.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: serverimg2,
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Dedicated Server",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "A dedicated server refers to a type of web hosting service where a single server is allocated exclusively to a single client or website. In other words, the entire physical server is dedicated to serving the needs of that client or website alone.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: serverimg3,
-            },
-          ],
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Dedicated Server",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "A dedicated server refers to a type of web hosting service where a single server is allocated exclusively to a single client or website. In other words, the entire physical server is dedicated to serving the needs of that client or website alone.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value: serverimg3,
-            },
-          ],
-        },
-      ],
-    });
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "A design-led approach guides the team, implementing practices, products and services that are thoughtful and environmentally sound. Family of professionals that creates intelligent designs that help the face of hospitality."
+    })
+
     this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 4,
+      type: "string",
+      key: "buttonText",
+      displayer: "Text of Button",
+      value: "About resort"
     });
+
+    this.addProp({
+      type: "string",
+      key: "phone",
+      displayer: "PhoneNumber",
+      value: "1 800 222 000"
+    })
+
+    this.addProp({
+      type: "image",
+      key: "image1",
+      displayer: "image1",
+      value: "https://craftohtml.themezaa.com/images/demo-hotel-and-resort-home-01.jpg"
+    })
+
+    this.addProp({
+      type: "image",
+      key: "image2",
+      displayer: "image2",
+      value: "https://craftohtml.themezaa.com/images/demo-hotel-and-resort-home-02.jpg"
+    })
+
+
+
+
   }
 
   getName(): string {
@@ -154,35 +66,43 @@ class Content4 extends BaseContent {
   }
 
   render() {
+
+   
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("header")}>
-            <span className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</span>
-            <h2 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h2>
+          <div className={this.decorateCSS("title-container")}>
+            <span className={this.decorateCSS("about-title")}>{this.getPropValue("about-title")}</span>
+            <h2 className={this.decorateCSS("maintitle")}>{this.getPropValue("maintitle")}</h2>
+            <p className={this.decorateCSS("description")}>{this.getPropValue("description")}</p>
+            <div className={this.decorateCSS("button-icon")} >
+
+              <button className={this.decorateCSS("about-button")}>{this.getPropValue("buttonText")}</button>
+             <div className={this.decorateCSS("phone-icon")}>
+              <span className={this.decorateCSS("icon")}>📞</span>
+              <h1 className={this.decorateCSS("phone")}>{this.getPropValue("phone")}</h1>
+
+             </div>
+            </div>
           </div>
-          <div className={this.decorateCSS("card-child")}>
-            {this.castToObject<Card[]>("content-card").map(
-              
-              (card: Card, index: number) => (
-                <div className={this.decorateCSS("card-item-count")} style={{
-                  width: 90 / this.getPropValue("itemCount") + "%",
-                }}>
-                  <div key={`cnt-4-card-${index}`} className={this.decorateCSS("card")}>
-                    <i>
-                      <img alt="server" src={card.image} />
-                    </i>
-                    <h5 className={this.decorateCSS("card-title")}>{card.title}</h5>
-                    <p className={this.decorateCSS("card-description")}>{card.description}</p>
-                  </div>
-              </div>
-              )
-            )}
+          <div className={this.decorateCSS("right-page")}>
+            <div className={this.decorateCSS("image-container")}>
+              <span className={this.decorateCSS("text")}> Started In</span>
+              <span className={this.decorateCSS("year")}>1995</span>
+              {/* //array yap */}
+              <img src={this.getPropValue("image1")} alt="" className={this.decorateCSS("image1")} />
+              <img src={this.getPropValue("image2")} alt="" className={this.decorateCSS("image2")} />
+
+            </div>
+            
           </div>
+         
         </div>
+        
       </div>
     );
   }
 }
 
-export default Content4;
+export default Content13;
