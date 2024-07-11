868f9c37fa3271cd54d6fede8343e2ffb391de08
Avassaa <tutuncubaha@gmail.com>
2024-07-11 10:58:11 +0300

    Fulfilled change request requirements

--- editor-components/feature/feature12/feature12.tsx
+++ editor-components/feature/feature12/feature12.tsx
@@ -1,16 +1,63 @@
 import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature12.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type IMessages = {
   title: string;
-  description: string;
-  image: string;
-  buttonText: string;
+  button: string;
+  description?: string;
+  icon?: string;
+  iconColor?: string;
+  backgroundColor: string;
 };
+
 class Feature12 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
+    this.addProp({
+      type: "string",
+      key: "upper-title",
+      displayer: "Upper Title",
+      value: "BEST FEATURES",
+    });
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "SERVICES",
+    });
+    this.addProp({
+      type: "string",
+      key: "title-backing-text",
+      displayer: "Title Backing Text",
+      value: "services",
+    });
+    this.addProp({
+      type: "object",
+      key: "message",
+      displayer: "Message",
+      value: [
+        {
+          type: "string",
+          key: "title",
+          displayer: "Title",
+          value: "Best Of Our Features",
+        },
+        {
+          type: "string",
+          key: "button",
+          displayer: "Button",
+          value: "SEE ALL SERVICES",
+        },
+        {
+          type: "string",
+          key: "background-image",
+          displayer: "background",
+          value: "https://avo.themescamp.com/wp-content/uploads/2020/09/1-2.jpg",
+        },
+      ],
+    });
     this.addProp({
       type: "array",
       key: "message-card",
@@ -25,13 +72,25 @@ class Feature12 extends BaseFeature {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "The Power of Positive Thinking",
+              value: "Graphic Clean Design",
             },
             {
               type: "string",
               key: "description",
               displayer: "Description",
-              value: "Positive thinking can have a profound impact on mental health and overall well-being. By focusing on positive thoughts and reframing negative ones, individuals can experience reduced stress, increased resilience, and greater self-confidence.",
+              value: "Consectetur adipiscing elit",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "PiPaintBucketFill",
+            },
+            {
+              type: "color",
+              key: "backgroundColor",
+              displayer: "Background Color",
+              value: "#181b21",
             },
           ],
         },
@@ -44,13 +103,25 @@ class Feature12 extends BaseFeature {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "The Benefits of Volunteering",
+              value: "Web & Mobile Design",
             },
             {
               type: "string",
               key: "description",
               displayer: "Description",
-              value: "Volunteering can provide numerous benefits, including increased social connections, improved mental health, and a sense of purpose and fulfillment. It can also develop new skills and enhance job prospects. ",
+              value: "Sed do eiusmod tempor incididunt.",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "IoPhonePortrait",
+            },
+            {
+              type: "color",
+              key: "backgroundColor",
+              displayer: "Background Color",
+              value: "#1a1e25",
             },
           ],
         },
@@ -63,23 +134,37 @@ class Feature12 extends BaseFeature {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "The Importance of Time Management",
+              value: "Social Media Marketing",
             },
             {
               type: "string",
               key: "description",
               displayer: "Description",
-              value: "Effective time management is essential for achieving goals, reducing stress, and increasing productivity. It involves setting priorities, breaking tasks into manageable chunks, and eliminating distractions. ",
+              value: "Uttt labore et dolore magna aliqua.",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "HiPresentationChartLine",
+            },
+            {
+              type: "color",
+              key: "backgroundColor",
+              displayer: "Background Color",
+              value: "#20242c",
             },
           ],
         },
       ],
     });
+
+    const val = this.getPropValue("itemCount") > 5 ? 5 : 4;
     this.addProp({
       type: "number",
       key: "itemCount",
       displayer: "Item count in a row",
-      value: 3,
+      value: val,
     });
   }
 
@@ -88,22 +173,80 @@ class Feature12 extends BaseFeature {
   }
 
   render() {
+    const upperTitle = this.getPropValue("upper-title");
+    const title = this.getPropValue("title");
+    const titleBackingText = this.getPropValue("title-backing-text");
+    const itemCount = this.getPropValue("itemCount");
+    const messageCards = this.castToObject<IMessages[]>("message-card");
+    const firstItem = this.getPropValue("message");
+    const firstCardTitle = firstItem[0].value;
+    const buttonText = firstItem[1].value;
+    const backgroundImage = firstItem[2].value;
+    if (itemCount > 5) {
+      this.setProp("itemCount", 5);
+    }
+
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
+      <div className={this.decorateCSS("container")} style={{ paddingBottom: "0px" }}>
+        <div className={this.decorateCSS("upper-title")} style={{ fontSize: "1.2em", textAlign: "center" }}>
+          {upperTitle}
+        </div>
+        <div className={this.decorateCSS("shadow-header-title")} style={{ fontSize: "12.5em", textAlign: "center", marginBottom: "20px" }}>
+          {titleBackingText}
+        </div>
+        <div className={this.decorateCSS("header-title")} style={{ fontSize: "5em", textAlign: "center", marginTop: "35px" }}>
+          {title}
+        </div>
+        <div className={this.decorateCSS("max-content")} style={{ marginTop: "20px" }}>
           <div className={this.decorateCSS("content")}>
-            {this.castToObject<IMessages[]>("message-card").map(
-              (message: any, index: number) => (
-                <div className={this.decorateCSS("card-item-count")} style={{
-                  width: 90 / this.getPropValue("itemCount") + "%",
-                }}>
-                <div className={this.decorateCSS("message")} key={index}>
-                  <h3 className={this.decorateCSS("title")}>{message.title}</h3>
-                  <p className={this.decorateCSS("long-text")}>{message.description}</p>
-                </div>
-                </div>
-              )
+            {firstItem.length > 0 && (
+              <div
+                className={this.decorateCSS("card-item-first")}
+                style={{
+                  width: `${100 / itemCount}%`,
+                  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.75)), url(${backgroundImage})`,
+                 
+                }}
+              >
+                <div className={this.decorateCSS("blurry-background")}></div>
+                <h3 className={this.decorateCSS("title")}>{firstCardTitle}</h3>
+                <button className={this.decorateCSS("button")}>
+                  {buttonText}
+                </button>
+              </div>
             )}
+
+            {messageCards.map((message: IMessages, index: number) => {
+              const shouldRender = message.title || message.description || message.button || message.icon;
+
+              if (shouldRender) {
+                return (
+                  <div
+                    className={`
+                      ${this.decorateCSS("card-item-count")}
+                      ${this.decorateCSS(`card-item-${index % 3}`)}
+                    `}
+                    style={{
+                      width: `${100 / itemCount}%`,
+                      marginBottom: "100px",
+                      height: "50vh",
+                      backgroundColor: message.backgroundColor,
+                    }}
+                    key={index}
+                  >
+                    <div className={this.decorateCSS("message")}>
+                      <div className={this.decorateCSS("Icon")}>
+                        <ComposerIcon propsIcon={{ className: this.decorateCSS("Icon") }} name={message.icon} />
+                      </div>
+                      <h3 className={this.decorateCSS("title")}>{message.title}</h3>
+                      <p className={this.decorateCSS("long-text")}>{message.description}</p>
+                    </div>
+                  </div>
+                );
+              } else {
+                return null;
+              }
+            })}
           </div>
         </div>
       </div>
