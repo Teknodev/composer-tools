4539dddb9a839abf1cda17937641fbcb2cebfcd5
Mustafa ozger <ozgermustafa118@gmail.com>
2024-07-16 11:04:39 +0300

    content 19 change request complated

--- editor-components/content/content19/content19.tsx
+++ editor-components/content/content19/content19.tsx
@@ -2,46 +2,22 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseContent } from "../../EditorComponent";
 import styles from "./content19.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Button } from "@mui/material";
+
+type ButtonProps = {
+  buttonText: string;
+  url: string;
+};
 
 class Content19 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
-      type: "string",
-      key: "title-text",
-      displayer: "Above description",
-      value: "We Think About Reflections On The Wisdom From The Planets"
-    })
-    this.addProp({
-      type: "string",
-      key: "description",
-      displayer: "Description",
-      value: "Donec orci enim, vulputate et rutrum id, varius at nibh. Sed sagittis feugiat augue maximus hendrerit. Phasellus volutpat dictum sem, sed dignissim diam sodales eu. Suspendisse ut lorem posuere, tincidunt leo a, ultricies odio. Nulla porta sagittis turpis vel tristique. Curabitur ac maximus est. Proin placerat mauris eu eros varius imperdiet id at ligula."
-    });
-    this.addProp({
       type: "video",
       displayer: "Video Link",
       key: "video",
       value: "https://www.youtube.com/embed/yB7MtnWmYzI"
-    });
-  
-    this.addProp({
-      type:"image",
-      key:"author-icon",
-      displayer:"Author Image",
-      value:"https://kamleshyadav.com/wp/astrologer/demo8/wp-content/uploads/sites/9/2021/12/about-user.svg"
-    });
-    this.addProp({
-      type: "string",
-      key: "author-name",
-      displayer: "Author Name",
-      value: "Tarot &amp; Horoscope"
-    });
-    this.addProp({
-      type: "string",
-      key: "author-description",
-      displayer: "Author Description",
-      value: "15 years of Experience with"
     });
     this.addProp({
       type:"image",
@@ -49,65 +25,148 @@ class Content19 extends BaseContent {
       displayer:"Video Image",
       value:"https://kamleshyadav.com/wp/astrologer/demo8/wp-content/uploads/sites/9/2021/09/video-bg.jpg"
     });
-
+  
     this.addProp({
-      type:"string",
-      key:"button-text",
-      displayer:"Button Text",
-      value:"Read More"
-    });
-    this.addProp({
-      type:"page",
-      key:"button-url",
-      displayer:"Button URL",
-      value:"https://google.com"
-    });
-    this.addProp({
-      type:"image",
+      type:"icon",
       key:"play-icon",
       displayer:"Play Image",
-      value:"	https://kamleshyadav.com/wp/astrologer/demo8/wp-content/uploads/sites/9/2021/09/play.png"
+      value:"AiOutlinePlayCircle"
+    });
+    this.addProp({
+      type: "string",
+      key: "title-text",
+      displayer: "Title",
+      value: "We Think About Reflections On The Wisdom From The Planets"
+    });
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "Donec orci enim, vulputate et rutrum id, varius at nibh. Sed sagittis feugiat augue maximus hendrerit. Phasellus volutpat dictum sem, sed dignissim diam sodales eu. Suspendisse ut lorem posuere, tincidunt leo a, ultricies odio. Nulla porta sagittis turpis vel tristique. Curabitur ac maximus est. Proin placerat mauris eu eros varius imperdiet id at ligula."
+    });
 
-    })
+
+    this.addProp({
+      type:"icon",
+      key:"author-icon",
+      displayer:"Author Icon",
+      value:"BiLogoWhatsapp"
+    });
+    this.addProp({
+      type: "string",
+      key: "author-name",
+      displayer: "Author Name",
+      value: "Tarot & Horoscope"
+    });
+    this.addProp({
+      type: "string",
+      key: "author-description",
+      displayer: "Author Description",
+      value: "15 years of Experience with"
+    });
+
+
+    this.addProp({
+      type: "array",
+      key: "buttons",
+      additionalParams: {
+        maxElementCount: 2,
+      },
+      displayer: "Button ",
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Next",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "dsad",
+            },
+          ],
+        }, 
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Back",
+            },
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "gfd",
+            }
+          ],
+        }
+      ]
+    });
 
     this.setComponentState("is_video_visible", false);
-
   }
 
   getName(): string {
     return "Content 19";
   }
+  
   render() {
     const videoImage = this.getPropValue("video-image");
-    const authorImage=this.getPropValue("author-icon");
+    const authorImage = this.getPropValue("author-icon");
+    const playIcon = this.getPropValue("play-icon");
+    const description = this.getProp("description");
 
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("content")}>
-            <div className={this.decorateCSS("video-part")}>
-              <img alt="" src={videoImage} className={this.decorateCSS("video-images")} />
-              {videoImage && (
+            {videoImage && (
+              <div className={this.decorateCSS("video-part")}>
+                <img alt="" src={videoImage} className={this.decorateCSS("video-images")} />
                 <div className={this.decorateCSS("play-part")}>
                   <span className="as-play" onClick={() => {
                     this.setComponentState("is_video_visible", true);
                   }}>
-                    <div className={this.decorateCSS("play-container")}>
-                      <div className={this.decorateCSS("outer-circle")}></div>
-                      <div className={this.decorateCSS("inner-circle")}></div>
-                      <img alt="" src={this.getPropValue("play-icon")} className={this.decorateCSS("play-image")}></img>
-                    </div>
+                    {playIcon && (
+                      <div className={this.decorateCSS("play-container")}>
+                        <div className={this.decorateCSS("outer-circle")}></div>
+                        <div className={this.decorateCSS("inner-circle")}></div>
+                        <ComposerIcon name={this.getPropValue("play-icon")} propsIcon={{
+                          className: this.decorateCSS("play-image")
+                        }}></ComposerIcon>
+                      </div>
+                    )}
                   </span>
                 </div>
-              )}
-            </div>
+              </div>
+            )}
             <div className={this.decorateCSS("text-side")}>
+              {this.getProp("title-text").value && (
               <p className={this.decorateCSS("title-text")}>{this.getPropValue("title-text")}</p>
-              <h1 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h1>
+              )}
+              {description.value && (
+                <div className={this.decorateCSS("description-div")}>
+                  <h2 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h2>
+                </div>
+              )}
               <div className={this.decorateCSS("description-author")}>
-                {authorImage &&(
+                {authorImage && (
                   <div className={this.decorateCSS("author-icon")}>
-                   <img alt="" src={this.getPropValue("author-icon")} className={this.decorateCSS("author-icon-photo")}></img>
+                    <ComposerIcon name={this.getPropValue("author-icon")} propsIcon={{
+                      className: this.decorateCSS("author-icon-photo")
+                    }}></ComposerIcon>
                   </div>
                 )}
                 <div className={this.decorateCSS("author-info")}>
@@ -115,7 +174,22 @@ class Content19 extends BaseContent {
                   <h1 className={"author--text"}>{this.getPropValue("author-name")}</h1>
                 </div>
               </div>
-              <a className={this.decorateCSS("button")} href={this.getPropValue("button-url")}>{this.getPropValue("button-text")}</a>
+          
+              <div className={this.decorateCSS("button-container")}>
+                {(this.getProp("buttons").value as any[]).map((buttonObj, index) => {
+                  const buttonData = buttonObj.value;
+                  const buttonText = buttonData.find((item: any) => item.key === "buttonText")?.value || "";
+                  const url = buttonData.find((item: any) => item.key === "url")?.value || "";
+                  if (buttonText.trim() !== "") {
+                    return (
+                      <ComposerLink key={index} path={url}>
+                        <Button className={this.decorateCSS("button")}>{buttonText}</Button>
+                      </ComposerLink>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
             </div>
           </div>
           {this.getComponentState("is_video_visible") && (
