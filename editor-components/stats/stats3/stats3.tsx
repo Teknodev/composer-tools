370923a3256054bf1938bc515a9aeffa8d655baf
Dogukan Celik <dceliknodes@gmail.com>
2024-07-19 16:10:58 +0300

    review req: stats3

--- editor-components/stats/stats3/stats3.tsx
+++ editor-components/stats/stats3/stats3.tsx
@@ -4,13 +4,26 @@ import styles from "./stats3.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
+type Button = {
+  text: JSX.Element;
+  isPrimary: boolean;
+  url: JSX.Element;
+};
+
+type CardContent = {
+  text: JSX.Element;
+  url: string;
+  icon: string;
+  buttonText: JSX.Element;
+};
+
 class Stats3Page extends BaseStats {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
       type: "string",
       key: "subTitle",
-      displayer: "subTitle",
+      displayer: "Subtitle",
       value: "ABOUT PERSONA",
     });
     this.addProp({
@@ -23,16 +36,15 @@ class Stats3Page extends BaseStats {
       type: "string",
       key: "description",
       displayer: "Description",
-      value:
-        "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.Etiam ac nisi tellus.Morbi at velit nisl.Donec ut felis libero.",
+      value: "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.Etiam ac nisi tellus.Morbi at velit nisl.Donec ut felis libero.",
     });
     this.addProp({
       type: "string",
       key: "descriptionMore",
-      displayer: "DescriptionMore",
-      value:
-        "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.",
+      displayer: "Description More",
+      value: "Nullam fermentum ullamcorper diam sit amet porta. Etiam ac enim velit.Ut ut mi sed turpis accumsan sagittis ac eu magna.",
     });
+
     this.addProp({
       type: "array",
       key: "buttons",
@@ -161,77 +173,117 @@ class Stats3Page extends BaseStats {
       displayer: "is box visible",
       value: true,
     });
+
+    const ed = this.castToObject<any>("subTitle");
+
+    console.log(ed);
   }
+
   getName(): string {
     return "Stats 3";
   }
+
   render() {
+
+
+
+    const isSubtitleExist = this.castToString(this.getPropValue("subTitle"));
+    const isTitleExist = this.castToString(this.getPropValue("title"));
+    const isDescExist = this.castToString(this.getPropValue("description"));
+    const isDescMoreExist = this.castToString(this.getPropValue("descriptionMore"));
+
+    const buttonList = this.castToObject<Button[]>("buttons");
+    const cardList = this.castToObject<CardContent[]>("card-content");
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("stats3-page")}>
             <div className={this.decorateCSS("bottom-child")}>
               <div className={this.decorateCSS("text")}>
-                <h1 className={this.decorateCSS("subTitle")}>
-                  {this.getPropValue("subTitle")}
-                </h1>
-                <h1 className={this.decorateCSS("title")}>
-                  {this.getPropValue("title")}
-                </h1>
-                <h1 className={this.decorateCSS("description")}>
-                  {this.getPropValue("description")}
-                </h1>
-                {this.getPropValue("descriptionMore") !== "" && (
+                {isSubtitleExist &&
+                  <h1 className={this.decorateCSS("subTitle")}>
+                    {this.getPropValue("subTitle")}
+                  </h1>
+                }
+                {isTitleExist &&
+                  <h1 className={this.decorateCSS("title")}>
+                    {this.getPropValue("title")}
+                  </h1>
+                }
+                {isDescExist &&
+                  <h1 className={this.decorateCSS("description")}>
+                    {this.getPropValue("description")}
+                  </h1>
+                }
+                {isDescMoreExist &&
                   <h3 className={this.decorateCSS("descriptionMore")}>
                     {this.getPropValue("descriptionMore")}
                   </h3>
-                )}
-
+                }
                 <div className={this.decorateCSS("box")}>
-                  {this.castToObject<any>("buttons").map((item: any, index: number) => {
-                    return (
-                      <ComposerLink key={`stats-${index}`} path={item.url}>
-                        <button
-                          className={`${this.decorateCSS("button")} ${
-                            item.isPrimary && this.decorateCSS("button-color")
-                          }`}
-                        >
-                          {item.buttonText}
-                        </button>
-                      </ComposerLink>
-                    );
+                  {buttonList.map((item: Button, index: number) => {
+
+                    const isTextExist = this.castToString(item.text);
+
+                    if (isTextExist)
+                      return (
+                        <ComposerLink key={`stats-${index}`} path={item.url}>
+                          <button
+                            className={`${this.decorateCSS("button")} ${item.isPrimary && this.decorateCSS("button-color")
+                              }`}
+                          >
+                            {item.text}
+                          </button>
+                        </ComposerLink>
+                      );
                   })}
                 </div>
               </div>
 
               <div className={this.decorateCSS("card")}>
-                <img
-                  src={this.getPropValue("backgroundImage")}
-                  alt="image"
-                  className={this.decorateCSS("image")}
-                />
+                {this.getPropValue("backgroundImage") &&
+                  <img
+                    src={this.getPropValue("backgroundImage")}
+                    alt="image"
+                    className={this.decorateCSS("image")}
+                  />
+                }
                 {this.getPropValue("is_box_visible") && (
                   <div className={this.decorateCSS("boxs")}>
-                    {this.castToObject<any>("card-content").map(
-                      (item: any, index: number) => {
-                        return (
-                          <ComposerLink key={`stats-${index}`} path={item.url}>
-                            <button>
-                              <ComposerIcon
-                                name={item.icon}
-                                propsIcon={{ className: this.decorateCSS("icon") }}
-                              />
-                              <div className={this.decorateCSS("text")}>
-                                <span className={this.decorateCSS("button-text")}>
-                                  {item.text}
-                                </span>
-                                <span className={this.decorateCSS("button-text")}>
-                                  {item.buttonText}
-                                </span>
-                              </div>
-                            </button>
-                          </ComposerLink>
-                        );
+                    {cardList.map(
+                      (item: CardContent, index: number) => {
+
+                        const isTextExist = this.castToString(item.text);
+                        const isButtonTextExist = this.castToString(item.buttonText);
+
+                        if (item.icon || isTextExist || isButtonTextExist)
+                          return (
+                            <ComposerLink key={`stats-${index}`} path={item.url}>
+                              <button>
+                                {item.icon &&
+                                  <ComposerIcon
+                                    name={item.icon}
+                                    propsIcon={{ className: this.decorateCSS("icon") }}
+                                  />
+                                }
+                                {(isTextExist || isButtonTextExist) &&
+                                  <div className={this.decorateCSS("text")}>
+                                    {isTextExist &&
+                                      <span className={this.decorateCSS("button-text")}>
+                                        {item.text}
+                                      </span>
+                                    }
+                                    {isButtonTextExist &&
+                                      <span className={this.decorateCSS("button-text")}>
+                                        {item.buttonText}
+                                      </span>
+                                    }
+                                  </div>
+                                }
+                              </button>
+                            </ComposerLink>
+                          );
                       }
                     )}
                   </div>
