5bf59c98420c101b809385fec3397c29625b1817
emirhandumann <emirdumanhan@gmail.com>
2024-07-22 12:19:01 +0300

    fix: navbar5 updated

--- editor-components/navigators/navbar5/navbar5.tsx
+++ editor-components/navigators/navbar5/navbar5.tsx
@@ -4,11 +4,22 @@ import React from "react";
 import styles from "./navbar5.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { TypeUsableComponentProps } from "../../EditorComponent";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Button = {
   buttonText: string;
   url: string;
   isPrimary: string;
+};
+
+type LeftItem = {
+  icon: string;
+  link: string;
+};
+
+type Item = {
+  item: string;
+  url: string;
 };
 
 class Navbar5 extends BaseNavigator {
@@ -18,6 +29,13 @@ class Navbar5 extends BaseNavigator {
 
   constructor(props?: any) {
     super(props, styles);
+
+    this.addProp({
+      type: "icon",
+      key: "icon",
+      displayer: "Mobile Icon",
+      value: "GiHamburgerMenu",
+    });
 
     this.addProp({
       type: "image",
@@ -101,38 +119,38 @@ class Navbar5 extends BaseNavigator {
       ],
     });
 
-    let button: TypeUsableComponentProps = {
-      type: "object",
-      key: "button",
+    this.addProp({
+      type: "array",
+      key: "buttonList",
       displayer: "Button",
       value: [
         {
-          type: "string",
-          key: "buttonText",
-          displayer: "Button Text",
-          value: "Contact",
-        },
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Contact",
+            },
 
-        {
-          type: "page",
-          key: "url",
-          displayer: "Button Link",
-          value: "https://www.google.com/",
-        },
-        {
-          type: "boolean",
-          key: "isPrimary",
-          displayer: "Is Primary",
-          value: false,
+            {
+              type: "page",
+              key: "url",
+              displayer: "Button Link",
+              value: "https://www.google.com/",
+            },
+            {
+              type: "boolean",
+              key: "isPrimary",
+              displayer: "Is Primary",
+              value: false,
+            },
+          ],
         },
       ],
-    };
-
-    this.addProp({
-      type: "array",
-      key: "buttons",
-      displayer: "Buttons",
-      value: [JSON.parse(JSON.stringify(button))],
     });
 
     this.addProp({
@@ -218,62 +236,118 @@ class Navbar5 extends BaseNavigator {
         },
       ],
     });
+
+    this.state["componentProps"]["navActive"] = true;
+  }
+
+  navClick() {
+    this.setComponentState("navActive", !this.getComponentState("navActive"));
   }
 
   render(): ReactNode {
+    const leftItems = this.castToObject<[]>("left-items");
+    const hasLeftItems = leftItems && leftItems.length > 0;
+    const itemList = this.castToObject<[]>("itemList");
+    const hasItemList = itemList && itemList.length > 0;
+    const buttonList = this.castToObject<Button[]>("buttonList");
+    const hasButtonList = buttonList && buttonList.length > 0;
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("content")}>
-            <div className={this.decorateCSS("content-left")}>
-              {this.getPropValue("left-items").map((leftItem: any) => {
+            {hasLeftItems && (
+              <div className={this.decorateCSS("content-left")}>
+                {leftItems.map((leftItem: LeftItem) => {
+                  return (
+                    <ComposerLink path={leftItem.link}>
+                      <img src={leftItem.icon} width={20} height={20} alt="" />
+                    </ComposerLink>
+                  );
+                })}
+              </div>
+            )}
+
+            {this.getPropValue("image") && (
+              <div className={this.decorateCSS("logo-image")}>
+                <img
+                  src={this.getPropValue("image")}
+                  width={200}
+                  height={170}
+                  alt=""
+                />
+              </div>
+            )}
+
+            {hasButtonList &&
+              this.castToObject<Button[]>("buttonList").map(
+                (item: Button, indexButtons: number) => {
+                  return (
+                    <ComposerLink key={indexButtons} path={item.url}>
+                      <button
+                        className={`${this.decorateCSS("button")} ${
+                          item.isPrimary ? this.decorateCSS("primary") : ""
+                        }`}
+                      >
+                        {item.buttonText}
+                      </button>
+                    </ComposerLink>
+                  );
+                }
+              )}
+          </div>
+
+          <div className={this.decorateCSS("bottom-line")}></div>
+
+          {hasItemList && (
+            <div className={this.decorateCSS("secondline-content")}>
+              {itemList.map((data: Item, indexItemList: number) => {
                 return (
-                  <ComposerLink path={leftItem.value[1].value}>
-                    <img src={leftItem.value[0].value} width={20} height={20} alt="" />
+                  <ComposerLink key={indexItemList} path={data.url}>
+                    <span className={this.decorateCSS("data-value")}>
+                      {data.item}
+                    </span>
                   </ComposerLink>
                 );
               })}
             </div>
+          )}
 
-            <div className={this.decorateCSS("logo-image")}>
-              <img
-                src={this.getPropValue("image")}
-                width={200}
-                height={170}
-                alt=""
+          <nav className={this.decorateCSS("navigator-mobile")}>
+            <div className={this.decorateCSS("navbar")}>
+              <h2 className={this.decorateCSS("title")}>
+                {this.getPropValue("title")}
+              </h2>
+              <ComposerIcon
+                propsIcon={{
+                  className: `${this.decorateCSS("img-hamburger")} ${
+                    this.getComponentState("navActive")
+                      ? this.decorateCSS("rotate")
+                      : ""
+                  }`,
+                  onClick: () => {
+                    this.navClick();
+                  },
+                }}
+                name={this.getPropValue("icon")}
               />
             </div>
-
-            {this.castToObject<Button[]>("buttons").map(
-              (item: Button, indexButtons: number) => {
-                return (
-                  <ComposerLink key={indexButtons} path={item.url}>
-                    <button
-                      className={`${this.decorateCSS("button")} ${
-                        item.isPrimary ? this.decorateCSS("primary") : ""
-                      }`}
-                    >
-                      {item.buttonText}
-                    </button>
-                  </ComposerLink>
-                );
-              }
+            {this.getComponentState("navActive") && (
+              <div className={this.decorateCSS("navbar-child")}>
+                {this.castToObject<Item[]>("itemList").map(
+                  (data: Item, indexItemList: number) => {
+                    return (
+                      <div className={this.decorateCSS("mobile-item")}>
+                        <ComposerLink key={indexItemList} path={data.url}>
+                          <h3 key={indexItemList}>{data.item}</h3>
+                        </ComposerLink>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
             )}
-          </div>
-          
-          <div className={this.decorateCSS("bottom-line")}></div>
-
-          <div className={this.decorateCSS("secondline-content")}>
-          {this.castToObject<[]>("itemList").map(
-            (data: any, indexItemList: number) => {
-              return (
-                <ComposerLink key={indexItemList} path={data.value[1].value}>
-                  <span className={this.decorateCSS("data-value")}>{data.value[0].value}</span>
-                </ComposerLink>
-              );
-            }
-          )}
-          </div>
+          </nav>
         </div>
       </div>
     );
