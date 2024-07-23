035ec092989f8e3e53cf5c002b94f297a430311d
Samed Nayki <20200808015@ogr.akdeniz.edu.tr>
2024-07-23 13:15:15 +0300

    Width Adjustment: Changes to the width setting now correctly reflect the adjustments made. Feature Properties Title: The feature properties title has been removed as a prop. Image Prop: The image is now opened as a separate prop, not within item 0. Main Title and Subtitle: The main title and subtitle have been opened as separate props. Marketing in Title: The word "marketing" will no longer be underlined in the title. Removing Marketing from Title: The word "marketing" no longer remains when deleted from the title prop. List Texts: List texts are now opened as an array and no longer appear as an array within an array in the content tab. Button as Array: The button is now taken as an array. Icons in List: Icons next to the list items are now provided as props. Color Adjustments: Subtitle color: composer-primary-color Title and description color: composer-font-color-primary List items and icon color: composer-font-color-primary Button Hover: When hovering over the button, only the colors will change, and the text, icon, and underline color will be composer-secondary-color. Button Text and Icon Deletion: When the button text and icon are deleted, the line below it will disappear. Element Deletion: When elements such as image, title, subtitle, list items, and button are deleted from the props in the content tab, their divs are also deleted to ensure correct positioning (example: content11).

--- editor-components/feature/feature7/feature7.tsx
+++ editor-components/feature/feature7/feature7.tsx
@@ -2,80 +2,108 @@ import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature7.module.scss";
 
-type FeatureProps = {
-  image: string;
-  mainTitle: string;
-  subtitle: string;
-  features: string[];
-  learnMore: string;
-  arrow: string;
-};
-
 class Feature7 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
+      type: "number",
+      key: "width",
+      displayer: "Width",
+      value: 100,
+    });
+
+    this.addProp({
+      type: "string",
+      key: "image",
+      displayer: "Image URL",
+      value:
+        "https://vault.uicore.co/marketing-agency/wp-content/uploads/sites/41/2022/08/Marketing-Agency-Features-Image.webp",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "mainTitle",
+      displayer: "Main Title",
+      value: "Everything starts with a Marketing plan.",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value:
+        "Turn your business into a sales machine today with lorem Things go wrong have questions.",
+    });
+
+    this.addProp({
       type: "array",
-      key: "featureProps",
-      displayer: "Feature Properties",
+      key: "features",
+      displayer: "Features",
       value: [
         {
           type: "string",
-          key: "image",
-          displayer: "Image URL ",
-          value:
-            "https://vault.uicore.co/marketing-agency/wp-content/uploads/sites/41/2022/08/Marketing-Agency-Features-Image.webp",
+          key: "feature1",
+          displayer: "Feature 1",
+          value: "Amazing communication.",
         },
         {
           type: "string",
-          key: "mainTitle",
-          displayer: "Main Title",
-          value: "Everything starts with a Marketing plan.",
+          key: "feature2",
+          displayer: "Feature 2",
+          value: "Best trending designing experience.",
         },
         {
           type: "string",
-          key: "subtitle",
-          displayer: "Subtitle",
-          value:
-            "Turn your business into a sales machine today with lorem Things go wrong have questions.",
+          key: "feature3",
+          displayer: "Feature 3",
+          value: "Email & Live chat.",
         },
-        {
-          type: "array",
-          key: "features",
-          displayer: "Features",
-          value: [
-            {
-              type: "string",
-              key: "feature1",
-              displayer: "Feature 1",
-              value: "Amazing communication.",
-            },
-            {
-              type: "string",
-              key: "feature2",
-              displayer: "Feature 2",
-              value: "Best trending designing experience.",
-            },
-            {
-              type: "string",
-              key: "feature3",
-              displayer: "Feature 3",
-              value: "Email & Live chat.",
-            },
-          ],
-        },
+      ],
+    });
+
+    this.addProp({
+      type: "array",
+      key: "button",
+      displayer: "Button",
+      value: [
         {
           type: "string",
           key: "learnMore",
           displayer: "Learn More Text",
-          value: "Want to learn more? Click here",
+          value: "",
         },
         {
           type: "string",
           key: "arrow",
           displayer: "Arrow",
-          value: "➜",
+          value: "➔",
+        },
+      ],
+    });
+
+    this.addProp({
+      type: "array",
+      key: "icons",
+      displayer: "Icons",
+      value: [
+        {
+          type: "string",
+          key: "icon1",
+          displayer: "Icon 1",
+          value: "✔",
+        },
+        {
+          type: "string",
+          key: "icon2",
+          displayer: "Icon 2",
+          value: "✔",
+        },
+        {
+          type: "string",
+          key: "icon3",
+          displayer: "Icon 3",
+          value: "✔",
         },
       ],
     });
@@ -86,60 +114,65 @@ class Feature7 extends BaseFeature {
   }
 
   render() {
-    const featurePropsArray = this.getPropValue("featureProps") as any[];
+    const width = this.getPropValue("width") as number;
+    const image = this.getPropValue("image") as string;
+    const mainTitle =
+      (this.getPropValue("mainTitle") as string) ||
+      "Everything starts with a Marketing plan.";
+    const subtitle = this.getPropValue("subtitle") as string;
+    const features = this.getPropValue("features") as any[];
+    const buttonProps = this.getPropValue("button") as any[];
+    const icons = this.getPropValue("icons") as any[];
 
-    const featureProps: FeatureProps = featurePropsArray.reduce(
-      (acc: any, prop: any) => {
-        if (Array.isArray(prop.value)) {
-          acc[prop.key] = prop.value.map((item: any) => item.value);
-        } else {
-          acc[prop.key] = prop.value;
-        }
-        return acc;
-      },
-      {} as FeatureProps
-    );
-
-    const mainTitleParts = featureProps.mainTitle.split("Marketing");
+    const learnMoreText =
+      buttonProps[0].value || "Want to learn more? Click here";
+    const arrow = buttonProps[1].value || "➔";
 
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("image-container")}>
-          <img
-            src={featureProps.image}
-            alt="Marketing Agency Features"
-            className={this.decorateCSS("image")}
-          />
-        </div>
+      <div
+        className={this.decorateCSS("container")}
+        style={{ width: `${width}%` }}
+      >
+        {image ? (
+          <div className={this.decorateCSS("image-container")}>
+            <img
+              src={image}
+              alt="Marketing Agency Features"
+              className={this.decorateCSS("image")}
+              onError={(e) => {
+                e.currentTarget.src = "https://via.placeholder.com/300";
+              }}
+            />
+          </div>
+        ) : null}
         <div className={this.decorateCSS("text-container")}>
           <p className={this.decorateCSS("features-heading")}>FEATURES</p>
-          <h1>
-            {mainTitleParts[0]}
-            <span className={this.decorateCSS("highlight")}>Marketing</span>
-            {mainTitleParts[1]}
-          </h1>
-          <p className={this.decorateCSS("subtitle")}>
-            {featureProps.subtitle}
-          </p>
+          {mainTitle && <h1>{mainTitle}</h1>}
+          {subtitle && (
+            <p className={this.decorateCSS("subtitle")}>{subtitle}</p>
+          )}
           <ul className={this.decorateCSS("features-list")}>
-            {featureProps.features.map((feature, index) => (
+            {features.map((feature: any, index: number) => (
               <li key={index}>
-                <span className={this.decorateCSS("tick")}>✔</span> {feature}
+                <span className={this.decorateCSS("tick")}>
+                  {icons[index].value}
+                </span>
+                {feature.value}
               </li>
             ))}
           </ul>
-          <div className={this.decorateCSS("learn-more")}>
-            <a
-              href="https://vault.uicore.co/marketing-agency/about-us/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>{featureProps.learnMore}</span>
-              <span className={this.decorateCSS("arrow")}>
-                {featureProps.arrow}
-              </span>
-            </a>
-          </div>
+          {learnMoreText && arrow && (
+            <div className={this.decorateCSS("learn-more")}>
+              <a
+                href="https://vault.uicore.co/marketing-agency/about-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span>{learnMoreText}</span>
+                <span className={this.decorateCSS("arrow")}>{arrow}</span>
+              </a>
+            </div>
+          )}
         </div>
       </div>
     );
