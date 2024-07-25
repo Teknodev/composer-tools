28e6e4d46f312aef043d204fbfa06209a90c2242
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-07-25 15:07:30 +0300

    stats-7

--- editor-components/stats/stats7/stats7.tsx
+++ editor-components/stats/stats7/stats7.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { BaseStats } from "../../EditorComponent";
 import styles from "./stats7.module.scss";
 
-type ICard = {
-  subtitle: string;
+
+interface Item {
   title: string;
-  description: string;
-};
+  progress: number;
+  progressText: string;
+}
 
 class Stats7Page extends BaseStats {
   constructor(props?: any) {
@@ -14,91 +15,106 @@ class Stats7Page extends BaseStats {
 
     this.addProp({
       type: "string",
+      key: "subtitle",
+      displayer: "subtitle",
+      value: "Our Skills."
+    })
+
+    this.addProp({
+      type: "string",
       key: "title",
       displayer: "Title",
-      value: "User Statistics on the Website",
+      value: "We work with organisations immersive customer.",
     });
     this.addProp({
       type: "string",
       key: "description",
       displayer: "Description",
       value:
-        "Understanding user behavior on a website is important for improving user experience and engagement.",
-    });
-    this.addProp({
-      type: "color",
-      key: "backgroundColor",
-      displayer: "Background Color",
-      value:
-        "#CDC1A9",
+        "Fierent abhorreant intellegam nam no. Eam minim di neglegentur te, ei etiamas corpora eam disentiun sea. Ut aeterno invidunt sententiae vel, assum adipisci eu vix. Ea ferri cetero ceteros eos, mea ne cibo dis entiet."
     });
     this.addProp({
       type: "array",
-      key: "card-content",
-      displayer: "Card Content",
+      key: "items",
+      displayer: "Items",
       value: [
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "item",
+          displayer: "Item",
           value: [
             {
               type: "string",
-              key: "subtitle",
-              displayer: "Subtitle",
-              value: "Products",
+              key: "progess-title",
+              displayer: "Progress Title",
+              value: "Design"
             },
             {
               type: "number",
-              key: "title",
-              displayer: "Title",
-              value: 300,
+              key: "progres-percent",
+              displayer: "Progress",
+              value: 75,
             },
-          
+            {
+              type: "string",
+              key: "progress-text",
+              displayer: "Progress Text",
+              value: "75%"
+            }
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "item",
+          displayer: "Item",
           value: [
             {
               type: "string",
-              key: "subtitle",
-              displayer: "Subtitle",
-              value: "Web Template",
+              key: "progess-title",
+              displayer: "Progress Title",
+              value: "Brand Identity"
             },
             {
               type: "number",
-              key: "title",
-              displayer: "Title",
-              value: 85,
+              key: "progres-percent",
+              displayer: "Progress",
+              value: 57,
             },
-          
+            {
+              type: "string",
+              key: "progress-text",
+              displayer: "Progress Text",
+              value: "57%"
+            }
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "item",
+          displayer: "Item",
           value: [
             {
               type: "string",
-              key: "subtitle",
-              displayer: "Subtitle",
-              value: "Mobile Template",
+              key: "progess-title",
+              displayer: "Progress Title",
+              value: "Sketch"
             },
             {
               type: "number",
-              key: "title",
-              displayer: "Title",
-              value: 200,
+              key: "progres-percent",
+              displayer: "Progress",
+              value: 84,
             },
-            
+            {
+              type: "string",
+              key: "progress-text",
+              displayer: "Progress Text",
+              value: "84%"
+            }
           ],
-        },
-      ],
-    });
+        }
+      ]
+    })
 
   }
 
@@ -107,34 +123,66 @@ class Stats7Page extends BaseStats {
   }
 
   render() {
-    const styling = {
-      backgroundColor: this.getPropValue("backgroundColor"),
-    };
+    const subtitle = this.getPropValue("subtitle")
+    const title = this.getPropValue("title")
+    const description = this.getPropValue("description")
+  
+
 
     return (
       <div className={this.decorateCSS("container")} >
-        <div className={this.decorateCSS("max-content")} style={styling}>
-          <div className={this.decorateCSS("stats1-page")}>
-            <div className={this.decorateCSS("title-child")}>
-              <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-              <h3 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h3>
-            </div>
-            <div className={this.decorateCSS("bottom-child")}>
-              {this.castToObject<ICard[]>("card-content").map(
-                (cardData: any, indexCard: number) => (
-                  <div key={`sts-7-${indexCard}`} className={this.decorateCSS("card")}>
-                    <h5 className={this.decorateCSS("card-data-subtitle")}>{cardData.subtitle}</h5>
-                    <h4 className={this.decorateCSS("card-data-title")}>{cardData.title}</h4>
-                    <p className={this.decorateCSS("card-data-description")}>{cardData.description}</p>
+        <div className={this.decorateCSS("max-content")}>
+
+          {subtitle && <div className={this.decorateCSS("title-child")}>
+            {subtitle && (
+              <h1 className={this.decorateCSS("subtitle")}>{subtitle}</h1>
+            )}
+            {title && (
+              <h1 className={this.decorateCSS("title")}>{title}</h1>
+            )}
+            {description && (
+              <p className={this.decorateCSS("description")}>
+                {description}
+              </p>
+            )}
+          </div>}
+
+          <div className={this.decorateCSS("progress-container")}>
+            {this.getPropValue("items").map((item: any, index: number) => {
+              const title = item.value[0].value;
+              const percent = item.value[1].value;
+              const text = item.value[2].value;
+
+              if (percent === null || percent === undefined || text === "") {
+                return null;
+              }
+
+              return (
+                <div className={this.decorateCSS("item")} key={index}>
+                  <div className={this.decorateCSS("progress-title")}>
+                    {title}
+                    <div className={this.decorateCSS("progress-percent")}>
+                      <div className={this.decorateCSS("progress-text")}>
+                        {text}
+                      </div>
+                    </div>
                   </div>
-                )
-              )}
-            </div>
+                  <div className={this.decorateCSS("progress-active")}>
+                    <div
+                      className={this.decorateCSS("progress-passive")}
+                      style={{ width: `${percent}%` }}
+                    ></div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
+
     );
   }
 }
 
 export default Stats7Page;
+
