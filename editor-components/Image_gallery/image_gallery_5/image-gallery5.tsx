99d42b8d7363b3b8ebb636a5e41c08a52588f647
sudeakinci <akincisude07@gmail.com>
2024-07-25 17:03:47 +0300

    changes done

--- editor-components/Image_gallery/image_gallery_5/image-gallery5.tsx
+++ editor-components/Image_gallery/image_gallery_5/image-gallery5.tsx
@@ -2,13 +2,6 @@ import * as React from "react";
 import { BaseImageGallery } from "../../EditorComponent";
 import styles from "./image-gallery5.module.scss";
 
-interface ImageType {
-  type: string;
-  key: string;
-  displayer: string;
-  value: string;
-}
-
 class ImageGalleryComponent5 extends BaseImageGallery {
   constructor(probs?: any) {
     super(probs, styles);
@@ -22,39 +15,47 @@ class ImageGalleryComponent5 extends BaseImageGallery {
           type: "image",
           key: "imageGallery",
           displayer: "Image1",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-1.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20b8c2f8a5b002ce65828?alt=media"
         },
         {
           type: "image",
           key: "imageGallery",
           displayer: "Image2",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-2.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c6a2f8a5b002ce65834?alt=media"
         },
         {
           type: "image",
           key: "imageGallery",
           displayer: "Image3",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-3.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c962f8a5b002ce65840?alt=media"
         },
         {
           type: "image",
           key: "imageGallery",
           displayer: "Image4",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-4.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cbc2f8a5b002ce6584c?alt=media"
         },
         {
           type: "image",
           key: "imageGallery",
           displayer: "Image5",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-5.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cd82f8a5b002ce65858?alt=media"
         },
         {
           type: "image",
           key: "imageGallery",
           displayer: "Image6",
-          value: "https://arty.flatheme.net/assets/images/p-clean-2-6.jpg"
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cee2f8a5b002ce6586d?alt=media"
         }
       ]
+    });
+
+    this.addProp({
+      type: "number",
+      key: "itemCount",
+      displayer: "Item Count in a Row",
+      value: 3,
+      max: 5,
     });
 
     this.setComponentState("is_image_clicked", false);
@@ -92,13 +93,18 @@ class ImageGalleryComponent5 extends BaseImageGallery {
     const galleries = this.getPropValue("gallery");
     const isImageClicked = this.getComponentState("is_image_clicked");
     const clickedImageIndex = this.getComponentState("clicked_image_index");
-
+    
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-            <div className={this.decorateCSS("images")}>
-              {galleries.map((image: ImageType, index: number) => (
-                <div className={this.decorateCSS("image-container")} key={index}>
+            <div className={this.decorateCSS("images")}
+                style={{gridTemplateColumns: `repeat(${this.getPropValue("itemCount")}, 1fr)`}}
+            >
+              {galleries.map((image: any, index: number) => (
+                <div 
+                className={this.decorateCSS("image-container")} 
+                key={index}
+                >
                   <img
                     src={image.value}
                     alt=""
