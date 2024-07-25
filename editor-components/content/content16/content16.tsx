569c6bedf3a7f0f2e08eee017bd1ebe7addd532a
Mehmet Can Özen <omehmetcan785@gmail.com>
2024-07-25 17:34:05 +0300

    Requested changes.

--- editor-components/content/content16/content16.tsx
+++ editor-components/content/content16/content16.tsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./content16.module.scss";
 
-type Card = {
+type CardType = {
   title?: string;
   subtitle?: string;
   description?: string;
@@ -46,13 +46,13 @@ class Content16 extends BaseContent {
           value: [
             {
               type: "string",
-              key: "image_subtitle",
+              key: "imageSubtitle",
               displayer: "Image Subtitle",
               value: "May 18, 2023  /  Insights",
             },
             {
               type: "string",
-              key: "image_title",
+              key: "imageTitle",
               displayer: "Image Title",
               value: "Smart Cities of Tomorrow",
             },
@@ -72,18 +72,18 @@ class Content16 extends BaseContent {
         },
         {
           type: "object",
-          key: "slidercontent",
+          key: "sliderContent",
           displayer: "Slider Content",
           value: [
             {
               type: "string",
-              key: "image_subtitle",
+              key: "imageSubtitle",
               displayer: "Image Subtitle",
               value: "May 18, 2023  /  News",
             },
             {
               type: "string",
-              key: "image_title",
+              key: "imageTitle",
               displayer: "Image Title",
               value: "Architectural Marvels",
             },
@@ -103,18 +103,18 @@ class Content16 extends BaseContent {
         },
         {
           type: "object",
-          key: "slidercontent",
+          key: "sliderContent",
           displayer: "Slider Content",
           value: [
             {
               type: "string",
-              key: "image_subtitle",
+              key: "imageSubtitle",
               displayer: "Image Subtitle",
               value: "May 15, 2023  /  Insights",
             },
             {
               type: "string",
-              key: "image_title",
+              key: "imageTitle",
               displayer: "Image Title",
               value: "Exploring the Evolution of Architecture",
             },
@@ -137,7 +137,7 @@ class Content16 extends BaseContent {
 
     this.setComponentState(
       "prevSlide",
-      this.castToObject<Card[]>("items").length - 1
+      this.castToObject<CardType[]>("items").length - 1
     );
     this.setComponentState("activeSlide", 0);
     this.setComponentState("nextSlide", 1);
@@ -149,120 +149,119 @@ class Content16 extends BaseContent {
 
   render() {
     const settings = {
-        arrows: false,
-        dots: true,
-        infinite: true,
-        speed: 725,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 885,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-        beforeChange: (current: number, next: number) => {
-            this.setComponentState(
-                "prevSlide",
-                next - 1 < 0
-                    ? this.castToObject<Card[]>("items").length - 1
-                    : next - 1
-            );
-            this.setComponentState("activeSlide", next);
-            this.setComponentState(
-                "nextSlide",
-                next + 1 > this.castToObject<Card[]>("items").length - 1
-                    ? 0
-                    : next + 1
-            );
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 725,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
         },
+        {
+          breakpoint: 885,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+      beforeChange: (current: number, next: number) => {
+        this.setComponentState(
+          "prevSlide",
+          next - 1 < 0
+            ? this.castToObject<CardType[]>("items").length - 1
+            : next - 1
+        );
+        this.setComponentState("activeSlide", next);
+        this.setComponentState(
+          "nextSlide",
+          next + 1 > this.castToObject<CardType[]>("items").length - 1
+            ? 0
+            : next + 1
+        );
+      },
     };
 
     const title = this.getPropValue("title");
     const subtitle = this.getPropValue("subtitle");
-    const items = this.castToObject<Card[]>("items").filter(
-        (item: Card) => item.image || item.image_title || item.image_subtitle || item.url
+    const items = this.castToObject<CardType[]>("items").filter(
+      (item: CardType) => item.image || item.image_title || item.image_subtitle || item.url
     );
 
     return (
-        <div className={this.decorateCSS("container")}>
-            <div className={this.decorateCSS("max-content")}>
-                {(title || subtitle) && (
-                    <header>
-                        {title && (
-                            <h1 className={this.decorateCSS("title")}>
-                                {title}
-                            </h1>
+      <div className={this.decorateCSS("container")}>
+        <div className={this.decorateCSS("max-content")}>
+          {(title || subtitle) && (
+            <header>
+              {title && (
+                <h1 className={this.decorateCSS("title")}>
+                  {title}
+                </h1>
+              )}
+              {title && subtitle && (
+                <hr className={this.decorateCSS("faint-line")} />
+              )}
+              {subtitle && (
+                <h2 className={this.decorateCSS("subtitle")}>
+                  {subtitle}
+                </h2>
+              )}
+            </header>
+          )}
+          <main className={this.decorateCSS("wrapper")}>
+            {items.length > 0 && (
+              <div className={this.decorateCSS("slider-parent")}>
+                <Slider {...settings} className={this.decorateCSS("carousel")}>
+                  {items.map((item: CardType, index: number) => (
+                    <article
+                      className={`${this.decorateCSS("slider-inner-div")} ${this.getComponentState("prevSlide") === index ? this.decorateCSS("prevSlide") : ""} ${this.getComponentState("nextSlide") === index ? this.decorateCSS("nextSlide") : ""}`}
+                      key={`sld-16-${index}`}
+                    >
+                      <div className={this.decorateCSS("content-div")}>
+                        {item.image && (
+                          <div className={this.decorateCSS("img-div")}>
+                            <a href={item.url} target="_blank" rel="noopener noreferrer">
+                              <img
+                                alt={item.image_title || item.image_subtitle}
+                                src={item.image}
+                                className={this.decorateCSS("img")}
+                              />
+                            </a>
+                          </div>
                         )}
-                        {title && subtitle && (
-                            <hr className={this.decorateCSS("faint-line")} />
+                        {(item.image_subtitle || item.image_title) && (
+                          <div className={this.decorateCSS("item-page")}>
+                            {item.image_subtitle && (
+                              <h3 className={this.decorateCSS("first-item")}>
+                                {item.image_subtitle}
+                              </h3>
+                            )}
+                            {item.image_title && (
+                              <h2 className={this.decorateCSS("item-title")}>
+                                {item.image_title}
+                              </h2>
+                            )}
+                          </div>
                         )}
-                        {subtitle && (
-                            <h2 className={this.decorateCSS("subtitle")}>
-                                {subtitle}
-                            </h2>
-                        )}
-                    </header>
-                )}
-                <main className={this.decorateCSS("wrapper")}>
-                    {items.length > 0 && (
-                        <div className={this.decorateCSS("slider-parent")}>
-                            <Slider {...settings} className={this.decorateCSS("carousel")}>
-                                {items.map((item: Card, index: number) => (
-                                    <article
-                                        className={`${this.decorateCSS("slider-inner-div")} ${this.getComponentState("prevSlide") === index ? this.decorateCSS("prevSlide") : ""} ${this.getComponentState("nextSlide") === index ? this.decorateCSS("nextSlide") : ""}`}
-                                        key={`sld-16-${index}`}
-                                    >
-                                        <div className={this.decorateCSS("content-div")}>
-                                            {item.image && (
-                                                <div className={this.decorateCSS("img-div")}>
-                                                    <a href={item.url} target="_blank" rel="noopener noreferrer">
-                                                        <img
-                                                            alt={item.image_title || item.image_subtitle}
-                                                            src={item.image}
-                                                            className={this.decorateCSS("img")}
-                                                        />
-                                                    </a>
-                                                </div>
-                                            )}
-                                            {(item.image_subtitle || item.image_title) && (
-                                                <div className={this.decorateCSS("item-page")}>
-                                                    {item.image_subtitle && (
-                                                        <h3 className={this.decorateCSS("first-item")}>
-                                                            {item.image_subtitle}
-                                                        </h3>
-                                                    )}
-                                                    {item.image_title && (
-                                                        <h2 className={this.decorateCSS("item-title")}>
-                                                            {item.image_title}
-                                                        </h2>
-                                                    )}
-                                                </div>
-                                            )}
-                                        </div>
-                                    </article>
-                                ))}
-                            </Slider>
-                        </div>
-                    )}
-                </main>
-            </div>
+                      </div>
+                    </article>
+                  ))}
+                </Slider>
+              </div>
+            )}
+          </main>
         </div>
+      </div>
     );
+  }
 }
-}
-
 
 export default Content16;
