cc5ebbb8dd4476a5aeb0cac40309618b4f0537cd
emirhandumann <emirdumanhan@gmail.com>
2024-07-29 10:39:32 +0300

    fix: update header32

--- editor-components/header/header32/header32.tsx
+++ editor-components/header/header32/header32.tsx
@@ -6,23 +6,18 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from "./header32.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
-import { Settings } from "react-slick";
-import { ReactNode } from "react";
 
 type Card = {
   backgroundImage: string;
-  imageTitle: string;
-  imageDescription: string;
-  urlTitle: string;
+  imageTitle: JSX.Element;
+  imageDescription: JSX.Element;
+  urlTitle: JSX.Element;
   url: string;
-}
-
-type Button = {
-  leftIcon: string;
-  rightIcon: string;
 };
 
-type AppendDotsFunction = NonNullable<Settings["appendDots"]>;
+type Button = {
+  buttonIcon: string;
+};
 
 class Header32 extends BaseHeader {
   constructor(props?: any) {
@@ -42,7 +37,8 @@ class Header32 extends BaseHeader {
               type: "image",
               key: "backgroundImage",
               displayer: "Background Image",
-              value: "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/joel-filipe-PkE6yQwE-6Y-unsplash.jpg",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a383502f8a5b002ce6aa53?alt=media",
             },
             {
               type: "string",
@@ -54,7 +50,8 @@ class Header32 extends BaseHeader {
               type: "string",
               key: "imageDescription",
               displayer: "Description",
-              value: "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
+              value:
+                "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
             },
             {
               type: "string",
@@ -67,8 +64,8 @@ class Header32 extends BaseHeader {
               key: "url",
               displayer: "URL",
               value: "",
-            }
-          ]
+            },
+          ],
         },
         {
           type: "object",
@@ -79,7 +76,8 @@ class Header32 extends BaseHeader {
               type: "image",
               key: "backgroundImage",
               displayer: "Background Image",
-              value: "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/daniel-stone-A4TtfFo_qRw-unsplash.jpg",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a383962f8a5b002ce6aa92?alt=media",
             },
             {
               type: "string",
@@ -91,7 +89,8 @@ class Header32 extends BaseHeader {
               type: "string",
               key: "imageDescription",
               displayer: "Description",
-              value: "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
+              value:
+                "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
             },
             {
               type: "string",
@@ -104,8 +103,8 @@ class Header32 extends BaseHeader {
               key: "url",
               displayer: "URL",
               value: "",
-            }
-          ]
+            },
+          ],
         },
         {
           type: "object",
@@ -116,7 +115,8 @@ class Header32 extends BaseHeader {
               type: "image",
               key: "backgroundImage",
               displayer: "Background Image",
-              value: "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/simone-hutsch-D7nSqgT-_GE-unsplash.jpg",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a383c62f8a5b002ce6aac7?alt=media",
             },
             {
               type: "string",
@@ -128,7 +128,8 @@ class Header32 extends BaseHeader {
               type: "string",
               key: "imageDescription",
               displayer: "Description",
-              value: "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
+              value:
+                "Architects can conduct site analysis and evaluation to determine the best location for a building or development project.",
             },
             {
               type: "string",
@@ -141,8 +142,8 @@ class Header32 extends BaseHeader {
               key: "url",
               displayer: "URL",
               value: "",
-            }
-          ]
+            },
+          ],
         },
         {
           type: "object",
@@ -153,7 +154,8 @@ class Header32 extends BaseHeader {
               type: "image",
               key: "backgroundImage",
               displayer: "Background Image",
-              value: "https://themes.pixelwars.org/archy/demo-01/wp-content/uploads/sites/2/2023/05/low-angle-red-geometrical-structure-blue-sky.jpg",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a383fd2f8a5b002ce6aae3?alt=media",
             },
             {
               type: "string",
@@ -165,7 +167,8 @@ class Header32 extends BaseHeader {
               type: "string",
               key: "imageDescription",
               displayer: "Description",
-              value: "Architects offer design and planning services for buildings, landscapes, and interiors.",
+              value:
+                "Architects offer design and planning services for buildings, landscapes, and interiors.",
             },
             {
               type: "string",
@@ -178,39 +181,30 @@ class Header32 extends BaseHeader {
               key: "url",
               displayer: "URL",
               value: "",
-            }
-          ]
-        },
-
-      ],
-    })
-
-    this.addProp({
-      type: "object",
-      key: "leftButton",
-      displayer: "Left Button",
-      value: [
-        {
-          type: "icon",
-          key: "icon",
-          displayer: "Button Icon",
-          value: "FaArrowLeftLong",
+            },
+          ],
         },
       ],
     });
+    this.addProp({
+      type: "boolean",
+      key: "enableOverlay",
+      displayer: "Enable Overlay",
+      value: true,
+    });
 
     this.addProp({
-      type: "object",
-      key: "rightButton",
-      displayer: "Right Button",
-      value: [
-        {
-          type: "icon",
-          key: "icon",
-          displayer: "Button Icon",
-          value: "FaArrowRightLong",
-        },
-      ],
+      type: "icon",
+      key: "leftButtonIcon",
+      displayer: "Left Button",
+      value: "FaArrowLeftLong",
+    });
+
+    this.addProp({
+      type: "icon",
+      key: "rightButtonIcon",
+      displayer: "Left Button",
+      value: "FaArrowRightLong",
     });
 
     this.setComponentState("slider-ref", React.createRef());
@@ -221,14 +215,13 @@ class Header32 extends BaseHeader {
   }
 
   render() {
-
     const settings = {
       arrows: false,
       dots: true,
       infinite: true,
-      speed: 2000,
+      speed: 1500,
       autoplay: true,
-      autoplaySpeed: 500,
+      autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
       dotsClass: this.decorateCSS("dots"),
@@ -242,6 +235,10 @@ class Header32 extends BaseHeader {
       },
     };
 
+    const hasleftButtonIcon = this.getPropValue("leftButtonIcon");
+    const hasrightButtonIcon = this.getPropValue("rightButtonIcon");
+    const enableOverlay = this.getPropValue("enableOverlay");
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
@@ -252,80 +249,124 @@ class Header32 extends BaseHeader {
                 className={this.decorateCSS("carousel")}
                 ref={this.getComponentState("slider-ref")}
               >
-                {this.castToObject<Card[]>("header").map((item: Card, index: number) => {
-                  return (
-                    <div
-                      className={this.decorateCSS("slide-inner")}
-                      key={`hdr-32-${index}`}
-                    >
+                {this.castToObject<Card[]>("header").map(
+                  (item: Card, index: number) => {
+                    return (
                       <div
-                        className={this.decorateCSS("content")}
-                        style={{ backgroundImage: `url(${item.backgroundImage})` }}
+                        className={this.decorateCSS("slide-inner")}
+                        key={`hdr-32-${index}`}
                       >
-                        <div className={this.decorateCSS("content-inner")}>
-                          <div className={this.decorateCSS("slide-shape")}>
-                          </div>
+                        <div
+                          className={this.decorateCSS("content")}
+                          style={{
+                            backgroundImage: `url(${item.backgroundImage})`,
+                          }}
+                        >
+                          <div className={this.decorateCSS("content-inner")}>
+                            {enableOverlay && (
+                              <div
+                                className={this.decorateCSS("slide-shape")}
+                              ></div>
+                            )}
 
-                          <h2 className={this.decorateCSS("image-title")}>
-                            {item.imageTitle}
-                          </h2>
+                            {this.castToString(item.imageTitle) && (
+                              <h2 className={this.decorateCSS("image-title")}>
+                                {item.imageTitle}
+                              </h2>
+                            )}
 
-                          <div className={this.decorateCSS("image-details")}>
-                            <p className={this.decorateCSS("description")}>
-                              {item.imageDescription}
-                            </p>
-
-                            <div className={this.decorateCSS("stick")}></div>
-
-                            <div className={this.decorateCSS("url-container")}>
-                              <ComposerLink
-                                key={`hdr-32-${index}`}
-                                path={item.url}
+                            {(this.castToString(item.imageDescription) ||
+                              this.castToString(item.urlTitle)) && (
+                              <div
+                                className={this.decorateCSS("image-details")}
                               >
-                                <p className={this.decorateCSS("url-title")}>
-                                  {item.urlTitle}
-                                </p>
-                              </ComposerLink>
-                            </div>
+                                {this.castToString(item.imageDescription) && (
+                                  <p
+                                    className={this.decorateCSS("description")}
+                                  >
+                                    {item.imageDescription}
+                                  </p>
+                                )}
 
-                          </div>
+                                <div
+                                  className={this.decorateCSS("stick")}
+                                ></div>
 
-                          <div className={this.decorateCSS("nav-buttons")}>
-                            <button
-                              className={this.decorateCSS("nav-button")}
-                              onClick={() => {
-                                this.getComponentState("slider-ref").current.slickPrev();
-                              }}
-                            >
-                              <ComposerIcon
-                                name={this.getPropValue("leftButton")[0].value}
-                                propsIcon={{
-                                  className: `${this.decorateCSS("Icon")}`,
-                                  size: 20,
-                                }}
-                              />
-                            </button>
-                            <button
-                              className={this.decorateCSS("nav-button")}
-                              onClick={() => {
-                                this.getComponentState("slider-ref").current.slickNext();
-                              }}
-                            >
-                              <ComposerIcon
-                                name={this.getPropValue("rightButton")[0].value}
-                                propsIcon={{
-                                  className: `${this.decorateCSS("Icon")}`,
-                                  size: 20,
-                                }}
-                              />
-                            </button>
+                                {this.castToString(item.urlTitle) && (
+                                  <div
+                                    className={this.decorateCSS(
+                                      "url-container"
+                                    )}
+                                  >
+                                    <ComposerLink
+                                      key={`hdr-32-${index}`}
+                                      path={item.url}
+                                    >
+                                      <p
+                                        className={this.decorateCSS(
+                                          "url-title"
+                                        )}
+                                      >
+                                        {item.urlTitle}
+                                      </p>
+                                    </ComposerLink>
+                                  </div>
+                                )}
+                              </div>
+                            )}
+
+                            {(hasleftButtonIcon || hasrightButtonIcon) && (
+                              <div className={this.decorateCSS("nav-buttons")}>
+                                {hasleftButtonIcon && (
+                                  <button
+                                    className={this.decorateCSS("nav-button")}
+                                    onClick={() => {
+                                      this.getComponentState(
+                                        "slider-ref"
+                                      ).current.slickPrev();
+                                    }}
+                                  >
+                                    <ComposerIcon
+                                      name={this.getPropValue("leftButtonIcon")}
+                                      propsIcon={{
+                                        className: `${this.decorateCSS(
+                                          "Icon"
+                                        )}`,
+                                        size: 20,
+                                      }}
+                                    />
+                                  </button>
+                                )}
+                                {hasrightButtonIcon && (
+                                  <button
+                                    className={this.decorateCSS("nav-button")}
+                                    onClick={() => {
+                                      this.getComponentState(
+                                        "slider-ref"
+                                      ).current.slickNext();
+                                    }}
+                                  >
+                                    <ComposerIcon
+                                      name={this.getPropValue(
+                                        "rightButtonIcon"
+                                      )}
+                                      propsIcon={{
+                                        className: `${this.decorateCSS(
+                                          "Icon"
+                                        )}`,
+                                        size: 20,
+                                      }}
+                                    />
+                                  </button>
+                                )}
+                              </div>
+                            )}
                           </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
-
+                    );
+                  }
+                )}
               </Slider>
             </div>
           </div>
@@ -334,6 +375,5 @@ class Header32 extends BaseHeader {
     );
   }
 }
-
 
 export default Header32;
