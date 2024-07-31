67a77b75064701583c23103e1e00c580da542997
Furkan <kavrukfurkanumut@gmail.com>
2024-07-31 17:32:49 +0300

    feat: content 8

--- editor-components/content/content8/content8.tsx
+++ editor-components/content/content8/content8.tsx
@@ -3,20 +3,31 @@ import { BaseContent } from "../../EditorComponent";
 import styles from "./content8.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
+type Heading = {
+  badge: JSX.Element;
+  title: JSX.Element;
+}
 class Content8 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
-      type: "string",
-      key: "badge",
-      displayer: "Badge",
-      value: "Discover AI-Enhanced Visual Stories",
-    });
-    this.addProp({
-      type: "string",
-      key: "title",
-      displayer: "Title",
-      value: "The Latest Updates News & Blog",
+      type: "object",
+      key: "heading",
+      displayer: "Heading",
+      value: [
+        {
+          type: "string",
+          key: "badge",
+          displayer: "Badge",
+          value: "Discover AI-Enhanced Visual Stories",
+        },
+        {
+          type: "string",
+          key: "title",
+          displayer: "Title",
+          value: "The Latest Updates News & Blog",
+        },
+      ]
     });
     this.addProp({
       type: "array",
@@ -166,16 +177,24 @@ class Content8 extends BaseContent {
     return "Content 8";
   }
 
-  //deneme
-
   render() {
+    const heading = this.castToObject<Heading>("heading");
+    const isTitleExist = this.castToString(heading.title);
+    const isbadgeExist = this.castToString(heading.badge);
+
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          
+
           <div className={this.decorateCSS("up-page")}>
-            <div className={this.decorateCSS("badge")}>{this.getPropValue("badge")}</div>
-            <div className={this.decorateCSS("title")}>{this.getPropValue("title")}</div>
+            {
+              isbadgeExist && (
+                <div className={this.decorateCSS("badge")}>{heading.badge}</div>
+              )}
+            {
+              isTitleExist && (
+                <div className={this.decorateCSS("title")}>{heading.title}</div>
+              )}
           </div>
           <div className={this.decorateCSS("cards-box")} >
 
@@ -186,8 +205,11 @@ class Content8 extends BaseContent {
                 <div className={this.decorateCSS("card-div")}>
                   <div className={this.decorateCSS("badge")}>{card.getPropValue("badge")}</div>
                   <div className={this.decorateCSS("image-div")}>
-                    <img src={card.getPropValue("image")} alt={card.getPropValue("image")}
-                      className={this.decorateCSS("image")} />
+                    {card.image && <div className={this.decorateCSS("image-box")}>
+                      <img
+                        className={this.decorateCSS("image")}
+                        src={card.image} alt="" />
+                    </div>}
                   </div>
                   <div className={this.decorateCSS("card-body")}>
                     <div className={this.decorateCSS("description")}>{card.getPropValue("description")}</div>
