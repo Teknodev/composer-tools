15a287206c423e457e1fdfcdea3ed0a87bf0a64d
Mehmet Can Özen <omehmetcan785@gmail.com>
2024-07-31 14:19:33 +0300

    Reverted back the CallToAction7 files.

--- editor-components/call_to_action/call_to_action7/call_to_action7.tsx
+++ editor-components/call_to_action/call_to_action7/call_to_action7.tsx
@@ -1,54 +1,83 @@
 import * as React from "react";
-import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseCallToAction } from "../../EditorComponent";
 import styles from "./call_to_action7.module.scss";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 class CallToAction7Page extends BaseCallToAction {
   constructor(props?: any) {
     super(props, styles);
-
     this.addProp({
       type: "string",
       key: "title",
       displayer: "Title",
-      value: "Level up your portfolio, read the book today!",
-    });
-    this.addProp({
-      type: "image",
-      key: "image",
-      displayer: "Image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6687a7beba6bbe002b63bb11?alt=media",
-    });
-    this.addProp({
-      type: "object",
-      key: "button",
-      displayer: "Button",
-      value: [
-        {
-          type: "string",
-          key: "buttonText",
-          displayer: "Button Text",
-          value: "LOOK MORE",
-        },
-        {
-          type: "page",
-          key: "link",
-          displayer: "Link",
-          value: "",
-        }
-      ]
+      value: "Start Living Your Best Life Today",
     });
     this.addProp({
       type: "string",
-      key: "placeholder",
-      displayer: "Placeholder",
-      value: "Enter your E-Mail!",
+      key: "description",
+      displayer: "Description",
+      value:
+        "Starting to live your best life today means making the most of each day and being present in the moment. It means setting goals, pursuing your passions, and taking action towards the life you want to live. Don't wait for the perfect moment, because it may never come. Take risks, step outside of your comfort zone, and embrace new experiences. ",
     });
+    // this.addProp({
+    //   type: "color",
+    //   key: "cardBackgroundColor",
+    //   displayer: "Card Background Color",
+    //   value: "#e0e0e0",
+    // });
+
     this.addProp({
-      type: "boolean",
-      key: "disableAnimation",
-      displayer: "Disable Animation",
-      value: false,
+      type: "array",
+      key: "item-list",
+      displayer: "Card List",
+      value: [
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "string",
+              key: "Title",
+              displayer: "Title",
+              value: "About Life",
+            },
+            {
+              type: "string",
+              key: "Description",
+              displayer: "Description",
+              value:
+                "Life is a journey full of ups and downs, joys and sorrows, victories and defeats. It's a precious gift that should be cherished and lived to the fullest. Life is unpredictable and can change in an instant, so it's important to appreciate the present moment and make the most of every opportunity. ",
+            },
+            {
+              type: "string",
+              key: "ButtonText",
+              displayer: "Button Text",
+              value: "Reading",
+            },
+            {
+              type: "page",
+              key: "ButtonPage",
+              displayer: "Button Link",
+              value: "https://www.google.com/",
+            },
+            {
+              type: "image",
+              key: "ButtonIcon",
+              displayer: "Button Icon",
+              value:
+                "https://freepngimg.com/thumb/web_design/24713-2-right-arrow-transparent-background.png",
+            },
+            {
+              type: "image",
+              key: "card-icon",
+              displayer: "Card Icon",
+              value:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Globe_icon-white.svg/1200px-Globe_icon-white.svg.png",
+            },
+          ],
+        },
+      ],
     });
   }
 
@@ -57,45 +86,54 @@ class CallToAction7Page extends BaseCallToAction {
   }
 
   render() {
-    const title = this.getPropValue("title");
-    const image = this.getPropValue("image");
-    const button = this.getPropValue("button");
-    const placeholder = this.getPropValue("placeholder", { as_string: true });
-    const disableAnimation = this.getPropValue("disableAnimation");
-
+    const styling = {
+      backgroundColor: this.getPropValue("cardBackgroundColor"),
+    };
     return (
-      <div className={`${this.decorateCSS("container")}`}>
+      <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <section className={`${this.decorateCSS("call-to-action7-container")} ${disableAnimation ? this.decorateCSS("no-animation") : ""}`}>
-            {image && (
-              <img className={this.decorateCSS("image")} src={image} alt="" />
-            )}
-            {(title) && (
-              <div className={this.decorateCSS("call-to-action7")}>
-                {title && (
-                  <h1 className={this.decorateCSS("title")}>{title}</h1>
-                )}
-                <div className={this.decorateCSS("input-button-wrapper")}>
-                  <input
-                    className={this.decorateCSS("input")}
-                    type="text"
-                    id="email"
-                    name="email"
-                    placeholder={placeholder}
-                  />
-                  {button[0].value && (
-                    <div className={this.decorateCSS("button-container")}>
-                      <ComposerLink path={button[1].value}>
-                        <span className={this.decorateCSS("button")}>
-                          {button[0].value}
-                        </span>
-                      </ComposerLink>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-          </section>
+          <div className={this.decorateCSS("call-to-action7-page")}>
+            <div className={this.decorateCSS("left-child")}>
+              <h1 className={this.decorateCSS("title")}>
+                {this.getPropValue("title")}
+              </h1>
+              <h3 className={this.decorateCSS("description")}>
+                {this.getPropValue("description")}
+              </h3>
+            </div>
+            <div className={this.decorateCSS("card-child")} style={styling}>
+              {this.getPropValue("item-list").map(
+                (item: any, index: number) => (
+                  <div>
+                    <img
+                      className={this.decorateCSS("image")}
+                      src={item.value[5].value}
+                      width={35}
+                      height={35}
+                      alt=""
+                    />
+                    <h1 className={this.decorateCSS("title-2")}>
+                      {item.value[0].value}
+                    </h1>
+                    <h3 className={this.decorateCSS("leftDescription")}>
+                      {item.value[1].value}
+                    </h3>
+                    <ComposerLink path={item.value[3].value}>
+                      <span className={this.decorateCSS("left-button-text")}>
+                        {item.value[2].value}
+                        <img
+                          alt=""
+                          src={item.value[4].value}
+                          width={15}
+                          height={15}
+                        />
+                      </span>
+                    </ComposerLink>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
         </div>
       </div>
     );
