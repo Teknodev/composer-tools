fbb408270b949fbc5d5008a4622b040ef8ad21b9
FurkanKavruk <kavrukfurkanumut@gmail.com>
2024-08-05 18:58:05 +0300

    feat: footer 11

--- editor-components/footer/footer11/footer11.tsx
+++ editor-components/footer/footer11/footer11.tsx
@@ -17,7 +17,7 @@ class Footer11Page extends BaseFooter {
       type: "string",
       key: "ref-text",
       displayer: "Referral Text",
-      value: "YELİZ SARIOGLU"
+      value: "Adam Smith"
     });
 
     this.addProp({
