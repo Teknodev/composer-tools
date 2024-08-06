d1baecbe4687f4548567429280fd1ed2e2049746
Dogukan Celik <dceliknodes@gmail.com>
2024-08-06 16:21:50 +0300

    hotfix: change reqs

--- editor-components/list/list3/list3.tsx
+++ editor-components/list/list3/list3.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ReactNode } from "react";
 import { BaseList } from "../../EditorComponent";
 import styles from "./list3.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
@@ -154,9 +153,12 @@ class List3 extends BaseList {
     });
   }
 
-  render(): ReactNode {
+  render() {
     const title = this.castToString(this.getPropValue("title"));
     const description = this.castToString(this.getPropValue("description"));
+
+    const listItems = this.castToObject<Item[]>("listItems");
+    const buttonExist = this.castToString(this.getPropValue("button"));
 
     return (
       <div className={this.decorateCSS("container")}>
@@ -174,15 +176,15 @@ class List3 extends BaseList {
                 <div className={this.decorateCSS("firstContent")}>
                   {title && (
                     <div className={this.decorateCSS("title")}>
-                      <h1>{title}</h1>
+                      <h1>{this.getPropValue("title")}</h1>
                     </div>
                   )}
                   {description && (
                     <div className={this.decorateCSS("description")}>
-                      {description}
+                      <p>{this.getPropValue("description")}</p>
                     </div>
                   )}
-                  {this.castToString(this.getPropValue("button")) && (
+                  {buttonExist && (
                     <div className={this.decorateCSS("buttondiv")}>
                       <ComposerLink path={this.getPropValue("buttonUrl")}>
                         <button className={`${this.decorateCSS("button")}`}>
@@ -194,13 +196,13 @@ class List3 extends BaseList {
                 </div>
               </div>
             )}
-            {this.castToObject<Item[]>("listItems").map(
-              (listItem: Item, index: number) => {
-                const itemTitle = this.castToString(listItem.itemTitle);
-                const itemText1= this.castToString(listItem.itemText1);
-                const itemText2= this.castToString(listItem.itemText2);
-                const itemText3= this.castToString(listItem.itemText3);
-                return(
+            {listItems.map((listItem: Item, index: number) => {
+              const itemTitle = this.castToString(listItem.itemTitle);
+              const itemText1 = this.castToString(listItem.itemText1);
+              const itemText2 = this.castToString(listItem.itemText2);
+              const itemText3 = this.castToString(listItem.itemText3);
+
+              return (
                 <div key={index} className={this.decorateCSS("card")}>
                   <div className={this.decorateCSS("card-content")}>
                     {itemTitle && (
@@ -213,26 +215,29 @@ class List3 extends BaseList {
                         {listItem.itemText1}
                       </p>
                     )}
-                    {itemText1 && (<span className={this.decorateCSS("spanItem")}></span>)}
+                    {itemText2 && (
+                      <span className={this.decorateCSS("spanItem")}></span>
+                    )}
                     {itemText2 && (
                       <p className={this.decorateCSS("itemText")}>
                         {listItem.itemText2}
                       </p>
                     )}
-                    {itemText2 && (<span className={this.decorateCSS("spanItem")}></span>)}
+                    {itemText3 && (
+                      <span className={this.decorateCSS("spanItem")}></span>
+                    )}
                     {itemText3 && (
                       <p className={this.decorateCSS("itemText")}>
                         {listItem.itemText3}
                       </p>
                     )}
-                  </div>  
-                    <h3 className={this.decorateCSS("index")}>
-                      {index < 9 ? `0${index + 1}` : index + 1}
-                    </h3>                                      
+                  </div>
+                  <h3 className={this.decorateCSS("index")}>
+                    {index < 9 ? `0${index + 1}` : index + 1}
+                  </h3>
                 </div>
-                );
-              }
-            )}
+              );
+            })}
           </div>
         </div>
       </div>
