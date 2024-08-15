8010156a30125568c04ee12e3b982f612a0a4ad0
dogusmiuw <dceliknodes@gmail.com>
2024-08-15 15:04:18 +0300

    fix: change reqs

--- editor-components/feature/feature6/feature6.tsx
+++ editor-components/feature/feature6/feature6.tsx
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { BaseFeature, TypeUsableComponentProps } from "../../EditorComponent";
+import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature6.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
-
 type Card = {
+  title: JSX.Element;
   image: string;
-  title: string;
+  overlay: boolean;
+  link: string;
 };
 
 type Button = {
-  icon: string;
-  url: string;
-}
-
-type Buttons = {
-  buttonText: string;
-  url: string;
-}
+  text: JSX.Element;
+  link: string;
+};
 
 class Feature6 extends BaseFeature {
   constructor(props?: any) {
@@ -25,18 +21,24 @@ class Feature6 extends BaseFeature {
 
     this.addProp({
       type: "string",
-      key: "header",
-      displayer: "Header",
-      value: "CASE STUDIES"
-    })
-
+      key: "title",
+      displayer: "Title",
+      value: "CASE STUDIES",
+    });
 
     this.addProp({
       type: "string",
-      key: "subHeader",
-      displayer: "Sub Header",
-      value: "from vision to reality"
-    })
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: "from vision to reality",
+    });
+
+    this.addProp({
+      type: "boolean",
+      key: "overlay",
+      displayer: "Overlay",
+      value: true,
+    });
 
     this.addProp({
       type: "array",
@@ -61,6 +63,12 @@ class Feature6 extends BaseFeature {
               displayer: "Title",
               value: "The Lakeside Redesign: a Rustic House for Modern Living",
             },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Link",
+              value: "",
+            },
           ],
         },
         {
@@ -81,6 +89,12 @@ class Feature6 extends BaseFeature {
               displayer: "Title",
               value: "Minimalist Magic: A Contemporary Studio Apartment",
             },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Link",
+              value: "",
+            },
           ],
         },
         {
@@ -93,13 +107,19 @@ class Feature6 extends BaseFeature {
               key: "image",
               displayer: "Image",
               value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/668562330181a1002c33af23?alt=media"
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/668562330181a1002c33af23?alt=media",
             },
             {
               type: "string",
               key: "title",
               displayer: "Title",
               value: "The Heritage Home: Restoring Charm in a Victorian House",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Link",
+              value: "",
             },
           ],
         },
@@ -112,35 +132,35 @@ class Feature6 extends BaseFeature {
       value: 3,
       max: 3,
     });
-    
-    let button: TypeUsableComponentProps = {
-      type: "object",
-      key: "button",
-      displayer: "Button",
-      value: [
-        {
-          type: "string",
-          key: "buttonText",
-          displayer: "Button Text",
-          value: "VIEW ALL CASE STUDIES",
-        },
-        {
-          type: "page",
-          key: "url",
-          displayer: "Button Link",
-          value: "",
-        },
-      ],
-    };
 
     this.addProp({
       type: "array",
       key: "buttons",
       displayer: "Buttons",
-      value: [JSON.parse(JSON.stringify(button))],
+      value: [
+        {
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "text",
+              displayer: "Button Text",
+              value: "VIEW ALL CASE STUDIES",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Button Link",
+              value: "",
+            },
+          ],
+        },
+      ],
       additionalParams: {
-        maxElementCount:  2
-      }
+        maxElementCount: 2,
+      },
     });
   }
 
@@ -149,45 +169,95 @@ class Feature6 extends BaseFeature {
   }
 
   render() {
+    const cards = this.castToObject<Card[]>("cards");
+    const buttons = this.castToObject<Button[]>("buttons");
+
+    const titleExist = this.getPropValue("title", { as_string: true });
+    const subtitleExist = this.getPropValue("subtitle", { as_string: true });
+
+    const overlay = this.getPropValue("overlay");
+
     return (
       <div className={this.decorateCSS("container")}>
-    <div className={this.decorateCSS("max-content")}>
-        <div className={this.decorateCSS("content")}>
-            <div className={this.decorateCSS("header-title")}>{this.getPropValue("header")}</div>
-            <div className={this.decorateCSS("header-subtitle")}>{this.getPropValue("subHeader")}</div>
+        <div className={this.decorateCSS("max-content")}>
+          <div className={this.decorateCSS("content")}>
+            {titleExist && (
+              <div className={this.decorateCSS("header-title")}>
+                {this.getPropValue("title")}
+              </div>
+            )}
+            {subtitleExist && (
+              <div className={this.decorateCSS("header-subtitle")}>
+                {this.getPropValue("subtitle")}
+              </div>
+            )}
 
-            {this.castToObject<Card[]>("cards").map((card: Card, indexCards: number) => (
-                <div className={this.decorateCSS("card-item-count")} style={{
-                    width: 90 / this.getPropValue("itemCount") + "%",
-                }}>
-                    <div key={indexCards} className={this.decorateCSS("listed")}>
-                        <img className={this.decorateCSS("image")} src={card.image} alt=""></img>
-                        <div className={this.decorateCSS("image-shadow")}>
-                            <p className={this.decorateCSS("title")}>{card.title}</p>
-                        </div>
-                    </div>
-                </div>
-            ))}
+            {cards?.length > 0 && (
+              <div className={this.decorateCSS("cards-container")}>
+                {cards.map((card: Card, idx: number) => {
+                  const titleExist = this.castToString(card.title);
 
-        <div className={this.decorateCSS("button-position")}>
-              {this.castToObject<Buttons[]>("buttons").map(
-                (item: Buttons, index: number) => {
                   return (
-                    <ComposerLink path={item.url} key={index}>
-                      <div className={this.decorateCSS("button")}>
-                        {item.buttonText}
-                      </div>
-                    </ComposerLink>
+                    <div
+                      className={this.decorateCSS("card-item-count")}
+                      style={{
+                        width: 90 / this.getPropValue("itemCount") + "%",
+                      }}
+                      key={idx}
+                    >
+                      <ComposerLink isFullWidth={true} path={card.link || ""}>
+                        {(card.image || titleExist) && (
+                          <div
+                            style={!card.image ? { height: "300px" } : {}}
+                            className={this.decorateCSS("listed")}
+                          >
+                            {card.image && (
+                              <img
+                                className={this.decorateCSS("image")}
+                                src={card.image}
+                                alt="feature"
+                              />
+                            )}
+                            <div
+                              style={!card.image ? { height: "100%" } : {}}
+                              className={`
+                                ${this.decorateCSS("image-shadow")}
+                                ${overlay ? this.decorateCSS("overlay") : ""}
+                              `}
+                            >
+                              {titleExist && (
+                                <h4 className={this.decorateCSS("title")}>
+                                  {card.title}
+                                </h4>
+                              )}
+                            </div>
+                          </div>
+                        )}
+                      </ComposerLink>
+                    </div>
                   );
                 })}
+              </div>
+            )}
 
-            </div>
-
-            
+            {buttons?.length > 0 && (
+              <div className={this.decorateCSS("button-position")}>
+                {buttons.map((item: Button, idx: number) => {
+                  if (this.castToString(item.text))
+                    return (
+                      <ComposerLink path={item.link} key={idx}>
+                        <div className={this.decorateCSS("button")}>
+                          {item.text}
+                        </div>
+                      </ComposerLink>
+                    );
+                  return null;
+                })}
+              </div>
+            )}
+          </div>
         </div>
-    </div>
-</div>
-
+      </div>
     );
   }
 }
