66e3c5417fe9d1355e3a42192ded19dab8160cfb
Dogan <dogan.sam4@gmail.com>
2024-08-15 17:32:51 +0300

    calltoaction6 fix

--- editor-components/call_to_action/call_to_action6/call_to_action6.tsx
+++ editor-components/call_to_action/call_to_action6/call_to_action6.tsx
@@ -3,6 +3,13 @@ import ComposerLink from "../../../../custom-hooks/composer-base-components/Link
 import { BaseCallToAction } from "../../EditorComponent";
 import styles from "./call_to_action6.module.scss";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { array } from "yup";
+
+type ButtonItem = {
+  buttonText: JSX.Element;
+  link: string;
+  icon: string;
+}
 
 class CallToAction6Page extends BaseCallToAction {
   constructor(props?: any) {
@@ -13,7 +20,7 @@ class CallToAction6Page extends BaseCallToAction {
       key: "background_image",
       displayer: "Background Image",
       value:
-        "https://c4.wallpaperflare.com/wallpaper/869/24/987/hand-writing-book-man-wallpaper-preview.jpg",
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66bdaa2707399d002cb4130f?alt=media",
     });
 
     this.addProp({
@@ -38,38 +45,40 @@ class CallToAction6Page extends BaseCallToAction {
     });
 
     this.addProp({
-      type: "object",
-      key: "button",
-      displayer: "Button",
+      type: "array",
+      key: "arrayItem",
+      displayer: "Button Array",
       value: [
         {
-          type: "string",
-          key: "button_text",
-          displayer: "Button Text",
-          value: "Subscribe",
-        },
-        {
-          type: "page",
-          key: "link",
-          displayer: "Button Link",
-          value: "https://www.google.com/",
+          type: "object",
+          key: "button",
+          displayer: "Button",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Subscribe",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "IoIosArrowRoundForward",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Button Link",
+              value: "",
+            },
+          ],
         },
       ],
+
     });
 
-    this.addProp({
-      type: "icon",
-      key: "icon",
-      displayer: "Icon",
-      value: "IoIosArrowRoundForward",
-    });
 
-    this.addProp({
-      type: "object",
-      key: "space",
-      displayer: "Empty Space",
-      value: [],
-    });
   }
 
   getName(): string {
@@ -80,16 +89,7 @@ class CallToAction6Page extends BaseCallToAction {
     const title = this.getPropValue("title", { as_string: true });
     const placeholder = this.getPropValue("placeholder", { as_string: true });
     const comment = this.getPropValue("comment", { as_string: true });
-
-    const button = this.castToObject<{
-      button_text: JSX.Element;
-      link: string;
-    }>("button");
-
-    const icon = this.getPropValue("icon");
-
-    const isButtonEmpty = icon || this.castToString(button.button_text);
-
+    const arrayItem = this.castToObject<ButtonItem[]>("arrayItem");
     return (
       <div
         className={this.decorateCSS("container")}
@@ -98,13 +98,17 @@ class CallToAction6Page extends BaseCallToAction {
         }}
       >
         <div className={this.decorateCSS("max-content")}>
+
           <div className={this.decorateCSS("call-to-action6-page")}>
             {title && (
               <h1 className={this.decorateCSS("title")}>
                 {this.getPropValue("title")}
               </h1>
             )}
-
+            {title &&
+              <div className={this.decorateCSS("Space")}>
+                <hr className={this.decorateCSS("space")} />
+              </div>}
             {placeholder && (
               <input
                 type="text"
@@ -118,31 +122,28 @@ class CallToAction6Page extends BaseCallToAction {
                 {this.getPropValue("comment")}
               </h3>
             )}
-
-            {isButtonEmpty && (
-              <ComposerLink
-                path={button.link}
-                isFullWidth={true}
-                className={this.decorateCSS("button-link")}
-              >
-                <div className={this.decorateCSS("button-container")}>
-                  <button className={this.decorateCSS("button")}>
-                    {button.button_text && (
-                      <div className={this.decorateCSS("button_text")}>
-                        {button.button_text}
+            {arrayItem?.length > 0 && (
+              <div className={this.decorateCSS("buttonS")}>
+                {arrayItem.map((item: ButtonItem, index: number) => (
+                  <ComposerLink key={index} path={item.link}>
+                    {(this.castToString(item.buttonText) || item.icon) &&
+                      <div className={this.decorateCSS("button")}>
+                        <div className={this.decorateCSS("button_text")}>
+                          {item.buttonText}
+                        </div>
+                        <div className={this.decorateCSS("icon-container")}>
+                          <ComposerIcon
+                            name={item.icon}
+                            propsIcon={{
+                              className: this.decorateCSS("icon")
+                            }}
+                          />
+                        </div>
                       </div>
-                    )}
-                    {icon && (
-                      <ComposerIcon
-                        name={icon}
-                        propsIcon={{
-                          className: this.decorateCSS("icon"),
-                        }}
-                      />
-                    )}
-                  </button>
-                </div>
-              </ComposerLink>
+                    }
+                  </ComposerLink>
+                ))}
+              </div>
             )}
           </div>
         </div>
