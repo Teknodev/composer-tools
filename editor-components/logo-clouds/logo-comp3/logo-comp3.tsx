eb855125759a15ca1472cd7c502072a9b122350c
dogusmiuw <dceliknodes@gmail.com>
2024-08-19 13:31:30 +0300

    fix: change reqs

--- editor-components/logo-clouds/logo-comp3/logo-comp3.tsx
+++ editor-components/logo-clouds/logo-comp3/logo-comp3.tsx
@@ -2,12 +2,6 @@ import * as React from "react";
 import { LogoClouds } from "../../EditorComponent";
 import styles from "./logo-comp3.module.scss";
 
-type Item = {
-  image: string;
-};
-type ISection = {
-  section: Item[];
-};
 class LogoComp3Page extends LogoClouds {
   constructor(props?: any) {
     super(props, styles);
@@ -25,7 +19,7 @@ class LogoComp3Page extends LogoClouds {
     });
     this.addProp({
       type: "number",
-      key: "itemCount",
+      key: "itemCountInRow",
       displayer: "Item Count in a Row",
       value: 4,
       max: 6,
@@ -85,6 +79,13 @@ class LogoComp3Page extends LogoClouds {
             "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d3?alt=media&timestamp=1719584962573",
           displayer: "Image",
         },
+        {
+          type: "image",
+          key: "image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64551869f72de2002caaf0d1?alt=media&timestamp=1719584962573",
+          displayer: "Image",
+        },
       ],
     });
   }
@@ -94,48 +95,64 @@ class LogoComp3Page extends LogoClouds {
   }
 
   render() {
-    const itemCount = this.getPropValue("itemCount") || 4;
-    const columnCount = Math.min(itemCount, 6);
+    const items = this.getPropValue("items");
+
+    const itemCountInRow: number = this.getPropValue("itemCountInRow");
+    const itemCount: number = items.length;
+
+    const titleExist = this.getPropValue("title", { as_string: true });
+    const subtitleExist = this.getPropValue("subtitle", { as_string: true });
+
     return (
       <div className={`${this.decorateCSS("container")}`}>
         <div className={`${this.decorateCSS("max-content")}`}>
-          <div className={`${this.decorateCSS("logo-comp3-page")}`}>
-            <div className={`${this.decorateCSS("titles")} `}>
-              <h1 className={this.decorateCSS("title")}>
-                {this.getPropValue("title")}
-              </h1>
-              <div className={this.decorateCSS("title-line")}></div>
-              <h1 className={this.decorateCSS("subtitle")}>
-                {this.getPropValue("subtitle")}
-              </h1>
-            </div>
-            <section>
-              <center>
-                <div
-                  className={this.decorateCSS("image-child")}
-                  style={{
-                    gridTemplateColumns: `repeat(${columnCount}, minmax(200px, 1fr))`,
-                  }}
-                >
-                  {this.getPropValue("items").map(
-                    (item: any, index: number) => (
+          <div className={`${this.decorateCSS("wrapper")}`}>
+            {(titleExist || subtitleExist) && (
+              <div className={`${this.decorateCSS("titles")} `}>
+                {titleExist && (
+                  <h1 className={this.decorateCSS("title")}>
+                    {this.getPropValue("title")}
+                  </h1>
+                )}
+                {titleExist && subtitleExist && (
+                  <div className={this.decorateCSS("title-line")}></div>
+                )}
+                {subtitleExist && (
+                  <h1 className={this.decorateCSS("subtitle")}>
+                    {this.getPropValue("subtitle")}
+                  </h1>
+                )}
+              </div>
+            )}
+            {items?.length > 0 && (
+              <section
+                className={this.decorateCSS("items")}
+                style={{
+                  gridTemplateColumns: `repeat(${itemCountInRow}, 1fr)`,
+                }}
+              >
+                {items.map((item: any, index: number) => {
+                  if (item.value)
+                    return (
                       <div
-                        className={this.decorateCSS(
-                          `img-container-${itemCount}`
-                        )}
+                        className={`
+                        ${this.decorateCSS("item-container")}
+                        ${this.decorateCSS(index % itemCountInRow !== itemCountInRow - 1 ? "insert-line-right" : "")}
+                        ${this.decorateCSS(index < itemCount - itemCountInRow ? "insert-line-bottom" : "")}
+                      `}
                         key={index}
                       >
                         <img
                           className={this.decorateCSS("image")}
                           src={item.value}
-                          alt=""
+                          alt="logo"
                         />
                       </div>
-                    )
-                  )}
-                </div>
-              </center>
-            </section>
+                    );
+                  return null;
+                })}
+              </section>
+            )}
           </div>
         </div>
       </div>
