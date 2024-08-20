7c6f5cc652c6161ab9652f694d46499e0cda0abf
dogusmiuw <dceliknodes@gmail.com>
2024-08-20 11:25:16 +0300

    feat: add to registerables

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -202,7 +202,8 @@ import NavbarComponent6 from "./navigators/navbar6/navbar6";
 import NavbarComponent7 from "./navigators/navbar7/navbar7";
 import NavbarComponent8 from "./navigators/navbar8/navbar8";
 import NavbarComponent9 from "./navigators/navbar9/navbar9";
-import ImageGalleryCompanent1 from "./Image_gallery/image_gallery_1/image-gallery1";
+import ImageGalleryComponent1 from "./Image_gallery/image_gallery_1/image-gallery1";
+import ImageGalleryComponent4 from "./Image_gallery/image_gallery_4/image-gallery4";
 import TermsOfUseModal from "./modal/terms-of-use/terms-of-use";
 import SurveyModal from "./modal/survey-modal/survey-modal";
 import NoticeModal from "./modal/notice-modal/notice";
@@ -338,7 +339,8 @@ function Registerables(composer: ComponentsRegistery) {
     new NavbarComponent7(),
     new NavbarComponent8(),
     new NavbarComponent9(),
-    new ImageGalleryCompanent1(),
+    new ImageGalleryComponent1(),
+    new ImageGalleryComponent4(),
     new FeatureComponent1(),
     new FeatureComponent2(),
     new FeatureComponent3(),
