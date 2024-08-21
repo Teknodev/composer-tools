323dddceffed77f4a40909ab2c65d449ebaa1958
KaanSuslu <kaan.suslu@hotmail.com>
2024-08-21 13:44:50 +0300

    ModalNotification2 change request

--- editor-components/modal/notificationmodal2/notificationmodal2.tsx
+++ editor-components/modal/notificationmodal2/notificationmodal2.tsx
@@ -15,10 +15,10 @@ class NotificationModal2 extends BaseModal {
     });
 
     this.addProp({
-      type: "string",
-      key: "exit-button",
-      displayer: "exit-button",
-      value: "X",
+      type: "icon",
+      key: "exit-icon",
+      displayer: "exit-icon",
+      value: "RxCross1",
     });
 
     this.addProp({
@@ -57,6 +57,7 @@ class NotificationModal2 extends BaseModal {
 
   render() {
     const icon = this.getPropValue("icon");
+    const exitIcon = this.getPropValue("exit-icon");
     const buttonval = this.getPropValue("button", { as_string: true });
     const emailPlaceholder = this.getPropValue("email", { as_string: true });
     const description = this.getPropValue("displayerMessage", {
@@ -66,12 +67,13 @@ class NotificationModal2 extends BaseModal {
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
-          <button
-            className={`${this.decorateCSS("button")} ${this.decorateCSS(
-              "exit-button"
-            )}`}
-          >
-            {this.getPropValue("exit-button")}
+          <button className={this.decorateCSS("exit-icon")}>
+            {exitIcon && (
+              <ComposerIcon
+                propsIcon={{ className: this.decorateCSS("Icon") }}
+                name={exitIcon}
+              />
+            )}
           </button>
           <div className={this.decorateCSS("content")}>
             {icon && (
