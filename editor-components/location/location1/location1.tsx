db3af5866a5737d132b5755016bcfd3bb2d8323d
dogusmiuw <dceliknodes@gmail.com>
2024-08-28 18:36:48 +0300

    feat: composer map component

--- editor-components/location/location1/location1.tsx
+++ editor-components/location/location1/location1.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-
+import React from 'react';
 import { Location } from '../../EditorComponent';
 import styles from './location1.module.scss';
-import { Map, Marker } from '@vis.gl/react-google-maps';
+import ComposerMap from '../../../composer-base-components/map/map';
 
 class LocationComponent1 extends Location {
   constructor(props?: any) {
@@ -36,12 +35,10 @@ class LocationComponent1 extends Location {
   }
 
   render() {
-    console.log("LOCATION PROP : ", this.getPropValue('location'))
     const { markers, zoom } = this.getPropValue('location');
 
     return (
       <div className={this.decorateCSS('container')}>
-        <h1>{JSON.stringify(markers)}</h1>
         <div className={this.decorateCSS('max-content')}>
           <div className={this.decorateCSS('wrapper')}>
             <h1 className={this.decorateCSS('title')}>{this.getPropValue('title')}</h1>
@@ -53,49 +50,6 @@ class LocationComponent1 extends Location {
       </div>
     );
   }
-}
-
-type MapProps = {
-  markers: { lat: number; lng: number; }[];
-  zoom: number;
-  className: string;
-};
-
-function ComposerMap({ markers, zoom, className }: MapProps) {
-
-
-  const getCenter = () => {
-    if(markers.length > 0) {
-      return {
-        lat: markers[0].lat,
-        lng: markers[0].lng
-      }
-    }
-
-    return {
-      lat: 36.8968908,
-      lng: 30.7133233,
-    }
-  }
-
-  return (
-    <Map
-      className={className}
-      center={getCenter()}
-      zoom={zoom}
-    >
-      {markers.map((marker, index) => (
-        <Marker
-          key={index}
-          position={{
-            lat: marker.lat,
-            lng: marker.lng
-          }}
-          title="Location"
-        />
-      ))}
-    </Map>
-  );
 }
 
 export default LocationComponent1;
