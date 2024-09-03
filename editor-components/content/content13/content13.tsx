d1880e4569ad932916683d04920c6dc93033894e
Burcu Açan <burcuuacann@gmail.com>
2024-09-03 18:54:50 +0300

    fix: reorganize page

--- editor-components/content/content13/content13.tsx
+++ editor-components/content/content13/content13.tsx
@@ -141,10 +141,9 @@ class Content13 extends BaseContent {
                   </div>}
 
                 {isImage2Exist &&
-                  <div className={this.decorateCSS("image2-container")}>
+                  <div className={isImage1Exist ? this.decorateCSS("image2-container") : this.decorateCSS("image2-container-without-image1")}>
                     <img src={this.getPropValue("image2")} alt="Image 2" className={this.decorateCSS("image2")} />
                   </div>
-
                 }
 
               </div>
