e9ab4594f90483339233a3a429341ca8845994cd
bedialp <fvzlpktk@gmail.com>
2024-09-11 18:35:10 +0300

    fix: change reqs

--- editor-components/slider/slider8/slider8.tsx
+++ editor-components/slider/slider8/slider8.tsx
@@ -229,7 +229,7 @@ class Slider8 extends BaseSlider {
       dots: true,
       infinite: true,
       speed: 1500,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
