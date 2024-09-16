cb4430738e491ddb19d1260b6f7c8e325bf377f8
“Emrecoru” <emre.coru07@hotmail.com>
2024-09-16 18:45:06 +0300

    navbar-6 change request is fixed

--- editor-components/navigators/navbar6/navbar6.tsx
+++ editor-components/navigators/navbar6/navbar6.tsx
@@ -1,5 +1,5 @@
 import { BaseNavigator } from "../../EditorComponent";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar6.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
@@ -19,38 +19,123 @@ class Navbar6 extends BaseNavigator {
     });
 
     this.addProp({
-      type: "array",
-      key: "left-items",
-      displayer: "Left Items",
-      value: [
-        {
-          type: "object",
-          key: "item",
-          displayer: "Item",
-          value: [
-            {
-              type: "string",
-              key: "label",
-              value: "ABOUT US",
-              displayer: "Text",
-            },
-            {
-              type: "page",
-              key: "link",
-              value: "",
-              displayer: "Navigate To",
-            },
-          ],
-        },
-      ],
-    });
-
-    this.addProp({
       type: "image",
       key: "center-image",
       displayer: "Center Image",
       value: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
     });
+
+    this.addProp(
+      {
+        type: "array",
+        key: "extendable",
+        displayer: "Extendable",
+        value: [
+          {
+            type: "object",
+            key: "item",
+            displayer: "Main Text",
+            value: [
+              {
+                type: "string",
+                key: "label",
+                value: "HOME",
+                displayer: "Text",
+              },
+              {
+                type: "page",
+                key: "link",
+                value: "",
+                displayer: "Navigate To",
+              },
+              {
+                type: "array",
+                key: "section",
+                displayer: "Home Section",
+                value: [
+                  {
+                    type: "object",
+                    key: "item1",
+                    displayer: "Home Section Item",
+                    value: [
+                      {
+                        type: "string",
+                        key: "text",
+                        value: "Creative 01",
+                        displayer: "Info Text 1",
+                      },
+                      {
+                        type: "page",
+                        key: "link",
+                        value: "",
+                        displayer: "Navigate To",
+                      },
+                      {
+                        type: "image",
+                        key: "image",
+                        value: "https://picsum.photos/200/200",
+                        displayer: "Image",
+                      },
+                    ],
+                  },
+                  {
+                    type: "object",
+                    key: "item2",
+                    displayer: "Home Section Item",
+                    value: [
+                      {
+                        type: "string",
+                        key: "text",
+                        value: "Creative 02",
+                        displayer: "Info Text 2",
+                      },
+                      {
+                        type: "page",
+                        key: "link",
+                        value: "",
+                        displayer: "Navigate To",
+                      },
+                      {
+                        type: "image",
+                        key: "image",
+                        value: "https://picsum.photos/200/200",
+                        displayer: "Image",
+                      },
+                    ],
+                  },
+                  {
+                    type: "object",
+                    key: "item3",
+                    displayer: "Home Section Item",
+                    value: [
+                      {
+                        type: "string",
+                        key: "text",
+                        value: "Creative 03",
+                        displayer: "Info Text 3",
+                      },
+                      {
+                        type: "page",
+                        key: "link",
+                        value: "",
+                        displayer: "Navigate To",
+                      },
+                      {
+                        type: "image",
+                        key: "image",
+                        value: "https://picsum.photos/200/200",
+                        displayer: "Image",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ]
+      }
+
+    )
 
     this.addProp({
       type: "array",
@@ -65,7 +150,45 @@ class Navbar6 extends BaseNavigator {
             {
               type: "string",
               key: "label",
-              value: "PORTFOLIO",
+              value: "ABOUT",
+              displayer: "Text",
+            },
+            {
+              type: "page",
+              key: "link",
+              value: "",
+              displayer: "Navigate To",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "string",
+              key: "label",
+              value: "BLOG",
+              displayer: "Text",
+            },
+            {
+              type: "page",
+              key: "link",
+              value: "",
+              displayer: "Navigate To",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "string",
+              key: "label",
+              value: "CONTACT",
               displayer: "Text",
             },
             {
@@ -88,15 +211,22 @@ class Navbar6 extends BaseNavigator {
   }
 
   render() {
-    const leftItems = this.getPropValue("left-items");
+    const extendable = this.getPropValue("extendable");
     const rightItems = this.getPropValue("right-items");
     const navActive = this.getComponentState("navActive");
 
+    // console.log(extendable[0].value[2].value[0].value , "arr")
+
+    console.log(extendable[0].value[2].value, "arr")
+
+
+    console.log(extendable[0].value[2].displayer)
+    // console.log(rightItems, "right")
+
     return (
       <div
-        className={`${this.decorateCSS("container")} ${
-          this.getPropValue("sticky") ? this.decorateCSS("sticky") : ""
-        }`}
+        className={`${this.decorateCSS("container")} ${this.getPropValue("sticky") ? this.decorateCSS("sticky") : ""
+          }`}
       >
         <div className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("content")}>
@@ -107,28 +237,44 @@ class Navbar6 extends BaseNavigator {
             <div className={this.decorateCSS("content-right")}>
               {navActive && (
                 <div
-                  className={`${this.decorateCSS("navbar-child")} ${
-                    navActive ? this.decorateCSS("active") : this.decorateCSS("inactive")
-                  }`}
+                  className={`${this.decorateCSS("navbar-child")} ${navActive ? this.decorateCSS("active") : this.decorateCSS("inactive")
+                    }`}
                 >
-                  {[...leftItems, ...rightItems].map(
-                    (item: any, index: number) => (
-                      <ComposerLink key={index} path={item.value[1].value}>
-                        <h3>{item.value[0].value}</h3>
+                  {[...extendable, ...rightItems].map((item: any, index: number) => (
+                    <>
+                      <ComposerLink path={item.value[1].value}>
+                        {item.displayer === "Main Text" ? (
+                          <h3 className={this.decorateCSS("extendable")}>
+                            {item.value[0].value}
+                            {item.value[2].value.length > 0 && <div className={this.decorateCSS("dropdown-content")}>
+                                {item.value[2].value.map((dropdownItem: any, index: number) => {
+                                  return (
+                                    <a href={dropdownItem.value[1].value}>
+                                      <div key={index} className={this.decorateCSS("image-container")}>
+                                      <img src={dropdownItem.value[2].value} alt="" className={this.decorateCSS("image")}/>
+                                      <div className={this.decorateCSS("text-overlay")}>{dropdownItem.value[0].value}</div>
+                                    </div>
+                                    </a>
+                                )})}
+                              </div>}
+                          </h3>
+                        ) : (
+                          <h3>{item.value[0].value}</h3>
+                        )}
                       </ComposerLink>
-                    )
-                  )}
+                    </>
+                  ))}
                 </div>
+
               )}
-              
+
               <div
                 className={this.decorateCSS("hamburger-container")}
                 onClick={() => this.navClick()}
               >
                 <span
-                  className={`${this.decorateCSS("hamburger")} ${
-                    navActive ? this.decorateCSS("open") : ""
-                  }`}
+                  className={`${this.decorateCSS("hamburger")} ${navActive ? this.decorateCSS("open") : ""
+                    }`}
                 >
                   <span></span>
                   <span></span>
