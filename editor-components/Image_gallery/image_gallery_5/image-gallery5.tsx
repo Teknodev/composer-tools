e31bd1a009f00369fb8c4a2c5c5610157dd3c265
volky13 <volkanekici13@gmail.com>
2024-09-24 12:09:29 +0300

    fixed image_gallery_5

--- editor-components/Image_gallery/image_gallery_5/image-gallery5.tsx
+++ editor-components/Image_gallery/image_gallery_5/image-gallery5.tsx
@@ -16,46 +16,124 @@ class ImageGalleryComponent5 extends BaseImageGallery {
       displayer: "Gallery",
       value: [
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image1",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20b8c2f8a5b002ce65828?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image1",
+              value:
+              "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20b8c2f8a5b002ce65828?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption1",
+              displayer: "Caption 1",
+              value: "Gallery Image 1 Caption",
+            },
+          ],
         },
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image2",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c6a2f8a5b002ce65834?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image2",
+              value:
+              "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c6a2f8a5b002ce65834?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption2",
+              displayer: "Caption 2",
+              value: "Gallery Image 2 Caption",
+            },
+          ],
         },
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image3",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c962f8a5b002ce65840?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image3",
+              value:
+              "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20c962f8a5b002ce65840?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption3",
+              displayer: "Caption 3",
+              value: "Gallery Image 3 Caption",
+            },
+          ],
         },
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image4",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cbc2f8a5b002ce6584c?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image4",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cbc2f8a5b002ce6584c?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption4",
+              displayer: "Caption 4",
+              value: "Gallery Image 4 Caption",
+            },
+          ],
         },
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image5",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cd82f8a5b002ce65858?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image5",
+              value:
+              "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cd82f8a5b002ce65858?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption5",
+              displayer: "Caption 5",
+              value: "Gallery Image 5 Caption",
+            },
+          ],
         },
         {
-          type: "image",
+          type: "object",
           key: "imageGallery",
           displayer: "Image6",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cee2f8a5b002ce6586d?alt=media",
+          value: [
+            {
+              type: "image",
+              key: "imageGallery",
+              displayer: "Image6",
+              value:
+              "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66a20cee2f8a5b002ce6586d?alt=media",
+            },
+            {
+              type: "string",
+              key: "caption6",
+              displayer: "Caption 6",
+              value: "Gallery Image 6 Caption",
+            },
+          ],
         },
       ],
     });
@@ -66,13 +144,6 @@ class ImageGalleryComponent5 extends BaseImageGallery {
       displayer: "Item Count in a Row",
       value: 3,
       max: 5,
-    });
-
-    this.addProp({
-      type: "string",
-      key: "imageCaptionText",
-      displayer: "Image Caption Text",
-      value: "Gallery Image",
     });
 
     this.addProp({
@@ -99,14 +170,12 @@ class ImageGalleryComponent5 extends BaseImageGallery {
       type: "boolean",
       key: "imageIndex",
       displayer: "Image Index Enabled",
-      value: false,
+      value: true,
     });
 
     this.setComponentState("is_image_clicked", false);
     this.setComponentState("clicked_image_index", 0);
   }
-
-
 
   getName(): string {
     return "Image Gallery 5";
@@ -149,7 +218,6 @@ class ImageGalleryComponent5 extends BaseImageGallery {
     const galleries = this.getPropValue("gallery");
     const isImageClicked = this.getComponentState("is_image_clicked");
     const clickedImageIndex = this.getComponentState("clicked_image_index");
-    const imageCaptionText = this.getPropValue("imageCaptionText");
     const nextIcon = this.getPropValue("nextIcon");
     const prevIcon = this.getPropValue("prevIcon");
     const imageIndex = this.getPropValue("imageIndex");
@@ -166,21 +234,22 @@ class ImageGalleryComponent5 extends BaseImageGallery {
           <div
             className={this.decorateCSS("images")}
             style={{
-              gridTemplateColumns: `repeat(${this.getPropValue(
-                "itemCount"
-              )}, 1fr)`,
+              gridTemplateColumns: `repeat(${this.getPropValue("itemCount")}, 1fr)`,
             }}
           >
-            {galleries.map((image: any, index: number) => (
-              <div className={this.decorateCSS("image-container")} key={index}>
-                <img
-                  src={image.value}
-                  alt=""
-                  className={this.decorateCSS("image")}
-                  onClick={() => this.handleImageClick(index)}
-                />
-              </div>
-            ))}
+            {galleries.map((galleryItem: any, index: number) => {
+              const image = galleryItem.value.find((item: any) => item.type === "image").value;
+              return (
+                <div className={this.decorateCSS("image-container")} key={index}>
+                  <img
+                    src={image}
+                    alt={`Gallery Image ${index + 1}`}
+                    className={this.decorateCSS("image")}
+                    onClick={() => this.handleImageClick(index)}
+                  />
+                </div>
+              );
+            })}
           </div>
           {isImageClicked && (
             <div
@@ -190,7 +259,7 @@ class ImageGalleryComponent5 extends BaseImageGallery {
               <div className={this.decorateCSS("overlay-content")}>
                 <div className={this.decorateCSS("middle-content")}>
                   <img
-                    src={galleries[clickedImageIndex].value}
+                    src={galleries[clickedImageIndex].value.find((item: any) => item.type === "image").value}
                     alt=""
                     className={this.decorateCSS("large-image")}
                     onClick={(e) => {
@@ -206,9 +275,8 @@ class ImageGalleryComponent5 extends BaseImageGallery {
                       {clickedImageIndex + 1} of {galleries.length}
                     </div>
                   )}
-
                   <div className={this.decorateCSS("gallery-image")}>
-                    {imageCaptionText}
+                    {galleries[clickedImageIndex].value.find((item: any) => item.type === "string").value}
                   </div>
                 </div>
                 <button
@@ -235,7 +303,8 @@ class ImageGalleryComponent5 extends BaseImageGallery {
         </div>
       </div>
     );
-  }
+}
+
 }
 
 export default ImageGalleryComponent5;
