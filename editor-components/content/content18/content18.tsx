e4ae6ea6585b69f7e06a89825700ea165cfcf0bb
Burcu Açan <burcuuacann@gmail.com>
2024-09-25 19:02:51 +0300

    feat: added video player and fix: image design and reponsive design

--- editor-components/content/content18/content18.tsx
+++ editor-components/content/content18/content18.tsx
@@ -29,10 +29,23 @@ class Content18 extends BaseContent {
         "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661bc49bd2970002c62890f?alt=media&timestamp=1719564433797",
     });
     this.addProp({
+      type: "icon",
+      key: "playIcon",
+      displayer: "Play Icon",
+      value: "FaRegPlayCircle"
+
+    });
+    this.addProp({
+      type: "page",
+      displayer: "Video",
+      key: "video",
+      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e75d50181a1002c334f4f?alt=media&timestamp=1719563750188"
+    });
+    this.addProp({
       type: "string",
       key: "badge",
       displayer: "Badge",
-      value: "Why Choose Us",
+      value: "WHY CHOOSE US",
     });
     this.addProp({
       type: "string",
@@ -187,12 +200,17 @@ class Content18 extends BaseContent {
       displayer: "progress-text-is-primary",
       value: true,
     });
+    this.setComponentState("is_video_visible", false);
   }
   getName(): string {
     return "Content 18";
   }
 
   render() {
+    const badge = this.castToString(this.getPropValue("badge"));
+    const title = this.castToString(this.getPropValue("title"));
+    const description = this.castToString(this.getPropValue("description"));
+    const itemsLength = this.getPropValue("items").length;
     return (
       <div className={this.decorateCSS("container")}>
         <div className={this.decorateCSS("max-content")}>
@@ -211,59 +229,80 @@ class Content18 extends BaseContent {
                   src={this.getPropValue("image2")}
                   alt=""
                 />
+                <div onClick={() => {
+                  this.setComponentState("is_video_visible", true)
+                }}>
+                  <ComposerIcon name={this.getPropValue("playIcon")} propsIcon={{ className: this.decorateCSS("icon") }} />
+                </div>
+                {this.getComponentState("is_video_visible") && <div className={this.decorateCSS("player")} onClick={() => this.setComponentState("is_video_visible", false)}>
+                  <video onClick={(event) => event.stopPropagation()} controls className={this.decorateCSS("image2")} src={this.getPropValue("video")}></video>
+                </div>}
               </div>
             </div>
             <div className={this.decorateCSS("right-page")}>
-              <div className={this.decorateCSS("badge")}>
-                {this.getPropValue("badge")}
-              </div>
-              <div className={this.decorateCSS("title")}>
-                {this.getPropValue("title")}
-              </div>
-              <p className={this.decorateCSS("description")}>
-                {this.getPropValue("description")}
-              </p>
+              {badge && (
+                <div className={this.decorateCSS("badge")}>
+                  {this.getPropValue("badge")}
+                </div>
+              )}
+              {title && (
+                <div className={this.decorateCSS("title")}>
+                  {this.getPropValue("title")}
+                </div>
+              )}
+              {description && (
+                <p className={this.decorateCSS("description")}>
+                  {this.getPropValue("description")}
+                </p>
+              )}
 
-              <div className={this.decorateCSS("progress-container")}>
-                {this.castToObject<ProgressItem[]>("items").map(
-                  (item: ProgressItem, index: number) => (
-                    <div className={this.decorateCSS("item")} key={index}>
-                      <div
-                        className={this.decorateCSS("progress-title-container")}
-                      >
+              {itemsLength > 0 && (
+                <div className={this.decorateCSS("progress-container")}>
+                  {this.castToObject<ProgressItem[]>("items").map(
+                    (item: ProgressItem, index: number) => (
+                      <div className={this.decorateCSS("item")} key={index}>
                         <div
-                          className={this.decorateCSS("progress-title-icon")}
+                          className={this.decorateCSS("progress-title-container")}
                         >
-                          <ComposerIcon
-                            name={item.icon}
-                            propsIcon={{ className: this.decorateCSS("icon") }}
-                          />
-                        </div>
-                        <div className={this.decorateCSS("progress-title")}>
-                          {item.progressTitle}
-                        </div>
-                      </div>
-                      {this.castToString(item.progressText) && (
-                        <div
-                          className={this.decorateCSS("progress-bar-container")}
-                        >
-                          <div className={this.decorateCSS("progress-bar")}>
-                            <div
-                              className={this.decorateCSS("progress")}
-                              style={{
-                                width: `${item.progress}%`,
-                              }}
+                          <div
+                            className={this.decorateCSS("progress-title-icon")}
+                          >
+                            <ComposerIcon
+                              name={item.icon}
+                              propsIcon={{ className: this.decorateCSS("icon") }}
                             />
                           </div>
-                          <span className={this.decorateCSS("progress-text")}>
-                            {item.progressText}
-                          </span>
+                          <div className={this.decorateCSS("progress-title")}>
+                            {item.progressTitle}
+                          </div>
                         </div>
-                      )}
-                    </div>
-                  )
-                )}
-              </div>
+                        {this.castToString(item.progressText) && (
+                          <div
+                            className={this.decorateCSS("progress-bar-container")}
+                          >
+                            <div className={this.decorateCSS("progress-bar")}>
+                              <div
+                                className={this.decorateCSS("progress")}
+                                style={{
+                                  width: `${item.progress}%`,
+                                }}
+                              />
+                            </div>
+                            <span className={this.decorateCSS("progress-text")}>
+                              {item.progressText}
+                            </span>
+                          </div>
+                        )}
+                      </div>
+                    )
+                  )}
+                </div>
+
+              )
+
+              }
+
+
             </div>
           </div>
         </div>
