7407c2b5a8605b3cdcb0b895d86ebd3f3cfbb758
Yusuf Kıtlık <yusufkitlik@Yusuf-MacBook-Air.local>
2024-10-08 12:18:05 +0300

    fix: fixed location1

--- editor-components/location/location1/location1.tsx
+++ editor-components/location/location1/location1.tsx
@@ -3,17 +3,6 @@ import { Location } from "../../EditorComponent";
 import styles from "./location1.module.scss";
 import ComposerMap from "../../../composer-base-components/map/map";
 
-type Coordinate = {
-  lat: number;
-  lng: number;
-  icon?: {
-    url: string;
-    width?: number;
-    height?: number;
-  };
-  address?: string;
-};
-
 class LocationComponent1 extends Location {
   constructor(props?: any) {
     super(props, styles);
@@ -52,7 +41,7 @@ class LocationComponent1 extends Location {
             },
             {
               type: "image",
-              key: "marker-image1",
+              key: "marker-image",
               displayer: "Marker Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66dffd65343034002c462ded?alt=media&timestamp=1725955430378",
             },
@@ -76,15 +65,9 @@ class LocationComponent1 extends Location {
           displayer: "Marker",
           value: [
             {
-              type: "string",
-              displayer: "Title",
-              key: "title2",
-              value: "Second Location",
-            },
-            {
               type: "location",
               displayer: "Coordinate",
-              key: "coordinate2",
+              key: "coordinate",
               value: {
                 lat: 37.1234,
                 lng: -76.9876,
@@ -93,25 +76,71 @@ class LocationComponent1 extends Location {
             {
               type: "string",
               displayer: "Office",
-              key: "office2",
+              key: "office",
               value: "Arapsuyu 601 Sokak",
             },
             {
               type: "image",
-              key: "marker-image2",
+              key: "marker-image",
               displayer: "Marker Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66dffd65343034002c462ded?alt=media&timestamp=1725955430378",
             },
             {
               type: "string",
               displayer: "Description",
-              key: "description2",
+              key: "description",
               value: "Bu, ikinci lokasyonun açıklamasıdır.",
             },
             {
               type: "string",
               displayer: "Popup Title",
-              key: "popupTitle2",
+              key: "popupTitle",
+              value: "İkinci Lokasyon",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "marker",
+          displayer: "Marker",
+          value: [
+            {
+              type: "string",
+              displayer: "Title",
+              key: "title",
+              value: "Second Location",
+            },
+            {
+              type: "location",
+              displayer: "Coordinate",
+              key: "coordinate",
+              value: {
+                lat: 37.1234,
+                lng: -76.9876,
+              },
+            },
+            {
+              type: "string",
+              displayer: "Office",
+              key: "office",
+              value: "Arapsuyu 601 Sokak",
+            },
+            {
+              type: "image",
+              key: "marker-image",
+              displayer: "Marker Image",
+              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66dffd65343034002c462ded?alt=media&timestamp=1725955430378",
+            },
+            {
+              type: "string",
+              displayer: "Description",
+              key: "description",
+              value: "Bu, ikinci lokasyonun açıklamasıdır.",
+            },
+            {
+              type: "string",
+              displayer: "Popup Title",
+              key: "popupTitle",
               value: "İkinci Lokasyon",
             },
           ],
@@ -124,18 +153,48 @@ class LocationComponent1 extends Location {
     return "Location 1";
   }
 
-  popupContent = (marker: Coordinate) => {
-    return (
-      <div className={this.decorateCSS("popup")}>
-        <h4 className={this.decorateCSS("popup-title")}>Popup Title</h4>
-        <p className={this.decorateCSS("popup-content")}>Popup Content: {marker.address || "No address"}</p>
-        <button className={this.decorateCSS("popup-button")}>Custom Button</button>
-      </div>
-    );
-  };
-
   render() {
-    const { markers } = this.getPropValue("location");
+    const addresses = this.getPropValue("addresses");
+
+    const markers = addresses.reduce((acc: any[], address: any) => {
+      if (address.type === "object" && Array.isArray(address.value)) {
+        const markerData = address.value.find((addr: any) => addr.type === "location");
+        const lat = markerData?.value.lat;
+        const lng = markerData?.value.lng;
+        const officeAddress = address.value.find((a: any) => a.key.startsWith("office"))?.value || "";
+        const description = address.value.find((a: any) => a.key.startsWith("description"))?.value || "";
+        const popupTitle = address.value.find((a: any) => a.key.startsWith("popupTitle"))?.value || "";
+        const markerImage = address.value.find((a: any) => a.key.startsWith("marker-image"))?.value;
+        const width = address.value.find((a: any) => a.key.startsWith("marker-width"))?.value || 32;
+        const height = address.value.find((a: any) => a.key.startsWith("marker-height"))?.value || 32;
+
+        console.log(markerImage, "markerImage");
+
+        if (lat !== undefined && lng !== undefined) {
+          const content =
+            officeAddress || description || popupTitle ? (
+              <div style={{ backgroundColor: "white", padding: "10px" }}>
+                {officeAddress && <p>{officeAddress}</p>}
+                {description && <p>{description}</p>}
+                {popupTitle && <p>{popupTitle}</p>}
+              </div>
+            ) : null;
+
+          acc.push({
+            content,
+            lat,
+            lng,
+            icon: {
+              url: markerImage,
+              scaledSize: new google.maps.Size(width, height),
+              width,
+              height,
+            },
+          });
+        }
+      }
+      return acc;
+    }, []);
 
     return (
       <div className={this.decorateCSS("container")}>
