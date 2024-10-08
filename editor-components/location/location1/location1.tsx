3f07ed3259830a16e74c7be2bf155696425e2017
Yusuf Kıtlık <yusufkitlik@Yusuf-MacBook-Air.local>
2024-10-08 12:05:08 +0300

    fix removed popupcontent in location1

--- editor-components/location/location1/location1.tsx
+++ editor-components/location/location1/location1.tsx
@@ -3,6 +3,17 @@ import { Location } from "../../EditorComponent";
 import styles from "./location1.module.scss";
 import ComposerMap from "../../../composer-base-components/map/map";
 
+type Coordinate = {
+  lat: number;
+  lng: number;
+  icon?: {
+    url: string;
+    width?: number;
+    height?: number;
+  };
+  address?: string;
+};
+
 class LocationComponent1 extends Location {
   constructor(props?: any) {
     super(props, styles);
@@ -24,12 +35,6 @@ class LocationComponent1 extends Location {
           key: "marker",
           displayer: "Marker",
           value: [
-            {
-              type: "string",
-              displayer: "Title",
-              key: "title",
-              value: "First Location",
-            },
             {
               type: "location",
               displayer: "Coordinate",
@@ -119,46 +124,18 @@ class LocationComponent1 extends Location {
     return "Location 1";
   }
 
+  popupContent = (marker: Coordinate) => {
+    return (
+      <div className={this.decorateCSS("popup")}>
+        <h4 className={this.decorateCSS("popup-title")}>Popup Title</h4>
+        <p className={this.decorateCSS("popup-content")}>Popup Content: {marker.address || "No address"}</p>
+        <button className={this.decorateCSS("popup-button")}>Custom Button</button>
+      </div>
+    );
+  };
+
   render() {
-    const addresses = this.getPropValue("addresses");
-
-    const markers = addresses.reduce((acc: any[], address: any) => {
-      if (address.type === "object" && Array.isArray(address.value)) {
-        address.value.forEach((addr: any) => {
-          if (addr.key.startsWith("title")) {
-            const lat = addr.key.includes("2") ? address.value.find((a: any) => a.key === "coordinate2")?.value.lat : address.value.find((a: any) => a.key === "coordinate")?.value.lat;
-
-            const lng = addr.key.includes("2") ? address.value.find((a: any) => a.key === "coordinate2")?.value.lng : address.value.find((a: any) => a.key === "coordinate")?.value.lng;
-
-            const officeAddress = addr.key.includes("2") ? address.value.find((a: any) => a.key === "office2")?.value || "" : address.value.find((a: any) => a.key === "office")?.value || "";
-            const description = addr.key.includes("2") ? address.value.find((a: any) => a.key === "description2")?.value || "" : address.value.find((a: any) => a.key === "description")?.value || "";
-            const popupTitle = addr.key.includes("2") ? address.value.find((a: any) => a.key === "popupTitle2")?.value || "" : address.value.find((a: any) => a.key === "popupTitle")?.value || "";
-
-            const markerImage = addr.key.includes("2") ? address.value.find((a: any) => a.key === "marker-image2")?.value : address.value.find((a: any) => a.key === "marker-image1")?.value;
-
-            if (lat !== undefined && lng !== undefined) {
-              acc.push({
-                content: (
-                  <div style={{ backgroundColor: "white", padding: "10px" }}>
-                    <p>{officeAddress}</p>
-                    <p>{description}</p>
-                    <p>{popupTitle}</p>
-                  </div>
-                ),
-                lat,
-                lng,
-                icon: {
-                  url: markerImage,
-                  scaledSize: new google.maps.Size(32, 32),
-                  anchor: new google.maps.Point(16, 32),
-                },
-              });
-            }
-          }
-        });
-      }
-      return acc;
-    }, []);
+    const { markers } = this.getPropValue("location");
 
     return (
       <div className={this.decorateCSS("container")}>
