51119f2ea46458be532596d99bb4a3751fe749bb
Burcu Açan <burcuuacann@gmail.com>
2024-10-11 17:00:51 +0300

    feat: checkbox for animastions and fix: reponsive design and when items is deleted deisgn is not broken

--- editor-components/header/header6/header6.tsx
+++ editor-components/header/header6/header6.tsx
@@ -2,29 +2,90 @@ import * as React from "react";
 import { BaseHeader } from "../../EditorComponent";
 import styles from "./header6.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { Button } from "../../contacts/form2/form2";
+
+interface TitleItem {
+  title: JSX.Element;
+  titleAnimation: boolean;
+}
+interface Title2Item {
+  title2: JSX.Element;
+  title2Animation: boolean;
+}
+interface DescriptionItem {
+  description: JSX.Element;
+  descriptionAnimation: boolean;
+}
+interface Image2Item {
+  image2: string;
+  image2Animation: boolean;
+}
+interface ButtonItem {
+  buttonText: JSX.Element;
+  url: string;
+  buttonAnimation: boolean;
+}
 
 class Header6 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
-      type: "string",
-      key: "title",
-      displayer: "Title",
-      value: "Envision",
-    });
+      type: "object",
+      key: "titleItem",
+      displayer: "Title Item",
+      value: [
+        {
+          type: "string",
+          key: "title",
+          displayer: "Title",
+          value: "Envision",
+        },
+        {
+          type: "boolean",
+          key: "titleAnimation",
+          displayer: "Title Animation",
+          value: true,
+        }
+      ]
+    })
     this.addProp({
-      type: "string",
-      key: "title2",
-      displayer: "Title2",
-      value: "Brand 2020",
-    });
+      type: "object",
+      key: "title2Item",
+      displayer: "Title2 Item",
+      value: [
+        {
+          type: "string",
+          key: "title2",
+          displayer: "Title2",
+          value: "Brand 2020",
+        },
+        {
+          type: "boolean",
+          key: "title2Animation",
+          displayer: "Title Animation",
+          value: true,
+        }
+      ]
+    })
     this.addProp({
-      type: "string",
-      key: "description",
-      displayer: "Description",
-      value:
-        "Carefully crafted with unique layouts you can easily create websites.",
-    });
+      type: "object",
+      key: "descriptionItem",
+      displayer: "Description Item",
+      value: [
+        {
+          type: "string",
+          key: "description",
+          displayer: "Description",
+          value: "Carefully crafted with unique layouts you can easily create websites.",
+        },
+        {
+          type: "boolean",
+          key: "descriptionAnimation",
+          displayer: "Description Animation",
+          value: true,
+        }
+      ]
+    })
     this.addProp({
       type: "image",
       key: "image1",
@@ -32,18 +93,34 @@ class Header6 extends BaseHeader {
       value:
         "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617f52bd2970002c624523?alt=media&timestamp=1719483639150",
     });
+
     this.addProp({
-      type: "image",
-      key: "image2",
-      displayer: "Image",
-      value:
-        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617f52bd2970002c624524?alt=media&timestamp=1719483639150",
-    });
+      type: "object",
+      key: "image2Item",
+      displayer: "Image2 Item",
+      value: [
+        {
+          type: "page",
+          key: "image2",
+          displayer: "Image2",
+          value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617f52bd2970002c624524?alt=media&timestamp=1719483639150",
+        },
+        {
+          type: "boolean",
+          key: "image2Animation",
+          displayer: "Image2 Animation",
+          value: true,
+        }
+      ]
+    })
 
     this.addProp({
       type: "array",
       key: "buttons",
       displayer: "Buttons",
+      additionalParams: {
+        maxElementCount: 2,
+      },
       value: [
         {
           type: "object",
@@ -56,13 +133,18 @@ class Header6 extends BaseHeader {
               displayer: "Button Text",
               value: "Explore",
             },
-
             {
               type: "page",
               key: "url",
               displayer: "Button Link",
-              value: "https://www.google.com/",
+              value: "",
             },
+            {
+              type: "boolean",
+              key: "buttonAnimation",
+              displayer: "Button Animation",
+              value: true,
+            }
           ],
         },
       ],
@@ -72,82 +154,92 @@ class Header6 extends BaseHeader {
   getName(): string {
     return "Header 6";
   }
-render() {
-  const showLeftContent =
-    this.castToString(this.getPropValue("title")) ||
-    this.castToString(this.getPropValue("title2")) ||
-    this.castToString(this.getPropValue("description")) ||
-    (this.getPropValue("buttons") && this.getPropValue("buttons").length > 0);
+  render() {
+    const title = this.castToObject<TitleItem>("titleItem");
+    const title2 = this.castToObject<Title2Item>("title2Item");
+    const description = this.castToObject<DescriptionItem>("descriptionItem");
+    const image2 = this.castToObject<Image2Item>("image2Item");
 
-  const showRightContent =
-    this.getPropValue("image1") || this.getPropValue("image2");
+    const showLeftContent =
+      this.castToString(title.title) ||
+      this.castToString(title2.title2) ||
+      this.castToString(description.description) ||
+      (this.getPropValue("buttons") && this.getPropValue("buttons").length > 0);
 
-  return (
-    <div className={this.decorateCSS("container")}>
-      <div className={this.decorateCSS("max-content")}>
-        <section className={this.decorateCSS("child-container")}>
-          {showLeftContent ? (
-            <div className={this.decorateCSS("left-content")}>
-              {this.getPropValue("title") && (
-                <h2 className={this.decorateCSS("title")}>
-                  {this.getPropValue("title")}
-                </h2>
-              )}
-              {this.getPropValue("title2") && (
-                <p className={this.decorateCSS("title2")}>
-                  {this.getPropValue("title2")}
-                </p>
-              )}
-              {this.getPropValue("description") && (
-                <p className={this.decorateCSS("description")}>
-                  {this.getPropValue("description")}
-                </p>
-              )}
-              {this.getPropValue("buttons") && (
-                <div className={this.decorateCSS("buttondiv")}>
-                  {this.getPropValue("buttons").map(
-                    (item: any, indexButtons: number) => {
-                      const buttonText = item.value[0].value;
-                      return buttonText ? (
-                        <ComposerLink
-                          key={indexButtons}
-                          path={item.value[1].value}
-                        >
-                          <button className={`${this.decorateCSS("button")}`}>
-                            {buttonText}
-                          </button>
-                        </ComposerLink>
-                      ) : null;
-                    }
-                  )}
-                </div>
-              )}
-            </div>
-          ) : null}
-          
-          {showRightContent ? (
-            <div className={this.decorateCSS("right-content")}>
-              {this.getPropValue("image1") && (
-                <img
-                  src={this.getPropValue("image1")}
-                  alt=""
-                  className={this.decorateCSS("image1")}
-                />
-              )}
-              {this.getPropValue("image2") && (
-                <img
-                  src={this.getPropValue("image2")}
-                  alt=""
-                  className={this.decorateCSS("image2")}
-                />
-              )}
-            </div>
-          ) : null}
-        </section>
+    const showRightContent =
+      this.getPropValue("image1") || image2.image2;
+
+    return (
+      <div className={this.decorateCSS("container")}>
+        <div className={this.decorateCSS("max-content")}>
+          <section className={this.decorateCSS("child-container")}>
+            {showLeftContent ? (
+              <div className={this.decorateCSS("left-content")}>
+                {(this.castToString(title.title) || this.castToString(title2.title2)) && (
+                  <div className={this.decorateCSS("title-container")}>
+                    {this.castToString(title.title) && (
+                      <h2 className={title.titleAnimation ? this.decorateCSS("title") : this.decorateCSS("title-noanimation")}>
+                        {this.castToString(title.title)}
+                      </h2>
+                    )}
+                    {this.castToString(title2.title2) && (
+                      <h3 className={title2.title2Animation ? this.decorateCSS("title2") : this.decorateCSS("title2-noanimation")}>
+                        {this.castToString(title2.title2)}
+                      </h3>
+                    )}
+                  </div>
+                )}
+                {this.castToString(description.description) && (
+                  <p className={description.descriptionAnimation ? this.decorateCSS("description") : this.decorateCSS("description-noanimation")}>
+                    {this.castToString(description.description)}
+                  </p>
+                )}
+                {this.getPropValue("buttons").length > 0 && (
+                  <div className={this.decorateCSS("buttondiv")}>
+                    {this.castToObject<ButtonItem[]>("buttons").map(
+                      (item: any, indexButtons: number) => {
+                        const buttonText = this.castToString(item.buttonText);
+                        return buttonText ? (
+                          <ComposerLink
+                            key={indexButtons}
+                            path={item.url}
+                          >
+                            <button className={item.buttonAnimation ? this.decorateCSS("button") : this.decorateCSS("button-noanimation")}>
+                              {buttonText}
+                            </button>
+                          </ComposerLink>
+                        ) : null;
+                      }
+                    )}
+                  </div>
+                )}
+              </div>
+            ) : null}
+
+            {showRightContent ? (
+              <div className={this.decorateCSS("right-content")}>
+                {this.getPropValue("image1") && (
+                  <img
+                    src={this.getPropValue("image1")}
+                    alt=""
+                    className={this.decorateCSS("image1")}
+                  />
+                )}
+                {image2.image2 && (
+                  <img
+                    src={image2.image2}
+                    alt=""
+                    className={this.getPropValue("image1") ? (image2.image2Animation ? this.decorateCSS("image2") : this.decorateCSS("image2-noanimation"))
+                      : (image2.image2Animation ? this.decorateCSS("image2-without-image1") : this.decorateCSS("image2-noanimation"))}
+                  />
+                )}
+              </div>
+            ) : null}
+          </section>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default Header6;
