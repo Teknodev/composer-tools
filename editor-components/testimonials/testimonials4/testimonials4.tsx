08b63844addbb98960e17a96990a35d1cd5a521f
Burcu Açan <burcuuacann@gmail.com>
2024-10-24 19:14:02 +0300

    fix: container

--- editor-components/testimonials/testimonials4/testimonials4.tsx
+++ editor-components/testimonials/testimonials4/testimonials4.tsx
@@ -3,6 +3,15 @@ import { Testimonials } from "../../EditorComponent";
 import styles from "./testimonials4.module.scss";
 import ComposerSlider from "../../../composer-base-components/slider/slider";
 import { Base } from "../../../composer-base-components/base/base"
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+
+interface SliderItem {
+  icon: string,
+  description: JSX.Element,
+  title: JSX.Element,
+  subtitle: JSX.Element,
+  image: string,
+}
 
 
 class Testimonials4Page extends Testimonials {
@@ -16,13 +25,19 @@ class Testimonials4Page extends Testimonials {
       value: [
         {
           type: "object",
-          key: "slidercontent",
-          displayer: "Slider Content",
+          key: "item",
+          displayer: "Item",
           value: [
             {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "ImQuotesRight",
+            },
+            {
               type: "string",
-              key: "longtext",
-              displayer: "Long Text",
+              key: "description",
+              displayer: "Description",
               value:
                 "I really like the clean and simple aesthetic of flat design. It's great for creating a modern and minimal look, and it also works well for responsive design since it's easy to adapt to different screen sizes. Plus, the lack of textures and gradients makes it easier to focus on the content itself.",
             },
@@ -39,36 +54,28 @@ class Testimonials4Page extends Testimonials {
               value: "UX Developer",
             },
             {
-              type: "object",
-              key: "button",
-              displayer: "Button",
-              value: [
-                {
-                  type: "image",
-                  key: "image-button",
-                  displayer: "Image-button",
-                  value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623724?alt=media&timestamp=1719483639150",
-                },
-                {
-                  type: "page",
-                  key: "src",
-                  displayer: "Button Link",
-                  value: "",
-                }
-              ]
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623724?alt=media&timestamp=1719483639150",
             },
-
           ],
         },
         {
           type: "object",
-          key: "slidercontent",
-          displayer: "Slider Content",
+          key: "item",
+          displayer: "Item",
           value: [
             {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "ImQuotesRight",
+            },
+            {
               type: "string",
-              key: "longtext",
-              displayer: "Long Text",
+              key: "description",
+              displayer: "Description",
               value:
                 "This product has changed my life! It's incredibly useful and packed with creative features. I would highly recommend it to everyone!",
             },
@@ -85,36 +92,29 @@ class Testimonials4Page extends Testimonials {
               value: "Solutions Architect",
             },
             {
-              type: "object",
-              key: "button",
-              displayer: "Button",
-              value: [
-                {
-                  type: "image",
-                  key: "image-button",
-                  displayer: "Image-button",
-                  value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623723?alt=media&timestamp=1719483639150",
-                },
-                {
-                  type: "page",
-                  key: "src",
-                  displayer: "Button Link",
-                  value: "",
-                },
-              ],
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623723?alt=media&timestamp=1719483639150",
             },
 
           ],
         },
         {
           type: "object",
-          key: "slidercontent",
-          displayer: "Slider Content",
+          key: "item",
+          displayer: "Item",
           value: [
             {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "ImQuotesRight",
+            },
+            {
               type: "string",
-              key: "longtext",
-              displayer: "Long Text",
+              key: "description",
+              displayer: "Description",
               value:
                 "I have been consistently impressed with the results achieved by this team. Their strategic approach and innovative ideas have greatly contributed to our marketing success.",
             },
@@ -131,26 +131,11 @@ class Testimonials4Page extends Testimonials {
               value: "UX Developer",
             },
             {
-              type: "object",
-              key: "button",
-              displayer: "Button",
-              value: [
-                {
-                  type: "image",
-                  key: "image-button",
-                  displayer: "Image-button",
-                  value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623726?alt=media&timestamp=1719483639150",
-                },
-                {
-                  type: "page",
-                  key: "src",
-                  displayer: "Button Link",
-                  value: "",
-                },
-              ],
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661701bbd2970002c623726?alt=media&timestamp=1719483639150",
             },
-
-
           ],
         },
       ],
@@ -185,7 +170,7 @@ class Testimonials4Page extends Testimonials {
     const settings = {
       dots: false,
       infinite: true,
-      arrows: false,
+      arrows: true,
       speed: 700,
       autoplay: false,
       autoplaySpeed: 3000,
@@ -195,54 +180,40 @@ class Testimonials4Page extends Testimonials {
       }
 
     };
-
+    const sliderItem = this.castToObject<SliderItem[]>("items");
+    console.log("sliderItem", sliderItem)
     return (
-
       <div className={this.decorateCSS("overlay")} style={{
         backgroundImage: `url(${this.getPropValue("cover-image")})`,
       }}>
         <Base.Container className={this.decorateCSS("container")} >
           <Base.MaxContent className={this.decorateCSS("max-content")}>
             <div className={this.decorateCSS("testimonials4")}>
-              <div className={this.decorateCSS("cover-image")}>
-                <ComposerSlider {...settings} ref={this.getComponentState("slider-ref")}>
-
-                  {this.getPropValue("items").map((item: any, index: number) => (
-                    <div className={this.decorateCSS("items")}>
-
-                      <h1 className={this.decorateCSS("longtext")}>
-                        {item.value[0].value}
-                      </h1>
-                      <h3 className={this.decorateCSS("title")}>
-                        {item.value[1].value}
-                      </h3>
-                      <h4 className={this.decorateCSS("subtitle")}>
-                        {item.value[2].value}
-                      </h4>
-
-
-                    </div>
-                  ))}
-
-                </ComposerSlider>
-                <div className={this.decorateCSS("arrows")}>
-
-                </div>
-                <div className={this.decorateCSS("images")}>
-                  {this.getPropValue("items").map((item: any, itemIndex: number) => {
-
-                    const imageSrc = item.getPropValue("button")[0].value;
-
-                    return <img
-                      src={imageSrc}
-                      className={`${this.decorateCSS("image")} ${this.getComponentState("active_index") == itemIndex && this.decorateCSS("active")}`}
-                      onClick={() => this.onImageClick(itemIndex)} />
-                  }
-                  )}
-                </div>
+              <ComposerSlider {...settings} ref={this.getComponentState("slider-ref")}>
+                {sliderItem.map((item: any, index: number) => (
+                  <div className={this.decorateCSS("items")}>
+                    <ComposerIcon name={item.icon} propsIcon={{ className: this.decorateCSS("icon") }}></ComposerIcon>
+                    <Base.P className={this.decorateCSS("longtext")}>
+                      {item.description}
+                    </Base.P>
+                    <h3 className={this.decorateCSS("title")}>
+                      {item.title}
+                    </h3>
+                    <h4 className={this.decorateCSS("subtitle")}>
+                      {item.subtitle}
+                    </h4>
+                  </div>
+                ))}
+              </ComposerSlider>
+              <div className={this.decorateCSS("images")}>
+                {sliderItem.map((item: any, itemIndex: number) => {
+                  return <img
+                    src={item.image}
+                    className={`${this.decorateCSS("image")} ${this.getComponentState("active_index") == itemIndex && this.decorateCSS("active")}`}
+                    onClick={() => this.onImageClick(itemIndex)} />
+                }
+                )}
               </div>
-
-
             </div>
           </Base.MaxContent>
         </Base.Container>
