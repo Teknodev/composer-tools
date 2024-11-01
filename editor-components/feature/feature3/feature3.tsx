5834bba0d29f1f548d770420960e456c19b79f26
dogusmiuw <dceliknodes@gmail.com>
2024-11-01 15:32:44 +0300

    feat: standardization

--- editor-components/feature/feature3/feature3.tsx
+++ editor-components/feature/feature3/feature3.tsx
@@ -2,21 +2,36 @@ import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature3.module.scss";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
-type Logos = {
+type Logo = {
   image: string;
-  url:string;
+  url: string;
 };
 
-type Cards = {
+type Card = {
   title: string;
   icon: string;
-}
-
+};
 
 class Feature3 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
+
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "The Right Digital Partner For Success"
+    });
+
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "Lorem ipsum dolor sit amet consectetur. In nulla nunc arcu velit consectetur massa mauris molestie hac. Hac arcu amet nullam pellentesque. Urna eu suspendisse felis.",
+    });
 
     this.addProp({
       type: "array",
@@ -24,8 +39,8 @@ class Feature3 extends BaseFeature {
       displayer: "Logo Images",
       value: [
         {
-          type:"object",
-          key:"logo",
+          type: "object",
+          key: "logo",
           displayer: "Logo",
           value: [
             {
@@ -40,11 +55,11 @@ class Feature3 extends BaseFeature {
               displayer: "Logo Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667b1ec34fe95d002b35e6dd?alt=media&timestamp=1719344851686",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"logo",
+          type: "object",
+          key: "logo",
           displayer: "Logo",
           value: [
             {
@@ -59,11 +74,11 @@ class Feature3 extends BaseFeature {
               displayer: "Logo Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667b1ec34fe95d002b35e6da?alt=media&timestamp=1719344851686",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"logo",
+          type: "object",
+          key: "logo",
           displayer: "Logo",
           value: [
             {
@@ -78,11 +93,11 @@ class Feature3 extends BaseFeature {
               displayer: "Logo Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667b1ec34fe95d002b35e6d9?alt=media&timestamp=1719344851686",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"logo",
+          type: "object",
+          key: "logo",
           displayer: "Logo",
           value: [
             {
@@ -97,11 +112,11 @@ class Feature3 extends BaseFeature {
               displayer: "Logo Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667b1ec34fe95d002b35e6dc?alt=media&timestamp=1719344851686",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"logo",
+          type: "object",
+          key: "logo",
           displayer: "Logo",
           value: [
             {
@@ -116,10 +131,10 @@ class Feature3 extends BaseFeature {
               displayer: "Logo Image",
               value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667b1ec34fe95d002b35e6db?alt=media&timestamp=1719344851686",
             },
-        ]
+          ]
         },
       ]
-    })
+    });
 
     this.addProp({
       type: "array",
@@ -127,8 +142,8 @@ class Feature3 extends BaseFeature {
       displayer: "Card",
       value: [
         {
-          type:"object",
-          key:"card",
+          type: "object",
+          key: "card",
           displayer: "Card",
           value: [
             {
@@ -143,11 +158,11 @@ class Feature3 extends BaseFeature {
               displayer: "Icon",
               value: "FcSearch",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"card",
+          type: "object",
+          key: "card",
           displayer: "Card",
           value: [
             {
@@ -162,11 +177,11 @@ class Feature3 extends BaseFeature {
               displayer: "Icon",
               value: "FcStatistics",
             },
-        ]
+          ]
         },
         {
-          type:"object",
-          key:"card",
+          type: "object",
+          key: "card",
           displayer: "Card",
           value: [
             {
@@ -181,25 +196,10 @@ class Feature3 extends BaseFeature {
               displayer: "Card Icon",
               value: "FcComboChart",
             },
-        ]
+          ]
         },
       ]
-    })
-
-    this.addProp({
-      type: "string",
-      key: "description",
-      displayer: "Description",
-      value: "Lorem ipsum dolor sit amet consectetur. In nulla nunc arcu velit consectetur massa mauris molestie hac. Hac arcu amet nullam pellentesque. Urna eu suspendisse felis.",
     });
-    this.addProp({
-      type: "string",
-      key: "title",
-      displayer: "Title",
-      value: "The Right Digital Partner For Success"
-    });
-
-   
   }
 
   getName(): string {
@@ -207,60 +207,61 @@ class Feature3 extends BaseFeature {
   }
 
   render() {
+    const cards = this.castToObject<Card[]>("cards");
+    const logos = this.castToObject<Logo[]>("logos");
+
+    const titleExist = !!this.getPropValue("title");
+    const descExist = !!this.getPropValue("description");
+
     return (
-      <div
-        className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.VerticalContent className={this.decorateCSS("wrapper")}>
+            {(titleExist || descExist) && (
+              <header className={this.decorateCSS("header")}>
+                {titleExist && (
+                  <div className={this.decorateCSS("feature-circle")}>
+                    <Base.SectionTitle className={this.decorateCSS("title")}>
+                      {this.getPropValue("title")}
+                    </Base.SectionTitle>
+                  </div>
+                )}
+                {descExist && (
+                  <Base.SectionDescription className={this.decorateCSS("description")}>
+                    {this.getPropValue("description")}
+                  </Base.SectionDescription>
+                )}
+              </header>
+            )}
 
-           <div className={this.decorateCSS("content")}>
+            {cards?.length > 0 && (
+              <Base.ContainerGrid className={this.decorateCSS("cards-container")}>
+                {cards.map((card: Card, index: number) => (
+                  <Base.VerticalContent key={index} className={this.decorateCSS("card")}>
+                    <ComposerIcon
+                      name={card.icon}
+                      propsIcon={{ className: this.decorateCSS("card-icon") }}
+                    />
+                    <Base.H2 className={this.decorateCSS("card-title")}>{card.title}</Base.H2>
+                  </Base.VerticalContent>
+                ))}
+              </Base.ContainerGrid>
+            )}
 
-            <div className={this.decorateCSS("top-content")}>
-
-                <div className={this.decorateCSS("feature-item")}>
-                    <div className={this.decorateCSS("feature-circle")}>
-                     
-                    </div>
-                    <div className={this.decorateCSS("feature-contents")}>
-                        <h1 className={this.decorateCSS("title")}>
-                            {this.getPropValue("title")}
-                        </h1>
-
-                        <p className={this.decorateCSS("description")}>
-                          {this.getPropValue("description")}
-                      </p>
-                      </div>
-                </div>
-                
-                <div className={this.decorateCSS("cards")}>
-                  {this.castToObject<Cards[]>("cards").map(
-                      (card:Cards, index: number) => (
-                        <div className={this.decorateCSS("card")}>
-                            <ComposerIcon
-                              name={card.icon}
-                              propsIcon={{
-                                className: `${this.decorateCSS("card-icon")}`}}
-                            />
-                            <h1 className={this.decorateCSS("card-title")}>{card.title}</h1>
-                        </div>
-                      )
-                  )}
-                </div>
-              </div>
-
-              <div className={this.decorateCSS("bottom-content")}>
-              {this.castToObject<Logos[]>("logos").map(
-                    (logo:Logos, index: number) => (
-                      <div className={this.decorateCSS("logos")}>
-                          <img className={this.decorateCSS("logo-image")} src={logo.image} alt="logo image" />
-                      </div>
-                    )
-              )}
-              </div>
-
-           </div>
-
-        </div>
-      </div>
+            {logos?.length > 0 && (
+              <Base.ContainerGrid className={this.decorateCSS("logos-container")}>
+                {logos.map((logo: Logo, index: number) => (
+                  <Base.VerticalContent key={index} className={this.decorateCSS("logo-container")}>
+                    <ComposerLink path={logo.url}>
+                      <img className={this.decorateCSS("logo-image")} src={logo.image} alt={`logo ${index + 1}`} />
+                    </ComposerLink>
+                  </Base.VerticalContent>
+                ))}
+              </Base.ContainerGrid>
+            )}
+          </Base.VerticalContent>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
