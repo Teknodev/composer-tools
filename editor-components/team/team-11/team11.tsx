8fdd91c2c4bda54dcf6dac64d6b02fff72960255
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2024-11-03 17:03:12 +0300

    fix: standardization

--- editor-components/team/team-11/team11.tsx
+++ editor-components/team/team-11/team11.tsx
@@ -6,13 +6,6 @@ import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 import { Base } from "../../../composer-base-components/base/base";
 import ComposerSlider from "../../../composer-base-components/slider/slider";
 
-interface Card {
-  image: string;
-  name: string;
-  position: string;
-  icons: { icon: string; url: string }[];
-  getPropValue: (key: string) => any;
-}
 class Team11 extends Team {
   constructor(props?: any) {
     super(props, styles);
@@ -98,99 +91,7 @@ class Team11 extends Team {
       displayer: "Title",
       value: "Our Team",
     });
-    // let card1: TypeUsableComponentProps = {
-    //   type: "object",
-    //   key: "card",
-    //   displayer: "Card",
-    //   value: [
-    //     {
-    //       type: "image",
-    //       key: "image",
-    //       displayer: "Image",
-    //       value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b588bd2970002c6282c6?alt=media&timestamp=1719558632841",
-    //     },
-    //     {
-    //       type: "array",
-    //       key: "icons",
-    //       displayer: "Social Media Platforms",
-    //       value: [JSON.parse(JSON.stringify(twitter)), JSON.parse(JSON.stringify(facebook)), JSON.parse(JSON.stringify(instagram)), JSON.parse(JSON.stringify(youtube))],
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "name",
-    //       displayer: "Person Name",
-    //       value: "Willie Perry",
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "position",
-    //       displayer: "position",
-    //       value: "Defender",
-    //     },
-    //   ],
-    // };
-    // let card2: TypeUsableComponentProps = {
-    //   type: "object",
-    //   key: "card",
-    //   displayer: "Card",
-    //   value: [
-    //     {
-    //       type: "image",
-    //       key: "image",
-    //       displayer: "Image",
-    //       value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64370fa168c3c2002cd30833?alt=media&timestamp=1719558632843",
-    //     },
-    //     {
-    //       type: "array",
-    //       key: "icons",
-    //       displayer: "Social Media Platforms",
-    //       value: [JSON.parse(JSON.stringify(twitter)), JSON.parse(JSON.stringify(facebook)), JSON.parse(JSON.stringify(instagram)), JSON.parse(JSON.stringify(youtube))],
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "name",
-    //       displayer: "Person Name",
-    //       value: "Paul Hicks",
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "position",
-    //       displayer: "position",
-    //       value: "Defender",
-    //     },
-    //   ],
-    // };
-    // let card3: TypeUsableComponentProps = {
-    //   type: "object",
-    //   key: "card",
-    //   displayer: "Card",
-    //   value: [
-    //     {
-    //       type: "image",
-    //       key: "image",
-    //       displayer: "Image",
-    //       value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64370fa168c3c2002cd30832?alt=media&timestamp=1719558632843",
-    //     },
-    //     {
-    //       type: "array",
-    //       key: "icons",
-    //       displayer: "Social Media Platforms",
-    //       value: [JSON.parse(JSON.stringify(twitter)), JSON.parse(JSON.stringify(facebook)), JSON.parse(JSON.stringify(instagram)), JSON.parse(JSON.stringify(youtube))],
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "name",
-    //       displayer: "Person Name",
-    //       value: "Sarah Brown",
-    //     },
-    //     {
-    //       type: "string",
-    //       key: "position",
-    //       displayer: "position",
-    //       value: "Defender",
-    //     },
-    //   ],
-    // };
+
     this.addProp({
       type: "number",
       key: "itemCount",
@@ -198,12 +99,6 @@ class Team11 extends Team {
       value: 3,
       max: 3,
     });
-    // this.addProp({
-    //   type: "array",
-    //   key: "team-members",
-    //   displayer: "Team Members",
-    //   value: [JSON.parse(JSON.stringify(card1)), JSON.parse(JSON.stringify(card2)), JSON.parse(JSON.stringify(card3))],
-    // });
 
     this.addProp({
       type: "array",
@@ -444,16 +339,7 @@ class Team11 extends Team {
                 const itemNameExist = this.castToString(item.name);
                 const itemPositionExist = this.castToString(item.position);
 
-                console.log(itemNameExist, "itemNameExist");
-                console.log(itemPositionExist, "itemPositionExist");
-
-                console.log("itemNameExist:", itemNameExist);
-                console.log("itemPositionExist:", itemPositionExist);
-                console.log("item.image:", item.image);
-                console.log("item.icons:", item.icons);
-
                 const itemExits = itemNameExist || itemPositionExist || item.image || item.icons.length > 0;
-
                 return (
                   itemExits && (
                     <div key={index} className={this.decorateCSS("item")}>
