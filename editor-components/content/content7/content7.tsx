fdb49c97b03ccbf1753e02a08361f9b8400ccf3a
MacBook Pro <m.mertsolak@gmail.com>
2024-11-05 19:11:09 +0300

    fix: standardization

--- editor-components/content/content7/content7.tsx
+++ editor-components/content/content7/content7.tsx
@@ -3,16 +3,17 @@ import { BaseContent } from "../../EditorComponent";
 import styles from "./content7.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
 interface Card {
-  title: string;
+  title: JSX.Element;
   button: {
-    buttonText: string;
+    buttonText: JSX.Element;
     link: string;
   };
   next_icon: string;
   backgroundImage: string;
   foregroundImage: string;
-  description: string;
+  description: JSX.Element;
   beforeText: string;
   afterText: string;
 }
@@ -34,12 +35,6 @@ class Content7 extends BaseContent {
       value: "LOREM IPSUM",
     });
     this.addProp({
-      type: "string",
-      key: "subtitle",
-      displayer: "Subtitle",
-      value: "Lorem Ipsum",
-    });
-    this.addProp({
       type: "array",
       key: "cards",
       displayer: "Cards",
@@ -49,7 +44,6 @@ class Content7 extends BaseContent {
           key: "card",
           displayer: "Card",
           value: [
-
             {
               type: "string",
               key: "title",
@@ -60,7 +54,8 @@ class Content7 extends BaseContent {
               type: "string",
               key: "description",
               displayer: "Description",
-              value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              value:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             },
             {
               type: "object",
@@ -91,130 +86,190 @@ class Content7 extends BaseContent {
               type: "image",
               key: "backgroundImage",
               displayer: "Background Image",
-              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b8d0bd2970002c628671?alt=media&timestamp=1719561551671"
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b8d0bd2970002c628671?alt=media&timestamp=1719561551671",
             },
             {
               type: "string",
               key: "beforeText",
               displayer: "Before Text",
-              value: "Before"
+              value: "Before",
             },
             {
               type: "image",
               key: "foregroundImage",
               displayer: "Foreground Image",
-              value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b8d0bd2970002c628670?alt=media&timestamp=1719561551671"
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b8d0bd2970002c628670?alt=media&timestamp=1719561551671",
             },
             {
               type: "string",
               key: "afterText",
               displayer: "After Text",
-              value: "After"
+              value: "After",
             },
-          ]
+          ],
         },
-
-      ]
-    }
-    )
-
+      ],
+    });
   }
 
   getName(): string {
     return "Content 7";
   }
 
-  handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  handleSliderChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
     const slider = event.target as HTMLInputElement;
     const foregroundImage = document.getElementById(`foregroundImage-${index}`);
     const sliderButton = document.getElementById(`slider-button-${index}`);
     if (foregroundImage && sliderButton) {
       const sliderValue = Number(slider.value);
       foregroundImage.style.clipPath = `inset(0% 0% 0% ${sliderValue}%)`;
-      sliderButton.style.left = `calc(${slider.value}% - 18px)`;
+      sliderButton.style.left = `${slider.value}%`;
     }
   };
 
-
   render() {
-
     return (
-      <div
+      <Base.Container
         className={this.decorateCSS("container")}
         style={{
           backgroundImage: `url(${this.getPropValue("cover-image")})`,
         }}
       >
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("up-page")}>
-            <span className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</span>
-            <span className={this.decorateCSS("title")}>{this.getPropValue("title")}</span>
-          </div>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          {this.castToString(this.getPropValue("title")) && (
+            <Base.SectionTitle
+              className={
+                this.decorateCSS("title") +
+                ` ${
+                  !this.getPropValue("cover-image")
+                    ? this.decorateCSS("no-image")
+                    : ""
+                }`
+              }
+            >
+              {this.getPropValue("title")}
+            </Base.SectionTitle>
+          )}
           <div className={this.decorateCSS("down-page")}>
             {this.castToObject<Card[]>("cards").map(
               (card: Card, indexCards: number) => {
                 return (
                   <div key={indexCards} className={this.decorateCSS("card")}>
-                    <div className={this.decorateCSS("left-card")}>
-                      <span className={this.decorateCSS("title")}>{card.title}</span>
-                      <span className={this.decorateCSS("description")}>{card.description}</span>
-                      <ComposerLink path={card.button.link}>
-                        <button className={this.decorateCSS("button")}>
-                          {card.button.buttonText}
-                          <ComposerIcon
-                            name={card.next_icon}
-                            propsIcon={{ className: this.decorateCSS("icon") }}
-                          />
-                        </button>
-                      </ComposerLink>
-                    </div>
-                    <div className={this.decorateCSS("right-card")}>
-                      <div className={this.decorateCSS("image-container")}>
-                        <img src={card.backgroundImage}
-                          alt="Before"
-                          className={this.decorateCSS("background-image")}
-                        />
-                        <img
-                          src={card.foregroundImage}
-                          alt="After"
-                          className={this.decorateCSS("foreground-image")}
-                          id={`foregroundImage-${indexCards}`}
-                        />
-                        <div className={this.decorateCSS("slider-wrapper")}>
-                          <input
-                            type="range"
-                            min="1"
-                            max="100"
-                            defaultValue="50"
-                            className={this.decorateCSS("slider")}
-                            id={`slider-${indexCards}`}
-                            onChange={(e) => this.handleSliderChange(e, indexCards)}
-                            onMouseDown={() => { console.log("onMouseDown") }}
-                            onMouseUp={() => { console.log("onMouseUp") }}
+                    {(this.castToString(card.title) ||
+                      this.castToString(card.description)) && (
+                        <Base.VerticalContent
+                          className={this.decorateCSS("left-card")}
+                        >
+                          {this.castToString(card.title) && (
+                            <Base.H2 className={this.decorateCSS("title")}>
+                              {card.title}
+                            </Base.H2>
+                          )}
+                          {this.castToString(card.description) && (
+                            <Base.P className={this.decorateCSS("description")}>
+                              {card.description}
+                            </Base.P>
+                          )}
+                          {this.castToString(card.button.buttonText) && (
+                            <ComposerLink path={card.button.link}>
+                              <button className={this.decorateCSS("button")}>
+                                {card.button.buttonText}
+                                {card.next_icon && (
+                                  <ComposerIcon
+                                    name={card.next_icon}
+                                    propsIcon={{
+                                      className: this.decorateCSS("icon"),
+                                    }}
+                                  />
+                                )}
+                              </button>
+                            </ComposerLink>
+                          )}
+                        </Base.VerticalContent>
+                      )}
 
-                          />
-                          <div className={this.decorateCSS("text")}>
-                            <div className={this.decorateCSS("before-text")}>
-                              {card.beforeText}
+                    {(card.backgroundImage || card.foregroundImage) && (
+                      <div className={this.decorateCSS("right-card")}>
+                        <div
+                          className={
+                            this.decorateCSS("image-container") +
+                            ` ${
+                              card.backgroundImage && card.backgroundImage
+                                ? styles["active"]
+                                : ""
+                            }`
+                          }
+                        >
+                          {card.backgroundImage && (
+                            <img
+                              src={card.backgroundImage}
+                              alt="Before"
+                              className={this.decorateCSS("background-image")}
+                              id={`background-${indexCards}`}
+                            />
+                          )}
+
+                          {card.foregroundImage && (
+                            <img
+                              src={card.foregroundImage}
+                              alt="After"
+                              className={this.decorateCSS("foreground-image")}
+                              id={`foregroundImage-${indexCards}`}
+                            />
+                          )}
+
+                          {card.foregroundImage && card.backgroundImage && (
+                            <div className={this.decorateCSS("slider-wrapper")}>
+                              <input
+                                type="range"
+                                min="0"
+                                max="100"
+                                defaultValue="50"
+                                className={this.decorateCSS("slider")}
+                                id={`slider-${indexCards}`}
+                                onChange={(e) =>
+                                  this.handleSliderChange(e, indexCards)
+                                }
+                                onMouseDown={() => {
+                                  console.log("onMouseDown");
+                                }}
+                                onMouseUp={() => {
+                                  console.log("onMouseUp");
+                                }}
+                              />
+                              <div className={this.decorateCSS("text")}>
+                                <div
+                                  className={this.decorateCSS("before-text")}
+                                >
+                                  {card.beforeText}
+                                </div>
+                                <div className={this.decorateCSS("after-text")}>
+                                  {card.afterText}
+                                </div>
+                              </div>
+                              <div
+                                className={this.decorateCSS("slider-button")}
+                                id={`slider-button-${indexCards}`}
+                              ></div>
                             </div>
-                            <div className={this.decorateCSS("after-text")}>
-                              {card.afterText}
-                            </div>
-                          </div>
-                          <div className={this.decorateCSS("slider-button")} id={`slider-button-${indexCards}`}></div>
+                          )}
                         </div>
                       </div>
-                    </div>
+                    )}
                   </div>
                 );
               }
             )}
           </div>
-        </div>
-      </div>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
-
 
 export default Content7;
