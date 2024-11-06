4b8c886b225bf6926508b3b307c5e87e9e98984d
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2024-11-06 16:45:04 +0300

    fix: standardization

--- editor-components/modal/terms-of-use/terms-of-use.tsx
+++ editor-components/modal/terms-of-use/terms-of-use.tsx
@@ -2,6 +2,14 @@ import * as React from "react";
 import ComposerModalClose from "../../../composer-base-components/close/close";
 import { BaseModal } from "../../EditorComponent";
 import styles from "./terms-of-use.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { Base } from "../../../composer-base-components/base/base";
+
+type Button = {
+  buttonText: JSX.Element;
+  link: string;
+};
 
 class TermsOfUseModal extends BaseModal {
   constructor(props?: any) {
@@ -11,55 +19,150 @@ class TermsOfUseModal extends BaseModal {
       type: "image",
       key: "image",
       displayer: "Image",
-      value: "https://picsum.photos/200",
+      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/672b576c7acba6002c5ee19d?alt=media&timestamp=1730893727358",
     });
+
     this.addProp({
       type: "string",
-      key: "terms-of-use-title",
-      value: "Acceptance of Terms",
+      key: "title",
       displayer: "Title",
+      value: "Accept Terms and Conditions",
     });
+
     this.addProp({
       type: "string",
-      key: "term-of-use-content",
-      value: "By accessing or using our services, you acknowledge that you have read, understood, and agree to be bound by these terms, as well as any additional terms and conditions provided within our services.",
+      key: "termsDescription",
+      displayer: "Terms Description",
+      value: "To proceed, please accept our",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "termsText",
+      displayer: "Terms Text",
+      value: "Terms and Conditions",
+    });
+
+    this.addProp({
+      type: "page",
+      key: "termsLink",
+      displayer: "Terms Link",
+      value: "",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "description",
       displayer: "Description",
+      value: "It's important that you read and understand them before continuing to use our services.",
     });
+
     this.addProp({
-      type: "string",
-      key: "button-text",
-      displayer: "Button Text",
-      value: "Close",
+      type: "icon",
+      key: "exitIcon",
+      displayer: "ExitIcon",
+      value: "IoCloseSharp",
+    });
+
+    this.addProp({
+      type: "array",
+      key: "buttons",
+      displayer: "Buttons",
+      additionalParams: {
+        maxElementCount: 2,
+      },
+      value: [
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Maybe later",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Button Link",
+              value: "",
+            },
+          ],
+        },
+        {
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "string",
+              key: "buttonText",
+              displayer: "Button Text",
+              value: "Accept",
+            },
+            {
+              type: "page",
+              key: "link",
+              displayer: "Button Link",
+              value: "",
+            },
+          ],
+        },
+      ],
     });
   }
 
   getName(): string {
-    return "Terms of Use Modal";
+    return "TermsOfUseModal";
   }
 
   render() {
+    const image = this.getPropValue("image");
+    const title = this.castToString(this.getPropValue("title"));
+    const termsText = this.castToString(this.getPropValue("termsText"));
+    const termsDescription = this.castToString(this.getPropValue("termsDescription"));
+    const description = this.castToString(this.getPropValue("description"));
+    const buttons = this.castToObject<Button[]>("buttons");
+    const validButtons = buttons.filter((item: Button) => this.castToString(item.buttonText));
     return (
-      <div className={this.decorateCSS("container")} >
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")} >
-            <img
-              alt=""
-              className={this.decorateCSS("image")}
-              
-              src={this.getPropValue("image")}
-            ></img>
-            <h2 className={this.decorateCSS("first-header")} >
-              {this.getPropValue("terms-of-use-title")}
-            </h2>
-            <span className={this.decorateCSS("term-of-use-content")}>{this.getPropValue("term-of-use-content")}</span>
-            <ComposerModalClose>
-              <button className={this.decorateCSS("button")} >
-                {this.getPropValue("button-text")}
-              </button>
-            </ComposerModalClose>
-          </div>
-        </div>
-      </div>
+      <Base.Container isModal={true} className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.VerticalContent className={this.decorateCSS("content")}>
+            <div className={this.decorateCSS("top-wrapper")}>
+              <div className={this.decorateCSS("exit-icon")}>
+                <ComposerModalClose>
+                  <ComposerIcon propsIcon={{ className: this.decorateCSS("icon") }} name={this.getPropValue("exitIcon")} />
+                </ComposerModalClose>
+              </div>
+
+              {image && <img className={this.decorateCSS("image")} src={image} />}
+            </div>
+
+            <div className={this.decorateCSS("buttom-wrapper")}>
+              <div className={this.decorateCSS("second-div")}>
+                {title && <Base.H3 className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.H3>}
+                <div className={this.decorateCSS("terms-container")}>
+                  {termsDescription && <Base.P className={this.decorateCSS("termsDescription")}>{this.getPropValue("termsDescription")}</Base.P>}
+                  {termsText && <Base.H5 className={this.decorateCSS("termsText")}>{this.getPropValue("termsText")}</Base.H5>}
+                </div>
+                {description && <Base.P className={this.decorateCSS("description")}>{this.getPropValue("description")}</Base.P>}
+              </div>
+
+              {validButtons.length > 0 && (
+                <div className={this.decorateCSS("button-background")}>
+                  {validButtons.map((item: Button, index: number) => (
+                    <ComposerLink path={item.link}>
+                      <div className={this.decorateCSS("button")}>{item.buttonText}</div>
+                    </ComposerLink>
+                  ))}
+                </div>
+              )}
+            </div>
+          </Base.VerticalContent>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
