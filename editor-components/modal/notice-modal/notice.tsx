074ca10836b170850459cb339f6f01f0da3f4046
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2024-11-07 13:43:46 +0300

    feat: notice modal

--- editor-components/modal/notice-modal/notice.tsx
+++ editor-components/modal/notice-modal/notice.tsx
@@ -1,64 +1,103 @@
 import * as React from "react";
-import ComposerModalClose from "../../../composer-base-components/close/close";
 import { BaseModal } from "../../EditorComponent";
 import styles from "./notice.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import ComposerModalClose from "../../../composer-base-components/close/close";
 
 class Notice extends BaseModal {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
-      type: "image",
-      key: "image",
-      displayer: "Image",
-      value: "https://picsum.photos/200",
+      type: "icon",
+      key: "exitIcon",
+      displayer: "Exit Icon",
+      value: "IoCloseSharp",
     });
+
+    this.addProp({
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "FaFlagUsa",
+    });
+
     this.addProp({
       type: "string",
-      key: "notice-title",
-      value: "Important Announcement",
+      key: "title",
       displayer: "Title",
+      value: "VPS Servers Now Available",
     });
+
     this.addProp({
       type: "string",
-      key: "notice-content",
-      value: "Dear users, we would like to inform you about an important announcement. Due to a scheduled maintenance work on our website, there will be a temporary interruption between 22:00 and 23:59 on June 8, 2023. Please take note that there might be limitations in accessing our website during this period. This planned maintenance is being carried out to enhance the performance and user experience of our website. As a team, we are constantly working to provide you with a better service, and this maintenance work is part of our efforts.",
+      key: "subtitle",
+      displayer: "SubTitle",
+      value: "in New York",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "description",
       displayer: "Description",
+      value: "We would like to inform you that our VPS Servers, are now available to purchase in New York, United States. Fully new datacenter is now available to purchase in our order process!",
     });
+
     this.addProp({
       type: "string",
-      key: "button-text",
+      key: "buttonText",
       displayer: "Button Text",
-      value: "Okay",
+      value: "Get Started Now!",
+    });
+
+    this.addProp({
+      type: "page",
+      key: "buttonUrl",
+      displayer: "Button URL",
+      value: "",
     });
   }
 
   getName(): string {
-    return "Notice Modal";
+    return "Notice";
   }
 
   render() {
+    const icon = this.getPropValue("icon");
+    const title = this.castToString(this.getPropValue("title"));
+    const subtitle = this.castToString(this.getPropValue("subtitle"));
+    const description = this.castToString(this.getPropValue("description"));
+    const buttonText = this.castToString(this.getPropValue("buttonText"));
+
     return (
-      <div className={this.decorateCSS("container")} >
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")} >
-            <img
-              alt=""
-              className={this.decorateCSS("image")}
-              src={this.getPropValue("image")}
-            ></img>
-            <h2 className={this.decorateCSS("first-header")} >
-              {this.getPropValue("notice-title")}
-            </h2>
-            <span className={this.decorateCSS("notice-content")}>{this.getPropValue("notice-content")}</span>
-            <ComposerModalClose>
-              <button className={this.decorateCSS("button")} >
-                {this.getPropValue("button-text")}
-              </button>
-            </ComposerModalClose>
-          </div>
-        </div>
-      </div>
+      <Base.Container isModal={true} className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.VerticalContent className={this.decorateCSS("content")}>
+            <div className={this.decorateCSS("exit-icon")}>
+              <ComposerModalClose>
+                <ComposerIcon propsIcon={{ className: this.decorateCSS("exitIcon") }} name={this.getPropValue("exitIcon")} />
+              </ComposerModalClose>
+            </div>
+            {icon && (
+              <div className={this.decorateCSS("icon")}>
+                <ComposerIcon propsIcon={{ className: this.decorateCSS("icon-element") }} name={icon} />
+              </div>
+            )}
+            {title && <Base.H2 className={this.decorateCSS("title")}>{title}</Base.H2>}
+            {subtitle && <Base.H2 className={this.decorateCSS("subtitle")}>{subtitle}</Base.H2>}
+
+            {description && <Base.P className={this.decorateCSS("description")}>{description}</Base.P>}
+
+            <div className={this.decorateCSS("button-container")}>
+              <ComposerLink path={this.getPropValue("buttonUrl")}>
+                <button className={this.decorateCSS("button-element")}>{buttonText}</button>
+              </ComposerLink>
+            </div>
+          </Base.VerticalContent>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
