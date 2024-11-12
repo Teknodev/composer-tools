1b093866a5dc5bdb665a1e490a946df5d266ea52
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2024-11-12 10:43:26 +0300

    fix: change header elements and image delete condition

--- editor-components/team/team-3/team3.tsx
+++ editor-components/team/team-3/team3.tsx
@@ -319,8 +319,8 @@ class Team3 extends Team {
           <Base.VerticalContent className={this.decorateCSS("page")}>
             <Base.VerticalContent>
               <Base.VerticalContent className={this.decorateCSS("text-group")}>
-                {titleValue && <Base.H1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.H1>}
-                {subTitleValue && <Base.P className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</Base.P>}
+                {titleValue && <Base.SectionSubTitle className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.SectionSubTitle>}
+                {subTitleValue && <Base.SectionTitle className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</Base.SectionTitle>}
               </Base.VerticalContent>
             </Base.VerticalContent>
             <Base.ListGrid gridCount={{ pc: this.getPropValue("itemCount"), tablet: 1, phone: 1 }} className={this.decorateCSS("down-page")}>
@@ -333,8 +333,10 @@ class Team3 extends Team {
                     <div className={this.decorateCSS("card")}>
                       <div className={this.decorateCSS("card-items")}>
                         <div className={this.decorateCSS("item-content")}>
-                          <div className={this.decorateCSS("box-image")}>{item.profile && <img className={this.decorateCSS("profile-image")} src={item.profile} alt="" />}</div>
-                          <Base.VerticalContent className={this.decorateCSS("box-text")}>
+                          <div className={this.decorateCSS("box-image")}>
+                            <img className={item.profile ? this.decorateCSS("profile-image") : this.decorateCSS("no-image")} src={item.profile} alt="" />
+                          </div>
+                          <Base.VerticalContent className={item.profile ? this.decorateCSS("box-text") : this.decorateCSS("no-image-box")}>
                             <Base.H2 className={this.decorateCSS("item-name")}>{item.name}</Base.H2>
                             <Base.P className={this.decorateCSS("item-position")}>{item.position}</Base.P>
                             <div className={this.decorateCSS("icon-group")}>
