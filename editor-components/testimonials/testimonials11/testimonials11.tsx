00021c0bf7e20d9a1f33024802a9454cd7b9ec1e
Burcu Açan <burcuuacann@gmail.com>
2024-11-12 18:58:50 +0300

    fix: images and texts is centred

--- editor-components/testimonials/testimonials11/testimonials11.tsx
+++ editor-components/testimonials/testimonials11/testimonials11.tsx
@@ -39,26 +39,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623daf?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Sarah Lewin",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Project manager in Pixels",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "I think if you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long.",
             },
@@ -72,26 +72,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623db0?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Gregory Tabot",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Photographer",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "The principal element of Suprematism in painting, as in architecture, is its liberation from all social or materialist tendencies. Through Suprematism, art comes into its pure and unpolluted form.",
             },
@@ -105,26 +105,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623db1?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Jess Simpson",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Team lead in TBB",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "I think if you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long.",
             },
@@ -138,26 +138,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623db1?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Jess Simpson",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Team lead in TBB",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "I think if you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long.",
             },
@@ -171,26 +171,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623db1?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Jess Simpson",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Team lead in TBB",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "I think if you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long.",
             },
@@ -204,26 +204,26 @@ class Testimonials11Page extends Testimonials {
             {
               type: "image",
               key: "image",
-              displayer: "Card Image",
+              displayer: "Author Image",
               value:
                 "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617822bd2970002c623db1?alt=media&timestamp=1719483639150",
             },
             {
               type: "string",
               key: "title",
-              displayer: "Card Title",
+              displayer: "Author Name",
               value: "Jess Simpson",
             },
             {
               type: "string",
               key: "subtitle",
-              displayer: "Card Subtitle",
+              displayer: "Author Position",
               value: "Team lead in TBB",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Card Description",
+              displayer: "Review Text",
               value:
                 "I think if you do something and it turns out pretty good, then you should go do something else wonderful, not dwell on it for too long.",
             },
