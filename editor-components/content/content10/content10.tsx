441d2368d30d2851e2599c298dea7843bbdd6075
MacBook Pro <m.mertsolak@gmail.com>
2024-11-13 16:41:30 +0300

    fix: standardization

--- editor-components/content/content10/content10.tsx
+++ editor-components/content/content10/content10.tsx
@@ -2,30 +2,38 @@ import * as React from "react";
 import { BaseContent } from "../../EditorComponent";
 import styles from "./content10.module.scss";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
-
+import { Base } from "../../../composer-base-components/base/base";
+interface Card {
+  icon: string;
+  title: JSX.Element;
+  subtitle: JSX.Element;
+  description: JSX.Element;
+}
 class Content10 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
-      type: "string",
+      type: "video",
       displayer: "Video",
       key: "video",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e75d50181a1002c334f4f?alt=media&timestamp=1719563750188"
+      value:
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e75d50181a1002c334f4f?alt=media&timestamp=1719563750188",
     });
 
     this.addProp({
       type: "image",
       displayer: "Cover image of video",
       key: "cover_image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b9b7bd2970002c6286c7?alt=media&timestamp=1719561551671"
+      value:
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b9b7bd2970002c6286c7?alt=media&timestamp=1719561551671",
     });
 
     this.addProp({
       type: "icon",
       displayer: "Icon of play button",
       key: "play_icon",
-      value: "FaPlay"
+      value: "FaPlay",
     });
 
     this.addProp({
@@ -42,27 +50,28 @@ class Content10 extends BaseContent {
               type: "icon",
               key: "icon",
               displayer: "Icon",
-              value: "TbChartHistogram"
+              value: "BsFillHandThumbsUpFill",
             },
             {
               type: "string",
               key: "subtitle",
               displayer: "Subtitle",
-              value: "Future One"
+              value: "Future One",
             },
             {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "EFECTIVITY FIRST"
+              value: "EFECTIVITY FIRST",
             },
             {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi."
-            }
-          ]
+              value:
+                "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi.",
+            },
+          ],
         },
         {
           type: "object",
@@ -73,27 +82,28 @@ class Content10 extends BaseContent {
               type: "icon",
               key: "icon",
               displayer: "Icon",
-              value: "GrClose"
+              value: "BsFillHandThumbsUpFill",
             },
             {
               type: "string",
               key: "subtitle",
               displayer: "Subtitle",
-              value: "Future Two"
+              value: "Future Two",
             },
             {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "NO SIDE EFFECT"
+              value: "NO SIDE EFFECT",
             },
             {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi."
-            }
-          ]
+              value:
+                "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi.",
+            },
+          ],
         },
         {
           type: "object",
@@ -104,27 +114,28 @@ class Content10 extends BaseContent {
               type: "icon",
               key: "icon",
               displayer: "Icon",
-              value: "GiFruitBowl"
+              value: "BsFillHandThumbsUpFill",
             },
             {
               type: "string",
               key: "subtitle",
               displayer: "Subtitle",
-              value: "Future Three"
+              value: "Future Three",
             },
             {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "ORGANIC PRODUCT"
+              value: "ORGANIC PRODUCT",
             },
             {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi."
-            }
-          ]
+              value:
+                "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi.",
+            },
+          ],
         },
         {
           type: "object",
@@ -135,34 +146,33 @@ class Content10 extends BaseContent {
               type: "icon",
               key: "icon",
               displayer: "Icon",
-              value: "GiScooter"
+              value: "BsFillHandThumbsUpFill",
             },
             {
               type: "string",
               key: "subtitle",
               displayer: "Subtitle",
-              value: "Future Four"
+              value: "Future Four",
             },
             {
               type: "string",
               key: "title",
               displayer: "Title",
-              value: "QUICK DELEVARY"
+              value: "QUICK DELEVARY",
             },
             {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi."
-            }
-          ]
-        }
-      ]
-    })
+              value:
+                "Nostra netus porttitor cursus Imperdiet proin. Vestibulum malesuada faucibus. Quam vivamus eu consectetuer praesent phasellus sapien dis natoque dignissim imperdiet morbi.",
+            },
+          ],
+        },
+      ],
+    });
 
     this.setComponentState("is_video_visible", false);
-
-
   }
 
   getName(): string {
@@ -171,34 +181,81 @@ class Content10 extends BaseContent {
 
   render() {
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("cards")}>
-            {this.getPropValue("cards").map((card: any, index: number) => <div key={index} className={this.decorateCSS("card")}>
-              <div className={this.decorateCSS("icon-box")}>
-                <ComposerIcon name={card.getPropValue("icon")} propsIcon={{ className: this.decorateCSS("icon") }} />
-              </div>
-              <div className={this.decorateCSS("labels")}>
-                <span className={this.decorateCSS("subtitle")}>{card.getPropValue("subtitle")}</span>
-                <h1 className={this.decorateCSS("title")}>{card.getPropValue("title")}</h1>
-                <p className={this.decorateCSS("description")}>{card.getPropValue("description")}</p>
-              </div>
-            </div>)}
-          </div>
-          <div className={this.decorateCSS("video-container")}>
-            <img className={this.decorateCSS("image")} src={this.getPropValue("cover_image")} alt="Cover image" />
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.ListGrid
+            gridCount={{ pc: 2 }}
+            className={this.decorateCSS("cards")}
+          >
+            {this.castToObject<Card[]>("cards").map(
+              (card: Card, index: number) => (
+                <div key={index} className={this.decorateCSS("card")}>
+                  {card.icon && (
+                    <div className={this.decorateCSS("icon-box")}>
+                      <ComposerIcon
+                        name={card.icon}
+                        propsIcon={{ className: this.decorateCSS("icon") }}
+                      />
+                    </div>
+                  )}
+                  <Base.VerticalContent className={this.decorateCSS("labels")}>
+                    {this.castToString(card.subtitle) && (
+                      <Base.H3 className={this.decorateCSS("subtitle")}>
+                        {card.subtitle}
+                      </Base.H3>
+                    )}
+                    {this.castToString(card.title) && (
+                      <Base.H2 className={this.decorateCSS("title")}>
+                        {card.title}
+                      </Base.H2>
+                    )}
+                    {this.castToString(card.description) && (
+                      <Base.P className={this.decorateCSS("description")}>
+                        {card.description}
+                      </Base.P>
+                    )}
+                  </Base.VerticalContent>
+                </div>
+              )
+            )}
+          </Base.ListGrid>
+          {this.getPropValue("video") && (
+            <div className={this.decorateCSS("video-container")}>
+              <img
+                className={this.decorateCSS("image")}
+                src={this.getPropValue("cover_image")}
+                alt="Cover image"
+              />
 
-            <div className={this.decorateCSS("play-icon-box")} onClick={() => {
-              this.setComponentState("is_video_visible", true)
-            }}>
-              <ComposerIcon name={this.getPropValue("play_icon")} propsIcon={{ className: this.decorateCSS("play-icon") }} />
+              <div
+                className={this.decorateCSS("play-icon-box")}
+                onClick={() => {
+                  this.setComponentState("is_video_visible", true);
+                }}
+              >
+                <ComposerIcon
+                  name={this.getPropValue("play_icon")}
+                  propsIcon={{ className: this.decorateCSS("play-icon") }}
+                />
+              </div>
             </div>
-          </div>
-          {this.getComponentState("is_video_visible") && <div className={this.decorateCSS("video")} onClick={() => this.setComponentState("is_video_visible", false)}>
-            <video onClick={(event) => event.stopPropagation()} controls className={this.decorateCSS("player")} src={this.getPropValue("video")}></video>
-          </div>}
-        </div>
-      </div>
+          )}
+
+          {this.getComponentState("is_video_visible") && (
+            <div
+              className={this.decorateCSS("video")}
+              onClick={() => this.setComponentState("is_video_visible", false)}
+            >
+              <video
+                onClick={(event) => event.stopPropagation()}
+                controls
+                className={this.decorateCSS("player")}
+                src={this.getPropValue("video")}
+              ></video>
+            </div>
+          )}
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
