99232d25f0c3b0f7ad0843dfe573f7c9a9b1fbe4
MacBook Pro <m.mertsolak@gmail.com>
2024-11-19 11:05:33 +0300

    fix: standardization

--- editor-components/content/content11/content11.tsx
+++ editor-components/content/content11/content11.tsx
@@ -3,15 +3,20 @@ import { BaseContent } from "../../EditorComponent";
 import styles from "./content11.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
+import { Type } from "react-toastify/dist/utils";
 
-
+interface ListItem {
+  title: string;
+  description: string;
+}
 class Content11 extends BaseContent {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
       type: "string",
-      key: "box",
+      key: "sectionTitle",
       displayer: "title",
       value: "ABOUT",
     });
@@ -20,14 +25,15 @@ class Content11 extends BaseContent {
       type: "icon",
       key: "icon",
       displayer: "Icon",
-      value: "GoChevronDown"
-    })
+      value: "GoChevronDown",
+    });
 
     this.addProp({
       type: "image",
       key: "image",
       displayer: "Image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b9f9bd2970002c6286f3?alt=media&timestamp=1719564173697"
+      value:
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661b9f9bd2970002c6286f3?alt=media&timestamp=1719564173697",
     });
 
     this.addProp({
@@ -50,9 +56,10 @@ class Content11 extends BaseContent {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum"
-            }
-          ]
+              value:
+                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique nulla, tenetur necessitatibus asperiores voluptatum hic animi natus nisi quaerat cumque tempora laudantium ad voluptas dolorem neque repellendus totam aperiam alias! Similique nulla, tenetur necessitatibus asperiores voluptatum",
+            },
+          ],
         },
         {
           type: "object",
@@ -69,9 +76,10 @@ class Content11 extends BaseContent {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?"
-            }
-          ]
+              value:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?",
+            },
+          ],
         },
         {
           type: "object",
@@ -88,12 +96,12 @@ class Content11 extends BaseContent {
               type: "string",
               displayer: "Description",
               key: "description",
-              value: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?"
-            }
-          ]
-        }
-      ]
-
+              value:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quibusdam similique expedita, unde tempore necessitatibus possimus maiores corrupti nostrum. Sed quos culpa vero labore magnam, impedit asperiores ullam inventore quidem?",
+            },
+          ],
+        },
+      ],
     });
 
     this.addProp({
@@ -109,8 +117,7 @@ class Content11 extends BaseContent {
             {
               type: "icon",
               key: "icon",
-              value:
-                "FaFacebook",
+              value: "FaFacebook",
               displayer: "Icon",
             },
             {
@@ -129,8 +136,7 @@ class Content11 extends BaseContent {
             {
               type: "icon",
               key: "icon",
-              value:
-                "FaInstagram",
+              value: "FaInstagram",
               displayer: "Icon",
             },
             {
@@ -149,8 +155,7 @@ class Content11 extends BaseContent {
             {
               type: "icon",
               key: "icon",
-              value:
-                "FaPinterest",
+              value: "FaPinterest",
               displayer: "Icon",
             },
             {
@@ -169,8 +174,7 @@ class Content11 extends BaseContent {
             {
               type: "icon",
               key: "icon",
-              value:
-                "FaLinkedin",
+              value: "FaLinkedin",
               displayer: "Icon",
             },
             {
@@ -189,54 +193,71 @@ class Content11 extends BaseContent {
   }
   render() {
     const image = this.getPropValue("image");
-    const title = this.getPropValue("box");
+    const title = this.getPropValue("sectionTitle");
     const icon = this.getPropValue("icon");
     const rightItems = this.getPropValue("right-items");
 
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          {title && <div className={this.decorateCSS("boxs-text-left")}>
-            <div className={this.decorateCSS("box-up")}>
-              <h2 className={this.decorateCSS("box")}>{this.getPropValue("box")}</h2>
-            </div>
-            {icon && <div className={this.decorateCSS("text-left")}>
-              <ComposerIcon
-                propsIcon={{ className: this.decorateCSS("text") }}
-                name={this.getPropValue("icon")}
-              />
-
-            </div>}
-          </div>}
-          <div className={this.decorateCSS("content")}>
-            {image && <div className={this.decorateCSS("image-box")}>
-              <img className={this.decorateCSS("image")} src={this.getPropValue("image")} alt="faq image" />
-            </div>}
-            <div className={this.decorateCSS("content-right")}>
-              {this.castToObject<Array<{ title: string, description: string }>>("items").map(item =>
-                <div className={this.decorateCSS("item")}>
-                  <h2 className={this.decorateCSS("title")}>{item.title}</h2>
-                  <p className={this.decorateCSS("description")}>{item.description}</p>
-                </div>
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          {title && (
+            <Base.VerticalContent className={this.decorateCSS("heading")}>
+              <Base.SectionTitle className={this.decorateCSS("section-title")}>
+                {this.getPropValue("sectionTitle")}
+              </Base.SectionTitle>
+              {icon && (
+                <ComposerIcon
+                  propsIcon={{ className: this.decorateCSS("icon") }}
+                  name={this.getPropValue("icon")}
+                />
               )}
+            </Base.VerticalContent>
+          )}
+          <Base.ContainerGrid className={this.decorateCSS("content")}>
+            {image && (
+              <Base.GridCell className={this.decorateCSS("image-box")}>
+                <img
+                  className={this.decorateCSS("image")}
+                  src={image}
+                  alt={this.castToString(title)}
+                />
+              </Base.GridCell>
+            )}
+            <Base.GridCell className={this.decorateCSS("content-right")}>
+              {this.castToObject<ListItem[]>("items").map((item) => (
+                <Base.VerticalContent className={this.decorateCSS("item")}>
+                  {item.title && (
+                    <Base.H2 className={this.decorateCSS("title")}>
+                      {item.title}
+                    </Base.H2>
+                  )}
+                  {item.description && (
+                    <Base.P className={this.decorateCSS("description")}>
+                      {item.description}
+                    </Base.P>
+                  )}
+                </Base.VerticalContent>
+              ))}
+            </Base.GridCell>
+          </Base.ContainerGrid>
+          {rightItems.length > 0 && (
+            <div className={this.decorateCSS("icons")}>
+              {rightItems.map((leftItem: any) => {
+                return (
+                  leftItem.value[0].value && (
+                    <ComposerLink path={leftItem.value[1].value}>
+                      <ComposerIcon
+                        propsIcon={{ className: this.decorateCSS("Icon") }}
+                        name={leftItem.value[0].value}
+                      />
+                    </ComposerLink>
+                  )
+                );
+              })}
             </div>
-          </div>
-          {rightItems.length > 0 && <div className={this.decorateCSS("icons")}>
-            {rightItems.map((leftItem: any) => {
-              return (
-                <ComposerLink path={leftItem.value[1].value}>
-                  <ComposerIcon
-                    propsIcon={{ className: this.decorateCSS("Icon") }}
-                    name={leftItem.value[0].value}
-                  />
-                </ComposerLink>
-              );
-            })}
-          </div>}
-
-        </div>
-      </div>
-
+          )}
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
