098969dfea158c17285539d87e23e1306bf7aa07
dogusmiuw <dceliknodes@gmail.com>
2024-11-27 18:40:31 +0300

    feat: structure

--- editor-components/feature/feature8/feature8.tsx
+++ editor-components/feature/feature8/feature8.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature8.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 type Card = {
-  image: string;
+  icon: string;
   title: JSX.Element;
   description: JSX.Element;
 };
@@ -13,117 +13,95 @@ type Card = {
 class Feature8 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
+
     this.addProp({
       type: "string",
       key: "title",
-      value: "Card",
-      displayer: "Card Title",
+      displayer: "TITLE",
+      value: "Collections are your most important pages, but are frustrating to manage",
     });
-    this.addProp({
-      type: "string",
-      key: "buttonText",
-      value: "Learn More",
-      displayer: "Button Text",
-    });
-    this.addProp({
-      type: "page",
-      key: "buttonLink",
-      displayer: "Button Link",
-      value: "",
-    });
+
     this.addProp({
       type: "array",
       key: "cards",
-      displayer: "Cards",
+      displayer: "CARDS",
       value: [
         {
           type: "object",
           key: "card",
-          displayer: "Card",
+          displayer: "CARD",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "ICON",
+              value: "FaHandPointer",
+            },
             {
               type: "string",
               key: "title",
-              value: "Explore the Latest Tech Gadgets",
-              displayer: "Title",
+              displayer: "TITLE",
+              value: "Non-visual Curation",
             },
             {
               type: "string",
               key: "description",
-              value:
-                "From the newest smartphones to the latest smart home devices, our website features a wide range of tech gadgets to suit every need and budget. Browse our cards to discover the latest tech innovations and stay up-to-date with the ever-evolving world of technology.",
-              displayer: "Description",
+              displayer: "DESCRIPTION",
+              value: "Manually curating your collections feels like doing your taxes.",
             },
-            {
-              type: "image",
-              key: "image",
-              value:
-                "https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg",
-              displayer: "Image",
-            },
-          ],
+          ]
         },
         {
           type: "object",
           key: "card",
-          displayer: "Card",
+          displayer: "CARD",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "ICON",
+              value: "FaArrowsAltV",
+            },
             {
               type: "string",
               key: "title",
-              value: "Top 5 Hiking Trails in the Pacific Northwest",
-              displayer: "Title",
+              displayer: "TITLE",
+              value: "Limited Sorting",
             },
             {
               type: "string",
               key: "description",
-              value:
-                "If you love hiking and exploring the great outdoors, you won't want to miss these top 5 hiking trails in the Pacific Northwest. From scenic coastal hikes to challenging mountain treks, these trails offer stunning views and unforgettable experiences.",
-              displayer: "Description",
+              displayer: "DESCRIPTION",
+              value: "It's too basic to sort your collections by 'New In' or 'Best Sellers'.",
             },
-            {
-              type: "image",
-              key: "image",
-              value:
-                "https://koa.com/blog/images/diablo-lake-north-cascades-national-park.jpg?preset=blogPhoto",
-              displayer: "Image",
-            },
-          ],
+          ]
         },
         {
           type: "object",
           key: "card",
-          displayer: "Card",
+          displayer: "CARD",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "ICON",
+              value: "RiFoldersFill",
+            },
             {
               type: "string",
               key: "title",
-              value: "5 Easy DIY Home Decor Projects",
-              displayer: "Title",
+              displayer: "TITLE",
+              value: "Messy Data",
             },
             {
               type: "string",
               key: "description",
-              value:
-                "Want to give your home a fresh new look without breaking the bank? these 5 easy DIY home decor projects that you can do yourself! From creating a gallery wall to adding a pop of color with a painted accent piece, these projects are simple and affordable update home decor.",
-              displayer: "Description",
+              displayer: "DESCRIPTION",
+              value: "You need 5 tabs open to take data-driven decisions from excel sheets.",
             },
-            {
-              type: "image",
-              key: "image",
-              value:
-                "https://imageio.forbes.com/specials-images/imageserve/6123b033133106144df23946/0x0.jpg?format=jpg&width=1200",
-              displayer: "Image",
-            },
-          ],
+          ]
         },
       ],
-    });
-    this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item count in a row",
-      value: 3,
     });
   }
 
@@ -132,46 +110,51 @@ class Feature8 extends BaseFeature {
   }
 
   render() {
+    const titleExist = !!this.getPropValue("title", { as_string: true });
+    const title = this.getPropValue("title");
+
     const cards = this.castToObject<Card[]>("cards");
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
-          <Base.VerticalContent className={this.decorateCSS("wrapper")}>
+          {titleExist && (
             <Base.SectionTitle className={this.decorateCSS("section-title")}>
-              {this.getPropValue("title")}
+              {title}
             </Base.SectionTitle>
-
+          )}
+          {cards?.length > 0 && (
             <Base.ContainerGrid className={this.decorateCSS("cards-container")}>
-              {cards.map((item: Card, index: number) => {
-                const titleExist = !!this.castToString(item.title);
-                const descExist = !!this.castToString(item.description);
+              {cards.map((card, i) => {
+                const titleExist = !!this.castToString(card.title);
+                const descExist = !!this.castToString(card.description);
 
                 return (
-                  <Base.GridCell
-                    key={index}
+                  <Base.VerticalContent
+                    key={i}
                     className={this.decorateCSS("card")}
-                    style={{ width: 90 / this.getPropValue("itemCount") + "%" }}
                   >
-                    {!!item.image && <img className={this.decorateCSS("image")} src={item.image} alt="" />}
-                    {titleExist && <Base.H2 className={this.decorateCSS("title")}>{item.title}</Base.H2>}
+                    {!!card.icon && (
+                      <ComposerIcon
+                        name={card.icon}
+                        propsIcon={{ className: this.decorateCSS("icon") }}
+                      />
+                    )}
+                    {titleExist && (
+                      <Base.H2 className={this.decorateCSS("title")}>
+                        {card.title}
+                      </Base.H2>
+                    )}
                     {descExist && (
                       <Base.P className={this.decorateCSS("description")}>
-                        {item.description}
+                        {card.description}
                       </Base.P>
                     )}
-                  </Base.GridCell>
+                  </Base.VerticalContent>
                 );
-              }
-              )}
+              })}
             </Base.ContainerGrid>
-
-            <div className={this.decorateCSS("button")}>
-              <ComposerLink path={this.getPropValue("buttonLink")}>
-                {this.getPropValue("buttonText")}
-              </ComposerLink>
-            </div>
-          </Base.VerticalContent>
+          )}
         </Base.MaxContent>
       </Base.Container>
     );
