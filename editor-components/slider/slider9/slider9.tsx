e4edbb14fe4e068ed8d870cccce6f208a5852cf2
Serkan Ozdemir <serkanozdemir38080@gmail.com>
2024-12-03 12:05:52 +0300

    init

--- editor-components/slider/slider9/slider9.tsx
+++ editor-components/slider/slider9/slider9.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { BaseSlider, TypeUsableComponentProps } from "../../EditorComponent";
 import styles from "./slider9.module.scss";
-
 import ComposerSlider from "../../../composer-base-components/slider/slider";
+import { Base } from "../../../composer-base-components/base/base";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 class Slider9 extends BaseSlider {
   constructor(props?: any) {
@@ -22,88 +23,194 @@ class Slider9 extends BaseSlider {
     });
     this.addProp({
       type: "string",
-      key: "under-title-text",
-      displayer: "Under Title Text",
-      value: "SKU: 700.954.29",
-    });
-    this.addProp({
-      type: "string",
-      key: "pricing",
-      displayer: "Text",
+      key: "price",
+      displayer: "Price",
       value: "$100",
     });
     this.addProp({
       type: "array",
-      key: "slider",
-      displayer: "Slider",
+      key: "sliderItems",
+      displayer: "Slider Items",
       value: [
         {
           type: "image",
           key: "image",
           displayer: "Image",
           value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cceebd2970002c629755?alt=media&timestamp=1719584962578",
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc6d506a40002c305aa6?alt=media",
         },
         {
           type: "image",
           key: "image",
           displayer: "Image",
           value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6661cceebd2970002c629754?alt=media&timestamp=1719584962578",
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc83506a40002c305ab2?alt=media",
+        },
+        {
+          type: "image",
+          key: "image",
+          displayer: "Image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc99506a40002c305abc?alt=media",
+        },
+        {
+          type: "image",
+          key: "image",
+          displayer: "Image",
+          value:
+            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dccaa506a40002c305ac7?alt=media",
         },
       ],
     });
+    this.addProp({
+      type: "icon",
+      key: "previousArrow",
+      displayer: "Previous Arrow Icon",
+      value: "MdOutlineKeyboardArrowUp"
+    });
+    this.addProp({
+      type: "icon",
+      key: "nextArrow",
+      displayer: "Next Arrow Icon",
+      value: "MdOutlineKeyboardArrowDown"
+    });
+
+    this.setComponentState("slider-ref", React.createRef());
+    this.setComponentState("currentSlideIndex", 0);
   }
+
+  componentDidUpdate(prevProps: any, prevState: any) {
+    const currentSlideIndex = this.getComponentState("currentSlideIndex");
+    const sliderRef = this.getComponentState("slider-ref");
+
+    // Eğer currentSlideIndex değiştiyse HorizontalSlider'ı güncelle
+    if (currentSlideIndex !== prevState.currentSlideIndex) {
+      if (sliderRef.current) {
+        sliderRef.current.slickGoTo(currentSlideIndex);
+      }
+    }
+  }
+
   getName(): string {
     return "Slider 9";
   }
+
   render() {
-    const settings = {
+    const verticalSettings = {
       arrows: false,
-      dots: true,
+      dots: false,
+      infinite: false,
+      speed: 700,
+      autoplay: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      vertical: true,
+    };
+
+    const horizontalSettings = {
+      arrows: true,
+      dots: false,
       infinite: true,
       speed: 500,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
     };
+
+    const title = this.getPropValue("title");
+    const price = this.getPropValue("price");
+    const description = this.getPropValue("description");
+    const nextArrow = this.getPropValue("nextArrow");
+    const previousArrow = this.getPropValue("previousArrow");
+
+    const sliderRef = this.getComponentState("slider-ref");
+    const currentSlideIndex = this.getComponentState("currentSlideIndex");
+
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("slider-parent")}>
-            <ComposerSlider
-              {...settings}
-              className={this.decorateCSS("carousel")}
-            >
-              {this.getPropValue("slider").map(
-                (item: any, indexSlider: number) => (
-                  <img
-                    src={item.value}
-                    alt=""
-                    className={this.decorateCSS("img")}
-                    key={indexSlider}
-                  />
-                )
-              )}
-            </ComposerSlider>
-            <div className={this.decorateCSS("right-content")}>
-              <h3 className={this.decorateCSS("title")}>
-                {this.getPropValue("title")}
-              </h3>
-              <p className={this.decorateCSS("undertitle")}>
-                {this.getPropValue("under-title-text")}
-              </p>
-              <h5 className={this.decorateCSS("pricetag")}>
-                {this.getPropValue("pricing")}
-              </h5>
-              <p className={this.decorateCSS("description")}>
-                {this.getPropValue("description")}
-              </p>
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.ContainerGrid className={this.decorateCSS("content")}>
+            <div className={this.decorateCSS("sliders-parent")}>
+              <div className={this.decorateCSS("vertical-parent")}>
+
+                <ComposerSlider
+                  {...verticalSettings}
+                  className={this.decorateCSS("carousel-vertical")}
+                  ref={sliderRef}
+                >
+                  {this.getPropValue("sliderItems").map(
+                    (item: any, indexSlider: number) => (
+                      <div className={this.decorateCSS("img-container")}
+                        onClick={() => this.setComponentState("currentSlideIndex", indexSlider)}
+                        key={indexSlider}
+                      >
+                        <img
+                          src={item.value}
+                          alt=""
+                          className={this.decorateCSS("img")}
+                        />
+                      </div>
+                    )
+                  )}
+                </ComposerSlider>
+                {(previousArrow || nextArrow) &&
+                  <div className={this.decorateCSS("arrows")}>
+                    {previousArrow &&
+                      <ComposerIcon
+                        name={this.getPropValue("previousArrow")}
+                        propsIcon={{
+                          className: this.decorateCSS("prevArrow"),
+                          onClick: () => {
+                            sliderRef.current.slickPrev();
+                          },
+                        }}
+                      />}
+                    {nextArrow &&
+                      <ComposerIcon
+                        name={this.getPropValue("nextArrow")}
+                        propsIcon={{
+                          className: this.decorateCSS("nextArrow"),
+                          onClick: () => {
+                            sliderRef.current.slickNext();
+                          },
+                        }}
+                      />}
+                  </div>}
+              </div>
+
+              <div className={this.decorateCSS("horizontal-parent")}>
+                <ComposerSlider
+                  {...horizontalSettings}
+                  className={this.decorateCSS("carousel-horizontal")}
+                  ref={sliderRef}
+                >
+                  {this.getPropValue("sliderItems").map(
+                    (item: any, indexSlider: number) => (
+                      <div className={this.decorateCSS("img-container")} key={indexSlider}>
+                        <img
+                          src={item.value}
+                          alt=""
+                          className={this.decorateCSS("img")}
+                        />
+                      </div>
+                    )
+                  )}
+                </ComposerSlider>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+            <Base.VerticalContent className={this.decorateCSS("right-content")}>
+              <Base.SectionTitle className={this.decorateCSS("title")}>
+                {title}
+              </Base.SectionTitle>
+              <span className={this.decorateCSS("price")}>{price}</span>
+              <Base.P className={this.decorateCSS("description")}>
+                {description}
+              </Base.P>
+            </Base.VerticalContent>
+          </Base.ContainerGrid>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
