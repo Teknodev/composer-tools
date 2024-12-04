3908a274e17684ce01d7c19d5bcbeb2cab936fad
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2024-12-04 16:57:30 +0300

    Revert "Image gallery 7 (#549)"
    This reverts commit e35cf5da3bbb1fe9496bc537722cc41acc92249b.

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -206,7 +206,6 @@ import NavbarComponent9 from "./navigators/navbar9/navbar9";
 import ImageGalleryComponent1 from "./Image_gallery/image_gallery_1/image-gallery1";
 import ImageGalleryComponent2 from "./Image_gallery/image_gallery_2/image-gallery2";
 import ImageGalleryCompanent5 from "./Image_gallery/image_gallery_5/image-gallery5";
-import ImageGalleryCompanent7 from "./Image_gallery/image_gallery_7/image-gallery7";
 import NotificationModal1 from "./modal/notification-modal1/notificationmodal1";
 import ImageGalleryComponent4 from "./Image_gallery/image_gallery_4/image-gallery4";
 import TermsOfUseModal from "./modal/terms-of-use/terms-of-use";
@@ -350,7 +349,6 @@ function Registerables(composer: ComponentsRegistery) {
     new ImageGalleryComponent1(),
     new ImageGalleryComponent2(),
     new ImageGalleryCompanent5(),
-    new ImageGalleryCompanent7(),
     new ImageGalleryComponent4(),
     new FeatureComponent1(),
     new FeatureComponent2(),
