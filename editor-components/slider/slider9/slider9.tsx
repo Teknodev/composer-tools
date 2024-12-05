21d4fc950f662737b4f89ff8fb44a6de84e5ff95
Serkan Ozdemir <serkanozdemir38080@gmail.com>
2024-12-05 20:16:10 +0300

    feat: fullscreen image func. , icons, standardization

--- editor-components/slider/slider9/slider9.tsx
+++ editor-components/slider/slider9/slider9.tsx
@@ -5,6 +5,11 @@ import ComposerSlider from "../../../composer-base-components/slider/slider";
 import { Base } from "../../../composer-base-components/base/base";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
+type SliderItem = {
+  image: string;
+  video: string;
+}
+
 class Slider9 extends BaseSlider {
 
   constructor(props?: any) {
@@ -34,35 +39,92 @@ class Slider9 extends BaseSlider {
       displayer: "Slider Items",
       value: [
         {
-          type: "image",
-          key: "image",
-          displayer: "Image",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc6d506a40002c305aa6?alt=media",
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc6d506a40002c305aa6?alt=media",
+            },
+            {
+              type: "video",
+              key: "video",
+              displayer: "Video",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e77bd0181a1002c334f66?alt=media&timestamp=1719564238038",
+            },
+          ],
         },
         {
-          type: "image",
-          key: "image",
-          displayer: "Image",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc83506a40002c305ab2?alt=media",
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc83506a40002c305ab2?alt=media",
+            },
+            {
+              type: "video",
+              key: "video",
+              displayer: "Video",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e77bd0181a1002c334f66?alt=media&timestamp=1719564238038",
+            },
+          ],
         },
         {
-          type: "image",
-          key: "image",
-          displayer: "Image",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc99506a40002c305abc?alt=media",
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dcc99506a40002c305abc?alt=media",
+            },
+            {
+              type: "video",
+              key: "video",
+              displayer: "Video",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e77bd0181a1002c334f66?alt=media&timestamp=1719564238038",
+            },
+          ],
         },
         {
-          type: "image",
-          key: "image",
-          displayer: "Image",
-          value:
-            "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dccaa506a40002c305ac7?alt=media",
+          type: "object",
+          key: "item",
+          displayer: "Item",
+          value: [
+            {
+              type: "image",
+              key: "image",
+              displayer: "Image",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/674dccaa506a40002c305ac7?alt=media",
+            },
+            {
+              type: "video",
+              key: "video",
+              displayer: "Video",
+              value:
+                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667e77bd0181a1002c334f66?alt=media&timestamp=1719564238038",
+            },
+          ],
         },
       ],
     });
+
     this.addProp({
       type: "icon",
       key: "verticalPreviousArrow",
@@ -88,12 +150,57 @@ class Slider9 extends BaseSlider {
       displayer: "Horizontal Next Arrow Icon",
       value: "MdOutlineKeyboardArrowRight"
     });
+    this.addProp({
+      type: "icon",
+      key: "videoButton",
+      displayer: "Video Button Icon",
+      value: "CiPlay1"
+    });
+    this.addProp({
+      type: "string",
+      key: "videoButtonText",
+      displayer: "Video Button Text",
+      value: "Watch video"
+    });
+    this.addProp({
+      type: "icon",
+      key: "fsButton",
+      displayer: "Fullscreen Button Icon",
+      value: "TfiFullscreen"
+    });
+    this.addProp({
+      type: "string",
+      key: "fsButtonText",
+      displayer: "Fullscreen Button Text",
+      value: "Click to enlarge"
+    });
 
     this.setComponentState("vertical-slider-ref", React.createRef());
     this.setComponentState("horizontal-slider-ref", React.createRef());
     this.setComponentState("currentSlideIndex", 0);
 
+    this.setComponentState("isFullscreen", false);
+    this.setComponentState("fullscreenImageUrl", null);
   }
+
+  handleFullscreen = () => {
+    const currentIndex = this.getComponentState("currentSlideIndex");
+    const sliderItems = this.castToObject<SliderItem[]>("sliderItems");
+    const selectedImage = sliderItems[currentIndex]?.image;
+
+    if (selectedImage) {
+      this.setComponentState("isFullscreen", true);
+      this.setComponentState("fullscreenImageUrl", selectedImage);
+    } else {
+      this.setComponentState("fullscreenImageUrl", null);
+      this.setComponentState("isFullscreen", false);
+    }
+  };
+
+  handleCloseFullscreen = () => {
+    this.setComponentState("isFullscreen", false)
+    this.setComponentState("fullscreenImageUrl", null);
+  };
 
   getName(): string {
     return "Slider 9";
@@ -114,14 +221,24 @@ class Slider9 extends BaseSlider {
         if (horizontalSliderRef.current) {
           horizontalSliderRef.current.slickGoTo(current);
         }
-      }
+
+        this.setComponentState("animation-active", false);
+        this.setComponentState("display-none", false);
+      },
+      beforeChange: (oldIndex: number, newIndex: number) => {
+        if (oldIndex === newIndex) return;
+        if (this.getPropValue("textAnimation"))
+          this.setComponentState("animation-active", true);
+        this.setComponentState("from", oldIndex > newIndex ? "left" : "right");
+
+        this.setComponentState("display-none", true);
+
+        this.setComponentState("active-index", newIndex);
+      },
     };
 
-
-
-
     const horizontalSettings = {
-      arrows: true,
+      arrows: false,
       dots: false,
       infinite: false,
       speed: 800,
@@ -129,6 +246,9 @@ class Slider9 extends BaseSlider {
       autoplaySpeed: 3000,
       slidesToShow: 1,
       slidesToScroll: 1,
+      afterChange: (currentIndex: number) => {
+        this.setComponentState("currentSlideIndex", currentIndex);
+      },
     };
 
     const title = this.getPropValue("title");
@@ -139,6 +259,17 @@ class Slider9 extends BaseSlider {
 
     const horizontalPreviousArrow = this.getPropValue("horizontalPreviousArrow");
     const horizontalNextArrow = this.getPropValue("horizontalNextArrow");
+
+    const videoButton = this.getPropValue("videoButton");
+    const videoButtonText = this.getPropValue("videoButtonText");
+    const fsButton = this.getPropValue("fsButton");
+    const fsButtonText = this.getPropValue("fsButtonText");
+
+    const isFullscreen = this.getComponentState("isFullscreen");
+    const fullscreenImageUrl = this.getComponentState("fullscreenImageUrl");
+
+    const sliderItems = this.castToObject<SliderItem[]>("sliderItems");
+
     return (
       <Base.Container className={this.decorateCSS("container")} >
         <Base.MaxContent className={this.decorateCSS("max-content")}>
@@ -151,8 +282,8 @@ class Slider9 extends BaseSlider {
                   className={this.decorateCSS("carousel-vertical")}
                   ref={this.getComponentState("vertical-slider-ref")}
                 >
-                  {this.getPropValue("sliderItems").map(
-                    (item: any, indexSlider: number) => (
+                  {sliderItems.map(
+                    (item: SliderItem, indexSlider: number) => (
                       <div className={this.decorateCSS("img-container")}
                         onClick={() => {
                           this.setComponentState("currentSlideIndex", indexSlider);
@@ -165,7 +296,7 @@ class Slider9 extends BaseSlider {
                         key={indexSlider}
                       >
                         <img
-                          src={item.value}
+                          src={item.image}
                           alt=""
                           className={this.decorateCSS("img")}
                         />
@@ -207,11 +338,11 @@ class Slider9 extends BaseSlider {
                   className={this.decorateCSS("carousel-horizontal")}
                   ref={this.getComponentState("horizontal-slider-ref")}
                 >
-                  {this.getPropValue("sliderItems").map(
-                    (item: any, indexSlider: number) => (
+                  {sliderItems.map(
+                    (item: SliderItem, indexSlider: number) => (
                       <div className={this.decorateCSS("img-container")} key={indexSlider}>
                         <img
-                          src={item.value}
+                          src={item.image}
                           alt=""
                           className={this.decorateCSS("img")}
                         />
@@ -221,7 +352,7 @@ class Slider9 extends BaseSlider {
                 </ComposerSlider>
 
                 {(horizontalPreviousArrow || horizontalNextArrow) &&
-                  <div className={this.decorateCSS("verticalArrows")}>
+                  <div className={this.decorateCSS("horizontalArrows")}>
                     {horizontalPreviousArrow &&
                       <ComposerIcon name={horizontalPreviousArrow}
                         propsIcon={{
@@ -243,6 +374,64 @@ class Slider9 extends BaseSlider {
                         }}
                       />}
                   </div>}
+
+
+                <div className={this.decorateCSS("buttons")}>
+                  <div className={this.decorateCSS("button-wrapper")}>
+                    <div className={this.decorateCSS("icon-wrapper")}>
+                      <ComposerIcon
+                        name={videoButton}
+                        propsIcon={{
+                          className: this.decorateCSS("video-button"),
+                          onClick: () => { },
+                        }}
+                      />
+                    </div>
+                    <div className={this.decorateCSS("button-text-wrapper-video")}>
+                      <span className={this.decorateCSS("button-text")}>{videoButtonText}</span>
+                    </div>
+                  </div>
+
+                  <div className={this.decorateCSS("button-wrapper")}>
+                    <div className={this.decorateCSS("icon-wrapper")}>
+                      <ComposerIcon
+                        name={fsButton}
+                        propsIcon={{
+                          className: this.decorateCSS("fs-button"),
+                          onClick: () => this.handleFullscreen(),
+                        }}
+                      />
+                    </div>
+                    <div className={this.decorateCSS("button-text-wrapper-fs")}>
+                      <span className={this.decorateCSS("button-text")}>{fsButtonText}</span>
+                    </div>
+                  </div>
+                </div>
+
+                {isFullscreen && fullscreenImageUrl && (
+                  <div className={this.decorateCSS("fullscreen-modal")}>
+                    <div
+                      className={this.decorateCSS("fullscreen-overlay")}
+                      onClick={this.handleCloseFullscreen}
+                    ></div>
+                    <img
+                      src={fullscreenImageUrl}
+                      alt="Fullscreen"
+                      className={this.decorateCSS("fullscreen-image")}
+                    />
+                    <div
+                      className={this.decorateCSS("close-fullscreen")}
+                      onClick={this.handleCloseFullscreen}
+                    >
+                      <ComposerIcon
+                        name="CiClose"
+                        propsIcon={{
+                          className: this.decorateCSS("close-button-icon"),
+                        }}
+                      />
+                    </div>
+                  </div>)}
+
               </div>
             </div>
             <Base.VerticalContent className={this.decorateCSS("right-content")}>
@@ -256,7 +445,7 @@ class Slider9 extends BaseSlider {
             </Base.VerticalContent>
           </Base.ContainerGrid>
         </Base.MaxContent>
-      </Base.Container>
+      </Base.Container >
     );
   }
 }
