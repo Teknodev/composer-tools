16b11860e0751cb78b91ce1bba43fa01f6f12a51
dagsevenonur <onurdagseven01@gmail.com>
2024-12-05 10:31:01 +0300

    fix: change request fixed

--- editor-components/list/list2/list2.tsx
+++ editor-components/list/list2/list2.tsx
@@ -17,6 +17,27 @@ class List2 extends BaseList {
     super(props, styles);
 
     this.addProp({
+      type: "number",
+      key: "imageCountInitial",
+      displayer: "Image Count Initial",
+      value: 3
+  })
+  this.addProp({
+      type: "number",
+      key: "imageCount",
+      displayer: "More Image Count",
+      value: 1
+  })
+
+    this.addProp({
+      type: "number",
+      key: "itemCountRow",
+      displayer: "Item Count in a Row",
+      value: 3,
+      max: 4,
+    });
+
+    this.addProp({
       type: "string",
       key: "title",
       displayer: "Title",
@@ -31,49 +52,10 @@ class List2 extends BaseList {
     });
 
     this.addProp({
-      type: "array",
-      key: "buttons",
-      displayer: "Buttons",
-      value: [
-        {
-          type: "object",
-          key: "button",
-          displayer: "Action Button",
-          value: [
-            {
-              type: "string",
-              key: "text",
-              displayer: "Text of Button",
-              value: "View More Categories"
-            },
-            {
-              type: "page",
-              displayer: "Navigate",
-              value: "",
-              key: "navigate"
-            }
-          ]
-        },
-        {
-          type: "object",
-          key: "button",
-          displayer: "Action Button",
-          value: [
-            {
-              type: "string",
-              key: "text",
-              displayer: "Text of Button",
-              value: "View More Categories"
-            },
-            {
-              type: "page",
-              displayer: "Navigate",
-              value: "",
-              key: "navigate"
-            }
-          ]
-        }
-      ]
+      type: "string",
+      key: "button",
+      displayer: "Button Text",
+      value: "View More Categories"
     });
 
     this.addProp({
@@ -306,21 +288,30 @@ class List2 extends BaseList {
       ]
     })
     this.addProp({
-      type: "number",
-      key: "itemCount",
-      displayer: "Item Count in a Row",
-      value: 3,
-      max: 4,
+      type: "boolean",
+      key: "stick",
+      displayer: "Show Line",
+      value: true
     });
+
+    this.setComponentState("moreImages", 0);
   }
 
   getName(): string {
     return "List 2";
   }
 
+  handleLoadMoreButton = () => {
+    this.setComponentState("moreImages", this.getComponentState("moreImages") + this.getPropValue("imageCount"))
+  };
+
   render() {
+    if (this.getComponentState("imageCount") != this.getPropValue("imageCountInitial") + this.getComponentState("moreImages"))
+      this.setComponentState("imageCount", this.getPropValue("imageCountInitial") + this.getComponentState("moreImages"));
+
     const cards = this.castToObject<any>("cards");
-    return (
+
+    return(
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
           <Base.VerticalContent>
@@ -331,18 +322,22 @@ class List2 extends BaseList {
               {this.getPropValue("description")}
               </Base.SectionDescription>
             </Base.VerticalContent>
-          <Base.ListGrid className={this.decorateCSS("cards-box")} gridCount={{pc: this.getPropValue("itemCount")}}>
-            {cards.map((card: CardItem, index: number) => (
+          <Base.ListGrid className={this.decorateCSS("cards-box")} gridCount={{pc: this.getPropValue("itemCountRow")}}>
+            {cards.slice(0, this.getComponentState("imageCount")).map((card: CardItem, index: number) => (
               <ComposerLink key={index} path={card.page}>
                 <div className={this.decorateCSS("card")}>
-                  <img src={card.image} alt={this.castToString(card.card_text)} />
+                  {card.image && (
+                    <img src={card.image} alt={this.castToString(card.card_text)} />
+                  )}
                   <div className={this.decorateCSS("overlay")}></div>
                   <div className={this.decorateCSS("overlay2")}></div>
                   <div className={this.decorateCSS("card-content")}>
-                    <div className={this.decorateCSS("stick")}></div>
+                    {this.getPropValue("stick") && (
+                      <div className={this.decorateCSS("stick")}></div>
+                    )}
                     <div className={this.decorateCSS("labels")}>
                       <span className={this.decorateCSS("first")}>{card.card_text}</span>
-                      <span className={this.decorateCSS("second")}>{String(card.count) + " " + this.castToString(card.count_text)}</span>
+                      <span className={this.decorateCSS("second")}>{(card.count || 0) + " " + this.castToString(card.count_text)}</span>
                     </div>
                   </div>
                 </div>
@@ -350,15 +345,15 @@ class List2 extends BaseList {
             )}
           </Base.ListGrid>
 
-          <div className={this.decorateCSS("buttons-box")}>
-            {this.getPropValue("buttons").map((button: any) =>
-              <ComposerLink path={button.getPropValue("navigate")}>
+          {this.getPropValue("button") && (
+            <div className={this.decorateCSS("buttons-box")}>
+              <Base.Button onClick={this.handleLoadMoreButton}>
                 <div className={this.decorateCSS("button")}>
-                  <span>{button.getPropValue("text")}</span>
+                  <span>{this.getPropValue("button")}</span>
                 </div>
-              </ComposerLink>
-            )}
-          </div>
+              </Base.Button>
+            </div>
+          )}
         </Base.MaxContent>
       </Base.Container>
     );
