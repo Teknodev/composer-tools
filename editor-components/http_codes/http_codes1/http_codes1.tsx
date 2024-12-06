4cde474aca47fe2657f85282efbf9ecb12c4e1a7
Burcu Açan <burcuuacann@gmail.com>
2024-12-06 14:45:54 +0300

    fix: standardization

--- editor-components/http_codes/http_codes1/http_codes1.tsx
+++ editor-components/http_codes/http_codes1/http_codes1.tsx
@@ -2,20 +2,12 @@ import React from "react";
 import { BaseHTTPCodes } from "../../EditorComponent";
 import styles from "./http_codes1.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
-import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
 
 type ButtonType = {
-  icon?: string;
   text?: string;
   url?: string;
 };
-
-type IconType = {
-  icon: string;
-  url?: string;
-};
-
-type IconsArray = IconType[];
 type ButtonsArray = ButtonType[];
 
 class HTTP_CODES1 extends BaseHTTPCodes {
@@ -54,70 +46,10 @@ class HTTP_CODES1 extends BaseHTTPCodes {
           displayer: "Button",
           value: [
             {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "FaArrowLeft",
-            },
-            {
               type: "string",
               key: "text",
               displayer: "Text",
               value: "Go home",
-            },
-            {
-              type: "page",
-              key: "url",
-              displayer: "Url",
-              value: "",
-            },
-          ],
-        },
-      ],
-    });
-
-    this.addProp({
-      type: "array",
-      key: "left-icons",
-      displayer: "Left Icon",
-      value: [
-        {
-          type: "object",
-          key: "icons",
-          displayer: "icons",
-          value: [
-            {
-              type: "icon",
-              key: "icon",
-              value: "MdOutlineSubdirectoryArrowLeft",
-              displayer: "Icon",
-            },
-            {
-              type: "page",
-              key: "url",
-              displayer: "url",
-              value: "",
-            },
-          ],
-        },
-      ],
-    });
-
-    this.addProp({
-      type: "array",
-      key: "right-icons",
-      displayer: "Right Icon",
-      value: [
-        {
-          type: "object",
-          key: "icons",
-          displayer: "icons",
-          value: [
-            {
-              type: "icon",
-              key: "icon",
-              value: "MdCancel",
-              displayer: "Icon",
             },
             {
               type: "page",
@@ -137,93 +69,48 @@ class HTTP_CODES1 extends BaseHTTPCodes {
 
   render() {
     const image = this.getPropValue("image");
-    const left_icons = this.castToObject<IconsArray>("left-icons");
-    const right_icons = this.castToObject<IconsArray>("right-icons");
     const buttons = this.castToObject<ButtonsArray>("buttons");
-
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("content-box")}>
-            <div className={this.decorateCSS("icons")}>
-              <div>
-                {left_icons && (
-                  <div className={this.decorateCSS("left-icons")}>
-                    {left_icons.map((icon: IconType, index: number) => {
-                      if (icon.icon)
-                        return (
-                          <div className={this.decorateCSS("icon-container")}>
-                            <ComposerLink key={index} path={icon.url}>
-                              <ComposerIcon
-                                name={icon.icon}
-                                propsIcon={{
-                                  className: this.decorateCSS("icon"),
-                                }}
-                              />
-                            </ComposerLink>
-                          </div>
-                        );
-                    })}
-                  </div>
-                )}
-              </div>
-              <div>
-                {right_icons && (
-                  <div className={this.decorateCSS("right-icons")}>
-                    {right_icons.map((icon: IconType, index: number) => {
-                      if (icon.icon)
-                        return (
-                          <div className={this.decorateCSS("icon-container")}>
-                            <ComposerLink key={index} path={icon.url}>
-                              <ComposerIcon
-                                name={icon.icon}
-                                propsIcon={{
-                                  className: this.decorateCSS("icon"),
-                                }}
-                              />
-                            </ComposerLink>
-                          </div>
-                        );
-                    })}
-                  </div>
-                )}
-              </div>
-            </div>
-
             <div className={this.decorateCSS("content")}>
-              <div className={this.decorateCSS("left-side")}>
-                {image && (
+              {image && (
+                <div className={this.decorateCSS("left-side")}>
                   <div className={this.decorateCSS("image-box")}>
                     <img className={this.decorateCSS("image")} src={this.getPropValue("image")} alt="" />
                   </div>
-                )}
-              </div>
-              <div className={this.decorateCSS("right-side")}>
-                <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-                <h1 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h1>
-                <div>
-                  {buttons && (
-                    <div className={this.decorateCSS("button")}>
-                      {buttons.map((button: ButtonType, index: number) => {
-                        return (
-                          <div key={index} className={this.decorateCSS("button-container")}>
-                            <ComposerLink path={button.url}>
-                              <div className={this.decorateCSS("button-content")}>
-                                <div className={this.decorateCSS("button-icon")}>{button.icon && <ComposerIcon name={button.icon} />}</div>
-                                <div className={this.decorateCSS("button-text")}>{button.text && button.text}</div>
-                              </div>
-                            </ComposerLink>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  )}
                 </div>
-              </div>
+              )}
+              {(this.castToString(this.getPropValue("title")) || this.castToString(this.getPropValue("description")) || (buttons.length > 0)) && (
+                <div className={this.decorateCSS("right-side")}>
+                  <div className={this.decorateCSS("right-content")}>
+                    {this.castToString(this.getPropValue("title")) && (
+                      <div className={this.decorateCSS("title")}>{this.getPropValue("title")}</div>
+                    )}
+                    {this.castToString(this.getPropValue("description")) && (
+                      <div className={this.decorateCSS("description")}>{this.getPropValue("description")}</div>
+                    )}
+                    {(buttons.length > 0) && (
+                      <div className={this.decorateCSS("button-wrapper")}>
+                        {buttons.map((button: ButtonType, index: number) => {
+                          return (
+                            <ComposerLink path={button.url}>
+                              <Base.Button className={this.decorateCSS("button")}>
+                                {button.text}
+                              </Base.Button>
+                            </ComposerLink>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-        </div>
-      </div>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
