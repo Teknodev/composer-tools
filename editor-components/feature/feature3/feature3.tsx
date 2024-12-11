f806ceb39287d0846231943fd5529aa793ae5c4f
dogusmiuw <dceliknodes@gmail.com>
2024-12-11 18:33:02 +0300

    feat: change requests

--- editor-components/feature/feature3/feature3.tsx
+++ editor-components/feature/feature3/feature3.tsx
@@ -34,6 +34,86 @@ class Feature3 extends BaseFeature {
     });
 
     this.addProp({
+      type: "boolean",
+      key: "enableCircle",
+      displayer: "Enable Circle",
+      value: true,
+    });
+
+    this.addProp({
+      type: "array",
+      key: "cards",
+      displayer: "Card",
+      value: [
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Search Engine Optimization",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "FcSearch",
+            },
+          ]
+        },
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Marketing & Advertisement",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Icon",
+              value: "FcStatistics",
+            },
+          ]
+        },
+        {
+          type: "object",
+          key: "card",
+          displayer: "Card",
+          value: [
+            {
+              type: "string",
+              key: "title",
+              displayer: "Card Title",
+              value: "Reporting & Analysis",
+            },
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "FcComboChart",
+            },
+          ]
+        },
+      ]
+    });
+
+    this.addProp({
+      type: "number",
+      key: "cards-item-count",
+      displayer: "Cards Item Count in a Row",
+      value: 3,
+      max: 5
+    });
+
+    this.addProp({
       type: "array",
       key: "logos",
       displayer: "Logo Images",
@@ -137,69 +217,18 @@ class Feature3 extends BaseFeature {
     });
 
     this.addProp({
-      type: "array",
-      key: "cards",
-      displayer: "Card",
-      value: [
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Card Title",
-              value: "Search Engine Optimization",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "FcSearch",
-            },
-          ]
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Card Title",
-              value: "Marketing & Advertisement",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Icon",
-              value: "FcStatistics",
-            },
-          ]
-        },
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "string",
-              key: "title",
-              displayer: "Card Title",
-              value: "Reporting & Analysis",
-            },
-            {
-              type: "icon",
-              key: "icon",
-              displayer: "Card Icon",
-              value: "FcComboChart",
-            },
-          ]
-        },
-      ]
+      type: "number",
+      key: "logos-item-count",
+      displayer: "Logos Item Count in a Row",
+      value: 5,
+      max: 5
     });
+
+    window.addEventListener('resize', () => {
+      this.setComponentState('screenWidth', window.innerWidth);
+    });
+
+    this.setComponentState('screenWidth', window.innerWidth);
   }
 
   getName(): string {
@@ -210,53 +239,114 @@ class Feature3 extends BaseFeature {
     const cards = this.castToObject<Card[]>("cards");
     const logos = this.castToObject<Logo[]>("logos");
 
+    const cardsItemCount = this.getPropValue("cards-item-count");
+    const logosItemCount = this.getPropValue("logos-item-count");
+
+    const circleExist = !!this.getPropValue("enableCircle");
+
     const titleExist = !!this.getPropValue("title");
     const descExist = !!this.getPropValue("description");
+
+    const renderTitle = titleExist ? (
+      <Base.SectionTitle className={`
+      ${this.decorateCSS("title")}
+      ${circleExist ? this.decorateCSS("circle-exist") : ""}
+      `}>
+        {this.getPropValue("title")}
+      </Base.SectionTitle>
+    ) : null;
+
+    const renderDescription = descExist ? (
+      <Base.SectionDescription className={`
+      ${this.decorateCSS("description")}
+      ${circleExist ? this.decorateCSS("circle-exist") : ""}
+      `}>
+        {this.getPropValue("description")}
+      </Base.SectionDescription>
+    ) : null;
+
+    const mobileDevice = this.getComponentState("screenWidth") < 640;
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
           <Base.VerticalContent className={this.decorateCSS("wrapper")}>
-            {(titleExist || descExist) && (
-              <header className={this.decorateCSS("header")}>
-                {titleExist && (
-                  <div className={this.decorateCSS("feature-circle")}>
-                    <Base.SectionTitle className={this.decorateCSS("title")}>
-                      {this.getPropValue("title")}
-                    </Base.SectionTitle>
+            {(titleExist || descExist || !!cards?.length) && (
+              <Base.VerticalContent
+                className={`
+                  ${this.decorateCSS("upper")}
+                  ${circleExist ? this.decorateCSS("circle-exist") : ""}
+                  ${!logos?.length ? this.decorateCSS("no-logos") : ""}
+                `}
+              >
+                {circleExist && mobileDevice
+                  ?
+                  <div className={`${this.decorateCSS("circle")}`}>
+                    {renderTitle}
+                    {renderDescription}
                   </div>
+                  : circleExist && (renderTitle || renderDescription) ?
+                    <>
+                      <div className={`${this.decorateCSS("circle")}`}>
+                        {renderTitle}
+                      </div>
+                      {renderDescription}
+                    </>
+                    : circleExist ?
+                      <div className={`${this.decorateCSS("circle")} ${this.decorateCSS("absolute")}`}></div>
+                      :
+                      <>
+                        {renderTitle}
+                        {renderDescription}
+                      </>
+                }
+
+                {cards?.length > 0 && (
+                  <Base.ListGrid
+                    gridCount={{ pc: cardsItemCount, tablet: cardsItemCount <= 3 ? cardsItemCount : 3 }}
+                    className={`
+                      ${this.decorateCSS("cards-container")}
+                      ${circleExist ? this.decorateCSS("circle-exist") : ""}
+                    `}
+                  >
+                    {cards.map((card: Card, index: number) => {
+                      const titleExist = !!this.castToString(card.title);
+                      const iconExist = !!card.icon;
+
+                      if (!titleExist && !iconExist) return null;
+
+                      return (
+                        <Base.VerticalContent key={index} className={this.decorateCSS("card")}>
+                          <Base.VerticalContent className={this.decorateCSS("card-content")}>
+                            {iconExist && (
+                              <ComposerIcon
+                                name={card.icon}
+                                propsIcon={{ className: this.decorateCSS("card-icon") }}
+                              />
+                            )}
+                            {titleExist && (
+                              <Base.H2 className={this.decorateCSS("card-title")}>
+                                {card.title}
+                              </Base.H2>
+                            )}
+                          </Base.VerticalContent>
+                        </Base.VerticalContent>
+                      );
+                    })}
+                  </Base.ListGrid>
                 )}
-                {descExist && (
-                  <Base.SectionDescription className={this.decorateCSS("description")}>
-                    {this.getPropValue("description")}
-                  </Base.SectionDescription>
-                )}
-              </header>
-            )}
-
-            {cards?.length > 0 && (
-              <Base.ContainerGrid className={this.decorateCSS("cards-container")}>
-                {cards.map((card: Card, index: number) => {
-                  const titleExist = !!this.castToString(card.title);
-                  const iconExist = !!card.icon;
-
-                  if (!titleExist && !iconExist) return null;
-
-                  return (
-                    <Base.VerticalContent key={index} className={this.decorateCSS("card")}>
-                      <ComposerIcon
-                        name={card.icon}
-                        propsIcon={{ className: this.decorateCSS("card-icon") }}
-                      />
-                      <Base.H2 className={this.decorateCSS("card-title")}>{card.title}</Base.H2>
-                    </Base.VerticalContent>
-                  );
-                })}
-              </Base.ContainerGrid>
+              </Base.VerticalContent>
             )}
 
             {logos?.length > 0 && (
-              <Base.ContainerGrid className={this.decorateCSS("logos-container")}>
+              <Base.ListGrid
+                gridCount={{ pc: logosItemCount, tablet: logosItemCount <= 3 ? logosItemCount : 3 }}
+                className={`
+                  ${this.decorateCSS("logos-container")}
+                  ${circleExist ? this.decorateCSS("circle-exist") : ""}
+                  ${!cards?.length ? this.decorateCSS("no-cards") : ""}
+                `}
+              >
                 {logos.map((logo: Logo, index: number) => {
                   if (!logo.image) return null;
 
@@ -268,7 +358,7 @@ class Feature3 extends BaseFeature {
                     </Base.VerticalContent>
                   );
                 })}
-              </Base.ContainerGrid>
+              </Base.ListGrid>
             )}
           </Base.VerticalContent>
         </Base.MaxContent>
