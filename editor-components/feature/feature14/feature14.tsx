63cb723f155f9835e6dd5cecfc4450a6dd4549f6
dagsevenonur <onurdagseven01@gmail.com>
2024-12-11 18:44:31 +0300

    feature14

--- editor-components/feature/feature14/feature14.tsx
+++ editor-components/feature/feature14/feature14.tsx
@@ -2,134 +2,157 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature14.module.scss";
+import { Base } from "../../../composer-base-components/base/base";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type TopImages = {
-  title: string;
-  description: string;
-  image: string;
-  buttonText: string;
-  link: string;
+type CardItem = {
+  icon: JSX.Element,
+  title: string,
+  description: string,
 };
+
 class Feature14 extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Section Title",
+      value: "Here are couple reasons why We are the best",
+    });
+    this.addProp({
+      type: "string",
+      key: "firstdescription",
+      displayer: "First Section Description",
+      value: "New Concept Staining (NCS) is a woman-owned company that provides color concreate staining services to commercial and residential customers.",
+    });
+    this.addProp({
+      type: "string",
+      key: "seconddescription",
+      displayer: "Second Section Description",
+      value: "The company was formed in response to an overwhelming market demand for concreate staining services, predominantly in the commercial construction industry. NCS'work commonly includes the artisanal staining of bridges, sound walls, abutments, and buildings.",
+    });
+    this.addProp({
+      type: "string",
+      key: "linkTitle",
+      displayer: "Link Title",
+      value: "About Us",
+    });
+    this.addProp({
+      type: "icon",
+      key: "linkIcon",
+      displayer: "Link Icon",
+      value: "GoArrowRight",
+    });
+    this.addProp({
+      type: "page",
+      key: "linkPage",
+      displayer: "Link Page",
+      value: "",
+    });
+    this.addProp({
       type: "array",
-      key: "top-image-card",
-      displayer: "TopImage Card",
+      key: "cards",
+      displayer: "Cards",
       value: [
         {
           type: "object",
-          key: "top-image",
-          displayer: "TopImage",
+          key: "cardItem",
+          displayer: "Card Item",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "IoPricetagsOutline",
+            },
             {
               type: "string",
               key: "title",
-              displayer: "Title",
-              value: "Cybersecurity Best Practices for Individuals",
+              displayer: "Card Title",
+              value: "Best price guaranteed",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Description",
-              value: "With the rise of technology, cybersecurity threats have become increasingly prevalent. Individuals need to take proactive measures to protect their online presence from hacking, phishing, and other cyber attacks. In this information corner, we'll provide tips and best practices for individuals to ensure their online security.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644a7ddef72de2002caaa670?alt=media&timestamp=1682603498405",
-            },
-            {
-              type: "string",
-              key: "buttonText",
-              displayer: "Button Text",
-              value: "LEARN MORE",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              displayer: "Card Description",
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Vivamus vivamus semper vulputate venenatis vitae egestas commodo porta.",
             },
           ],
         },
         {
           type: "object",
-          key: "top-image",
-          displayer: "TopImage",
+          key: "cardItem",
+          displayer: "Card Item",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "MdSupportAgent",
+            },
             {
               type: "string",
               key: "title",
-              displayer: "Title",
-              value: "Sustainability in Business",
+              displayer: "Card Title",
+              value: "Great communication",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Description",
-              value: "As climate change continues to impact the world, businesses are increasingly expected to incorporate sustainable practices into their operations. In this information corner, we'll explore the concept of sustainability in business, the benefits of incorporating sustainable practices, and offer tips for businesses to implement sustainability.",
-            },
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644a7ddef72de2002caaa66f?alt=media&timestamp=1682603498405",
-            },
-            {
-              type: "string",
-              key: "buttonText",
-              displayer: "Button Text",
-              value: "LEARN MORE",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              displayer: "Card Description",
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Vivamus vivamus semper vulputate venenatis vitae egestas commodo porta.",
             },
           ],
         },
         {
           type: "object",
-          key: "top-image",
-          displayer: "TopImage",
+          key: "cardItem",
+          displayer: "Card Item",
           value: [
+            {
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "CiStar",
+            },
             {
               type: "string",
               key: "title",
-              displayer: "Title",
-              value: "The Importance of Mental Health Awareness",
+              displayer: "Card Title",
+              value: "We provide high standards",
             },
             {
               type: "string",
               key: "description",
-              displayer: "Description",
-              value: "Mental health is just as important as physical health, but unfortunately, there is still a stigma around mental health issues. In this information corner, we'll discuss the importance of mental health awareness, common mental health disorders, and offer tips for promoting mental health and seeking help when needed.",
+              displayer: "Card Description",
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Vivamus vivamus semper vulputate venenatis vitae egestas commodo porta.",
             },
+          ],
+        },
+        {
+          type: "object",
+          key: "cardItem",
+          displayer: "Card Item",
+          value: [
             {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644a7e4af72de2002caaa679?alt=media&timestamp=1682603606025",
+              type: "icon",
+              key: "icon",
+              displayer: "Card Icon",
+              value: "GrUserManager",
             },
             {
               type: "string",
-              key: "buttonText",
-              displayer: "Button Text",
-              value: "LEARN MORE",
+              key: "title",
+              displayer: "Card Title",
+              value: "We worked with many big companies",
             },
             {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
+              type: "string",
+              key: "description",
+              displayer: "Card Description",
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Vivamus vivamus semper vulputate venenatis vitae egestas commodo porta.",
             },
           ],
         },
@@ -139,7 +162,7 @@ class Feature14 extends BaseFeature {
       type: "number",
       key: "itemCount",
       displayer: "Item count in a row",
-      value: 3,
+      value: 4,
     });
   }
 
@@ -148,40 +171,67 @@ class Feature14 extends BaseFeature {
   }
 
   render() {
+
+    const cardItems = this.castToObject<CardItem[]>("cards");
+
+    const alignment = Base.getContentAlignment();
+
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("content")}>
-            {this.castToObject<TopImages[]>("top-image-card").map(
-              (top_image: any, index: number) => (
-                <div className={this.decorateCSS("card-item-count")} style={{
-                  width: 90 / this.getPropValue("itemCount") + "%",
-                }}>
-                <div className={this.decorateCSS("top-image")} key={index}>
-                  <div className={this.decorateCSS("image-background")}></div>
-                  <div className={this.decorateCSS("child")}>
-                    <img
-                      alt=""
-                      className={this.decorateCSS("image")}
-                      src={top_image.image}
-                    ></img>
-                    <h3 className={this.decorateCSS("title")}>{top_image.title}</h3>
-                    <p className={this.decorateCSS("long-text")}>
-                      {top_image.description}
-                    </p>
-                    <ComposerLink path={top_image.link}>
-                      <a>
-                        {top_image.buttonText}
-                      </a>
-                    </ComposerLink>
-                  </div>
-                </div>
-                </div>
-              )
+      <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.ListGrid className={this.decorateCSS("row")} gridCount={{pc: alignment == "center" ? 1 : 2, tablet: 1, phone: 1}}>
+            {this.castToString(this.getPropValue("title")) && (
+              <Base.SectionTitle className={this.decorateCSS("header")}>
+                {this.getPropValue("title")}
+              </Base.SectionTitle>
             )}
+            {this.castToString(this.getPropValue("firstdescription")) && (
+              <Base.SectionDescription className={this.decorateCSS("description-1")}>
+                {this.getPropValue("firstdescription")}
+              </Base.SectionDescription>
+            )}
+            {this.castToString(this.getPropValue("linkTitle")) && (
+              <div className={alignment == "center" ? this.decorateCSS("link-center") : this.decorateCSS("link")}>
+                <ComposerLink path={this.getPropValue("linkPage")}>
+                  {this.getPropValue("linkTitle")}
+                </ComposerLink>
+                <ComposerIcon name={this.getPropValue("linkIcon")} />
+              </div>
+            )}
+            {this.castToString(this.getPropValue("seconddescription")) && (
+              <Base.SectionDescription className={this.decorateCSS("description-2")}>
+                {this.getPropValue("seconddescription")}
+              </Base.SectionDescription>
+            )}
+          </Base.ListGrid>
+          <div className={this.decorateCSS("section")}>
+            <Base.ListGrid className={this.decorateCSS("cards")} gridCount={{pc: this.getPropValue("itemCount")}}>
+            {cardItems.map((item: any, index: number) => {
+              return (
+                <div className={this.decorateCSS("card")}>
+                  {item.icon && (
+                    <div className={this.decorateCSS("icon-box")}>
+                      <div className={this.decorateCSS("icon")}>
+                        <ComposerIcon name={item.icon} />
+                      </div>
+                    </div>
+                  )}
+                  
+                  <Base.VerticalContent className={this.decorateCSS("card-content")}>
+                    {this.castToString(item.title) && (
+                      <span className={this.decorateCSS("card-title")}>{item.title}</span>
+                    )}
+                    {this.castToString(item.description) && (
+                      <span className={this.decorateCSS("card-description")}>{item.description}</span>
+                    )}
+                  </Base.VerticalContent>
+                </div> 
+              );
+            })}
+            </Base.ListGrid>
           </div>
-        </div>
-      </div>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
