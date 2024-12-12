4598c666ebd48d9cd7479ce3f3cc3af16e6c7034
dagsevenonur <onurdagseven01@gmail.com>
2024-12-12 16:48:14 +0300

    header11

--- editor-components/header/header11/header11.tsx
+++ editor-components/header/header11/header11.tsx
@@ -2,12 +2,9 @@ import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseHeader } from "../../EditorComponent";
 import styles from "./header11.module.scss";
+import { Base } from "../../../composer-base-components/base/base";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
-type Card = {
-  title: string;
-  description: string;
-  image: string;
-};
 class Header11 extends BaseHeader {
   constructor(props?: any) {
     super(props, styles);
@@ -15,140 +12,44 @@ class Header11 extends BaseHeader {
     this.addProp({
       type: "string",
       key: "title",
-      value: "Why Composer ?",
       displayer: "Title",
+      value: "Explore the world",
     })
 
     this.addProp({
       type: "string",
       key: "description",
-      value: "Let our artificial intelligence generate creatives that convert, so you can save time and money to scale. Check out why and how you should use Composer",
       displayer: "Description",
+      value: "Our world is full of wonders. Mountains, rivers, desserts, jungles and much more is waiting for you.",
+    })
+
+    this.addProp({
+      type: "string",
+      key: "linktext",
+      displayer: "Link Text",
+      value: "Sign up now"
+    })
+
+    this.addProp({
+      type: "page",
+      key: "linkpage",
+      displayer: "Link Page",
+      value: ""
+    })
+
+    this.addProp({
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "IoMdPlay"
     })
 
     this.addProp({
       type: "image",
       key: "backgroundImage",
       displayer: "Background Image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64e3760c057bdf002c296572?alt=media&timestamp=1719483639148"
+      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/675aa2fe0655f8002ca633bf?alt=media"
     })
-    this.addProp({
-      type: "array",
-      key: "button",
-      displayer: "Buttons",
-      value: [
-        {
-          type: "object",
-          key: "buttonObject",
-          displayer: "Button",
-          value: [
-            {
-              type: "string",
-              key: "buttonText",
-              displayer: "Button Text",
-              value: "Go There",
-            },
-            {
-              type: "page",
-              key: "link",
-              displayer: "Button Link",
-              value: "",
-            },
-          ],
-        },
-      ],
-    });
-
-    this.addProp({
-      type: "array",
-      key: "header-card",
-      displayer: "Header Card",
-      value: [
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667d67b90181a1002c334885?alt=media&timestamp=1719494609268",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Trained A.I.",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Our machine learning model is learning every day and provides up-to-date creatives with a sole purpose: conversion.",
-            },
-          ],
-        },
-
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667d67b90181a1002c334886?alt=media&timestamp=1719494609268",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Better Conversions",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "Up to 14x better conversion rates than ad creatives designed with no data-driven approach.",
-            },
-          ],
-        },
-
-        {
-          type: "object",
-          key: "card",
-          displayer: "Card",
-          value: [
-            {
-              type: "image",
-              key: "image",
-              displayer: "Image",
-              value:
-                "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/667d67b90181a1002c334887?alt=media&timestamp=1719494609268",
-            },
-            {
-              type: "string",
-              key: "title",
-              displayer: "Title",
-              value: "Super Scalable",
-            },
-            {
-              type: "string",
-              key: "description",
-              displayer: "Description",
-              value:
-                "1 creative or 10,000 creatives a month, Composer is designed to match your ad creative/banner needs.",
-            },
-          ],
-        },
-      ],
-    });
   }
 
   getName(): string {
@@ -156,42 +57,48 @@ class Header11 extends BaseHeader {
   }
 
   render() {
+
     const styling = {
       backgroundImage: `url(${this.getPropValue("backgroundImage")})`,
     }
+
+    const Image = this.getPropValue("backgroundImage") ? true : false;
+
+    const aligment = Base.getContentAlignment();
+
     return (
-      <div className={this.decorateCSS("container")}>
-        <div className={this.decorateCSS("max-content")} style={styling}>
-          <div className={this.decorateCSS("content")}>
-            <div className={this.decorateCSS("header-page")}>
-              <h1 className={this.decorateCSS("title")}>{this.getPropValue("title")}</h1>
-              <h2 className={this.decorateCSS("description")}>{this.getPropValue("description")}</h2>
-              {this.castToObject<[]>("button").map((item: any, indexButton: number) => {
-                return (
-                  <ComposerLink key={`hdr-11-${indexButton}`} path={item.link}>
-                    <button className={this.decorateCSS("button")}>
-                      {item.buttonText}
-                    </button>
-                  </ComposerLink>
-                );
-              })}
-            </div>
-            <div className={this.decorateCSS("card-child")}>
-              {this.castToObject<Card[]>("header-card").map(
-                (card: Card, index: number) => (
-                  <div key={`hdr-11-card-${index}`} className={this.decorateCSS("card")}>
-                    <div className={this.decorateCSS("img-child")}>
-                      <img src={card.image} alt=""/>
-                    </div>
-                    <h5 className={this.decorateCSS("card-title")}>{card.title}</h5>
-                    <p className={this.decorateCSS("card-description")}>{card.description}</p>
-                  </div>
-                )
+      <Base.Container className={Image ? this.decorateCSS("container") : this.decorateCSS("container-noimage")} style={styling} isFull="true">
+        <div className={this.decorateCSS("box")}>
+          <Base.MaxContent className={this.decorateCSS("max-content")}>
+            <div className={this.decorateCSS("content")}>
+              {this.castToString(this.getPropValue("title")) && (
+                <Base.SectionTitle className={this.decorateCSS("title")}>
+                  {this.getPropValue("title")}
+                </Base.SectionTitle>
+              )}
+              {this.castToString(this.getPropValue("description")) && (
+                <Base.SectionDescription className={this.decorateCSS("description")}>
+                  {this.getPropValue("description")}
+                </Base.SectionDescription>
+              )}
+              {this.castToString(this.getPropValue("linktext")) && (
+                <ComposerLink path={this.getPropValue("linkpage")}>
+                  <Base.P className={aligment == "center" ? this.decorateCSS("link-text-center") : this.decorateCSS("link-text")}>
+                    {this.getPropValue("linktext")}
+                  </Base.P>
+                </ComposerLink>
               )}
             </div>
-          </div>
+          </Base.MaxContent>
         </div>
-      </div>
+        {this.getPropValue("icon") && (
+          <div className={this.decorateCSS("icon-box")}>
+            <div className={this.decorateCSS("icon")}>
+              <ComposerIcon name={this.getPropValue("icon")} />
+            </div>
+          </div>
+        )}
+      </Base.Container>
     );
   }
 }
