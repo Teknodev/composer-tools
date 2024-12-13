b03d31d0c6f6608fa021beb882e4890954b59de3
Yusuf Kıtlık <71187794+yusufktlk@users.noreply.github.com>
2024-12-13 14:20:25 +0300

    Location 5 (#566)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -222,8 +222,8 @@ import NoticeModal from "./modal/notice-modal/notice";
 import NotificationModal2 from "./modal/notificationmodal2/notificationmodal2";
 import SubscriptionModal from "./modal/subscription-modal/subscription-modal";
 import FormModal1 from "./modal/formmodal1/formmodal1";
+import LocationComponent5 from "./location/location5/location5";
 import LocationComponent2 from "./location/location2/location2";
-import FeedbackModal1 from "./feedback/feedbackmodal1/feedbackmodal1";
 import ConfirmationModal2 from "./modal/confirmation-modal-2/confirmation-modal-2";
 
 function Registerables(composer: ComponentsRegistery) {
@@ -440,13 +440,13 @@ function Registerables(composer: ComponentsRegistery) {
     new FooterComponent11(),
     new FooterComponent12(),
     new TermsOfUseModal(),
-    new FeedbackModal1(),
     new SurveyModal(),
     new NoticeModal(),
     new NotificationModal2(),
     new SubscriptionModal(),
     new FormModal1(),
     new LocationComponent1(),
+    new LocationComponent5(),
     new LocationComponent2(),
     new LocationComponent3(),
     new HttpCodes3(),
