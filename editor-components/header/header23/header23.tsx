5be07517fe5a550c8722289d0b70b6f7c223d8ea
Serkan Ozdemir <serkanozdemir38080@gmail.com>
2024-12-18 12:38:07 +0300

    fix: header23

--- editor-components/header/header23/header23.tsx
+++ editor-components/header/header23/header23.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import styles from "./header23.module.scss";
 import { BaseHeader } from "../../EditorComponent";
 import ComposerSlider from "../../../composer-base-components/slider/slider";
-import { BsAlignEnd } from "react-icons/bs";
-import { Base } from "../../../composer-base-components/base/base";
-import { TiThSmall } from "react-icons/ti";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
 
 
 type SliderItem = {
@@ -190,59 +188,23 @@ class Header23 extends BaseHeader {
         },
       ],
     });
+    this.addProp({
+      type: "icon",
+      key: "previousArrow",
+      displayer: "Previous Arrow Icon",
+      value: "IoIosArrowBack"
+    });
+    this.addProp({
+      type: "icon",
+      key: "nextArrow",
+      displayer: "Next Arrow Icon",
+      value: "IoIosArrowForward"
+    });
+
+
     this.setComponentState("active", 0);
+    this.setComponentState("slider-ref", React.createRef());
   }
-
-  // handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-  //   const container = e.currentTarget.getBoundingClientRect();
-
-  //   const xRatio = ((e.clientX - container.left) / container.width - 0.5) * 2;
-  //   const yRatio = ((e.clientY - container.top) / container.height - 0.5) * 2;
-
-  //   const animatedElements = e.currentTarget.querySelectorAll(`.${this.decorateCSS("animate")}`);
-  //   console.log(animatedElements);
-
-  //   const background1 = e.currentTarget.querySelector(`.${this.decorateCSS("background1")}`) as HTMLElement;
-  //   const background2 = e.currentTarget.querySelector(`.${this.decorateCSS("background2")}`) as HTMLElement;
-  //   const background3 = e.currentTarget.querySelector(`.${this.decorateCSS("background3")}`) as HTMLElement;
-  //   const imgBackground = e.currentTarget.querySelector(`.${this.decorateCSS("header23-img-background")}`) as HTMLElement;
-
-  //   const upperText = e.currentTarget.querySelector(`.${this.decorateCSS("upper-text")}`) as HTMLElement;
-  //   const lowerText = e.currentTarget.querySelector(`.${this.decorateCSS("lower-text")}`) as HTMLElement;
-  //   const topImg = e.currentTarget.querySelector(`.${this.decorateCSS("top-img")}`) as HTMLElement;
-
-  //   const factorBg1 = 10;
-  //   const factorBg2 = 15;
-  //   const factorBg3 = 20;
-  //   const factorImgBg = 10;
-  //   const factorText = 20;
-  //   const factorTopImg = 20;
-
-  //   if (background1) {
-  //     background1.style.transform = `translate(${xRatio * factorBg1}px, ${yRatio * factorBg1}px)`;
-  //   }
-  //   if (background2) {
-  //     background2.style.transform = `translate(-50%, -50%) translate(${xRatio * factorBg2}px, ${yRatio * factorBg2}px)`;
-  //   }
-  //   if (background3) {
-  //     background3.style.transform = `translate(${xRatio * factorBg3}px, ${yRatio * factorBg3}px)`;
-  //   }
-  //   if (imgBackground) {
-  //     imgBackground.style.transform = `translate(-50%, -50%) translate(${xRatio * factorImgBg}px, ${yRatio * factorImgBg}px)`;
-  //   }
-
-  //   if (upperText) {
-  //     upperText.style.transform = `translate(${xRatio * factorText}px, ${yRatio * factorText}px)`;
-  //   }
-
-  //   if (lowerText) {
-  //     lowerText.style.transform = `translate(${xRatio * factorText}px, ${yRatio * factorText}px)`;
-  //   }
-
-  //   if (topImg) {
-  //     topImg.style.transform = `translate(-50%, -50%) translate(${-xRatio * factorTopImg}px, ${-yRatio * factorTopImg}px)`;
-  //   }
-  // };
 
   handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const container = e.currentTarget.getBoundingClientRect();
@@ -250,24 +212,21 @@ class Header23 extends BaseHeader {
     const xRatio = ((e.clientX - container.left) / container.width - 0.5) * 2;
     const yRatio = ((e.clientY - container.top) / container.height - 0.5) * 2;
 
-    // Yardımcı fonksiyon: Transform uygulama
     const applyTransform = (elements: HTMLElement[], transform: string) => {
       elements.forEach(element => {
         element.style.transform = transform;
       });
     };
 
-    // Sarmalayıcı öğeleri seçin ve HTMLElement olarak tür dönüştürün
     const transformWrapperBg1Elements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-background1-wrapper")}`)) as HTMLElement[];
     const transformWrapperBg2Elements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-background2-wrapper")}`)) as HTMLElement[];
     const transformWrapperBg3Elements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-background3-wrapper")}`)) as HTMLElement[];
-    const transformWrapperImgBgElements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("img-bg")}`)) as HTMLElement[];
+    const transformWrapperImgBgElements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-img-bg")}`)) as HTMLElement[];
 
     const transformWrapperUpperTextElements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-wrapper-upperText")}`)) as HTMLElement[];
     const transformWrapperLowerTextElements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-wrapper-lowerText")}`)) as HTMLElement[];
     const transformWrapperTopImgElements = Array.from(e.currentTarget.querySelectorAll(`.${this.decorateCSS("header23-wrapper-topImg")}`)) as HTMLElement[];
 
-    // Transform faktörleri
     const factorBg1 = 10;
     const factorBg2 = 15;
     const factorBg3 = 20;
@@ -275,7 +234,6 @@ class Header23 extends BaseHeader {
     const factorText = 20;
     const factorTopImg = 20;
 
-    // Transformları uygulayın
     applyTransform(transformWrapperBg1Elements, `translate(${xRatio * factorBg1}px, ${yRatio * factorBg1}px)`);
     applyTransform(transformWrapperBg2Elements, `translate(-50%, -50%) translate(${xRatio * factorBg2}px, ${yRatio * factorBg2}px)`);
     applyTransform(transformWrapperBg3Elements, `translate(${xRatio * factorBg3}px, ${yRatio * factorBg3}px)`);
@@ -293,6 +251,7 @@ class Header23 extends BaseHeader {
   render() {
 
     const settings = {
+      arrows: true,
       dots: false,
       infinite: true,
       speed: 440,
@@ -302,7 +261,6 @@ class Header23 extends BaseHeader {
       slidesToScroll: 1,
       beforeChange: (current: number, next: number) => {
         if (this.getComponentState("active") !== next) {
-          console.log(this.getComponentState("active"));
           this.setComponentState("active", next);
         }
       },
@@ -310,23 +268,59 @@ class Header23 extends BaseHeader {
 
     const slider = this.castToObject<SliderItem[]>("slider");
     const activeSlide = this.getComponentState("active");
+    const sliderRef = this.getComponentState("slider-ref");
+
+    const nextArrow = this.getPropValue("nextArrow");
+    const previousArrow = this.getPropValue("previousArrow");
 
     return (
       <div className={this.decorateCSS("container")} onMouseMove={this.handleMouseMove}>
         <div className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("wrapper")}
-          >
+          <div className={this.decorateCSS("wrapper")}>
 
-            <ComposerSlider {...settings} className={this.decorateCSS("carousel")}>
+            {(slider.length > 1 && previousArrow) &&
+              <ComposerIcon
+                name={this.getPropValue("previousArrow")}
+                propsIcon={{
+                  className: this.decorateCSS("prevArrow"),
+                  onClick: () => {
+                    sliderRef.current.slickPrev();
+                  },
+                }}
+              />}
+            {nextArrow &&
+              <ComposerIcon
+                name={this.getPropValue("nextArrow")}
+                propsIcon={{
+                  className: this.decorateCSS("nextArrow"),
+                  onClick: () => {
+                    sliderRef.current.slickNext();
+                  },
+                }}
+              />
+            }
+            {
+              slider.length > 1 &&
+              <ul className={this.decorateCSS("dots")}>
+                {slider.map((_, index) => (
+                  <li
+                    key={`dot-${index}`}
+                    className={activeSlide === index && this.decorateCSS("slick-active")}
+                    onClick={() => sliderRef.current.slickGoTo(index)}>
+                    <button />
+                  </li>
+                ))}
+              </ul>
+            }
+
+            <ComposerSlider {...settings} className={this.decorateCSS("carousel")} ref={sliderRef}>
               {slider.map((item: SliderItem, index: number) => {
                 const isActive = activeSlide === index;
 
                 return (
-                  <div className={`${this.decorateCSS("items")} ${isActive && this.decorateCSS("active-slide")}`}
-
+                  <div className={this.decorateCSS("items")}
                     key={`key${index}`} >
-                    <div className={this.decorateCSS("wrapper-slick")}
-                    >
+                    <div className={this.decorateCSS("wrapper-slick")}>
                       {item.background1 && (
                         <div className={this.decorateCSS("header23-background1-wrapper")}>
                           <img
@@ -361,9 +355,9 @@ class Header23 extends BaseHeader {
                           />
                         </div>}
                       {item.background4 &&
-                        <div className={this.decorateCSS("img-bg")}>
+                        <div className={this.decorateCSS("header23-img-bg")}>
                           <img
-                            className={`${this.decorateCSS("header23-img-background")} ${isActive && this.decorateCSS("animate")}`}
+                            className={`${this.decorateCSS("img-background")} ${isActive && this.decorateCSS("animate")}`}
                             src={item.background4}
                             alt=""
                           />
