e7297117fa3c28fba41dace3ad2a66011f7428d0
Şahin Ateş <92436793+Ayazelar@users.noreply.github.com>
2024-12-23 14:11:00 +0300

    fix: rendering inline editor (#666)

--- editor-components/EditorComponent.tsx
+++ editor-components/EditorComponent.tsx
@@ -352,16 +352,23 @@ export abstract class Component
         ],
       };
 
-      return (
-        <InlineEditor
-          initialConfig={editorConfig}
-          onChange={this.onChange}
-          HTML={
-            //@ts-ignore
-            () => (<blinkpage dangerouslySetInnerHTML={sanitizedHtml}></blinkpage>)
-          }
-        />
-      );
+      if (process.env.REACT_APP_APP_ENV == "editor") {
+        return (
+          <InlineEditor
+            initialConfig={editorConfig}
+            onChange={this.onChange}
+            HTML={
+              () => (
+                //@ts-ignore
+                <blinkpage dangerouslySetInnerHTML={sanitizedHtml}></blinkpage>
+              )
+            }
+          />
+        );
+      } else {
+        //@ts-ignore
+        return <blinkpage dangerouslySetInnerHTML={sanitizedHtml}></blinkpage>;
+      }
     };
 
     return <SanitizeHTML html={prop?.value}></SanitizeHTML>;
