6966913c44f48f911a865f281a9ad73a162f8d96
Burcu Açan <burcuuacann@gmail.com>
2024-12-24 17:53:29 +0300

    fix: standardization

--- editor-components/navigators/navbar2/navbar2.tsx
+++ editor-components/navigators/navbar2/navbar2.tsx
@@ -3,29 +3,26 @@ import { BaseNavigator } from "../../EditorComponent";
 import styles from "./navbar2.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "composer-tools/composer-base-components/base/base";
 
 class Navbar2 extends BaseNavigator {
   constructor(props?: any) {
     super(props, styles);
+    this.addProp({
+      type: "select",
+      key: "position",
+      displayer: "Position",
+      value: "Normal",
+      additionalParams: {
+        selectItems: ["Normal", "Absolute", "Sticky Colorful", "Sticky Transparent"],
+      },
+    });
     this.addProp({
       type: "icon",
       key: "icon",
       displayer: "Icon",
       value:
         "MdMenu",
-    });
-    this.addProp({
-      type: "boolean",
-      key: "sticky",
-      displayer: "Is sticky",
-      value: false,
-    });
-
-    this.addProp({
-      type: "boolean",
-      key: "middle",
-      displayer: "Item List Middle",
-      value: false,
     });
     this.addProp({
       type: "string",
@@ -35,10 +32,24 @@ class Navbar2 extends BaseNavigator {
     });
     this.addProp({
       type: "image",
-      key: "image",
-      displayer: "Image",
-      value: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
+      key: "logo_default",
+      displayer: "Logo 1",
+      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/676a9ac20655f8002caba0b7?alt=media",
     });
+
+    this.addProp({
+      type: "image",
+      key: "logo_transparent",
+      displayer: "Logo 2",
+      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6762cc190655f8002ca8c66b?alt=media",
+    });
+
+    this.addProp({
+      type: "page",
+      key: "logo_navigate",
+      displayer: "Logo Navigation",
+      value: "",
+    })
     this.addProp({
       type: "array",
       key: "itemList",
@@ -125,6 +136,8 @@ class Navbar2 extends BaseNavigator {
     this.setComponentState("navActive", false);
     this.setComponentState("isAnimating", false);
     this.setComponentState("isVisible", false);
+    this.setComponentState("isScrolled", false);
+    this.setComponentState("maxheight", 0);
   }
   getName(): string {
     return "Navbar 2";
@@ -143,20 +156,49 @@ class Navbar2 extends BaseNavigator {
       }, 1000);
     }
   }
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const scrollPosition = window.scrollY;
+      this.setComponentState("isScrolled", scrollPosition);
+    });
 
+    const elements = document.getElementsByClassName(this.decorateCSS("mobile-item-wrapper"));
+    let height = 0;
+    if (elements.length > 0) {
+      const element = elements[0] as HTMLElement;
+      height = element.offsetHeight;
+      this.setComponentState("maxheight", height);
+    }
+
+  }
 
   render() {
     const title = this.castToString(this.getPropValue("title"));
-    const image = this.getPropValue("image");
-    const active = this.getComponentState("navActive");
+
+    const navbarElement = document.getElementById(`navbar2`);
+    const navbarHeight = navbarElement?.clientHeight || "auto";
+
+    const navActive = this.getComponentState("navActive");
+    const logoDefault = this.getPropValue("logo_default");
+    const logoTransparent = this.getPropValue("logo_transparent");
+
+    const position = this.getPropValue("position");
+    const logo = position === "Absolute" || (position === "Sticky Transparent" && this.getComponentState("isScrolled") < navbarHeight) ? logoTransparent : logoDefault;
+    const formatClassName = (className: any) => className.replace(/\s+/g, "");
+
     return (
-      <div className={`${this.decorateCSS("container")} ${this.getPropValue("sticky") ? this.decorateCSS("sticky") : ""}`}>
-        <div className={this.decorateCSS("max-content")}>
+      <Base.Container id={"navbar2"} className={`${this.decorateCSS("container")} 
+      ${this.decorateCSS(formatClassName(this.getPropValue("position")))} 
+      ${navActive && this.decorateCSS("active")} 
+      ${this.getComponentState("isScrolled") > navbarHeight && this.decorateCSS("bg-color")}`}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("nav")}>
-            {image ? (
-              <div className={this.decorateCSS("image-container")}>
-                <img src={image} className={this.decorateCSS("image")} alt="Image" />
-              </div>
+            {logo ? (
+              <ComposerLink path={this.getPropValue("logo_navigate")}>
+                <div className={this.decorateCSS("image-container")}>
+                  <img src={logo} className={this.decorateCSS("image")} alt={logo} />
+                </div>
+              </ComposerLink>
             ) : (
               title && (
                 <div className={this.decorateCSS("title")}>
@@ -167,9 +209,7 @@ class Navbar2 extends BaseNavigator {
 
             <div className={this.decorateCSS("item-container")}>
               <div
-                className={`${this.decorateCSS("items")} ${this.getPropValue("middle") ? this.decorateCSS("middle") : ""
-                  }`}
-              >
+                className={this.decorateCSS("items")}>
                 {this.castToObject<[]>("itemList").map(
                   (data: any, indexItemList: number) => {
                     return (
@@ -186,9 +226,9 @@ class Navbar2 extends BaseNavigator {
           </div>
           <div className={this.decorateCSS("navigator-mobile")}>
             <div className={this.decorateCSS("navbar")}>
-              {image ? (
+              {logo ? (
                 <div className={this.decorateCSS("image-container")}>
-                  <img src={image} className={this.decorateCSS("image")} alt="Image" />
+                  <img src={logo} className={this.decorateCSS("image")} alt="Image" />
                 </div>
               ) : (
                 title && (
@@ -198,7 +238,6 @@ class Navbar2 extends BaseNavigator {
                 )
               )}
               <ComposerIcon
-
                 propsIcon={{
                   className: `${this.decorateCSS("img-hamburger")} ${this.getComponentState("navActive")
                     ? this.decorateCSS("rotate")
@@ -211,11 +250,10 @@ class Navbar2 extends BaseNavigator {
                 name={this.getPropValue("icon")}
               />
             </div>
-            {this.getComponentState("isVisible") && (
-              <div className={`${this.decorateCSS("navbar-child")} ${this.getComponentState("isAnimating")
-                ? this.decorateCSS("open")
-                : this.decorateCSS("close")
-                }`}>
+            <div className={this.decorateCSS("navbar-child")} style={this.getComponentState("navActive")
+              ? { maxHeight: `${this.getComponentState("maxheight")}px` }
+              : undefined}>
+              <div className={`${this.decorateCSS("mobile-item-wrapper")} ${this.getComponentState("navActive") ? this.decorateCSS("active") : ""}`}>
                 {this.castToObject<[]>("itemList").map((data: any, indexItemList: number) => {
                   const delay = indexItemList * 0.1;
                   return (
@@ -233,12 +271,10 @@ class Navbar2 extends BaseNavigator {
                   );
                 })}
               </div>
-            )}
-
-
+            </div>
           </div>
-        </div>
-      </div>
+        </Base.MaxContent>
+      </Base.Container>
     );
   }
 }
