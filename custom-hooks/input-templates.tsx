b7bde43f89899e09779dcfccd3a5019716f05739
Yusuf Kıtlık <71187794+yusufktlk@users.noreply.github.com>
2024-12-30 11:43:36 +0300

    feat: add icon prop to base button (#693)

--- custom-hooks/input-templates.tsx
+++ custom-hooks/input-templates.tsx
@@ -2,44 +2,57 @@ import { TypeButton } from "composer-tools/composer-base-components/base/base";
 import { TypeUsableComponentProps } from "composer-tools/editor-components/EditorComponent";
 
 export namespace INPUTS {
-  export type CastedButton = { text: JSX.Element; url: string; type: TypeButton };
+   export type CastedButton = {
+     icon: string;
+     text: JSX.Element;
+     url: string;
+     type: TypeButton;
+   };
 
-  export const BUTTON = (
-    buttonKey: string,
-    displayer: string,
-    text: string,
-    url: string = "",
-    type: TypeButton = "Primary"
-  ) => {
-    return {
-      type: "object",
-      key: buttonKey,
-      displayer: displayer,
-      value: [
-        {
-          type: "string",
-          key: "text",
-          displayer: "Text",
-          value: text,
-        },
-        {
-          type: "page",
-          key: "url",
-          displayer: "URL",
-          value: url,
-        },
-        {
-          type: "select",
-          key: "type",
-          displayer: "Type",
-          value: type,
-          additionalParams: {
-            selectItems: ["Primary", "Secondary", "Tertiary", "Link", "White", "Black"],
-          },
-        },
-      ],
-    } as TypeUsableComponentProps;
-  };
+   export const BUTTON = (buttonKey: string, displayer: string, text: string, url: string | null, icon: string | null, type: TypeButton = "Primary") => {
+     const button: TypeUsableComponentProps = {
+       type: "object",
+       key: buttonKey,
+       displayer: displayer,
+       value: [
+         {
+           type: "string",
+           key: "text",
+           displayer: "Text",
+           value: text,
+         },
+         {
+           type: "select",
+           key: "type",
+           displayer: "Type",
+           value: type,
+           additionalParams: {
+             selectItems: ["Primary", "Secondary", "Tertiary", "Link", "White", "Black"],
+           },
+         },
+       ],
+     };
+
+     if (url !== null) {
+       button.value.push({
+         type: "page",
+         key: "url",
+         displayer: "URL",
+         value: url,
+       });
+     }
+
+     if (icon !== null) {
+       button.value.push({
+         type: "icon",
+         key: "icon",
+         displayer: "Icon",
+         value: icon,
+       });
+     }
+
+     return button;
+   };
 
   export const LOGO = (key: string, displayer: string, defaultImage?: string) => {
     return {
