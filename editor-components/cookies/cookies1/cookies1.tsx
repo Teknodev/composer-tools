e6a4196c2f074edb56c786442559696677bd7d42
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2025-01-06 17:34:08 +0300

    feat: cookies 1

---
+++ editor-components/cookies/cookies1/cookies1.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import { BaseModal } from "../../EditorComponent";
+import styles from "./cokkies1.module.scss";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+import { Base } from "../../../composer-base-components/base/base";
+import { INPUTS } from "composer-tools/custom-hooks/input-templates";
+
+class Cookies1 extends BaseModal {
+  constructor(props?: any) {
+    super(props, styles);
+
+    this.addProp({
+      type: "icon",
+      key: "icon",
+      displayer: "Icon",
+      value: "FaCookieBite",
+    });
+
+    this.addProp({
+      type: "string",
+      key: "text",
+      displayer: "Text",
+      value: "We use third-arty cookies in order to personalize your site experience.",
+    });
+
+    this.addProp(INPUTS.BUTTON("buttonAccept", "Accept Button", "Allow", null, null, null, "Tertiary"));
+    this.addProp(INPUTS.BUTTON("buttonDecline", "Decline Button", "Decline", null, null, null, "Tertiary"));
+  }
+
+  getName(): string {
+    return "Cookies 1";
+  }
+
+  handleAccept = () => {
+    console.log("Accept button clicked");
+  };
+
+  handleDecline = () => {
+    console.log("Decline button clicked");
+  };
+
+  render() {
+    const textExist = this.castToString(this.getPropValue("text"));
+    const icon = this.getPropValue("icon");
+
+    const buttonAccept: INPUTS.CastedButton = this.castToObject<INPUTS.CastedButton>("buttonAccept");
+    const buttonDecline: INPUTS.CastedButton = this.castToObject<INPUTS.CastedButton>("buttonDecline");
+
+    return (
+      <Base.Container isModal={true} className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          <Base.VerticalContent className={this.decorateCSS("content")}>
+            {icon && (
+              <div className={this.decorateCSS("icon")}>
+                <ComposerIcon propsIcon={{ className: this.decorateCSS("icon") }} name={this.getPropValue("icon")} />
+              </div>
+            )}
+
+            {textExist && <Base.P className={this.decorateCSS("text")}>{this.getPropValue("text")}</Base.P>}
+
+            <div className={this.decorateCSS("button-wrapper")}>
+              <Base.Button onClick={this.handleAccept} buttonType={buttonAccept.type} className={this.decorateCSS("button")}>
+                {buttonAccept.text}
+              </Base.Button>
+
+              <Base.Button onClick={this.handleDecline} buttonType={buttonDecline.type} className={this.decorateCSS("button")}>
+                {buttonDecline.text}
+              </Base.Button>
+            </div>
+          </Base.VerticalContent>
+        </Base.MaxContent>
+      </Base.Container>
+    );
+  }
+}
+
+export default Cookies1;
--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -227,6 +227,7 @@ import LocationComponent5 from "./location/location5/location5";
 import LocationComponent2 from "./location/location2/location2";
 import ConfirmationModal2 from "./modal/confirmation-modal-2/confirmation-modal-2";
 import LocationComponent4 from "./location/location4/location4";
+import Cookies1 from "./cookies/cookies1/cookies1";
 
 function Registerables(composer: ComponentsRegistery) {
   composer.register([
@@ -458,6 +459,7 @@ function Registerables(composer: ComponentsRegistery) {
     HttpCodes4,
     HttpCodes1,
     ConfirmationModal2,
+    Cookies1,
   ]);
 }
 
