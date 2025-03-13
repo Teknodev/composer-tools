95b69276d5f49e0c0f3383998ff0f35769b53c92
BurcuAcan <burcuuacann@gmail.com>
2025-03-13 14:00:23 +0300

    fix: pricing1 animation fix and multiselect

--- editor-components/pricing-table/pricing-table1/pricing-table1.tsx
+++ editor-components/pricing-table/pricing-table1/pricing-table1.tsx
@@ -571,6 +571,15 @@ class PricingTable1 extends BasePricingTable {
         },
       ],
     });
+    this.addProp({
+      type:"multiSelect",
+      key: "animations",
+      displayer: "Animations",
+      value: ["animation1"],
+      additionalParams:{
+        selectItems:["animation1"]
+      }
+    })
   }
   static getName(): string {
     return "Pricing 1";
@@ -606,7 +615,7 @@ class PricingTable1 extends BasePricingTable {
                 const cardpricingTableTitleExist = this.castToString(table.pricingTableTitle);
 
                 return (
-                  <div key={index} className={`${this.decorateCSS("item-card")} ${table.isActive && this.decorateCSS("active")}`}>
+                  <div key={index} className={`${this.decorateCSS("item-card")} ${this.decorateCSS(this.getPropValue("animations"))} ${table.isActive && this.decorateCSS("active")} `}>
                     {popularText && (
                       <div className={`${this.decorateCSS("popular-box")} ${table.popular_settings.is_popular && this.decorateCSS("active")}`}>
                         <Base.P className={this.decorateCSS("popular-text")}>{table.popular_settings.text}</Base.P>
