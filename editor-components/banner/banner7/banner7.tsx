55dca52946c5527ced0376ca715677861c815b78
Christina <121299208+ChristinaTabor@users.noreply.github.com>
2025-03-19 12:23:54 +0300

    feat: banner7 component

---
+++ editor-components/banner/banner7/banner7.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import styles from "./banner7.module.scss";
+import { Base } from "../../../composer-base-components/base/base";
+import { BaseBanner } from "../../EditorComponent";
+import ComposerLink from "custom-hooks/composer-base-components/Link/link";
+import { ComposerIcon } from "../../../composer-base-components/icon/icon";
+
+class Banner7 extends BaseBanner {
+  constructor(props?: any) {
+    super(props, styles);
+    this.addProp({
+      type: "image",
+      key: "backgroundImage",
+      displayer: "Background Image",
+      value:
+        "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66ac465503b007002cc7448b?alt=media",
+    });
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "About",
+      value: "About",
+    });
+    this.addProp({
+      type: "string",
+      key: "homepage",
+      displayer: "Home",
+      value: "Home",
+    });
+    this.addProp({
+      type: "page",
+      key: "navigateTo",
+      displayer: "Navigate To",
+      value: "",
+    });
+    this.addProp({
+      type: "string",
+      key: "about",
+      displayer: "About",
+      value: "About",
+    });
+    this.addProp({
+      type: "icon",
+      key: "crumberIcon",
+      displayer: "Icon",
+      value: "IoIosArrowForward",
+    });
+  }
+
+  static getName(): string {
+    return "Banner 7";
+  }
+
+  render() {
+    const isTitleExist = this.castToString(this.getPropValue("title"));
+    const homepage = this.castToString(this.getPropValue("homepage"));
+    const about = this.castToString(this.getPropValue("about"));
+    const navigateToFromCrumber = this.getPropValue("navigateTo");
+    const navigateToUrl = navigateToFromCrumber || "";
+    const isCrumberVisible = homepage && about;
+
+    return (
+      <Base.Container
+        className={this.decorateCSS("container")}
+        style={{
+          backgroundImage: `url(${this.getPropValue("backgroundImage")})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {isCrumberVisible && (
+          <Base.MaxContent className={this.decorateCSS("breadcrumb-wrapper")}>
+            <div className={this.decorateCSS("crumber-content")}>
+              {isTitleExist && (
+                <Base.MaxContent className={this.decorateCSS("title")}>
+                  {this.getPropValue("title")}
+                </Base.MaxContent>
+              )}
+              <ComposerLink
+                path={navigateToUrl}
+                className={this.decorateCSS("home-link")}
+              >
+                <span className={this.decorateCSS("home-page")}>
+                  {this.getPropValue("homepage")}
+                </span>
+              </ComposerLink>
+              <ComposerIcon
+                name={this.getPropValue("crumberIcon")}
+                propsIcon={{
+                  className: this.decorateCSS("crumberIcon"),
+                }}
+              />
+              <span className={this.decorateCSS("about-page")}>
+                {this.getPropValue("about")}
+              </span>
+            </div>
+          </Base.MaxContent>
+        )}
+      </Base.Container>
+    );
+  }
+}
+
+export default Banner7;
