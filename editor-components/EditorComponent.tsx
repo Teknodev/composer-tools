978b769bf7cc5519b9ac738f5907c06969dbbffc
BurcuAcan <burcuuacann@gmail.com>
2025-04-10 18:07:10 +0300

    feat: social component 3

--- editor-components/EditorComponent.tsx
+++ editor-components/EditorComponent.tsx
@@ -242,6 +242,7 @@ export enum CATEGORIES {
   LOCATION = "location",
   HTTP_CODES = "HTTPCodes",
   BANNER = "banner",
+  SOCIAL = "social"
 }
 
 export function generateId(key: string): string {
@@ -770,6 +771,9 @@ export abstract class BaseContacts extends Component {
 
 export abstract class BaseFeature extends Component {
   static category = CATEGORIES.FEATURE;
+}
+export abstract class BaseSocial extends Component {
+  static category = CATEGORIES.SOCIAL;
 }
 
 export function generateAutoClassName(componentId: string, section: string){
