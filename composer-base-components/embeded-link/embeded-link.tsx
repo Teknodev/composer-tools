a57b261fa88c7eb2a755cdaf660793383b0fe9da
BurcuAcan <burcuuacann@gmail.com>
2025-04-11 15:13:28 +0300

    fix: widget.js entegration

--- composer-base-components/embeded-link/embeded-link.tsx
+++ composer-base-components/embeded-link/embeded-link.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useRef } from "react";
+
 type ComposerEmbededLinkProps = {
-    url: string;
-  };
-  
-  export function ComposerEmbededLink({ url }: ComposerEmbededLinkProps): React.JSX.Element {
-    return <div dangerouslySetInnerHTML={{ __html: url }} />;
-  }
-  
+  path: string;
+  className?: string
+};
+
+export function ComposerEmbededLink({ path , className}: ComposerEmbededLinkProps): React.JSX.Element {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    ref.current.innerHTML = path;
+
+    const existingScript = document.querySelector('script[src="https://platform.twitter.com/widgets.js"]');
+    if (!existingScript) {
+      const script = document.createElement("script");
+      script.setAttribute("src", "https://platform.twitter.com/widgets.js");
+      script.setAttribute("async", "true");
+      script.setAttribute("charset", "utf-8");
+      document.body.appendChild(script);
+    } else {
+      // @ts-ignore
+      if (window.twttr && window.twttr.widgets) {
+        // @ts-ignore
+        window.twttr.widgets.load(ref.current);
+      }
+    }
+  }, [path]);
+
+  return <div className={className} ref={ref} />;
+}
