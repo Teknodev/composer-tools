70cef8134a8ba879e209457322016c0ee4073a0c
Burcu Açan <79398729+BurcuAcan@users.noreply.github.com>
2025-04-21 13:52:54 +0300

    Update slider9.tsx

--- editor-components/slider/slider9/slider9.tsx
+++ editor-components/slider/slider9/slider9.tsx
@@ -290,8 +290,6 @@ class Slider9 extends BaseSlider {
       slidesToShow: 3,
       slidesToScroll: 1,
       vertical: true,
-      verticalSwiping: true,
-      swipeToSlide: true,
       afterChange: (current: number) => {
         this.setComponentState("currentSlideIndex", current);
         const horizontalSliderRef = this.getComponentState("horizontal-slider-ref");
