75d8c30e82795602002d068452b295e24fc26165
Burcu Açan <79398729+BurcuAcan@users.noreply.github.com>
2025-04-22 10:53:59 +0300

    feat: social3 widget (#844)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -228,6 +228,7 @@ import BannerComponent2 from "../editor-components/banner/banner2/banner2";
 import SocialComponent1 from "./social/social1/social1";
 import SocialComponent3 from "../editor-components/social/social3/social3";
 import SocialWidget1 from "../editor-components/social_widget/social_widget1/social_widget1";
+import SocialWidget3 from "../editor-components/social_widget/social_widget3/social_widget3";
 import SocialWidget4 from "../editor-components/social_widget/social_widget4/social_widget4";
 
 function Registerables(composer: ComponentsRegistery) {
@@ -460,7 +461,8 @@ function Registerables(composer: ComponentsRegistery) {
     BannerComponent2,
     SocialComponent3,
     SocialWidget1,
-    SocialWidget4
+    SocialWidget3,
+    SocialWidget4,
   ]);
 }
 
