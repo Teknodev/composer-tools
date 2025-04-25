a6eb622b6f0ea07be4accda67240a4e21a7424e2
dagsevenonur <onurdagseven01@gmail.com>
2025-04-25 15:48:03 +0300

    feat:  Banner4 component (#796)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -225,6 +225,7 @@ import Location6 from "./location/location6/location6";
 import ConfirmationModal2 from "./modal/confirmation-modal-2/confirmation-modal-2";
 import BannerComponent1 from "../editor-components/banner/banner1/banner1";
 import BannerComponent2 from "../editor-components/banner/banner2/banner2";
+import BannerComponent4 from "../editor-components/banner/banner4/banner4";
 import SocialComponent1 from "./social/social1/social1";
 import SocialComponent3 from "../editor-components/social/social3/social3";
 import SocialWidget1 from "../editor-components/social_widget/social_widget1/social_widget1";
@@ -459,6 +460,7 @@ function Registerables(composer: ComponentsRegistery) {
     ConfirmationModal2,
     BannerComponent1,
     BannerComponent2,
+    BannerComponent4,
     SocialComponent3,
     SocialWidget1,
     SocialWidget3,
