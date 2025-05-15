510a1646e99b613eefa3dc3e396e6f82a1e6db22
riavenn <riavennn@gmail.com>
2025-05-15 10:39:49 +0300

    fix: content 19 changes

--- editor-components/content/content19/content19.tsx
+++ editor-components/content/content19/content19.tsx
@@ -4,6 +4,9 @@ import { BaseContent } from "../../EditorComponent";
 import { Base } from "../../../composer-base-components/base/base";
 import { ComposerIcon } from "composer-tools/composer-base-components/icon/icon";
 import ComposerLink from "custom-hooks/composer-base-components/Link/link";
+import { INPUTS } from "composer-tools/custom-hooks/input-templates";
+
+type Side = 'left' | 'right';
 
 class Content19 extends BaseContent {
     constructor(props?: any) {
@@ -11,7 +14,7 @@ class Content19 extends BaseContent {
         this.addProp({
             type: "string",
             key: "leftSideSubTitle",
-            displayer: "Left Side Sub Title",
+            displayer: "Left Side Subtitle",
             value: "GANACHE"
         })
         this.addProp({
@@ -27,24 +30,6 @@ class Content19 extends BaseContent {
             value: "Lorem ipsum dolor sit amet, ea mel modo qualisque, possit nemore facilis vel te. Laudem aeterno dolorum no vix, ea sed falli option. Doming legendos his cu. Te nonumy eleifend expetenda usu. Quo appetere gubergren et."
         })
         this.addProp({
-            type: "string",
-            key: "leftNavigateText",
-            displayer: "Left Navigate Text",
-            value: "READ MORE"
-        })
-        this.addProp({
-            type: "page",
-            key: "leftNavigateTo",
-            displayer: "Navigate To",
-            value: ""
-        })
-        this.addProp({
-            type: "icon",
-            key: "leftNavigateIcon",
-            displayer: "Icon",
-            value: "BsArrowRight"
-        })
-        this.addProp({
             type: "image",
             key: "image",
             displayer: "Image",
@@ -53,7 +38,7 @@ class Content19 extends BaseContent {
         this.addProp({
             type: "string",
             key: "rightSideSubTitle",
-            displayer: "Right Side Sub Title",
+            displayer: "Right Side Subtitle",
             value: "SWEET"
         })
         this.addProp({
@@ -69,24 +54,6 @@ class Content19 extends BaseContent {
             value: "Lorem ipsum dolor sit amet, ea mel modo qualisque, possit nemore facilis vel te. Laudem aeterno dolorum no vix, ea sed falli option. Doming legendos his cu. Te nonumy eleifend expetenda usu. Quo appetere gubergren et."
         })
         this.addProp({
-            type: "string",
-            key: "rightNavigateText",
-            displayer: "Right Navigate Text",
-            value: "READ MORE"
-        })
-        this.addProp({
-            type: "page",
-            key: "rightNavigateTo",
-            displayer: "Navigate To",
-            value: ""
-        })
-        this.addProp({
-            type: "icon",
-            key: "rightNavigateIcon",
-            displayer: "Icon",
-            value: "BsArrowRight"
-        })
-        this.addProp({
             type: "multiSelect",
             key: "hoverAnimation",
             displayer: "Hover Animation Style",
@@ -95,137 +62,96 @@ class Content19 extends BaseContent {
                 selectItems: ["animate1", "animate2"]
             }
         });
+        this.addProp(INPUTS.BUTTON("leftSideButton", "Left Side Button", "READ MORE", "", "BsArrowRight", null, "Tertiary"));
+        this.addProp(INPUTS.BUTTON("rightSideButton", "Right Side Button", "READ MORE", "", "BsArrowRight", null, "Tertiary"));
     }
 
     static getName(): string {
         return "Content 19";
     }
 
-    render() {
-        const leftSideSubTitle = this.getPropValue("leftSideSubTitle");
-        const leftSideTitle = this.getPropValue("leftSideTitle");
-        const leftSideText = this.getPropValue("leftSideText");
+    private renderSide(side: Side) {
+        const subTitle = this.getPropValue(`${side}SideSubTitle`);
+        const title = this.getPropValue(`${side}SideTitle`);
+        const text = this.getPropValue(`${side}SideText`);
+        const sideButton = this.castToObject(`${side}SideButton`) as INPUTS.CastedButton;
 
-        const rightNavigateText = this.getPropValue("rightNavigateText");
-        const rightNavigateTo = this.getPropValue("rightNavigateTo");
-        const rightNavigateIcon = this.getPropValue("rightNavigateIcon");
-        const rightNavigateContainer = this.castToString(rightNavigateText) || (rightNavigateIcon);
+        const showNavigateContainer = sideButton.icon || this.castToString(sideButton.text);
+        const showSideContainer = this.castToString(subTitle) || this.castToString(title) || this.castToString(text) || showNavigateContainer;
 
-        const leftNavigateText = this.getPropValue("leftNavigateText");
-        const leftNavigateTo = this.getPropValue("leftNavigateTo");
-        const leftNavigateIcon = this.getPropValue("leftNavigateIcon");
-        const leftNavigateContainer = this.castToString(leftNavigateText) || (leftNavigateIcon);
-
-        const image = this.getPropValue("image");
-
-        const rightSideSubTitle = this.getPropValue("rightSideSubTitle");
-        const rightSideTitle = this.getPropValue("rightSideTitle");
-        const rightSideText = this.getPropValue("rightSideText");
-
-        const showLeftSide = this.castToString(leftSideSubTitle) || this.castToString(leftSideTitle) || this.castToString(leftSideText) || leftNavigateContainer;
-        const showRightSide = this.castToString(rightSideSubTitle) || this.castToString(rightSideTitle) || this.castToString(rightSideText) || rightNavigateContainer;
+        if (!showSideContainer) {
+            return null;
+        }
 
         return (
-            <Base.Container className={this.decorateCSS("container")}>
-                <Base.MaxContent className={this.decorateCSS("max-content")}>
-                    {showLeftSide && (
-                        <div className={this.decorateCSS("side-container")}>
-                            <div className={this.decorateCSS("side-container-wrap")}>
-                                <div className={this.decorateCSS("side")}>
-                                    {this.castToString(leftSideSubTitle) && (
-                                        <Base.SectionSubTitle className={this.decorateCSS("sub-title")}>
-                                            {leftSideSubTitle}
-                                        </Base.SectionSubTitle>
-                                    )}
-                                    {this.castToString(leftSideTitle) && (
-                                        <Base.SectionTitle className={this.decorateCSS("title")}>
-                                            {leftSideTitle}
-                                        </Base.SectionTitle>
-                                    )}
-                                    {this.castToString(leftSideText) && (
-                                        <Base.SectionDescription className={this.decorateCSS("text")}>
-                                            {leftSideText}
-                                        </Base.SectionDescription>
-                                    )}
-                                    {leftNavigateContainer && (
-                                        <ComposerLink
-                                            path={leftNavigateTo}>
-                                            <div className={this.decorateCSS("navigate-container")}
-                                                data-animation={this.getPropValue("hoverAnimation").join(" ")}>
-                                                {this.castToString(leftNavigateText) && (
+            <>
+                {showSideContainer && (
+                    <div className={this.decorateCSS("side-container")}>
+                        <div className={this.decorateCSS("side-container-wrap")}>
+                            <div className={this.decorateCSS("side")}>
+                                {this.castToString(subTitle) && (
+                                    <Base.SectionSubTitle className={this.decorateCSS("sub-title")}>
+                                        {subTitle}
+                                    </Base.SectionSubTitle>
+                                )}
+                                {this.castToString(title) && (
+                                    <Base.SectionTitle className={this.decorateCSS("title")}>
+                                        {title}
+                                    </Base.SectionTitle>
+                                )}
+                                {this.castToString(text) && (
+                                    <Base.SectionDescription className={this.decorateCSS("text")}>
+                                        {text}
+                                    </Base.SectionDescription>
+                                )}
+                                {showNavigateContainer &&
+                                    <Base.Button buttonType={sideButton.type} className={this.decorateCSS("button")}
+                                        data-animation={this.getPropValue("hoverAnimation").join(" ")}>
+                                        <ComposerLink path={sideButton.url}>
+                                            <div className={this.decorateCSS("text-and-icon")}>
+                                                {this.castToString(sideButton.text) && (
                                                     <div className={this.decorateCSS("navigate-text")}>
-                                                        {leftNavigateText}
+                                                        {sideButton.text}
                                                     </div>
                                                 )}
-                                                {rightNavigateIcon && (
+                                                {sideButton.icon && (
                                                     <ComposerIcon
-                                                        name={leftNavigateIcon}
+                                                        name={sideButton.icon}
                                                         propsIcon={{
-                                                            className: this.decorateCSS("left-navigate-icon"),
+                                                            className: this.decorateCSS(`${side}-navigate-icon`),
                                                         }}>
                                                     </ComposerIcon>
                                                 )}
                                             </div>
                                         </ComposerLink>
-                                    )}
-                                </div>
+                                    </Base.Button>
+                                }
                             </div>
                         </div>
-                    )}
+                    </div>
+                )}
+            </>
+        );
+    }
+
+    render() {
+        const image = this.getPropValue("image");
+
+        return (
+            <Base.Container className={this.decorateCSS("container")}>
+                <Base.MaxContent className={this.decorateCSS("max-content")}>
+                    {this.renderSide('left')}
                     {image && (
                         <div className={this.decorateCSS("middle-container")}
                             data-animation={this.getPropValue("hoverAnimation").join(" ")}>
                             <div className={this.decorateCSS("middle-container-wrap")}>
                                 <div className={this.decorateCSS("middle")}>
-                                    <img src={image} alt="" />
+                                    <img src={image} alt="" className={this.decorateCSS("image")} />
                                 </div>
                             </div>
                         </div>
                     )}
-                    {showRightSide && (
-                        <div className={this.decorateCSS("side-container")}>
-                            <div className={this.decorateCSS("side-container-wrap")}>
-                                <div className={this.decorateCSS("side")}>
-                                    {this.castToString(rightSideSubTitle) && (
-                                        <Base.SectionSubTitle className={this.decorateCSS("sub-title")}>
-                                            {rightSideSubTitle}
-                                        </Base.SectionSubTitle>
-                                    )}
-                                    {this.castToString(rightSideTitle) && (
-                                        <Base.SectionTitle className={this.decorateCSS("title")}>
-                                            {rightSideTitle}
-                                        </Base.SectionTitle>
-                                    )}
-                                    {this.castToString(rightSideText) && (
-                                        <Base.SectionDescription className={this.decorateCSS("text")}>
-                                            {rightSideText}
-                                        </Base.SectionDescription>
-                                    )}
-                                    {rightNavigateContainer && (
-                                        <ComposerLink
-                                            path={rightNavigateTo}>
-                                            <div className={this.decorateCSS("navigate-container")}
-                                                data-animation={this.getPropValue("hoverAnimation").join(" ")}>
-                                                {this.castToString(rightNavigateText) && (
-                                                    <div className={this.decorateCSS("navigate-text")}>
-                                                        {rightNavigateText}
-                                                    </div>
-                                                )}
-                                                {rightNavigateIcon && (
-                                                    <ComposerIcon
-                                                        name={rightNavigateIcon}
-                                                        propsIcon={{
-                                                            className: this.decorateCSS("right-navigate-icon"),
-                                                        }}>
-                                                    </ComposerIcon>
-                                                )}
-                                            </div>
-                                        </ComposerLink>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                    )}
+                    {this.renderSide('right')}
                 </Base.MaxContent>
             </Base.Container >
         )
