06523b23cbd73d377a9d6a2f933d096693312b7a
unknown <rayfattevke@gmail.com>
2025-07-23 14:03:24 +0300

    chore: removed comment lines

--- editor-components/call_to_action/call_to_action11/call_to_action11.tsx
+++ editor-components/call_to_action/call_to_action11/call_to_action11.tsx
@@ -1,4 +1,3 @@
-// call_to_action11.tsx
 import * as React from "react";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import { BaseCallToAction } from "../../EditorComponent";
@@ -12,12 +11,11 @@ class CallToAction11 extends BaseCallToAction {
   constructor(props?: any) {
     super(props, styles);
 
-    // Text prop eklendi (title yerine description olarak) - İstek 1 ve 2
     this.addProp({
       type: "string",
-      key: "text", // key değiştirildi: title -> text
+      key: "text",
       value: "Get answers and advice from people you want it from. Intact designers and developers will help you create awesome websites for your startup.",
-      displayer: "Text", // displayer değiştirildi: Title -> Text
+      displayer: "Text",
     });
 
     this.addProp({
@@ -35,20 +33,16 @@ class CallToAction11 extends BaseCallToAction {
   render() {
     const alignment = Base.getContentAlignment();
     const buttons = this.castToObject<Button[]>("buttons") || [];
-    const text = this.castToString(this.getPropValue("text")); // title -> text değiştirildi
+    const text = this.castToString(this.getPropValue("text"));
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
-          {/* wrapper div'i kaldırıldı - gereksiz class - İstek 7 */}
           {(text || buttons.length > 0) && (
             <div className={this.decorateCSS("content")}>
-              {/* header -> content olarak değiştirildi, center class kaldırıldı - İstek 7 */}
               {text && (
                 <div className={this.decorateCSS("text-container")}>
-                  {/* titles -> text-container, VerticalContent kaldırıldı - İstek 7 */}
                   <Base.SectionDescription className={this.decorateCSS("text")}>
-                    {/* SectionTitle -> SectionDescription (title değil description) - İstek 1 */}
                     {text}
                   </Base.SectionDescription>
                 </div>
@@ -59,10 +53,7 @@ class CallToAction11 extends BaseCallToAction {
                   {buttons.map((button, idx) => (
                     <ComposerLink path={button.url} key={idx}>
                       <Base.Button className={`${this.decorateCSS("button")} ${button.type?.toLowerCase() || 'primary'}`} buttonType={button.type}>
-
-
                         <span className={this.decorateCSS("button-text")}>
-                          {/* Button text için class eklendi - İstek 8 */}
                           {button.text}
                         </span>
                       </Base.Button>
