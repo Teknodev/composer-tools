7134fc89be7ef3f802a01750b4d2fd5bd218ace1
Şahin Ateş <92436793+Ayazelar@users.noreply.github.com>
2025-07-25 10:31:07 +0300

    feat: add base media to use unified media input (#925)

--- editor-components/EditorComponent.tsx
+++ editor-components/EditorComponent.tsx
@@ -221,6 +221,22 @@ export type TypeLocation = {
   lat: number;
 };
 
+export type TypeMediaInputValue =
+  | { type: "image"; url: string }
+  | { type: "icon"; name: string }
+  | {
+      type: "video";
+      url: string;
+      settings: {
+        autoplay?: boolean;
+        controls?: boolean;
+        loop?: boolean;
+        muted?: boolean;
+      };
+    };
+
+export type MediaType = "icon" | "image" | "video";
+
 type currencyAdditionalParams ={
   showCode?: boolean;
   showSymbol?:boolean;
@@ -291,6 +307,7 @@ type AvailablePropTypes =
   | { type: "dateTime"; value: string ; additionalParams? : {mode?:string, timeInterval?:number, yearRange? : number, yearStart?: number}}
   | { type: "multiSelect"; value: string[] }
   | { type: "file"; value: string }
+  | { type: "media"; value: TypeMediaInputValue }
   | { type: "embededLink"; value: string }
 
 export type TypeReactComponent = {
@@ -304,7 +321,7 @@ export type TypeUsableComponentProps = {
   id?: string;
   key: string;
   displayer: string;
-  additionalParams?: { selectItems?: string[]; maxElementCount?: number};
+  additionalParams?: { selectItems?: string[]; maxElementCount?: number; availableTypes?:  MediaType[]};
   max?: number;
 } & AvailablePropTypes & {
   getPropValue?: (
