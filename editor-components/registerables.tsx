14ad247e304aaaffe0fa45b6034cf116fd0e8df0
Yusuf Kıtlık <71187794+yusufktlk@users.noreply.github.com>
2025-07-29 14:58:40 +0300

    feat: create location 7 component (#921)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -230,6 +230,7 @@ import Location3 from "./location/location3/location3";
 import Location4 from "./location/location4/location4";
 import Location5 from "./location/location5/location5";
 import Location6 from "./location/location6/location6";
+import Location7 from "./location/location7/location7";
 import ConfirmationModal2 from "./modal/confirmation-modal-2/confirmation-modal-2";
 import ECommerce5 from "./e-commerce/e-commerce5";
 import BannerComponent1 from "./banner/banner1/banner1";
@@ -491,6 +492,7 @@ function Registerables(composer: ComponentsRegistery) {
     Location4,
     Location5,
     Location6,
+    Location7,
     ConfirmationModal2,
     BannerComponent1,
     BannerComponent2,
