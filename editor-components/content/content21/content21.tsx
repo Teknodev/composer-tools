e4b142ea082936cf5b95dcee53dc2252dfc05595
Semir Tatli <semirtatli@outlook.com>
2025-07-31 00:24:12 +0300

    refactor: props logic fixed and style changes

--- editor-components/content/content21/content21.tsx
+++ editor-components/content/content21/content21.tsx
@@ -3,10 +3,10 @@ import { BaseContent } from "../../EditorComponent";
 import styles from "./content21.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
-
+import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 interface StatItem {
-  value: string;
-  label: string;
+  value: React.JSX.Element;
+  label: React.JSX.Element;
 }
 
 class Content21 extends BaseContent {
@@ -15,13 +15,13 @@ class Content21 extends BaseContent {
 
     this.addProp({
       type: "array",
-      key: "stats",
-      displayer: "Stats",
+      key: "statItems",
+      displayer: "Stat Items",
       value: [
         {
           type: "object",
-          key: "stat",
-          displayer: "Stat",
+          key: "statItem",
+          displayer: "Stat Item",
           value: [
             { type: "string", key: "value", displayer: "Value", value: "12" },
             {
@@ -34,8 +34,8 @@ class Content21 extends BaseContent {
         },
         {
           type: "object",
-          key: "stat",
-          displayer: "Stat",
+          key: "statItem",
+          displayer: "Stat Item",
           value: [
             { type: "string", key: "value", displayer: "Value", value: "68" },
             {
@@ -48,8 +48,8 @@ class Content21 extends BaseContent {
         },
         {
           type: "object",
-          key: "stat",
-          displayer: "Stat",
+          key: "statItem",
+          displayer: "Stat Item",
           value: [
             { type: "string", key: "value", displayer: "Value", value: "16" },
             {
@@ -68,35 +68,42 @@ class Content21 extends BaseContent {
       key: "image",
       displayer: "Image",
       value:
-        "https://woodmart.xtemos.com/wp-content/uploads/2021/04/woodmart-architecture-landing-image.jpg",
+        "https://woodmart.xtemos.com/wp-content/uploads/2023/03/w-architecture-image.jpg.webp",
+    });
+    this.addProp({
+      type: "boolean",
+      key: "faintLine",
+      displayer: "Faint Line",
+      value: true,
     });
 
     this.addProp({
       type: "string",
-      key: "sectionTitle",
-      displayer: "Section Title",
+      key: "subTitle",
+      displayer: "Sub Title",
       value: "Our services",
     });
     this.addProp({
       type: "string",
-      key: "mainTitle",
-      displayer: "Main Title",
+      key: "title",
+      displayer: "Title",
       value: "Work with love",
     });
     this.addProp({
       type: "string",
-      key: "description1",
-      displayer: "Description 1",
+      key: "text1",
+      displayer: "Text",
       value:
         "We help our clients realise their ambitions through architecture that’s grounded in people, their experiences and their environments, applying our integrated expertise and the most advanced digital tools to unlock the potential in every project.",
     });
     this.addProp({
       type: "string",
-      key: "description2",
-      displayer: "Description 2",
+      key: "text2",
+      displayer: "Text 2",
       value:
         "It’s an approach that’s allowed us to create exhilarating sporting venues, bring greener futures to commercial developments, connect communities through more intuitive and efficient transport systems, and deliver cutting edge scientific and industrial facilities.",
     });
+
     this.addProp({
       type: "string",
       key: "buttonText",
@@ -109,6 +116,23 @@ class Content21 extends BaseContent {
       displayer: "Button Link",
       value: "",
     });
+
+    this.addProp({
+      type: "array",
+      key: "buttons",
+      displayer: "Buttons",
+      value: [
+        INPUTS.BUTTON(
+          "button",
+          "Button",
+          "Send Request",
+          "",
+          null,
+          null,
+          "Primary"
+        ),
+      ],
+    });
   }
 
   static getName(): string {
@@ -116,15 +140,15 @@ class Content21 extends BaseContent {
   }
 
   render() {
-    const stats = this.castToObject<StatItem[]>("stats");
+    const statItems = this.castToObject<StatItem[]>("statItems");
     const image = this.getPropValue("image");
-    const sectionTitle = this.getPropValue("sectionTitle");
-    const mainTitle = this.getPropValue("mainTitle");
-    const description1 = this.getPropValue("description1");
-    const description2 = this.getPropValue("description2");
-    const buttonText = this.getPropValue("buttonText");
-    const buttonLink = this.getPropValue("buttonLink");
+    const subTitle = this.getPropValue("subTitle");
+    const title = this.getPropValue("title");
+    const text1 = this.getPropValue("text1");
+    const text2 = this.getPropValue("text2");
     const alignment = Base.getContentAlignment();
+    const faintLine = this.getPropValue("faintLine");
+    const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
@@ -136,65 +160,70 @@ class Content21 extends BaseContent {
               }`}
             >
               <div className={this.decorateCSS("stats-list")}>
-                {stats.map((item, idx) => (
+                {statItems.map((item, idx) => (
                   <div className={this.decorateCSS("stat-item")} key={idx}>
-                    <Base.P className={this.decorateCSS("stat-value")}>
-                      {item.value}
-                    </Base.P>
-                    <Base.P className={this.decorateCSS("stat-label")}>
-                      {item.label}
-                    </Base.P>
+                    {this.castToString(item.value) && (
+                      <Base.P className={this.decorateCSS("stat-value")}>
+                        {item.value}
+                      </Base.P>
+                    )}
+                    {this.castToString(item.label) && (
+                      <Base.P className={this.decorateCSS("stat-label")}>
+                        {item.label}
+                      </Base.P>
+                    )}
                   </div>
                 ))}
               </div>
               {image && (
                 <div className={this.decorateCSS("image-box")}>
-                  <img
-                    className={this.decorateCSS("image")}
-                    src={image}
-                    alt="stat"
-                  />
+                  <img className={this.decorateCSS("image")} src={image} />
                 </div>
               )}
             </Base.GridCell>
+            {faintLine && <hr className={this.decorateCSS("stick")} />}
             <Base.GridCell
               className={`${this.decorateCSS("right")} ${
                 alignment === "center" && this.decorateCSS("center")
               }`}
             >
-              {sectionTitle && (
-                <Base.SectionSubTitle
-                  className={this.decorateCSS("section-title")}
-                >
-                  {sectionTitle}
+              {this.castToString(subTitle) && (
+                <Base.SectionSubTitle className={this.decorateCSS("subTitle")}>
+                  {subTitle}
                 </Base.SectionSubTitle>
               )}
-              {mainTitle && (
-                <Base.SectionTitle className={this.decorateCSS("main-title")}>
-                  {mainTitle}
+              {this.castToString(title) && (
+                <Base.SectionTitle className={this.decorateCSS("title")}>
+                  {title}
                 </Base.SectionTitle>
               )}
-              {description1 && (
-                <Base.SectionDescription
-                  className={this.decorateCSS("description")}
-                >
-                  {description1}
+              {text1 && (
+                <Base.SectionDescription className={this.decorateCSS("text")}>
+                  {text1}
                 </Base.SectionDescription>
               )}
-              {description2 && (
-                <Base.SectionDescription
-                  className={this.decorateCSS("description")}
-                >
-                  {description2}
+              {text2 && (
+                <Base.SectionDescription className={this.decorateCSS("text")}>
+                  {text2}
                 </Base.SectionDescription>
               )}
-              <div className={this.decorateCSS("button-container")}>
-                <ComposerLink path={buttonLink}>
-                  <Base.Button className={this.decorateCSS("button")}>
-                    {buttonText}
-                  </Base.Button>
-                </ComposerLink>
-              </div>
+              {buttons.length > 0 &&
+                buttons.map((item: INPUTS.CastedButton, index: number) => {
+                  return (
+                    <div className={this.decorateCSS("button-container")}>
+                      {this.castToString(item.text) && (
+                        <ComposerLink path={item.url}>
+                          <Base.Button
+                            buttonType={item.type}
+                            className={this.decorateCSS("button")}
+                          >
+                            {item.text}
+                          </Base.Button>
+                        </ComposerLink>
+                      )}
+                    </div>
+                  );
+                })}
             </Base.GridCell>
           </Base.ContainerGrid>
         </Base.MaxContent>
