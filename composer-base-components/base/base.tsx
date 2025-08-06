e8a9b8e5dbbe2e2bbf29a3de6bce232225c4e974
yusufktlk <yusufbjk__44@hotmail.com>
2025-08-06 15:28:53 +0300

    feat: create base card

--- composer-base-components/base/base.tsx
+++ composer-base-components/base/base.tsx
@@ -581,6 +581,18 @@ export namespace Base {
     }
   }
 
+  export function Card({ 
+    className, 
+    children, 
+    ...props 
+  }: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+      <div className={`${styles.card} ${className}`} {...props}>
+        {children}
+      </div>
+    );
+  }
+
   export function Media({
     value,
     className,
