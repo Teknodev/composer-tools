6bf2f629dc4bf3b798efed0bb128986b0842bac3
Semir Tatlı <62341982+semirtatli@users.noreply.github.com>
2025-08-11 17:56:37 +0300

    Feat: FAQ9 Component added (#920)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -73,6 +73,7 @@ import FaqComponent5 from "./faq/faq5/faq5";
 import FaqComponent6 from "./faq/faq6/faq6";
 import FaqComponent7 from "./faq/faq7/faq7";
 import FaqComponent8 from "./faq/faq8/faq8";
+import FaqComponent9 from "./faq/faq9/faq9";
 import FaqComponent10 from "./faq/faq10/faq10";
 import TeamComponent1 from "./team/team-1/team1";
 import TeamComponent2 from "./team/team-2/team2";
@@ -343,6 +344,7 @@ function Registerables(composer: ComponentsRegistery) {
     FaqComponent6,
     FaqComponent7,
     FaqComponent8,
+    FaqComponent9,
     FaqComponent10,
     TeamComponent1,
     TeamComponent2,
