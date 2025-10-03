3fc5705c2daa3f47a3ff071ae5194103e82e5b92
bestkse <bestekose141@gmail.com>
2025-10-03 11:00:00 +0300

    feat(testimonials15): add new testimonial to components (#940)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -183,6 +183,7 @@ import TestimonialsComponent11 from "./testimonials/testimonials11/testimonials1
 import TestimonialsComponent12 from "./testimonials/testimonials12/testimonials12";
 import TestimonialsComponent13 from "./testimonials/testimonials13/testimonials13";
 import TestimonialsComponent14 from "./testimonials/testimonials14/testimonials14";
+import TestimonialsComponent15 from "./testimonials/testimonials15/testimonials15";
 import StatsComponent1 from "./stats/stats1/stats1";
 import StatsComponent2 from "./stats/stats2/stats2";
 import StatsComponent3 from "./stats/stats3/stats3";
@@ -484,6 +485,7 @@ function Registerables(composer: ComponentsRegistery) {
     TestimonialsComponent12,
     TestimonialsComponent13,
     TestimonialsComponent14,
+    TestimonialsComponent15,
     StatsComponent1,
     StatsComponent2,
     StatsComponent3,
