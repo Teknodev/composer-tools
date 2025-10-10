feb2c6976bc1b27f1d119250921f2fca4cdeaeb6
Burak <sinann00345@gmail.com>
2025-10-10 09:53:23 +0300

    undo changes

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -146,7 +146,6 @@ import FeatureComponent13 from "./feature/feature13/feature13";
 import FeatureComponent14 from "./feature/feature14/feature14";
 import FeatureComponent15 from "./feature/feature15/feature15";
 import FeatureComponent16 from "./feature/feature16/feature16";
-import FeatureComponent17 from "./feature/feature17/feature17";
 import FeatureComponent18 from "./feature/feature18/feature18";
 import CallToAction1 from "./call_to_action/call_to_action1/call_to_action1";
 import CallToAction2 from "./call_to_action/call_to_action2/call_to_action2";
@@ -449,7 +448,6 @@ export const registerableComponents = {
     FeatureComponent14,
     FeatureComponent15,
     FeatureComponent16,
-    FeatureComponent17,
     FeatureComponent18,
     CallToAction1,
     CallToAction2,
@@ -730,7 +728,6 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent14,
     FeatureComponent15,
     FeatureComponent16,
-    FeatureComponent17,
     FeatureComponent18,
     CallToAction1,
     CallToAction2,
