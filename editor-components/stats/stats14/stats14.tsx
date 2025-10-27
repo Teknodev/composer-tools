592b6dad77d36c3f86a0a66ff151098622bae9ee
“mehmetmetin07” <mehmetin264@gmail.com>
2025-10-27 11:06:57 +0300

    feat(stats14): update feature-list

--- editor-components/stats/stats14/stats14.tsx
+++ editor-components/stats/stats14/stats14.tsx
@@ -10,23 +10,18 @@ type featuresItem = {
   topText: React.JSX.Element;
   bottomText: React.JSX.Element;
   title: React.JSX.Element;
-}
-
-type button = {
-  text: React.JSX.Element;
-}
+};
 
 class Stats14 extends BaseStats {
-
   constructor(props?: any) {
     super(props, styles);
 
-this.addProp({
-  type: "string",
-  key: "title",
-  displayer: "Title",
-  value: "Super Simple & Quick Way",
-});
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Super Simple & Quick Way",
+    });
 
     this.addProp({
       type: "array",
@@ -38,62 +33,26 @@ this.addProp({
           key: "features-item",
           displayer: "Feature 1",
           value: [
-            {
-              type: "string",
-              key: "topText",
-              displayer: "Top Text",
-              value:
-                "From 20 days",
-            },
-            {
-              type: "string",
-              key: "bottomText",
-              displayer: "Bottom Text",
-              value:
-                "minimal period",
-            },
+            { type: "string", key: "topText", displayer: "Top Text", value: "From 20 days" },
+            { type: "string", key: "bottomText", displayer: "Bottom Text", value: "minimal period" },
           ],
         },
-                {
+        {
           type: "object",
           key: "features-item",
           displayer: "Feature 2",
           value: [
-            {
-              type: "string",
-              key: "topText",
-              displayer: "Top Text",
-              value:
-                "Up to 7.5%",
-            },
-            {
-              type: "string",
-              key: "bottomText",
-              displayer: "Bottom Text",
-              value:
-                "yearly income",
-            },
+            { type: "string", key: "topText", displayer: "Top Text", value: "Up to 7.5%" },
+            { type: "string", key: "bottomText", displayer: "Bottom Text", value: "yearly income" },
           ],
         },
-                {
+        {
           type: "object",
           key: "features-item",
           displayer: "Feature 3",
           value: [
-            {
-              type: "string",
-              key: "topText",
-              displayer: "Top Text",
-              value:
-                "From $1,000 ",
-            },
-            {
-              type: "string",
-              key: "bottomText",
-              displayer: "Bottom Text",
-              value:
-                "minimal deposit",
-            },
+            { type: "string", key: "topText", displayer: "Top Text", value: "From $1,000" },
+            { type: "string", key: "bottomText", displayer: "Bottom Text", value: "minimal deposit" },
           ],
         },
       ],
@@ -109,25 +68,24 @@ this.addProp({
     this.addProp({
       type: "array",
       key: "buttons",
-      displayer: "Button ",
+      displayer: "Buttons",
       value: [
-        INPUTS.BUTTON("button", "Button", "Download Free Trial","", "FaRegCheckCircle", "", "Primary"),
-        INPUTS.BUTTON("button", "Button", "Learn More", "", "", "", "White")
+        INPUTS.BUTTON("button", "Button", "Download Free Trial", "", "FaRegCheckCircle", "", "Primary"),
+        INPUTS.BUTTON("button", "Button", "Learn More", "", "", "", "White"),
       ],
     });
 
-
-
-      this.addProp({
-        type: "media",
-        key: "image",
-        displayer: "Image",
-        additionalParams: { availableTypes: ["image"] },
-        value: { type: "image", url: "https://impreza-landing.us-themes.com/wp-content/uploads/2023/10/balazs-ketyi-sScmok4Iq1o-unsplash-1070x803.jpg" },
-      });
-
-}
-
+    this.addProp({
+      type: "media",
+      key: "image",
+      displayer: "Image",
+      additionalParams: { availableTypes: ["image"] },
+      value: {
+        type: "image",
+        url: "https://impreza-landing.us-themes.com/wp-content/uploads/2023/10/balazs-ketyi-sScmok4Iq1o-unsplash-1070x803.jpg",
+      },
+    });
+  }
 
   static getName(): string {
     return "Stats 14";
@@ -136,38 +94,40 @@ this.addProp({
   render() {
     const title = this.getPropValue("title");
     const isTitleExist = this.castToString(title);
-
     const FeaturesItem = this.castToObject<featuresItem[]>("features-item");
-    const image = this.getPropValue("image");
-
     const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
-
+    const image = this.getPropValue("image");
     const overlay = this.getPropValue("overlay");
-
     const alignment = Base.getContentAlignment();
-
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
-        
-          <div className={this.decorateCSS("content")}>
+          <div
+            className={`${this.decorateCSS("content")} ${
+              alignment === "left"
+                ? this.decorateCSS("alignment-left")
+                : this.decorateCSS("alignment-center")
+            }`}
+          >
             {(isTitleExist || FeaturesItem.length > 0 || buttons.length > 0) && (
               <Base.VerticalContent className={this.decorateCSS("left-container")}>
-                {isTitleExist && <Base.SectionTitle
+                {isTitleExist && (
+                  <Base.SectionTitle
                     className={`${this.decorateCSS("title")} ${
                       alignment === "center" ? this.decorateCSS("title-center") : ""
                     }`}
                   >
                     {title}
-                  </Base.SectionTitle>}
+                  </Base.SectionTitle>
+                )}
 
                 {FeaturesItem.length > 0 && (
-                    <Base.Row
-                      className={`${this.decorateCSS("features-list")} ${
-                        alignment === "center" ? this.decorateCSS("alignment-center") : ""
-                      }`}
-                    >
+                  <Base.Row
+                    className={`${this.decorateCSS("features-list")} ${
+                      alignment === "center" ? this.decorateCSS("alignment-center") : ""
+                    }`}
+                  >
                     {FeaturesItem.map((item: any, index: number) => (
                       <div key={index} className={this.decorateCSS("features-list-item")}>
                         {this.castToString(item.topText) && (
@@ -185,51 +145,39 @@ this.addProp({
                   </Base.Row>
                 )}
 
+                {buttons.length > 0 && (
+                  <Base.Row
+                    className={`${this.decorateCSS("button-container")} ${
+                      alignment === "center" ? this.decorateCSS("button-center") : ""
+                    }`}
+                  >
+                    {buttons.map((item, index) => {
+                      const buttonText = this.castToString(item.text || "");
+                      const buttonUrl = item.url || "#";
+                      if (!buttonText && !item.icon) return null;
 
-
-            {buttons.length > 0 && (
-              <Base.Row
-                className={`${this.decorateCSS("button-container")} ${
-                  alignment === "center" ? this.decorateCSS("button-center") : ""
-                }`}
-              >
-                {buttons.map((item, index) => {
-                  const buttonText = this.castToString(item.text || "");
-                  const buttonUrl = item.url || "#"; // url boşsa fallback
-
-                  if (!buttonText && !item.icon) return null;
-
-                  return (
-                    <ComposerLink key={`dw-1-btn-${index}`} path={buttonUrl}>
-                      <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
-                        {item.icon && (
-                          <Base.Icon name={item.icon} propsIcon={{ className: this.decorateCSS("icon") }} />
-                        )}
-                        <div className={this.decorateCSS("button-text")}>{item.text}</div>
-                      </Base.Button>
-                    </ComposerLink>
-                  );
-                })}
-              </Base.Row>
+                      return (
+                        <ComposerLink key={`dw-btn-${index}`} path={buttonUrl}>
+                          <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                            {item.icon && (
+                              <Base.Icon name={item.icon} propsIcon={{ className: this.decorateCSS("icon") }} />
+                            )}
+                            <div className={this.decorateCSS("button-text")}>{item.text}</div>
+                          </Base.Button>
+                        </ComposerLink>
+                      );
+                    })}
+                  </Base.Row>
+                )}
+              </Base.VerticalContent>
             )}
 
-
-                
-              </Base.VerticalContent>
-      )}
-
-              {image && (
-                <div className={`${this.decorateCSS("right-container")} `}>
-                  <Base.Media
-                    value={this.getPropValue("image")}
-                    className={this.decorateCSS("image-circle")}
-                  />
-                  {overlay && image?.url && <div className={this.decorateCSS("overlay")}></div>}
-                </div>
-              )}
-
-
-
+            {image && (
+              <div className={this.decorateCSS("right-container")}>
+                <Base.Media value={image} className={this.decorateCSS("image-circle")} />
+                {overlay && image?.url && <div className={this.decorateCSS("overlay")}></div>}
+              </div>
+            )}
           </div>
         </Base.MaxContent>
       </Base.Container>
@@ -238,7 +186,4 @@ this.addProp({
 }
 
 export default Stats14;
-
-
-
-
+ 
