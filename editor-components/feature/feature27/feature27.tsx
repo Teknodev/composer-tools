f43a09011a876f4344b2e05b3ef18129ce636dfb
“mehmetmetin07” <mehmetin264@gmail.com>
2025-11-03 17:12:12 +0300

    feat(feature27): add new stats to components

--- editor-components/feature/feature27/feature27.tsx
+++ editor-components/feature/feature27/feature27.tsx
@@ -1,52 +1,54 @@
-import * as React from "react";
 import { BaseFeature } from "../../EditorComponent";
 import styles from "./feature27.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
-import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 
-
-class FeatureComponent27 extends BaseFeature {
+class Feature27Component extends BaseFeature {
   constructor(props?: any) {
     super(props, styles);
 
     this.addProp({
       type: "string",
-      key: "subtitle",
-      displayer: "Subtitle",
-      value: "Your Business"
-    });
-
-    this.addProp({
-      type: "string",
       key: "title",
       displayer: "Title",
-      value: "#1 SEO Agency to Improve Your Business"
+      value: "#1 SEO Agency to Improve Your Business",
     });
 
     this.addProp({
       type: "string",
       key: "description",
       displayer: "Description",
-      value: "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test."
+      value:
+        "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test.",
     });
 
     this.addProp({
-      type: "image",
-      key: "image",
-      displayer: "Image",
-      value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/placeholder-image?alt=media"
+      type: "boolean",
+      key: "overlay",
+      displayer: "Overlay",
+      value: false,
     });
 
-   
-        this.addProp({
+    this.addProp({
       type: "array",
       key: "buttons",
       displayer: "Buttons",
       value: [
-        INPUTS.BUTTON("button", "Primary Button", "Get Started", "", "FaArrowRight", null, "Primary"),
-        INPUTS.BUTTON("button", "Secondary Button", "Learn More", "", "FaArrowRight", null, "Link")
-      ]
+        INPUTS.BUTTON("button", "Button", "Get Started", null, "", null, "Primary"),
+        INPUTS.BUTTON("button", "Button", "Learn More", null, "FaArrowRight", null, "White"),
+      ],
+    });
+
+    this.addProp({
+      type: "media",
+      key: "image",
+      displayer: "Image",
+      additionalParams: { availableTypes: ["image"] },
+      value: {
+        type: "image",
+        url: "https://storage.googleapis.com/download/storage/v1/b/hq-blinkpage-staging-bbc49/o/6908b7c52d05c1002bf4d35e?alt=media",
+      },
     });
   }
 
@@ -55,69 +57,78 @@ class FeatureComponent27 extends BaseFeature {
   }
 
   render() {
-    const subtitle = this.getPropValue("subtitle");
     const title = this.getPropValue("title");
+    const isTitleExist = this.castToString(title);
     const description = this.getPropValue("description");
+    const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
     const image = this.getPropValue("image");
-    const buttons = this.castToObject<Array<any>>("buttons") || [];
+    const overlay = this.getPropValue("overlay");
+    const alignment = Base.getContentAlignment();
+    const isLeftContainerExist = isTitleExist || this.castToString(description) || buttons.length > 0;
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
-          <div className={this.decorateCSS("wrapper")}>
-            <div className={this.decorateCSS("content")}>
-              {subtitle && (
-                <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>
-                  {subtitle}
-                </Base.SectionSubTitle>
-              )}
+          <div
+            className={`${this.decorateCSS("content")} ${
+              alignment === "left"
+                ? this.decorateCSS("alignment-left")
+                : this.decorateCSS("alignment-center")
+            }`}
+          >
+            {isLeftContainerExist && (
+              <Base.VerticalContent className={this.decorateCSS("left-container")}>
+                {isTitleExist && (
+                  <Base.SectionTitle
+                    className={`${this.decorateCSS("title")} ${
+                      alignment === "center" ? this.decorateCSS("title-center") : ""
+                    }`}
+                  >
+                    {title}
+                  </Base.SectionTitle>
+                )}
 
-              {title && (
-                <Base.SectionTitle className={this.decorateCSS("title")}>{title}</Base.SectionTitle>
-              )}
+                {this.castToString(description) && (
+                  <Base.P className={this.decorateCSS("description")}>{description}</Base.P>
+                )}
 
-              {description && (
-                <Base.SectionDescription className={this.decorateCSS("description")}>
-                  {description}
-                </Base.SectionDescription>
-              )}
+                {buttons.length > 0 && (
+                  <Base.Row className={this.decorateCSS("button-container")}>
+                    {buttons.map((item, index) => {
+                      const buttonText = this.castToString(item.text || "");
+                      const buttonUrl = item.url || "#";
+                      const iconName = (item as any)?.icon?.name || (item as any)?.icon;
+                      if (!buttonText && !iconName) return null;
 
-              {buttons.length > 0 && (
-                <div className={this.decorateCSS("buttons")}>
-                  {buttons.map((button: INPUTS.CastedButton, index: number) => {
-                    const buttonTextExists = this.castToString(button.text);
-                    const iconExists = button.icon;
-
-                    return (buttonTextExists || iconExists) && (
-                      <div key={index} className={this.decorateCSS("button-item")}>
-                        <ComposerLink path={button.url || '#'}>
-                          <Base.Button 
-                            buttonType={button.type}
-                            className={this.decorateCSS("button")}
-                          >
-                            {buttonTextExists && (
-                              <Base.P className={this.decorateCSS("button-text")}>
-                                {button.text}
-                              </Base.P>
-                            )}
-                            {iconExists && (
-                              <Base.Media 
-                                value={{ type: "icon", name: iconExists }}
-                                className={this.decorateCSS("btn-icon")}
+                      return (
+                        <ComposerLink key={`dw-btn-${index}`} path={buttonUrl}>
+                          <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                            {iconName && (
+                              <Base.Media
+                                className={this.decorateCSS("button-icon")}
+                                value={{ type: "icon", name: iconName }}
                               />
                             )}
+                            <Base.P className={this.decorateCSS("button-text")}>{item.text}</Base.P>
                           </Base.Button>
                         </ComposerLink>
-                      </div>
-                    );
-                  })}
-                </div>
-              )}
-            </div>
+                      );
+                    })}
+                  </Base.Row>
+                )}
+              </Base.VerticalContent>
+            )}
 
-            <div className={this.decorateCSS("visual")}>
-              <Base.Media value={{ type: "image", url: image }} className={this.decorateCSS("visual-image")} />
-            </div>
+            {image && (
+              <div 
+                className={`${this.decorateCSS("right-container")} ${
+                  !isLeftContainerExist ? this.decorateCSS("right-container-alone") : ""
+                }`}
+              >
+                <Base.Media value={image} className={this.decorateCSS("image-circle")} />
+                {overlay && image?.url && <div className={this.decorateCSS("overlay")}></div>}
+              </div>
+            )}
           </div>
         </Base.MaxContent>
       </Base.Container>
@@ -125,4 +136,7 @@ class FeatureComponent27 extends BaseFeature {
   }
 }
 
-export default FeatureComponent27;
+export default Feature27Component;
+ 
+
+
