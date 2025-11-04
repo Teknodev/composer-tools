6f3ee7fe34247b4858880b42dee60a7a8f30e08e
bitsinan <sinann0089@gmail.com>
2025-11-04 10:15:24 +0300

    feat(Stats13): -add new component -added component titles -add star rating system

--- editor-components/stats/stats13/stats13.tsx
+++ editor-components/stats/stats13/stats13.tsx
@@ -1,26 +1,24 @@
 import * as React from "react";
 import { BaseStats } from "../../EditorComponent";
 import styles from "./stats13.module.scss";
-import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
-
 import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
-class Stats13Page extends BaseStats {
+class Stats13 extends BaseStats {
     constructor(props?: any) {
         super(props, styles);
+
         this.addProp({
-            type: "image",
-            key: "backgroundImage",
-            displayer: "Background Image",
-            value: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6773f9390655f8002caf5eca?alt=media",
-        });
-        this.addProp({
-            type: "string",
-            key: "title",
-            displayer: "Title",
-            value: "Passionate Minds , Compassionate Hearts",
-        });
+            type: "media",
+            key: "image",
+            displayer: "Image",
+            value: {
+                url: "https://impreza-landing.us-themes.com/wp-content/uploads/2023/10/avel-chuklanov-DUmFLtMeAbQ-unsplash-1070x713.jpg",
+                type: "image",
+            }
+        })
+
         this.addProp({
             type: "array",
             key: "buttons",
@@ -28,50 +26,182 @@ class Stats13Page extends BaseStats {
             additionalParams: {
                 maxElementCount: 2,
             },
+            value: [INPUTS.BUTTON("button", "Button", "READ MORE", "", null, null, "Primary")],
+        });
+
+        this.addProp({
+            type: "array",
+            key: "title",
+            displayer: "Title",
             value: [
-                INPUTS.BUTTON("button", "Button", "Start Building", "", null, null, "Primary"),
-                INPUTS.BUTTON("button", "Button", "Test Drive FREE", "", null, null, "Secondary")
+                { type: "string", key: "line1", displayer: "Line 1", value: "Best-Rated" },
+                { type: "string", key: "line2", displayer: "Line 2", value: "Pormmerce" },
+                { type: "string", key: "line3", displayer: "Line 3", value: "Solution" },
+            ],
+        });
+
+        this.addProp({
+            type: "array",
+            key: "rating",
+            displayer: "Rating",
+            value: [
+                {
+                    type: "object",
+                    key: "star1",
+                    displayer: "star1",
+                    value: [
+                        {
+                            type: "icon",
+                            key: "icon",
+                            displayer: "icon",
+                            value: "FaStar",
+                        },
+                    ],
+                },
+                {
+                    type: "object",
+                    key: "star2",
+                    displayer: "star2",
+                    value: [
+                        {
+                            type: "icon",
+                            key: "icon",
+                            displayer: "icon",
+                            value: "FaStar",
+                        },
+                    ],
+                },
+                {
+                    type: "object",
+                    key: "star3",
+                    displayer: "star3",
+                    value: [
+                        {
+                            type: "icon",
+                            key: "icon",
+                            displayer: "icon",
+                            value: "FaStar",
+                        },
+                    ],
+                },
+                {
+                    type: "object",
+                    key: "star4",
+                    displayer: "star4",
+                    value: [
+                        {
+                            type: "icon",
+                            key: "icon",
+                            displayer: "icon",
+                            value: "FaStar",
+                        },
+                    ],
+                },
+                {
+                    type: "object",
+                    key: "star4",
+                    displayer: "star4",
+                    value: [
+                        {
+                            type: "icon",
+                            key: "icon",
+                            displayer: "icon",
+                            value: "FaStar",
+                        },
+                    ],
+                },
+            ],
+        });
+        this.addProp({
+            type: "array",
+            key: "contact-items",
+            displayer: "Contact Items",
+            value: [
+                {
+                    type: "object",
+                    key: "contact",
+                    displayer: "Contact",
+                    value: [
+                        {
+                            type: "string",
+                            key: "title",
+                            value: "Email:",
+                            displayer: "Text",
+                        }
+                    ],
+                },
+                {
+                    type: "object",
+                    key: "contact",
+                    displayer: "Contact",
+                    value: [
+                        {
+                            type: "string",
+                            key: "title",
+                            value: "Phone:",
+                            displayer: "Text",
+                        }
+                    ],
+                },
             ],
         });
     }
+
+
     static getName(): string {
         return "Stats 13";
     }
+
     render() {
-        const title = this.castToString(this.getPropValue("title"));
+        const image = this.getPropValue("image");
+        const title = this.getPropValue("title");
+        const rating = this.getPropValue("rating");
         const buttons = this.getPropValue("buttons");
-        const image = this.getPropValue("backgroundImage");
 
         return (
             <Base.Container className={this.decorateCSS("container")}>
-                <Base.MaxContent className={this.decorateCSS("max-content")}>
-                    <Base.ContainerGrid className={this.decorateCSS("page")}>
-                        {(title || buttons.length > 0) && (
-                            <Base.VerticalContent className={`${this.decorateCSS("left-page")} ${!image && this.decorateCSS("left-page-without-image")}`}>
-                                {title && <Base.SectionTitle className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.SectionTitle>}
-                                {buttons.length > 0 && (
-                                    <div className={this.decorateCSS("button-container")}>
-                                        {this.castToObject<any>("buttons").map((item: any, index: number) => {
-                                            const buttonTextExist = this.castToString(item.text);
-                                            return (
-                                                buttonTextExist && (
-                                                    <ComposerLink key={`stats-${index}`} path={item.url}>
-                                                        <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
-                                                            {item.text}
-                                                        </Base.Button>
-                                                    </ComposerLink>
-                                                )
-                                            );
-                                        })}
-                                    </div>
-                                )}
-                            </Base.VerticalContent>
-                        )}
-                        {image && (
-                            <Base.VerticalContent className={`${this.decorateCSS("right-container")} ${!image && this.decorateCSS("right-container-without-image")}`}>
-                                <img src={this.getPropValue("backgroundImage")} alt="" className={this.decorateCSS("image")} />
-                            </Base.VerticalContent>
-                        )}
+                <Base.MaxContent className={this.decorateCSS("content")}>
+                    <Base.ContainerGrid>
+                        <Base.GridCell className={this.decorateCSS("left-content")}>
+                            {rating.length > 0 && (
+                                <div className={this.decorateCSS("rating-container")}>
+                                    {this.castToObject<any>("rating").map((item: any, index: number) => {
+                                        return (
+                                            <div className={this.decorateCSS("rating-content")}>
+                                                <Base.Icon name={item.icon} propsIcon={{ className: this.decorateCSS("icon") }} />
+                                            </div>
+                                        );
+                                    })}
+                                    <Base.H3 className={this.decorateCSS("rating-number")}>4.89</Base.H3>
+                                </div>
+                            )}
+                            {Array.isArray(title) && title.length > 0 && (
+                                title.map((item: any, index: number) => (
+                                    <Base.SectionTitle key={index} className={this.decorateCSS("title")}>{item.value}</Base.SectionTitle>
+                                ))
+                            )}
+                            {buttons.length > 0 && (
+                                <div className={this.decorateCSS("button-container")}>
+                                    {this.castToObject<any>("buttons").map((item: any, index: number) => {
+                                        const buttonTextExist = this.castToString(item.text);
+                                        return (
+                                            buttonTextExist && (
+                                                <ComposerLink key={`stats-${index}`} path={item.url}>
+                                                    <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                                                        {item.text}
+                                                    </Base.Button>
+                                                </ComposerLink>
+                                            )
+                                        );
+                                    })}
+                                </div>
+                            )}
+                        </Base.GridCell>
+                        <Base.GridCell className={this.decorateCSS("right-content")}>
+                            {image?.url && (
+                                <img src={image.url} className={this.decorateCSS("image")} />
+                            )}
+                        </Base.GridCell>
                     </Base.ContainerGrid>
                 </Base.MaxContent>
             </Base.Container>
@@ -79,4 +209,4 @@ class Stats13Page extends BaseStats {
     }
 }
 
-export default Stats13Page;
+export default Stats13;
