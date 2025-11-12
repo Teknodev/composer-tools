b8b47c6f643f84e26ca2d3eaae5d09018f6e7511
KardelenArslan <36626613+KardelennArslan@users.noreply.github.com>
2025-11-12 17:18:55 +0300

    feat:introsection1 (#987)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -103,6 +103,7 @@ import HeroSection32 from "./hero-section/hero-section32/hero-section32";
 import HeroSection33 from "./hero-section/hero-section33/hero-section33";
 import HeroSection34 from "./hero-section/hero-section34/hero-section34";
 import HeroSection35 from "./hero-section/hero-section35/hero-section35";
+import IntroSection1 from "./intro-section/intro-section1/intro-section1";
 import IntroSection2 from "./intro-section/intro-section2/intro-section2";
 import FeatureComponent1 from "./feature/feature1/feature1";
 import FeatureComponent2 from "./feature/feature2/feature2";
@@ -399,6 +400,7 @@ function Registerables(composer: ComponentsRegistery) {
     HeroSection33,
     HeroSection34,
     HeroSection35,
+    IntroSection1,
     IntroSection2,
     NavbarComponent1,
     NavbarComponent2,
