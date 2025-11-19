b789417d6dee774e024a094bcef0281acfdb43ee
bitsinan <sinann0089@gmail.com>
2025-11-19 11:14:04 +0300

    feat(intro-section 4): -Added necessary button groups and text fields for IntroSection4.
    -Organized button layout and configured alignment settings.
    
    -Added background image and overlay features.

--- editor-components/intro-section/intro-section4/intro-section4.tsx
+++ editor-components/intro-section/intro-section4/intro-section4.tsx
@@ -1,11 +1,75 @@
 import * as React from "react";
 import { BaseIntroSection } from "../../EditorComponent";
 import styles from "./intro-section4.module.scss";
-import { Base } from "composer-tools/composer-base-components/base/base";
+import { Base } from "../../../composer-base-components/base/base";
+import { INPUTS } from "composer-tools/custom-hooks/input-templates";
+import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 class IntroSection4 extends BaseIntroSection {
     constructor(props?: any) {
         super(props, styles);
+
+        this.addProp({
+            type: "media",
+            key: "cover-image",
+            displayer: "Image",
+            additionalParams: {
+                availableTypes: ["image"],
+            },
+            value: {
+                type: "image",
+                url: "https://res.cloudinary.com/dmydg7kum/image/upload/v1763494879/thought-catalog-Nv-vx3kUR2A-unsplash_hlncis.jpg",
+            },
+        });
+
+        this.addProp({
+            type: "boolean",
+            key: "overlay",
+            displayer: "Overlay",
+            value: true,
+        });
+
+        this.addProp({
+            type: "string",
+            key: "subtitle",
+            displayer: "Subtitle",
+            value: "",
+        });
+
+        this.addProp({
+            type: "string",
+            key: "title",
+            displayer: "Title",
+            value: "Online University of In-Demand Professions",
+        });
+
+        this.addProp({
+            type: "string",
+            key: "description",
+            displayer: "Description",
+            value:
+                "Objectively innovate empowered manufactured products whereas parallel platforms.",
+        });
+
+        this.addProp({
+            type: "string",
+            key: "label",
+            displayer: "Label",
+            value:
+                "OUR COURSES",
+        });
+
+        this.addProp({
+            type: "array",
+            key: "buttons",
+            displayer: "Buttons",
+            value: [
+                INPUTS.BUTTON("button", "Button", "Business", "", null, null, "Primary"),
+                INPUTS.BUTTON("button", "Button", "Programming", "", null, null, "Primary"),
+                INPUTS.BUTTON("button", "Button", "Marketing", "", null, null, "Primary"),
+                INPUTS.BUTTON("button", "Button", "Design", "", null, null, "Primary"),
+            ],
+        });
     }
 
     static getName(): string {
@@ -13,8 +77,67 @@ class IntroSection4 extends BaseIntroSection {
     }
 
     render() {
+        const coverImage = this.getPropValue("cover-image");
+        const buttonItem = this.castToObject<INPUTS.CastedButton[]>("buttons");
+        const subtitle = this.castToString(this.getPropValue("subtitle")) || "";
+        const title = this.castToString(this.getPropValue("title")) || "";
+        const description = this.castToString(this.getPropValue("description")) || "";
+        const label = this.castToString(this.getPropValue("label")) || "";
+
         return (
-            <Base.Container className={this.decorateCSS("container")}>
+            <Base.Container
+                className={`${this.decorateCSS("container")} ${coverImage ? this.decorateCSS("has-bg-image") : ""
+                    } ${coverImage && this.getPropValue("overlay")
+                        ? this.decorateCSS("overlay")
+                        : ""
+                    }`}
+                style={{
+                    backgroundImage: coverImage ? `url(${coverImage.url})` : undefined,
+                }}
+            >
+                <Base.MaxContent className={this.decorateCSS("content")}>
+                    <Base.VerticalContent className={this.decorateCSS("vertical-content")}>
+                        {subtitle && (
+                            <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>
+                                {this.getPropValue("subtitle")}
+                            </Base.SectionSubTitle>
+                        )}
+                        {title && (
+                            <Base.SectionTitle className={this.decorateCSS("title")}>
+                                {this.getPropValue("title")}
+                            </Base.SectionTitle>
+                        )}
+                        {description && (
+                            <Base.SectionDescription
+                                className={this.decorateCSS("description")}
+                            >
+                                {this.getPropValue("description")}
+                            </Base.SectionDescription>
+                        )}
+                        {label && (
+                            <Base.P className={this.decorateCSS("label")}>
+                                {this.getPropValue("label")}
+                            </Base.P>
+                        )}
+                        {buttonItem.length > 0 && (
+                            <Base.Row className={this.decorateCSS("button-container")}>
+                                {buttonItem.map(
+                                    (buttonObj, index: number) => {
+                                        const buttonText = this.castToString(buttonObj.text);
+                                        return buttonText && (
+                                            <ComposerLink key={index} path={buttonObj.url}>
+                                                <Base.Button buttonType={buttonObj.type} className={this.decorateCSS("button")}>
+                                                    <Base.P className={this.decorateCSS("button-text")}>{buttonObj.text}</Base.P>
+                                                </Base.Button>
+                                            </ComposerLink>
+                                        );
+                                    }
+                                )}
+                            </Base.Row>
+                        )}
+
+                    </Base.VerticalContent>
+                </Base.MaxContent>
             </Base.Container>
         )
     }
