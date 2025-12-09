5045222528d19bb9c3a547d566272a58f81cdca7
bitsinan <sinann0089@gmail.com>
2025-12-10 00:14:30 +0300

    feat: create feature 32 component

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -133,6 +133,7 @@ import FeatureComponent25 from "./feature/feature25/feature25";
 import FeatureComponent26 from "./feature/feature26/feature26";
 import FeatureComponent27 from "./feature/feature27/feature27";
 import FeatureComponent30 from "./feature/feature30/feature30";
+import FeatureComponent32 from "./feature/feature32/feature32";
 import CallToAction1 from "./call_to_action/call_to_action1/call_to_action1";
 import CallToAction2 from "./call_to_action/call_to_action2/call_to_action2";
 import CallToAction3 from "./call_to_action/call_to_action3/call_to_action3";
@@ -456,6 +457,7 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent26,
     FeatureComponent27,
     FeatureComponent30,
+    FeatureComponent32,
     CallToAction1,
     CallToAction2,
     CallToAction3,
