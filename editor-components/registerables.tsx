b6284055886d1ab4a2e482d9dc879ae814ed1dc4
Testo <testotaylan@gmail.com>
2025-12-15 16:55:56 +0300

    fix: fixed registerables

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -111,7 +111,6 @@ import IntroSection4 from "./intro-section/intro-section4/intro-section4";
 import IntroSection6 from "./intro-section/intro-section6/intro-section6";
 import IntroSection7 from "./intro-section/intro-section7/intro-section7";
 import IntroSection9 from "./intro-section/intro-section9/intro-section9";
-import IntroSection11 from "./intro-section/intro-section11/intro-section11";
 import FeatureComponent1 from "./feature/feature1/feature1";
 import FeatureComponent2 from "./feature/feature2/feature2";
 import FeatureComponent3 from "./feature/feature3/feature3";
