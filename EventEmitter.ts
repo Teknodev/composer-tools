165172da2ea27e19e57c226660686440abe58726
setanihas <setanihas@gmail.com>
2025-12-19 14:02:49 +0300

    feat: add DETAILED_THEME_PANEL_TOGGLED event to EVENTS enum

--- EventEmitter.ts
+++ EventEmitter.ts
@@ -12,7 +12,8 @@ export enum EVENTS {
   INSERT_FORM = "insertForm",
   RENDER_CONTENT_TAB = "renderContentTab",
   COMPONENT_DID_UPDATE = "componentDidUpdate",
-  CSS_CLASSES_CHANGED = "cssClassesChanged"
+  CSS_CLASSES_CHANGED = "cssClassesChanged",
+  DETAILED_THEME_PANEL_TOGGLED = "detailedThemePanelToggled",
 }
 
 class EventQ {
