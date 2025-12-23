ee070696f99104877154917994d4fed4575417cf
Testo <testotaylan@gmail.com>
2025-12-23 11:51:36 +0300

    fix: fixed button type and design tree

--- editor-components/intro-section/intro-section8/intro-section8.tsx
+++ editor-components/intro-section/intro-section8/intro-section8.tsx
@@ -78,7 +78,7 @@ class IntroSection8 extends BaseIntroSection {
       type: "array",
       key: "buttons",
       displayer: "Buttons",
-      value: [INPUTS.BUTTON("button", "Button", "", "", null, null, "Primary")],
+      value: [INPUTS.BUTTON("button", "Button", "", "", null, null, "White")],
     });
   }
 
