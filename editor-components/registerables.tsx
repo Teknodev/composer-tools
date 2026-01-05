f4667a4060768a691bc9bcda3015969f1afb00dd
fabi3c <arashamsi6@gmail.com>
2026-01-05 17:18:57 +0300

    feat: added feature28 component (#999)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -138,6 +138,7 @@ import FeatureComponent22 from "./feature/feature22/feature22";
 import FeatureComponent25 from "./feature/feature25/feature25";
 import FeatureComponent26 from "./feature/feature26/feature26";
 import FeatureComponent27 from "./feature/feature27/feature27";
+import FeatureComponent28 from "./feature/feature28/feature28";
 import FeatureComponent29 from "./feature/feature29/feature29";
 import FeatureComponent30 from "./feature/feature30/feature30";
 import FeatureComponent31 from "./feature/feature31/feature31";
@@ -485,6 +486,7 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent25,
     FeatureComponent26,
     FeatureComponent27,
+    FeatureComponent28,
     FeatureComponent29,
     FeatureComponent30,
     FeatureComponent31,
