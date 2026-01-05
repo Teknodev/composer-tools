2e544af9872a1b78c1a245540f7deab29e257a4e
Sinan <sinann0089@gmail.com>
2026-01-05 14:12:08 +0300

    fix: Fix PR change

--- editor-components/intro-section/intro-section3/intro-section3.tsx
+++ editor-components/intro-section/intro-section3/intro-section3.tsx
@@ -1,19 +1,13 @@
 import * as React from "react";
-import { BaseIntroSection, TypeUsableComponentProps, TypeMediaInputValue } from "../../EditorComponent";
+import { BaseIntroSection } from "../../EditorComponent";
 import styles from "./intro-section3.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
 class IntroSection3 extends BaseIntroSection {
-  constructor(props?: TypeUsableComponentProps) {
+  constructor(props?: any) {
     super(props, styles);
-
-    this.addProp({ type: "string", key: "subtitle", displayer: "Subtitle", value: "" });
-    this.addProp({ type: "string", key: "title", displayer: "Title", value: "Everything You Need to Run Your Websites" });
-    this.addProp({ type: "string", key: "description", displayer: "Description", value: "" });
-    this.addProp({ type: "string", key: "subtitle1", displayer: "Text 1", value: "First 30 days for free." });
-    this.addProp({ type: "string", key: "subtitle2", displayer: "Text 2", value: "No credit card required." });
 
     this.addProp({
       type: "media",
@@ -26,12 +20,53 @@ class IntroSection3 extends BaseIntroSection {
       },
     });
 
-    this.addProp({ type: "boolean", key: "overlay", displayer: "Overlay", value: false });
+    this.addProp({
+      type: "boolean",
+      key: "overlay",
+      displayer: "Overlay",
+      value: false
+    });
+
+    this.addProp({
+      type: "string",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: ""
+    });
+
+    this.addProp({
+      type: "string",
+      key: "title",
+      displayer: "Title",
+      value: "Everything You Need to Run Your Websites"
+    });
+
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: ""
+    });
+
     this.addProp({
       type: "array",
       key: "buttons",
       displayer: "Buttons",
-      value: [INPUTS.BUTTON("button", "Button 1", "Start Now", "", "", null, "Primary")],
+      value: [INPUTS.BUTTON("button", "Button 1", "Start Now", "", null, null, "Primary")],
+    });
+
+    this.addProp({
+      type: "string",
+      key: "label1",
+      displayer: "Label 1",
+      value: "First 30 days for free."
+    });
+
+    this.addProp({
+      type: "string",
+      key: "label2",
+      displayer: "Label 2",
+      value: "No credit card required."
     });
   }
 
@@ -40,68 +75,64 @@ class IntroSection3 extends BaseIntroSection {
   }
 
   render() {
-    const subtitle = this.getPropValue("subtitle");
-    const title = this.getPropValue("title");
-    const description = this.getPropValue("description");
-    const subtitle1 = this.getPropValue("subtitle1");
-    const subtitle2 = this.getPropValue("subtitle2");
-    const image = this.getPropValue("image") as TypeMediaInputValue;
-    const overlay = this.getPropValue("overlay");
+    const subtitle = this.castToString(this.getPropValue("subtitle"));
+    const title = this.castToString(this.getPropValue("title"));
+    const description = this.castToString(this.getPropValue("description"));
+    const image = this.getPropValue("image");
+    const isImageExist = !!image?.url;
+    const enableOverlay = this.getPropValue("overlay");
     const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
-
-    const subtitleExist = this.castToString(subtitle);
-    const titleExist = this.castToString(title);
-    const descriptionExist = this.castToString(description);
-    const subtitle1Exist = this.castToString(subtitle1);
-    const subtitle2Exist = this.castToString(subtitle2);
-    const hasSubtitleGroup = subtitle1Exist || subtitle2Exist;
-    const hasImage = image?.url;
-    const isCenter = Base.getContentAlignment() === "center";
+    const label1 = this.castToString(this.getPropValue("label1"));
+    const label2 = this.castToString(this.getPropValue("label2"));
+    const hasTitleGroup = label1 || label2;
+    const hasContent = subtitle || title || description || buttons.length > 0 || hasTitleGroup;
 
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
-          <Base.VerticalContent className={this.decorateCSS("content")}>
-            {subtitleExist && <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>{subtitle}</Base.SectionSubTitle>}
-            {titleExist && <Base.SectionTitle className={this.decorateCSS("title")}>{title}</Base.SectionTitle>}
-            {descriptionExist && <Base.SectionDescription className={this.decorateCSS("description")}>{description}</Base.SectionDescription>}
-
-            {(buttons.length > 0 || hasSubtitleGroup) && (
-              <div className={`${this.decorateCSS("button-group-container")}${isCenter ? ` ${this.decorateCSS("center")}` : ""}`}>
-                {buttons.length > 0 && (
-                  <div className={this.decorateCSS("buttons-wrapper")}>
-                    {buttons.map((btn, i) => {
-                      const text = this.castToString(btn.text);
-                      const iconName = btn.icon?.name;
-                      if (!text && !iconName) return null;
-
-                      return (
-                        <ComposerLink key={i} path={btn.url}>
-                          <Base.Button buttonType={btn.type} className={this.decorateCSS("button")}>
-                            {iconName && <Base.Media value={{ type: "icon", name: iconName }} className={this.decorateCSS("button-icon")} />}
-                            {text && <Base.P className={this.decorateCSS("button-text")}>{text}</Base.P>}
-                          </Base.Button>
-                        </ComposerLink>
-                      );
-                    })}
+          <div className={this.decorateCSS("wrapper")}>
+            {hasContent && (
+              <Base.VerticalContent className={this.decorateCSS("vertical-content")}>
+                {subtitle && <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>{this.getPropValue("subtitle")}</Base.SectionSubTitle>}
+                {title && <Base.SectionTitle className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.SectionTitle>}
+                {description && <Base.SectionDescription className={this.decorateCSS("description")}>{this.getPropValue("description")}</Base.SectionDescription>}
+                {(buttons.length > 0 || hasTitleGroup) && (
+                  <div className={this.decorateCSS("button-group-container")}>
+                    {buttons.length > 0 && (
+                      <Base.Row className={this.decorateCSS("button-container")}>
+                        {buttons.map((item, index) => {
+                          const buttonText = this.castToString(item.text || "");
+                          const buttonUrl = item.url || "#";
+                          if (!buttonText) return null;
+                          return (
+                            <ComposerLink key={`dw-btn-${index}`} path={buttonUrl}>
+                              <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                                {buttonText && <Base.P className={this.decorateCSS("button-text")}>{item.text}</Base.P>}
+                              </Base.Button>
+                            </ComposerLink>
+                          );
+                        })}
+                      </Base.Row>
+                    )}
+                    {hasTitleGroup && (
+                      <div className={this.decorateCSS("label-group")}>
+                        {label1 && <Base.P className={this.decorateCSS("label1")}>{this.getPropValue("label1")}</Base.P>}
+                        {label2 && <Base.P className={this.decorateCSS("label2")}>{this.getPropValue("label2")}</Base.P>}
+                      </div>
+                    )}
                   </div>
                 )}
-
-                {hasSubtitleGroup && (
-                  <div className={this.decorateCSS("subtitle-group")}>
-                    {subtitle1Exist && <Base.P className={this.decorateCSS("subtitle1")}>{subtitle1}</Base.P>}
-                    {subtitle2Exist && <Base.P className={this.decorateCSS("subtitle2")}>{subtitle2}</Base.P>}
-                  </div>
-                )}
+              </Base.VerticalContent>
+            )}
+            {isImageExist && (
+              <div className={this.decorateCSS("image-content")}>
+                <div className={this.decorateCSS("image-wrapper")}>
+                  <Base.Media value={image} className={this.decorateCSS("image")} />
+                  {enableOverlay && <div className={this.decorateCSS("overlay")}></div>}
+                </div>
               </div>
             )}
-
-            {hasImage && (
-              <div className={`${this.decorateCSS("image-wrapper")}${overlay ? ` ${this.decorateCSS("overlay")}` : ""}`}>
-                <Base.Media value={image} className={this.decorateCSS("image-media")} />
-              </div>
-            )}
-          </Base.VerticalContent>
+          </div>
         </Base.MaxContent>
       </Base.Container>
     );
