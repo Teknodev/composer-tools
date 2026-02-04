8c8928d58295a2a2514c20eb311614ed76fa8cbc
Osman Ali Elma <osmanalielma4616@gmail.com>
2026-02-04 16:27:52 +0300

    feat: create new component Feature 49

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -154,6 +154,7 @@ import FeatureComponent38 from "./feature/feature38/feature38";
 import FeatureComponent39 from "./feature/feature39/feature39";
 import FeatureComponent40 from "./feature/feature40/feature40";
 import FeatureComponent41 from "./feature/feature41/feature41";
+import FeatureComponent49 from "./feature/feature49/feature49";
 import CallToAction1 from "./call_to_action/call_to_action1/call_to_action1";
 import CallToAction2 from "./call_to_action/call_to_action2/call_to_action2";
 import CallToAction3 from "./call_to_action/call_to_action3/call_to_action3";
@@ -509,6 +510,7 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent39,
     FeatureComponent40,
     FeatureComponent41,
+    FeatureComponent49,
     CallToAction1,
     CallToAction2,
     CallToAction3,
