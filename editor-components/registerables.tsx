3c31e12703845156f9fba1efcd6e400b40828b9f
nabil-ahmad-925 <nabilahmadchohan@gmail.com>
2026-02-09 23:25:10 +0500

    Remove border-bottom-color from FeatureComponent50's card button style and reorder its registration.

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -155,8 +155,8 @@ import FeatureComponent39 from "./feature/feature39/feature39";
 import FeatureComponent40 from "./feature/feature40/feature40";
 import FeatureComponent41 from "./feature/feature41/feature41";
 import FeatureComponent47 from "./feature/feature47/feature47";
-import FeatureComponent50 from "./feature/feature50/feature50";
 import FeatureComponent48 from "./feature/feature48/feature48";
+import FeatureComponent50 from "./feature/feature50/feature50";
 import CallToAction1 from "./call_to_action/call_to_action1/call_to_action1";
 import CallToAction2 from "./call_to_action/call_to_action2/call_to_action2";
 import CallToAction3 from "./call_to_action/call_to_action3/call_to_action3";
@@ -515,8 +515,8 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent40,
     FeatureComponent41,
     FeatureComponent47,
-    FeatureComponent50,
     FeatureComponent48,
+    FeatureComponent50,
     CallToAction1,
     CallToAction2,
     CallToAction3,
