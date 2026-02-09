5521102300031366a4c65059ac5ee82a37066ce0
Sinan <sinann0089@gmail.com>
2026-02-09 14:25:27 +0300

    feat: create new component CTO 16

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -172,6 +172,7 @@ import CallToAction11 from "./call_to_action/call_to_action11/call_to_action11";
 import CallToAction12 from "./call_to_action/call_to_action12/call_to_action12";
 import CallToAction13 from "./call_to_action/call_to_action13/call_to_action13";
 import CallToAction14 from "./call_to_action/call_to_action14/call_to_action14";
+import CallToAction16 from "./call_to_action/call_to_action16/call_to_action16";
 import LogoComponent1 from "./logo-clouds/logo-comp1/logo-comp1";
 import LogoComponent2 from "./logo-clouds/logo-comp2/logo-comp2";
 import LogoComponent3 from "./logo-clouds/logo-comp3/logo-comp3";
@@ -534,6 +535,7 @@ function Registerables(composer: ComponentsRegistery) {
     CallToAction12,
     CallToAction13,
     CallToAction14,
+    CallToAction16,
     LogoComponent1,
     LogoComponent2,
     LogoComponent3,
