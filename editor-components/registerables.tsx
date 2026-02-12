f72a9266c7798c24a314a5b72760bd730e7a0a0f
Yusuf Kıtlık <71187794+yusufktlk@users.noreply.github.com>
2026-02-12 16:54:38 +0300

    fix: remove duplicate import (#1101)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -156,10 +156,9 @@ import FeatureComponent38 from "./feature/feature38/feature38";
 import FeatureComponent39 from "./feature/feature39/feature39";
 import FeatureComponent40 from "./feature/feature40/feature40";
 import FeatureComponent41 from "./feature/feature41/feature41";
-import FeatureComponent45 from "./feature/feature45/feature45";
 import FeatureComponent43 from "./feature/feature43/feature43";
-import FeatureComponent46 from "./feature/feature46/feature46";
 import FeatureComponent45 from "./feature/feature45/feature45";
+import FeatureComponent46 from "./feature/feature46/feature46";
 import FeatureComponent47 from "./feature/feature47/feature47";
 import FeatureComponent48 from "./feature/feature48/feature48";
 import FeatureComponent49 from "./feature/feature49/feature49";
@@ -524,10 +523,9 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent39,
     FeatureComponent40,
     FeatureComponent41,
-    FeatureComponent45,
     FeatureComponent43,
-    FeatureComponent46,
     FeatureComponent45,
+    FeatureComponent46,
     FeatureComponent47,
     FeatureComponent48, 
     FeatureComponent49,
