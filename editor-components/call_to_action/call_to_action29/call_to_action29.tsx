f0a75ef1e911a26161d4b6dd073ae44503120bcb
tetwy <musagdg@hotmail.com>
2026-02-16 10:02:12 +0300

    fix: update CTA29 defaults and refine alignment styles

--- editor-components/call_to_action/call_to_action29/call_to_action29.tsx
+++ editor-components/call_to_action/call_to_action29/call_to_action29.tsx
@@ -20,7 +20,7 @@ class CallToAction29 extends BaseCallToAction {
             type: "string",
             key: "subtitle",
             displayer: "Subtitle",
-            value: "lorem",
+            value: "",
         });
 
         this.addProp({
