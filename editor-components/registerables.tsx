24917bb771c1f42c4394769271a8996f83ae3196
tetwy <83135729+tetwy@users.noreply.github.com>
2026-02-17 14:01:12 +0300

    feat: create Call to Action 24 component (#1103)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -184,6 +184,7 @@ import CallToAction18 from "./call_to_action/call_to_action18/call_to_action18";
 import CallToAction20 from "./call_to_action/call_to_action20/call_to_action20";
 import CallToAction21 from "./call_to_action/call_to_action21/call_to_action21";
 import CallToAction22 from "./call_to_action/call_to_action22/call_to_action22";
+import CallToAction24 from "./call_to_action/call_to_action24/call_to_action24";
 import LogoComponent1 from "./logo-clouds/logo-comp1/logo-comp1";
 import LogoComponent2 from "./logo-clouds/logo-comp2/logo-comp2";
 import LogoComponent3 from "./logo-clouds/logo-comp3/logo-comp3";
@@ -535,7 +536,7 @@ function Registerables(composer: ComponentsRegistery) {
     FeatureComponent45,
     FeatureComponent46,
     FeatureComponent47,
-    FeatureComponent48, 
+    FeatureComponent48,
     FeatureComponent49,
     CallToAction1,
     CallToAction2,
@@ -558,6 +559,7 @@ function Registerables(composer: ComponentsRegistery) {
     CallToAction20,
     CallToAction21,
     CallToAction22,
+    CallToAction24,
     LogoComponent1,
     LogoComponent2,
     LogoComponent3,
