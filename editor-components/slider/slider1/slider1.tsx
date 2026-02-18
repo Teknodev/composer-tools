f0164ba092daa7a55ac4e5a9b8b8ce493f89df8b
Sinan <sinann0089@gmail.com>
2026-02-18 14:25:41 +0300

    fix: update slider 1 component

--- editor-components/slider/slider1/slider1.tsx
+++ editor-components/slider/slider1/slider1.tsx
@@ -1,27 +1,26 @@
 import * as React from "react";
-import { BaseSlider } from "../../EditorComponent";
+import { BaseSlider, TypeMediaInputValue } from "../../EditorComponent";
 import styles from "./slider1.module.scss";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 import ComposerSlider from "../../../composer-base-components/slider/slider";
 import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 
-
 type SliderItem = {
   subtitle: React.JSX.Element;
   title: React.JSX.Element;
-  media: any;
+  description: React.JSX.Element;
+  media: TypeMediaInputValue;
 };
 
 type SocialItem = {
-  media: any;
+  media: TypeMediaInputValue;
   path: string;
 };
 
 class Slider1 extends BaseSlider {
   constructor(props?: any) {
     super(props, styles);
-
 
     this.addProp({
       type: "array",
@@ -44,6 +43,12 @@ class Slider1 extends BaseSlider {
               key: "title",
               displayer: "Title",
               value: "Travelling",
+            },
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "",
             },
             {
               type: "media",
@@ -75,6 +80,12 @@ class Slider1 extends BaseSlider {
               key: "title",
               displayer: "Title",
               value: "I LOVE IT",
+            },
+            {
+              type: "string",
+              key: "description",
+              displayer: "Description",
+              value: "",
             },
             {
               type: "media",
@@ -181,7 +192,11 @@ class Slider1 extends BaseSlider {
       displayer: "Overlay",
       value: true,
     });
-    this.addProp(INPUTS.SLIDER_SETTINGS("settings", "Slider Config"));
+
+    this.addProp(INPUTS.SLIDER_SETTINGS(
+      "settings",
+      "Slider Config"
+    ));
 
     this.setComponentState("slider-ref", React.createRef());
     this.setComponentState("activeSlide", 0);
@@ -199,65 +214,40 @@ class Slider1 extends BaseSlider {
         }
       },
     };
-
     const hasArrows = settings.arrows;
-
     const isOverlayActive = this.getPropValue("overlay");
     const icons = this.castToObject<SocialItem[]>("socials");
     const sliderItems = this.castToObject<SliderItem[]>("slider");
     const ImagesExist = sliderItems[this.getComponentState("activeSlide")]?.media;
 
-    const alignmentValue = Base.getContentAlignment();
-
     return (
       <Base.Container className={this.decorateCSS("container")}>
-        <Base.MaxContent className={this.decorateCSS("max-content")}>
+        <Base.MaxContent className={`${this.decorateCSS("max-content")} ${ImagesExist && this.decorateCSS("has-background")}`}>
           <div className={this.decorateCSS("slider-parent")}>
-            <ComposerSlider {...settings} className={`${this.decorateCSS("carousel")} ${hasArrows ? this.decorateCSS("has-arrows") : ""}`} ref={this.getComponentState("slider-ref")}>
+            <ComposerSlider {...settings} className={`${this.decorateCSS("carousel")} ${hasArrows && this.decorateCSS("has-arrows")} ${icons.length > 0 && this.decorateCSS("has-socials")}`} ref={this.getComponentState("slider-ref")}>
               {sliderItems.map((item: SliderItem, indexSlider: number) => (
                 <div key={indexSlider} className={this.decorateCSS("slider-item")}>
-                  <div className={this.decorateCSS("img-wrapper")}>
-                    {item.media && (
-                      <Base.Media value={item.media} className={this.decorateCSS("img")} />
-                    )}
+                  <div className={this.decorateCSS("image-wrapper")}>
+                    {item.media && <Base.Media value={item.media} className={this.decorateCSS("image")} />}
                     {isOverlayActive && item.media && <div className={this.decorateCSS("overlay")}></div>}
                   </div>
-
                   <div className={this.decorateCSS("content")}>
                     <div className={this.decorateCSS("box-parent")}>
-                      {(this.castToString(item.subtitle) || this.castToString(item.title)) && (
-                        <Base.VerticalContent
-                          className={`
-                        ${this.decorateCSS("box")} 
-                      ${item.media && this.decorateCSS("with-img")}
-                      ${alignmentValue === "center" && this.decorateCSS("center")}`}
-                        >
-                          {this.castToString(item.subtitle) && (
-                            <Base.SectionSubTitle
-                              className={`
-                        ${this.decorateCSS("subtitle")} 
-                        ${item.media && this.decorateCSS("with-img")}`}
-                            >
-                              {item.subtitle}
-                            </Base.SectionSubTitle>
-                          )}
-                          {this.castToString(item.title) && <Base.SectionTitle className={`${this.decorateCSS("title")} ${item.media && this.decorateCSS("with-img")}`}>{item.title}</Base.SectionTitle>}
+                      {(this.castToString(item.subtitle) || this.castToString(item.title) || this.castToString(item.description)) && (
+                        <Base.VerticalContent className={this.decorateCSS("vertical-content")}>
+                          {this.castToString(item.subtitle) && (<Base.SectionSubTitle className={this.decorateCSS("subtitle")}>{item.subtitle}</Base.SectionSubTitle>)}
+                          {this.castToString(item.title) && (<Base.SectionTitle className={this.decorateCSS("title")}>{item.title}</Base.SectionTitle>)}
+                          {this.castToString(item.description) && (<Base.SectionDescription className={this.decorateCSS("description")}>{item.description}</Base.SectionDescription>)}
                         </Base.VerticalContent>
                       )}
                     </div>
-
                     {icons.length > 0 && (
                       <div className={this.decorateCSS("socials")} id="slider1IconsHeight">
                         {icons.map((social: SocialItem, index: number) => {
-                          return (
-                            social.media && (
-                              <ComposerLink key={index} path={social.path}>
-                                <Base.Media
-                                  value={social.media}
-                                  className={`${this.decorateCSS("icon")} ${!item.media && this.decorateCSS("no-img")}`}
-                                />
-                              </ComposerLink>
-                            )
+                          return social.media && (
+                            <ComposerLink key={index} path={social.path}>
+                              <Base.Media value={social.media} className={`${this.decorateCSS("icon")} ${social.media.type === "image" && this.decorateCSS("has-image")}`} />
+                            </ComposerLink>
                           );
                         })}
                       </div>
@@ -267,18 +257,17 @@ class Slider1 extends BaseSlider {
               ))}
             </ComposerSlider>
           </div>
-
           {sliderItems.length > 1 && !!settings.dots && (
-            <div className={`${this.decorateCSS("dots")} ${!ImagesExist && this.decorateCSS("dots-no-image")}`}>
+            <div className={this.decorateCSS("dots")} >
               {sliderItems.map((_, index) => (
-                <div key={`dot-${index}`} className={`${this.decorateCSS("dot-item")} ${this.getComponentState("activeSlide") === index ? this.decorateCSS("active") : ""}`} onClick={() => this.getComponentState("slider-ref").current.slickGoTo(index)}>
+                <div key={`dot-${index}`} className={`${this.decorateCSS("dot-item")} ${this.getComponentState("activeSlide") === index && this.decorateCSS("active")}`} onClick={() => this.getComponentState("slider-ref").current.slickGoTo(index)}>
                   <Base.Button className={this.decorateCSS("dot-button")} />
                 </div>
               ))}
             </div>
           )}
         </Base.MaxContent>
-      </Base.Container>
+      </Base.Container >
     );
   }
 }
