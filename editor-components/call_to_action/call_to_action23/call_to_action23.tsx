26558037ba2fbf952ca44e7bb8560f32f173e5d3
Osman Ali Elma <osmanalielma4616@gmail.com>
2026-02-25 22:55:01 +0300

    fix: fix subtitle

--- editor-components/call_to_action/call_to_action23/call_to_action23.tsx
+++ editor-components/call_to_action/call_to_action23/call_to_action23.tsx
@@ -15,14 +15,14 @@ class CallToAction23Page extends BaseCallToAction {
       type: "boolean",
       key: "coloredBackground",
       displayer: "Colored Background",
-      value: false,
+      value: true,
     });
 
     this.addProp({
       type: "string",
       key: "subtitle",
       displayer: "Subtitle",
-      value: "TEST",
+      value: "",
     });
 
     this.addProp({
