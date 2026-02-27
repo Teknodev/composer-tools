329b70c85eac4d2eaa479b9111f49bee1edb0594
Rifat Tevke <rayfattevke@gmail.com>
2026-02-27 09:25:22 +0300

    feat: add CallToAction33 Component (#1116)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -191,6 +191,7 @@ import CallToAction28 from "./call_to_action/call_to_action28/call_to_action28";
 import CallToAction29 from "./call_to_action/call_to_action29/call_to_action29";
 import CallToAction30 from "./call_to_action/call_to_action30/call_to_action30";
 import CallToAction32 from "./call_to_action/call_to_action32/call_to_action32";
+import CallToAction33 from "./call_to_action/call_to_action33/call_to_action33";
 import LogoComponent1 from "./logo-clouds/logo-comp1/logo-comp1";
 import LogoComponent2 from "./logo-clouds/logo-comp2/logo-comp2";
 import LogoComponent3 from "./logo-clouds/logo-comp3/logo-comp3";
@@ -572,6 +573,7 @@ function Registerables(composer: ComponentsRegistery) {
     CallToAction29,
     CallToAction30,
     CallToAction32,
+    CallToAction33,
     LogoComponent1,
     LogoComponent2,
     LogoComponent3,
