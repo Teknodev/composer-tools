5c3a6065f6d0c5f307a6746bc553b9511e363fbb
Osman Ali Elma <osmanalielma4616@gmail.com>
2026-03-03 10:58:01 +0300

    fix: import fix

--- editor-components/call_to_action/call_to_action35/call_to_action35.tsx
+++ editor-components/call_to_action/call_to_action35/call_to_action35.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ComposerLink from '../../../../custom-hooks/composer-base-components/Link/link';
+import ComposerLink from '../../../composer-base-components/Link/ComposerLinkProvider';
 import { BaseCallToAction, TypeMediaInputValue } from '../../EditorComponent';
 import styles from './call_to_action35.module.scss';
 import { Base } from '../../../composer-base-components/base/base';
-import { INPUTS } from 'composer-tools/custom-hooks/input-templates';
+import { INPUTS } from '../../../custom-hooks/input-templates';
 
 type Button = INPUTS.CastedButton;
 class CallToAction35 extends BaseCallToAction {
