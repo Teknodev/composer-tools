a037d770b5b74699cff74d7cd397c6a5c8df489c
tetwy <musagdg@hotmail.com>
2026-03-11 09:28:05 +0300

    fix: pr changes

--- editor-components/stats/stats26/stats26.tsx
+++ editor-components/stats/stats26/stats26.tsx
@@ -3,7 +3,7 @@ import { BaseStats } from "../../EditorComponent";
 import styles from "./stats26.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
-import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import ComposerLink from "../../../composer-base-components/Link/ComposerLinkProvider";
 
 type StatItem = {
     prefix: string;
