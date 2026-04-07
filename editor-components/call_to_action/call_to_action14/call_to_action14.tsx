34b3f25dec20e9f82fa71fb5ac30f3e9302ce119
Sinan <sinann0089@gmail.com>
2026-04-07 10:32:26 +0300

    fix: In the Call to Action 14 component, the margin that should appear below the Section Subtitle is currently set to zero. The margin value coming from the Vertical content should be applied

--- editor-components/call_to_action/call_to_action14/call_to_action14.tsx
+++ editor-components/call_to_action/call_to_action14/call_to_action14.tsx
@@ -34,7 +34,7 @@ class CallToAction14 extends BaseCallToAction {
       key: "buttons",
       displayer: "Buttons",
       value: [
-        INPUTS.BUTTON("button", "Button", "Button Text", "", null, null,"Primary")
+        INPUTS.BUTTON("button", "Button", "Button Text", "", null, null, "Primary")
       ],
     });
     this.addProp({
@@ -54,7 +54,7 @@ class CallToAction14 extends BaseCallToAction {
     const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
     return (
       <Base.Container className={this.decorateCSS("container")}>
-        <Base.MaxContent 
+        <Base.MaxContent
           className={this.decorateCSS("max-content")}
           data-animation={this.getPropValue("hoverAnimation").join(" ")}
         >
