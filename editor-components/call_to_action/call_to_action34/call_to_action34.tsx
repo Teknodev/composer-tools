c7c870ee72047699db3e5c51f2b2cd8df90399a8
Sultan Akbaş <akbassultan29@gmail.com>
2026-04-08 16:48:13 +0300

    refactor: cta34 pr feedbacks

--- editor-components/call_to_action/call_to_action34/call_to_action34.tsx
+++ editor-components/call_to_action/call_to_action34/call_to_action34.tsx
@@ -5,7 +5,9 @@ import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
 import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
 
-class CallToAction34Page extends BaseCallToAction {
+type Button = INPUTS.CastedButton & { navigateTo?: string };
+
+class CallToAction34 extends BaseCallToAction {
     constructor(props?: any) {
         super(props, styles);
 
@@ -27,7 +29,7 @@ class CallToAction34Page extends BaseCallToAction {
             type: "string",
             key: "title",
             displayer: "Title",
-            value: "Special gifts for your loved ones",
+            value: "Special gifts for your loved ones.",
         });
 
         this.addProp({
@@ -37,22 +39,17 @@ class CallToAction34Page extends BaseCallToAction {
             value: "You can purchase ready-made gift certificates for any amount and any service",
         });
 
-        // this.addProp(INPUTS.BUTTON("button", "Button", "Sign Up Online", "", null, null, "Primary"));
-
-
         this.addProp({
             type: "array",
             key: "buttons",
             displayer: "Buttons",
-            value: [
-                INPUTS.BUTTON("button", "Button", "Sign Up Online", "", null, null, "Primary")
-            ]
-        })
+            value: [INPUTS.BUTTON("button", "Button", "Sign Up Online", "", null, null, "White")],
+        });
 
         this.addProp({
             type: "object",
             key: "mediaGroup",
-            displayer: "Media Settings",
+            displayer: "Media",
             value: [
                 {
                     type: "media",
@@ -63,27 +60,21 @@ class CallToAction34Page extends BaseCallToAction {
                     },
                     value: {
                         type: "image",
-                        url: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&q=80&w=1000"
+                        url: "https://storage.googleapis.com/download/storage/v1/b/hq-blinkpage-staging-bbc49/o/69a5989e771c03002ccbd6c4?alt=media",
                     },
+
+
                 },
                 {
                     type: "boolean",
                     key: "overlay",
                     displayer: "Overlay",
-                    value: true,
+                    value: false,
                 }
             ]
+
         });
 
-        this.addProp({
-            type: "select",
-            key: "subtitleType",
-            displayer: "Subtitle Type",
-            value: "line",
-            additionalParams: {
-                selectItems: ["default", "line", "badge"]
-            }
-        });
     }
 
     static getName(): string {
@@ -91,65 +82,85 @@ class CallToAction34Page extends BaseCallToAction {
     }
 
     render() {
+        const subtitle = this.castToString(this.getPropValue("subtitle"));
+        const title = this.castToString(this.getPropValue("title"));
+        const description = this.castToString(this.getPropValue("description"));
         const isColored = this.getPropValue("isBackgroundColored");
-        const subtitleType = this.getPropValue("subtitleType");
-        const currentSubtitleType = isColored && subtitleType === "badge" ? "default" : subtitleType;
-        const buttons = this.castToObject<INPUTS.CastedButton[]>("buttons");
+        const buttons = this.castToObject<Button[]>("buttons") || [];
+
+        const hasButtons = buttons.some(btn => {
+            return this.castToString(btn.text) || btn.icon;
+        });
+
+        const hasLeftContent = subtitle || title || description || hasButtons;
 
         const mediaGroup = this.castToObject<any>("mediaGroup");
         const media = mediaGroup.media;
         const showOverlay = mediaGroup.overlay;
+
+
         return (
             <Base.Container className={this.decorateCSS("container")}>
                 <Base.MaxContent className={this.decorateCSS("max-content")}>
-                    <div className={`${this.decorateCSS("card-wrapper")} ${isColored ? this.decorateCSS("colored") : ""}`}>
-                        <div className={`${this.decorateCSS("left-column")} ${isColored ? this.decorateCSS("colored") : ""}`}>
-                            <Base.VerticalContent>
-                                {this.castToString(this.getPropValue("subtitle")) && (
-                                    <Base.SectionSubTitle className={this.decorateCSS("subtitle")}
-                                        subtitleType={currentSubtitleType}>
+                    <Base.ListGrid gridCount={{ pc: 2, tablet: 2, phone: 1 }} className={`${this.decorateCSS("card-wrapper")} ${isColored ? this.decorateCSS("colored") : ""} ${!media?.url ? this.decorateCSS("no-media") : ""}${!hasLeftContent ? this.decorateCSS("no-text") : ""}`}>
+                        {hasLeftContent && (
+                            <Base.VerticalContent className={`${this.decorateCSS("left-column")} ${isColored ? this.decorateCSS("colored") : ""} ${!media?.url ? this.decorateCSS("no-media") : ""}`}>
+                                {subtitle && (
+                                    <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>
                                         {this.getPropValue("subtitle")}
                                     </Base.SectionSubTitle>
                                 )}
 
-                                {this.castToString(this.getPropValue("title")) && (
+                                {title && (
                                     <Base.SectionTitle className={this.decorateCSS("title")}>
                                         {this.getPropValue("title")}
                                     </Base.SectionTitle>
                                 )}
 
-                                {this.castToString(this.getPropValue("description")) && (
+                                {description && (
                                     <Base.SectionDescription className={this.decorateCSS("description")}>
                                         {this.getPropValue("description")}
                                     </Base.SectionDescription>
                                 )}
 
-                                {buttons.length > 0 && (
-                                    <div className={this.decorateCSS("button-container")}>
-                                        {buttons.map((button: INPUTS.CastedButton, index: number) => {
-                                            return (this.castToString(button.text) || button.icon) && (
-                                                <ComposerLink key={index} path={button.url}>
-                                                    <div className={this.decorateCSS("button")}>
-                                                        {this.castToString(button.text) && (
-                                                            <Base.P className={this.decorateCSS("button-text")}>
-                                                                {button.text}
-                                                            </Base.P>
-                                                        )}
-                                                    </div>
-                                                </ComposerLink>
-                                            );
-                                        })}
-                                    </div>
-                                )}
+                                <div className={this.decorateCSS("button-container")}>
+                                    {buttons.map((button: Button, index: number) => {
+                                        const buttonText = this.castToString(button.text);
+                                        const buttonIcon = button.icon;
 
+                                        if (!buttonText || !buttonIcon) return null;
 
+                                        return (
+                                            <ComposerLink key={index} path={button.navigateTo}>
+                                                <Base.Button
+                                                    className={this.decorateCSS("button")}
+                                                    buttonType={button.type}
+                                                    navigateTo={button.navigateTo}
+                                                >
+                                                    {buttonIcon && (
+                                                        <Base.Media
+                                                            value={buttonIcon as any}
+                                                            className={this.decorateCSS("button-icon")}
+                                                        />
+                                                    )}
+                                                    {buttonText && (
+                                                        <Base.P className={this.decorateCSS("button-text")}>
+                                                            {button.text}
+                                                        </Base.P>
+                                                    )}
+                                                </Base.Button>
+                                            </ComposerLink>
+                                        );
+                                    })}
+                                </div>
                             </Base.VerticalContent>
-                        </div>
+                        )}
 
                         {media?.url && (
                             <div className={this.decorateCSS("right-column")}>
                                 <div className={this.decorateCSS("media-container")}>
                                     {showOverlay && <div className={this.decorateCSS("overlay")}></div>}
+
                                     <Base.Media
                                         value={media}
                                         className={this.decorateCSS("media")}
@@ -157,11 +168,11 @@ class CallToAction34Page extends BaseCallToAction {
                                 </div>
                             </div>
                         )}
-                    </div>
+                    </Base.ListGrid>
                 </Base.MaxContent>
             </Base.Container>
         );
     }
 }
 
-export default CallToAction34Page;
+export default CallToAction34;
