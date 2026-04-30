b71b38cf9a33e0c93ca3c71359725bc94c204f81
tetwy <musagdg@hotmail.com>
2026-04-30 15:28:53 +0300

    chore: address pull request feedback

--- editor-components/call_to_action/call_to_action34/call_to_action34.tsx
+++ editor-components/call_to_action/call_to_action34/call_to_action34.tsx
@@ -3,7 +3,7 @@ import { BaseCallToAction } from "../../EditorComponent";
 import styles from "./call_to_action34.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
 import { INPUTS } from "composer-tools/custom-hooks/input-templates";
-import ComposerLink from "../../../../custom-hooks/composer-base-components/Link/link";
+import ComposerLink from "../../../composer-base-components/Link/ComposerLinkProvider";
 
 type Button = INPUTS.CastedButton;
 
