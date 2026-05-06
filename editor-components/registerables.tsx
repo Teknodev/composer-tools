61f4750bd19588acb352d7a698f78d6ec32dc254
akbassultan <147769977+akbassultan@users.noreply.github.com>
2026-05-06 14:01:06 +0300

    feat:add Stats36 Component (#1174)

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -245,6 +245,7 @@ import StatsComponent29 from "./stats/stats29/stats29";
 import StatsComponent32 from "./stats/stats32/stats32";
 import StatsComponent33 from "./stats/stats33/stats33";
 import StatsComponent34 from "./stats/stats34/stats34";
+import StatsComponent36 from "./stats/stats36/stats36";
 import StatsComponent38 from "./stats/stats38/stats38";
 import Form1 from "./contacts/form1/form1";
 import Form2 from "./contacts/form2/form2";
@@ -649,6 +650,7 @@ function Registerables(composer: ComponentsRegistery) {
     StatsComponent32,
     StatsComponent33,
     StatsComponent34,
+    StatsComponent36,
     StatsComponent38,
     Form1,
     Form2,
