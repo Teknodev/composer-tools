e838c4109d913c301e7dd5a06d2ecc7eb4723b32
tetwy <musagdg@hotmail.com>
2026-05-12 10:44:57 +0300

    refactor(testimonials9): standardize props, enhance layout structure, and unify styling variables across components

--- editor-components/testimonials/testimonials9/testimonials9.tsx
+++ editor-components/testimonials/testimonials9/testimonials9.tsx
@@ -2,18 +2,34 @@ import * as React from "react";
 import { Testimonials, TypeMediaInputValue } from "../../EditorComponent";
 import styles from "./testimonials9.module.scss";
 import ComposerSlider from "../../../composer-base-components/slider/slider";
-
 import { Base } from "../../../composer-base-components/base/base";
+import { INPUTS } from "../../../custom-hooks/input-templates";
+import ComposerLink from "../../../composer-base-components/Link/ComposerLinkProvider";
+
 type Card = {
   profileImage: TypeMediaInputValue;
   title: React.JSX.Element;
   subtitle: React.JSX.Element;
   description: React.JSX.Element;
 };
+
+type Button = {
+  text: React.JSX.Element;
+  url: string;
+  icon: TypeMediaInputValue;
+  type: string;
+};
+
 class Testimonials9Page extends Testimonials {
   constructor(props?: any) {
     super(props, styles);
 
+    this.addProp({
+      type: "string",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: "Lorem",
+    });
     this.addProp({
       type: "string",
       key: "title",
@@ -21,16 +37,32 @@ class Testimonials9Page extends Testimonials {
       value: "Testimonials",
     });
     this.addProp({
-      type: "icon",
-      key: "left_icon",
-      displayer: "Left İcon",
-      value: "RiDoubleQuotesL",
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     });
     this.addProp({
-      type: "icon",
+      type: "array",
+      key: "buttons",
+      displayer: "Buttons",
+      value: [
+        INPUTS.BUTTON("button", "Button", "Lorem", "", null, null, "Primary"),
+      ],
+    });
+    this.addProp({
+      type: "media",
+      key: "left_icon",
+      displayer: "Left Icon",
+      additionalParams: { availableTypes: ["icon", "image"] },
+      value: { type: "icon", name: "RiDoubleQuotesL" },
+    });
+    this.addProp({
+      type: "media",
       key: "right_icon",
-      displayer: "Right İcon",
-      value: "RiDoubleQuotesR",
+      displayer: "Right Icon",
+      additionalParams: { availableTypes: ["icon", "image"] },
+      value: { type: "icon", name: "RiDoubleQuotesR" },
     });
     this.addProp({
       type: "array",
@@ -46,9 +78,7 @@ class Testimonials9Page extends Testimonials {
               type: "media",
               key: "profileImage",
               displayer: "Author Image",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b38?alt=media&timestamp=1719483639150",
@@ -81,15 +111,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b33?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b33?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -101,7 +129,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "21st November 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -118,15 +146,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b34?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b34?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -138,7 +164,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "2nd July 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -155,15 +181,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b37?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b37?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -175,7 +199,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "15th June 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -192,15 +216,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b36?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b36?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -212,7 +234,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "18th December 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -229,15 +251,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b35?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b35?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -249,7 +269,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "21st November 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -266,15 +286,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617728bd2970002c623bd5?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/66617728bd2970002c623bd5?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -286,7 +304,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "2nd July 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -303,15 +321,13 @@ class Testimonials9Page extends Testimonials {
           value: [
             {
               type: "media",
-      key: "profileImage",
-      displayer: "Author Image",
-      additionalParams: {
-        availableTypes: ["image"],
-      },
-      value: {
-        type: "image",
-        url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b37?alt=media&timestamp=1719483639150",
-      },
+              key: "profileImage",
+              displayer: "Author Image",
+              additionalParams: { availableTypes: ["image"] },
+              value: {
+                type: "image",
+                url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/666176cabd2970002c623b37?alt=media&timestamp=1719483639150",
+              },
             },
             {
               type: "string",
@@ -323,7 +339,7 @@ class Testimonials9Page extends Testimonials {
               type: "string",
               key: "subtitle",
               value: "15th June 2022",
-              displayer: "subtitle",
+              displayer: "Subtitle",
             },
             {
               type: "string",
@@ -337,10 +353,28 @@ class Testimonials9Page extends Testimonials {
     });
     this.setComponentState("active-index", 1);
   }
+
   static getName(): string {
     return "Testimonials 9";
   }
+
   render() {
+    const subtitleExist = this.castToString(this.getPropValue("subtitle"));
+    const titleExist = this.castToString(this.getPropValue("title"));
+    const descriptionExist = this.castToString(this.getPropValue("description"));
+    const buttons = this.castToObject<Button[]>("buttons");
+    const hasValidButtons = buttons.some((btn: Button) => {
+      const buttonText = this.castToString(btn.text);
+      const iconExist = btn.icon && (btn.icon.type === "icon" ? btn.icon.name : btn.icon.url);
+      return buttonText || iconExist;
+    });
+    const hasAnyTopContent = subtitleExist || titleExist || descriptionExist || hasValidButtons;
+
+    const leftIconMedia = this.getPropValue("left_icon") as TypeMediaInputValue;
+    const leftIconExist = leftIconMedia && (leftIconMedia.type === "icon" ? leftIconMedia.name : leftIconMedia.url);
+    const rightIconMedia = this.getPropValue("right_icon") as TypeMediaInputValue;
+    const rightIconExist = rightIconMedia && (rightIconMedia.type === "icon" ? rightIconMedia.name : rightIconMedia.url);
+
     const slider = this.getPropValue("slider");
     const settings = {
       dots: false,
@@ -353,60 +387,87 @@ class Testimonials9Page extends Testimonials {
       vertical: true,
       verticalSwiping: slider.length > 1,
       arrows: false,
-
       beforeChange: (newIndex: number) => {
         let adjustedIndex = (newIndex + 1) % this.castToObject<Card[]>("slider").length;
         this.setComponentState("active-index", adjustedIndex);
       },
     };
+
     const activeIndex = this.getComponentState("active-index");
-    const title = this.getPropValue("title");
+
     return (
       <Base.Container className={this.decorateCSS("container")}>
         <Base.MaxContent className={this.decorateCSS("max-content")}>
           <div className={this.decorateCSS("wrapper")}>
-            {this.castToString(title) && (
-              <div className={this.decorateCSS("up-page")}>
-                <Base.SectionTitle className={this.decorateCSS("title")}>{title}</Base.SectionTitle>
-              </div>
+            {hasAnyTopContent && (
+              <Base.VerticalContent className={this.decorateCSS("top-content")}>
+                {subtitleExist && (
+                  <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>
+                    {this.getPropValue("subtitle")}
+                  </Base.SectionSubTitle>
+                )}
+                {titleExist && (
+                  <Base.SectionTitle className={this.decorateCSS("title")}>
+                    {this.getPropValue("title")}
+                  </Base.SectionTitle>
+                )}
+                {descriptionExist && (
+                  <Base.SectionDescription className={this.decorateCSS("description")}>
+                    {this.getPropValue("description")}
+                  </Base.SectionDescription>
+                )}
+                {hasValidButtons && (
+                  <div className={this.decorateCSS("button-container")}>
+                    {buttons.map((item: Button, index: number) => {
+                      const buttonText = this.castToString(item.text);
+                      const iconExist = item.icon && (item.icon.type === "icon" ? item.icon.name : item.icon.url);
+                      if (!buttonText && !iconExist) return null;
+                      return (
+                        <ComposerLink key={index} path={item.url}>
+                          <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                            {buttonText && (
+                              <Base.P className={this.decorateCSS("button-text")}>{item.text}</Base.P>
+                            )}
+                            {iconExist && (
+                              <Base.Media className={this.decorateCSS("button-icon")} value={item.icon!} />
+                            )}
+                          </Base.Button>
+                        </ComposerLink>
+                      );
+                    })}
+                  </div>
+                )}
+              </Base.VerticalContent>
             )}
             <Base.ContainerGrid className={this.decorateCSS("down-page")}>
               <Base.GridCell className={this.decorateCSS("carousel")}>
                 <ComposerSlider {...settings}>
-                  {this.castToObject<Card[]>("slider").map((item: Card, index: number) => {
-                    return (
-                      <div className={`${this.decorateCSS("card-inner")} ${activeIndex === index ? this.decorateCSS("active") : ""}`}>
-                        {item.profileImage && <Base.Media value={item.profileImage} className={this.decorateCSS("img")} />}
-                        {(this.castToString(item.title) || this.castToString(item.subtitle)) && (
-                          <Base.VerticalContent className={this.decorateCSS("text")}>
-                            {this.castToString(item.title) && <Base.H4 className={this.decorateCSS("title")}>{item.title}</Base.H4>}
-                            {this.castToString(item.subtitle) && <Base.H5 className={this.decorateCSS("subtitle")}>{item.subtitle}</Base.H5>}
-                          </Base.VerticalContent>
-                        )}
-                      </div>
-                    );
-                  })}
+                  {this.castToObject<Card[]>("slider").map((item: Card, index: number) => (
+                    <div key={index} className={`${this.decorateCSS("card-inner")} ${activeIndex === index ? this.decorateCSS("active") : ""}`}>
+                      {item.profileImage && <Base.Media value={item.profileImage} className={this.decorateCSS("img")} />}
+                      {(this.castToString(item.title) || this.castToString(item.subtitle)) && (
+                        <Base.VerticalContent className={this.decorateCSS("text")}>
+                          {this.castToString(item.title) && <Base.H4 className={this.decorateCSS("card-title")}>{item.title}</Base.H4>}
+                          {this.castToString(item.subtitle) && <Base.H5 className={this.decorateCSS("card-subtitle")}>{item.subtitle}</Base.H5>}
+                        </Base.VerticalContent>
+                      )}
+                    </div>
+                  ))}
                 </ComposerSlider>
               </Base.GridCell>
               <Base.GridCell className={this.decorateCSS("right-page")}>
-                {this.getPropValue("left_icon") && (
-                  <Base.Icon
-                    name={this.getPropValue("left_icon")}
-                    propsIcon={{
-                      className: this.decorateCSS("left-icon"),
-                    }}
-                  />
+                {leftIconExist && (
+                  <Base.Media value={leftIconMedia} className={this.decorateCSS("left-icon")} />
                 )}
                 {this.castToObject<Card[]>("slider").map((item: Card, index: number) => (
-                  <div className={this.decorateCSS("text")}>{index === activeIndex && this.castToString(item.description) && <Base.H3 className={this.decorateCSS("description")}>{item.description}</Base.H3>}</div>
+                  <div key={index} className={this.decorateCSS("text")}>
+                    {index === activeIndex && this.castToString(item.description) && (
+                      <Base.H3 className={this.decorateCSS("description")}>{item.description}</Base.H3>
+                    )}
+                  </div>
                 ))}
-                {this.getPropValue("right_icon") && (
-                  <Base.Icon
-                    name={this.getPropValue("right_icon")}
-                    propsIcon={{
-                      className: this.decorateCSS("right-icon"),
-                    }}
-                  />
+                {rightIconExist && (
+                  <Base.Media value={rightIconMedia} className={this.decorateCSS("right-icon")} />
                 )}
               </Base.GridCell>
             </Base.ContainerGrid>
