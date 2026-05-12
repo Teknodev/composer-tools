44d977c02d2c16bd99b51888418177f29fefc65f
tetwy <musagdg@hotmail.com>
2026-05-12 10:02:56 +0300

    refactor(testimonials6): standardize props, enhance layout structure, and unify styling variables across components

--- editor-components/testimonials/testimonials6/testimonials6.tsx
+++ editor-components/testimonials/testimonials6/testimonials6.tsx
@@ -2,25 +2,55 @@ import * as React from "react";
 import { Testimonials, TypeMediaInputValue } from "../../EditorComponent";
 import styles from "./testimonials6.module.scss";
 import { Base } from "../../../composer-base-components/base/base";
+import { INPUTS } from "../../../custom-hooks/input-templates";
+import ComposerLink from "../../../composer-base-components/Link/ComposerLinkProvider";
 
-
-interface CardItem {
+type CardItem = {
   star: number;
   description: React.JSX.Element;
-  profileImage: TypeMediaInputValue;
-  name: React.JSX.Element;
-  personTitle: string;
+  image: TypeMediaInputValue;
+  author: {
+    name: React.JSX.Element;
+    subtitle: React.JSX.Element;
+  };
   line: boolean;
-}
+};
+
+type Button = {
+  text: React.JSX.Element;
+  url: string;
+  icon: TypeMediaInputValue;
+  type: string;
+};
 
 class Testimonials6Page extends Testimonials {
   constructor(props?: any) {
     super(props, styles);
     this.addProp({
       type: "string",
+      key: "subtitle",
+      displayer: "Subtitle",
+      value: "Lorem",
+    });
+    this.addProp({
+      type: "string",
       key: "title",
-      value: "What our customers say about Pagedone",
       displayer: "Title",
+      value: "What our customers say about Pagedone",
+    });
+    this.addProp({
+      type: "string",
+      key: "description",
+      displayer: "Description",
+      value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat feugiat donec tristique. Nibh leo vestibulum scelerisque sed.",
+    });
+    this.addProp({
+      type: "array",
+      key: "buttons",
+      displayer: "Buttons",
+      value: [
+        INPUTS.BUTTON("button", "Button", "Lorem", "", null, null, "Primary"),
+      ],
     });
     this.addProp({
       type: "number",
@@ -29,10 +59,11 @@ class Testimonials6Page extends Testimonials {
       value: 3,
     });
     this.addProp({
-      type: "icon",
+      type: "media",
       key: "starIcon",
       displayer: "Icon",
-      value: "FaStar",
+      additionalParams: { availableTypes: ["icon", "image"] },
+      value: { type: "icon", name: "FaStar" },
     });
     this.addProp({
       type: "array",
@@ -41,8 +72,8 @@ class Testimonials6Page extends Testimonials {
       value: [
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card1",
+          displayer: "Card 1",
           value: [
             {
               type: "boolean",
@@ -64,10 +95,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644933b3f72de2002caa9a16?alt=media&timestamp=1719483639147",
@@ -75,23 +104,30 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Clover Ruth",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "Design Lead",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Clover Ruth",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "Design Lead",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card2",
+          displayer: "Card 2",
           value: [
             {
               type: "boolean",
@@ -113,10 +149,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64493501f72de2002caa9a46?alt=media&timestamp=1719483639147",
@@ -124,23 +158,30 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Jaylyn Kaleigh",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "Product Designer",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Jaylyn Kaleigh",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "Product Designer",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card3",
+          displayer: "Card 3",
           value: [
             {
               type: "boolean",
@@ -162,10 +203,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6449364af72de2002caa9ad0?alt=media&timestamp=1719483639147",
@@ -173,23 +212,30 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Candyce Jeannine",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "CEO",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Candyce Jeannine",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "CEO",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card4",
+          displayer: "Card 4",
           value: [
             {
               type: "boolean",
@@ -211,10 +257,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/644933b3f72de2002caa9a16?alt=media&timestamp=1719483639147",
@@ -222,23 +266,30 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Clover Ruth",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "Design Lead",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Clover Ruth",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "Design Lead",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card5",
+          displayer: "Card 5",
           value: [
             {
               type: "boolean",
@@ -260,10 +311,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/64493501f72de2002caa9a46?alt=media&timestamp=1719483639147",
@@ -271,23 +320,30 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Jaylyn Kaleigh",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "Product Designer",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Jaylyn Kaleigh",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "Product Designer",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
         {
           type: "object",
-          key: "card",
-          displayer: "Card",
+          key: "card6",
+          displayer: "Card 6",
           value: [
             {
               type: "boolean",
@@ -309,10 +365,8 @@ class Testimonials6Page extends Testimonials {
             },
             {
               type: "media",
-              key: "profileImage",
-              additionalParams: {
-                availableTypes: ["image"],
-              },
+              key: "image",
+              additionalParams: { availableTypes: ["image"] },
               value: {
                 type: "image",
                 url: "https://storage.googleapis.com/download/storage/v1/b/hq-composer-0b0f0/o/6449364af72de2002caa9ad0?alt=media&timestamp=1719483639147",
@@ -320,16 +374,23 @@ class Testimonials6Page extends Testimonials {
               displayer: "Author Image",
             },
             {
-              type: "string",
-              key: "name",
-              value: "Candyce Jeannine",
-              displayer: "Author Name",
-            },
-            {
-              type: "string",
-              key: "personTitle",
-              value: "CEO",
-              displayer: "Author Position",
+              type: "object",
+              key: "author",
+              displayer: "Author",
+              value: [
+                {
+                  type: "string",
+                  key: "name",
+                  value: "Candyce Jeannine",
+                  displayer: "Name",
+                },
+                {
+                  type: "string",
+                  key: "subtitle",
+                  value: "CEO",
+                  displayer: "Position",
+                },
+              ],
             },
           ],
         },
@@ -342,27 +403,92 @@ class Testimonials6Page extends Testimonials {
   }
 
   render() {
-    const card = this.castToObject<CardItem[]>("cards");
+    const subtitleExist = this.castToString(this.getPropValue("subtitle"));
+    const titleExist = this.castToString(this.getPropValue("title"));
+    const descriptionExist = this.castToString(this.getPropValue("description"));
+    const buttons = this.castToObject<Button[]>("buttons");
+    const hasValidButtons = buttons.some((btn: Button) => {
+      const buttonText = this.castToString(btn.text);
+      const iconExist = btn.icon && (btn.icon.type === "icon" ? btn.icon.name : btn.icon.url);
+      return buttonText || iconExist;
+    });
+    const hasAnyTopContent = subtitleExist || titleExist || descriptionExist || hasValidButtons;
+
+    const cards = this.castToObject<CardItem[]>("cards");
+    const starIcon = this.getPropValue("starIcon") as TypeMediaInputValue;
+    const starIconExist = starIcon && (starIcon.type === "icon" ? starIcon.name : starIcon.url);
+
     return (
       <Base.Container className={this.decorateCSS("container")}>
+        <Base.MaxContent className={this.decorateCSS("max-content")}>
+          {hasAnyTopContent && (
+            <Base.VerticalContent className={this.decorateCSS("top-content")}>
+              {subtitleExist && (
+                <Base.SectionSubTitle className={this.decorateCSS("subtitle")}>
+                  {this.getPropValue("subtitle")}
+                </Base.SectionSubTitle>
+              )}
+              {titleExist && (
+                <Base.SectionTitle className={this.decorateCSS("title")}>
+                  {this.getPropValue("title")}
+                </Base.SectionTitle>
+              )}
+              {descriptionExist && (
+                <Base.SectionDescription className={this.decorateCSS("description")}>
+                  {this.getPropValue("description")}
+                </Base.SectionDescription>
+              )}
+              {hasValidButtons && (
+                <div className={this.decorateCSS("button-container")}>
+                  {buttons.map((item: Button, index: number) => {
+                    const buttonText = this.castToString(item.text);
+                    const iconExist = item.icon && (item.icon.type === "icon" ? item.icon.name : item.icon.url);
+                    if (!buttonText && !iconExist) return null;
+                    return (
+                      <ComposerLink key={index} path={item.url}>
+                        <Base.Button buttonType={item.type} className={this.decorateCSS("button")}>
+                          {buttonText && (
+                            <Base.P className={this.decorateCSS("button-text")}>{item.text}</Base.P>
+                          )}
+                          {iconExist && (
+                            <Base.Media className={this.decorateCSS("button-icon")} value={item.icon!} />
+                          )}
+                        </Base.Button>
+                      </ComposerLink>
+                    );
+                  })}
+                </div>
+              )}
+            </Base.VerticalContent>
+          )}
+        </Base.MaxContent>
         <Base.MaxContent className={this.decorateCSS("maxContent")}>
-          {this.castToString(this.getPropValue("title")) && <Base.SectionTitle className={this.decorateCSS("title")}>{this.getPropValue("title")}</Base.SectionTitle>}
-          <Base.ListGrid gridCount={{ pc: this.getPropValue("itemCount"), tablet: 2, phone: 1 }} className={this.decorateCSS("cardContainer")}>
-            {card.map((item: any, index: number) => (
-              <div className={this.decorateCSS("card")}>
-                {this.castToString(item.description) && <Base.P className={this.decorateCSS("description")}>{item.description}</Base.P>}
+          <Base.ListGrid gridCount={{ pc: this.getPropValue("itemCount"), tablet: this.getPropValue("itemCount"), phone: 1 }} className={this.decorateCSS("cardContainer")}>
+            {cards.map((item: CardItem, index: number) => (
+              <div key={index} className={this.decorateCSS("card")}>
+                {this.castToString(item.description) && (
+                  <Base.P className={this.decorateCSS("description")}>{item.description}</Base.P>
+                )}
                 {item.line && <div className={this.decorateCSS("line")}></div>}
-                {(item.profileImage || this.castToString(item.name) || this.castToString(item.personTitle) || item.star > 0) && (
+                {(item.image || (item.author && (this.castToString(item.author.name) || this.castToString(item.author.subtitle))) || item.star > 0) && (
                   <div className={this.decorateCSS("bottomContainer")}>
-                    {item.profileImage && <Base.Media value={item.profileImage} className={this.decorateCSS("image")} />}
-                    <Base.VerticalContent className={this.decorateCSS("person")}>
-                      {this.castToString(item.name) && <div className={this.decorateCSS("name")}>{item.name}</div>}
-                      {this.castToString(item.personTitle) && <div className={this.decorateCSS("personTitle")}>{item.personTitle}</div>}
-                    </Base.VerticalContent>
-                    {item.star > 0 && (
+                    {item.image && (
+                      <Base.Media value={item.image} className={this.decorateCSS("image")} />
+                    )}
+                    {item.author && (this.castToString(item.author.name) || this.castToString(item.author.subtitle)) && (
+                      <Base.VerticalContent className={this.decorateCSS("person")}>
+                        {this.castToString(item.author.name) && (
+                          <Base.P className={this.decorateCSS("name")}>{item.author.name}</Base.P>
+                        )}
+                        {this.castToString(item.author.subtitle) && (
+                          <Base.P className={this.decorateCSS("personTitle")}>{item.author.subtitle}</Base.P>
+                        )}
+                      </Base.VerticalContent>
+                    )}
+                    {starIconExist && item.star > 0 && (
                       <div className={this.decorateCSS("star")}>
-                        {[...Array(Number(item.star))].map((_: any, index: number) => (
-                          <Base.Icon propsIcon={{ className: this.decorateCSS("icon") }} name={this.getPropValue("starIcon")} />
+                        {[...Array(Number(item.star))].map((_: unknown, starIndex: number) => (
+                          <Base.Media key={starIndex} value={starIcon} className={this.decorateCSS("icon")} />
                         ))}
                       </div>
                     )}
