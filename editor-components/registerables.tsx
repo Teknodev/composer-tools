9801db7b25dc188739d439142915c392d987779b
bitsinan <sinansshn@gmail.com>
2026-06-02 10:47:18 +0300

    feat: create new component pricing-table 11

--- editor-components/registerables.tsx
+++ editor-components/registerables.tsx
@@ -39,6 +39,7 @@ import PricingComponent7 from "./pricing-table/pricing-table7/pricing-table7";
 import PricingComponent8 from "./pricing-table/pricing-table8/pricing-table8";
 import PricingComponent9 from "./pricing-table/pricing-table9/pricing-table9";
 import PricingComponent10 from "./pricing-table/pricing-table10/pricing-table10";
+import PricingComponent11 from "./pricing-table/pricing-table11/pricing-table11";
 
 import FaqComponent1 from "./faq/faq1/faq1";
 import FaqComponent2 from "./faq/faq2/faq2";
@@ -429,6 +430,7 @@ function Registerables(composer: ComponentsRegistery) {
     PricingComponent8,
     PricingComponent9,
     PricingComponent10,
+    PricingComponent11,
     FaqComponent1,
     FaqComponent2,
     FaqComponent3,
