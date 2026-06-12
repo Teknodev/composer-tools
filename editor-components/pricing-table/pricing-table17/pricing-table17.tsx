48e8d9dc72c20972187e7a67a700e8ac483e9639
bitsinan <sinansshn@gmail.com>
2026-06-12 12:11:16 +0300

    fix: update prop label to Item Count in a Row using proper title casing and correct bottom card button alignment in mobile view

--- editor-components/pricing-table/pricing-table17/pricing-table17.tsx
+++ editor-components/pricing-table/pricing-table17/pricing-table17.tsx
@@ -218,7 +218,7 @@ class PricingTable17 extends BasePricingTable {
     this.addProp({
       type: "number",
       key: "itemCount",
-      displayer: "Item count in a row",
+      displayer: "Item Count in a Row",
       value: 2,
     });
   }
